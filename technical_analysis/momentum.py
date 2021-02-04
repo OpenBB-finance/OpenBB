229de959702101b134365e2f483545e408d091c4
didier <dro.lopes@campus.fct.unl.pt>
2021-02-04 23:07:32 +0000

    Improve MACD plotting

--- technical_analysis/momentum.py
+++ technical_analysis/momentum.py
@@ -29,7 +29,7 @@ def cci(l_args, s_ticker, s_interval, df_stock):
         if s_interval == "1440min":
             df_ta = ta.cci(high=df_stock['2. high'], low=df_stock['3. low'], close=df_stock['5. adjusted close'], 
                            length=ns_parser.n_length, scalar=ns_parser.n_scalar, offset=ns_parser.n_offset).dropna()
-
+            
             plt.subplot(211)
             plt.title(f"Commodity Channel Index (CCI) on {s_ticker}")
             plt.plot(df_stock.index, df_stock['5. adjusted close'].values, 'k', lw=3)
@@ -38,7 +38,6 @@ def cci(l_args, s_ticker, s_interval, df_stock):
             plt.grid(b=True, which='major', color='#666666', linestyle='-')
             plt.minorticks_on()
             plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-
             plt.subplot(212)
             plt.plot(df_ta.index, df_ta.values, 'k', lw=2)
             plt.xlim(df_stock.index[0], df_stock.index[-1])
@@ -59,7 +58,7 @@ def cci(l_args, s_ticker, s_interval, df_stock):
         else:
             df_ta = ta.cci(high=df_stock['2. high'], low=df_stock['3. low'], close=df_stock['4. close'], 
                            length=ns_parser.n_length, scalar=ns_parser.n_scalar, offset=ns_parser.n_offset).dropna()
-
+            
             plt.subplot(211)
             plt.title(f"Commodity Channel Index (CCI) on {s_ticker}")
             plt.plot(df_stock.index, df_stock['4. close'].values, 'k', lw=3)
@@ -68,7 +67,6 @@ def cci(l_args, s_ticker, s_interval, df_stock):
             plt.grid(b=True, which='major', color='#666666', linestyle='-')
             plt.minorticks_on()
             plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-
             plt.subplot(212)
             plt.plot(df_ta.index, df_ta.values, 'k', lw=2)
             plt.xlim(df_stock.index[0], df_stock.index[-1])
@@ -84,7 +82,6 @@ def cci(l_args, s_ticker, s_interval, df_stock):
             plt.ylim(plt.gca().get_ylim())
             plt.yticks([.2, .8], ('OVERSOLD', 'OVERBOUGHT'))
             plt.show()
-
         print("")
     except:
         print("")
@@ -123,13 +120,56 @@ def macd(l_args, s_ticker, s_interval, df_stock):
         if s_interval == "1440min":
             df_ta = ta.macd(df_stock['5. adjusted close'], fast=ns_parser.n_fast, slow=ns_parser.n_slow,
                             signal=ns_parser.n_signal, offset=ns_parser.n_offset).dropna()
-            plot_stock_and_ta(df_stock['5. adjusted close'], s_ticker, df_ta, "MACD")
+
+            plt.subplot(211)
+            plt.title(f"Moving Average Convergence Divergence (MACD) on {s_ticker}")
+            plt.plot(df_stock.index, df_stock['4. close'].values, 'k', lw=3)
+            plt.xlim(df_stock.index[0], df_stock.index[-1])
+            plt.ylabel(f'Share Price ($)')
+            plt.grid(b=True, which='major', color='#666666', linestyle='-')
+            plt.minorticks_on()
+            plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+            plt.subplot(212)
+            plt.plot(df_ta.index, df_ta.iloc[:,0].values, 'b', lw=2)
+            plt.plot(df_ta.index, df_ta.iloc[:,2].values, 'r', lw=2)
+            plt.bar(df_ta.index, df_ta.iloc[:,1].values, color='g')
+            plt.legend([f'MACD Line {df_ta.columns[0]}', 
+                        f'Signal Line {df_ta.columns[2]}',
+                        f'Histogram {df_ta.columns[1]}'])
+            plt.xlim(df_stock.index[0], df_stock.index[-1])
+            plt.grid(b=True, which='major', color='#666666', linestyle='-')
+            plt.minorticks_on()
+            plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+            plt.xlabel('Time')
+            plt.show()
+
         # Intraday 
         else:
             df_ta = ta.macd(df_stock['4. close'], fast=ns_parser.n_fast, slow=ns_parser.n_slow,
                             signal=ns_parser.n_signal, offset=ns_parser.n_offset).dropna()
-            plot_stock_and_ta(df_stock['4. close'], s_ticker, df_ta, "MACD")
 
+            plt.subplot(211)
+            plt.title(f"Moving Average Convergence Divergence (MACD) on {s_ticker}")
+            plt.plot(df_stock.index, df_stock['4. close'].values, 'k', lw=3)
+            plt.xlim(df_stock.index[0], df_stock.index[-1])
+            plt.ylabel(f'Share Price ($)')
+            plt.grid(b=True, which='major', color='#666666', linestyle='-')
+            plt.minorticks_on()
+            plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+            plt.subplot(212)
+            plt.plot(df_ta.index, df_ta.iloc[:,0].values, 'b', lw=2)
+            plt.plot(df_ta.index, df_ta.iloc[:,2].values, 'r', lw=2)
+            plt.bar(df_ta.index, df_ta.iloc[:,1].values, color='g')
+            plt.legend([f'MACD Line {df_ta.columns[0]}', 
+                        f'Signal Line {df_ta.columns[2]}',
+                        f'Histogram {df_ta.columns[1]}'])
+            plt.xlim(df_stock.index[0], df_stock.index[-1])
+            plt.grid(b=True, which='major', color='#666666', linestyle='-')
+            plt.minorticks_on()
+            plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+            plt.xlabel('Time')
+            plt.show()
+        print("")
     except:
         print("")
     
