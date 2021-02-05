d7d9db53177a5e30da8f6f1e5490f478b5344a14
didier <dro.lopes@campus.fct.unl.pt>
2021-02-05 01:41:24 +0000

    Improve Aroon plotting

--- technical_analysis/trend.py
+++ technical_analysis/trend.py
@@ -28,7 +28,7 @@ def adx(l_args, s_ticker, s_interval, df_stock):
         if s_interval == "1440min":
             df_ta = ta.adx(high=df_stock['2. high'], low=df_stock['3. low'], close=df_stock['5. adjusted close'], length=ns_parser.n_length, 
                            scalar=ns_parser.n_scalar, drift=ns_parser.n_drift, offset=ns_parser.n_offset).dropna()
-            
+
             plt.subplot(211)
             plt.plot(df_stock.index, df_stock['5. adjusted close'].values, 'k', lw=2)
             plt.title(f"Average Directional Movement Index (ADX) on {s_ticker}")
@@ -101,7 +101,7 @@ def aroon(l_args, s_ticker, s_interval, df_stock):
                                      the Aroon Up is below 30. Also look for crossovers. When the Aroon Down crosses above 
                                      the Aroon Up, it indicates a weakening of the upward trend (and vice versa). """)
 
-    parser.add_argument('-l', "--length", action="store", dest="n_length", type=check_positive, default=14, help='length')
+    parser.add_argument('-l', "--length", action="store", dest="n_length", type=check_positive, default=25, help='length')
     parser.add_argument('-s', "--scalar", action="store", dest="n_scalar", type=check_positive, default=100, help='scalar')
     parser.add_argument('-o', "--offset", action="store", dest="n_offset", type=check_positive, default=0, help='offset')
 
@@ -114,12 +114,43 @@ def aroon(l_args, s_ticker, s_interval, df_stock):
 
         df_ta = ta.aroon(high=df_stock['2. high'], low=df_stock['3. low'], length=ns_parser.n_length,
                          scalar=ns_parser.n_scalar, offset=ns_parser.n_offset).dropna()
+        
+        plt.subplot(311)
         # Daily
         if s_interval == "1440min":
-            plot_stock_and_ta(df_stock['5. adjusted close'], s_ticker, df_ta.iloc[:,-1], "AROON")
+            #plot_stock_and_ta(df_stock['5. adjusted close'], s_ticker, df_ta.iloc[:,-1], "AROON")
+            plt.plot(df_stock.index, df_stock['5. adjusted close'].values, 'k', lw=2)
         # Intraday
         else:
-            plot_stock_and_ta(df_stock['4. close'], s_ticker, df_ta.iloc[:,-1], "AROON")
+            #plot_stock_and_ta(df_stock['4. close'], s_ticker, df_ta.iloc[:,-1], "AROON")
+            plt.plot(df_stock.index, df_stock['4. close'].values, 'k', lw=2)
+        plt.title(f"Aroon on {s_ticker}")
+        plt.xlim(df_stock.index[0], df_stock.index[-1])
+        plt.ylabel(f'Share Price ($)')
+        plt.grid(b=True, which='major', color='#666666', linestyle='-')
+        plt.minorticks_on()
+        plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+        plt.subplot(312)
+        plt.plot(df_ta.index, df_ta.iloc[:,0].values, 'r', lw=2)
+        plt.plot(df_ta.index, df_ta.iloc[:,1].values, 'g', lw=2)
+        plt.xlim(df_stock.index[0], df_stock.index[-1])
+        plt.axhline(50, linewidth=1, color='k', ls='--')
+        plt.legend([f'Aroon DOWN ({df_ta.columns[0]})', 
+                    f'Aroon UP ({df_ta.columns[1]})'])
+        plt.grid(b=True, which='major', color='#666666', linestyle='-')
+        plt.minorticks_on()
+        plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+        plt.ylim([0, 100])
+        plt.subplot(313)
+        plt.plot(df_ta.index, df_ta.iloc[:,2].values, 'b', lw=2)
+        plt.xlabel('Time')
+        plt.legend([f'Aroon OSC ({df_ta.columns[2]})'])
+        plt.grid(b=True, which='major', color='#666666', linestyle='-')
+        plt.minorticks_on()
+        plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+        plt.ylim([-100, 100])
+        plt.show()
+        print("")
 
     except:
         print("")
