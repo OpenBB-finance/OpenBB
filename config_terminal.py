d4dafba8381a6eb3a607f22022fe63f3cdfe4b0a
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2021-02-19 16:51:14 +0000

    Create config_terminal file with API keys

---
+++ config_terminal.py
@@ -0,0 +1,20 @@
+# https://www.alphavantage.co
+API_KEY_ALPHAVANTAGE = "REPLACE_ME"
+
+# https://financialmodelingprep.com/developer
+API_KEY_FINANCIALMODELINGPREP = "REPLACE_ME"
+
+# https://www.quandl.com/tools/api
+API_KEY_QUANDL = "REPLACE_ME"
+
+# https://www.reddit.com/prefs/apps
+API_REDDIT_CLIENT_ID = "REPLACE_ME"
+API_REDDIT_CLIENT_SECRET = "REPLACE_ME"
+API_REDDIT_USERNAME = "REPLACE_ME"
+API_REDDIT_USER_AGENT = "REPLACE_ME"
+API_REDDIT_PASSWORD = "REPLACE_ME"
+
+# https://developer.twitter.com
+API_KEY_TWITTER = "REPLACE_ME"
+API_SECRET_KEY = "REPLACE_ME"
+API_BEARER_TOKEN = "REPLACE_ME"
