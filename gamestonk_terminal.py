96227baffbf71b8478763f231db98d32a2f54998
Ben Kulbertis <ben@kulbertis.org>
2021-03-01 00:15:27 -0700

    Fixed undefined timeseries var

--- gamestonk_terminal.py
+++ gamestonk_terminal.py
@@ -310,6 +310,7 @@ def main():
                 b_quit = pm.pred_menu(df_stock, s_ticker, s_start, s_interval)
             # If stock data is intradaily, we need to get data again as prediction techniques work on daily adjusted data
             else:
+                ts = TimeSeries(key=cfg.API_KEY_ALPHAVANTAGE, output_format='pandas')
                 df_stock_pred, _ = ts.get_daily_adjusted(symbol=s_ticker, outputsize='full')
                 df_stock_pred = df_stock_pred.sort_index(ascending=True)
                 df_stock_pred = df_stock_pred[s_start:]
