e45d76e2de3c0daeb0d7e08c5e4d226b48bf8003
Artem Veremey <artem@veremey.net>
2021-03-05 02:36:17 -0800

    Added more descripting message when Financialmodelingprep.com is returning empty responce

--- gamestonk_terminal/due_diligence/financial_modeling_prep_api.py
+++ gamestonk_terminal/due_diligence/financial_modeling_prep_api.py
@@ -27,7 +27,7 @@ def rating(l_args, s_ticker):
 
     except KeyError:
         print(
-            f"Financialmodelingprep.com is returning empty responses the ticker {s_ticker}."
+            f"Financialmodelingprep.com is returning empty responce the ticker {s_ticker}."
         )
         print("")
         return
