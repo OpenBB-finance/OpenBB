8884cdd4d2f59ec83cf8e9e728c59dea27b22c3c
didier <dro.lopes@campus.fct.unl.pt>
2021-03-06 03:17:55 +0000

    last pylint issue??

--- gamestonk_terminal/fundamental_analysis/alpha_vantage_api.py
+++ gamestonk_terminal/fundamental_analysis/alpha_vantage_api.py
@@ -141,7 +141,15 @@ def income_statement(l_args, s_ticker):
     parser = argparse.ArgumentParser(
         prog="incom",
         description="""
-            Prints a complete income statement over time. This can be either quarterly or annually. The following fields are expected: Accepted date, Cost and expenses, Cost of revenue, Depreciation and amortization, Ebitda, Ebitdaratio, Eps, Epsdiluted, Filling date, Final link, General and administrative expenses, Gross profit, Gross profit ratio, Income before tax, Income before tax ratio, Income tax expense, Interest expense, Link, Net income, Net income ratio, Operating expenses, Operating income, Operating income ratio, Other expenses, Period, Research and development expenses, Revenue, Selling and marketing expenses, Total other income expenses net, Weighted average shs out, Weighted average shs out dil [Source: Alpha Vantage]""",
+            Prints a complete income statement over time. This can be either quarterly or annually.
+            The following fields are expected: Accepted date, Cost and expenses, Cost of revenue,
+            Depreciation and amortization, Ebitda, Ebitdaratio, Eps, Epsdiluted, Filling date,
+            Final link, General and administrative expenses, Gross profit, Gross profit ratio,
+            Income before tax, Income before tax ratio, Income tax expense, Interest expense, Link,
+            Net income, Net income ratio, Operating expenses, Operating income, Operating income
+            ratio, Other expenses, Period, Research and development expenses, Revenue, Selling and
+            marketing expenses, Total other income expenses net, Weighted average shs out, Weighted
+            average shs out dil [Source: Alpha Vantage]""",
     )
 
     parser.add_argument(
