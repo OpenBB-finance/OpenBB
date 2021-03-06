bf745ca41a49186072e276e618750bdec0109b71
didier <dro.lopes@campus.fct.unl.pt>
2021-03-06 02:59:49 +0000

    Shorten description of growth command to comply w lint

--- gamestonk_terminal/fundamental_analysis/financial_modeling_prep_api.py
+++ gamestonk_terminal/fundamental_analysis/financial_modeling_prep_api.py
@@ -489,10 +489,20 @@ def financial_ratios(l_args, s_ticker):
 def financial_statement_growth(l_args, s_ticker):
     parser = argparse.ArgumentParser(
         prog="growth",
-        description="""
-            Prints the growth of several financial statement items and ratios over time. This can be
-            either annually and quarterly. These are, among other things, Revenue Growth (3, 5 and 10 years), inventory growth and operating cash flow growth (3, 5 and 10 years). The following fields are expected: Asset growth, Book valueper share growth, Debt growth, Dividendsper share growth, Ebitgrowth, Epsdiluted growth, Epsgrowth, Five y dividendper share growth per share, Five y net income growth per share, Five y operating c f growth per share, Five y revenue growth per share, Five y shareholders equity growth per share, Free cash flow growth, Gross profit growth, Inventory growth, Net income growth, Operating cash flow growth, Operating income growth, Rdexpense growth, Receivables growth, Revenue growth, Sgaexpenses growth, Ten y dividendper share growth per share,
-            Ten y net income growth per share, Ten y operating c f growth per share, Ten y revenue growth per share, Ten y shareholders equity growth per share, Three y dividendper share growth per share, Three y net income growth per share, Three y operating c f growth per share, Three y revenue growth per share, Three y shareholders equity growth per share, Weighted average shares diluted growth, and Weighted average shares growth [Source: Financial Modeling Prep]
+        description=""" Prints the growth of several financial statement items and ratios over
+        time. This can be either annually and quarterly. These are, among other things, Revenue
+        Growth (3, 5 and 10 years), inventory growth and operating cash flow growth (3, 5 and 10 years). The following fields are expected: Asset growth, Book valueper share growth, Debt
+        growth, Dividendsper share growth, Ebitgrowth, Epsdiluted growth, Epsgrowth, Five y
+        dividendper share growth per share, Five y net income growth per share, Five y operating c
+        f growth per share, Five y revenue growth per share, Five y shareholders equity growth per
+        share, Free cash flow growth, Gross profit growth, Inventory growth, Net income growth,
+        Operating cash flow growth, Operating income growth, Rdexpense growth, Receivables growth,
+        Revenue growth, Sgaexpenses growth, Ten y dividendper share growth per share, Ten y net
+        income growth per share, Ten y operating c f growth per share, Ten y revenue growth per
+        share, Ten y shareholders equity growth per share, Three y dividendper share growth per
+        share, Three y net income growth per share, Three y operating c f growth per share, Three y
+        revenue growth per share, Three y shareholders equity growth per share, Weighted average
+        shares diluted growth, and Weighted average shares growth [Source: Financial Modeling Prep]
         """,
     )
 
