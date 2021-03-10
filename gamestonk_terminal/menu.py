75fed7faa621a4d3baa184bb61d2cf5e02e14417
didier <dro.lopes@campus.fct.unl.pt>
2021-03-10 23:55:05 +0000

    also clear flake8

--- gamestonk_terminal/menu.py
+++ gamestonk_terminal/menu.py
@@ -7,5 +7,5 @@ history_file = os.path.join(os.path.expanduser("~"), ".gamestonk_terminal.his")
 
 try:
     session = PromptSession(history=FileHistory(history_file))
-except Exception as e:
+except Exception as e:  # noqa: F841
     session = None
