9af480dc624f37e1116db2b85a06deacb6c406d3
Artem Veremey <artem@veremey.net>
2021-03-10 13:59:45 -0800

    Fix the prompt behavior if terminal is not tty

--- gamestonk_terminal/menu.py
+++ gamestonk_terminal/menu.py
@@ -4,4 +4,8 @@ from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 
 history_file = os.path.join(os.path.expanduser("~"), ".gamestonk_terminal.his")
-session = PromptSession(history=FileHistory(history_file))
+
+try:
+    session = PromptSession(history=FileHistory(history_file))
+except Exception as e:
+    session = None
