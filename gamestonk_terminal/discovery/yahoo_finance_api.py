9502ab599b189f2df49591e5481c54459296c31f
didier <dro.lopes@campus.fct.unl.pt>
2021-03-14 20:24:55 +0000

    Add top losers

--- gamestonk_terminal/discovery/yahoo_finance_api.py
+++ gamestonk_terminal/discovery/yahoo_finance_api.py
@@ -7,7 +7,7 @@ def gainers(l_args):
     parser = argparse.ArgumentParser(
         add_help=False,
         prog="gainers",
-        description="Print up to 25 top ticker gainers in terminal. [Source: Yahoo Finance]",
+        description="Print up to 25 top ticker gainers. [Source: Yahoo Finance]",
     )
 
     parser.add_argument(
@@ -29,4 +29,33 @@ def gainers(l_args):
         "https://finance.yahoo.com/screener/predefined/day_gainers"
     )[0]
     print(df_gainers.head(ns_parser.n_gainers).to_string(index=False))
+    print("")
+
+
+def losers(l_args):
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        prog="losers",
+        description="Print up to 25 top ticker losers. [Source: Yahoo Finance]",
+    )
+
+    parser.add_argument(
+        "-n",
+        "--num",
+        action="store",
+        dest="n_losers",
+        type=int,
+        default=5,
+        choices=range(1, 25),
+        help="Number of the top losers stocks to retrieve.",
+    )
+
+    ns_parser = parse_known_args_and_warn(parser, l_args)
+    if not ns_parser:
+        return
+
+    df_losers = pd.read_html(
+        "https://finance.yahoo.com/screener/predefined/day_losers"
+    )[0]
+    print(df_losers.head(ns_parser.n_losers).to_string(index=False))
     print("")
