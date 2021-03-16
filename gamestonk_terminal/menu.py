6807c00226bae5e152418097b4b7df9825dc14bc
clothespin <ian@unstu.cc>
2021-03-15 21:00:04 -0700

    whoops pass the function don't call it

--- gamestonk_terminal/menu.py
+++ gamestonk_terminal/menu.py
@@ -8,6 +8,6 @@ from prompt_toolkit.history import FileHistory
 history_file = os.path.join(os.path.expanduser("~"), ".gamestonk_terminal.his")
 
 try:
-    session = PromptSession(history=FileHistory(history_file), inputhook=pyplot.draw())
+    session = PromptSession(history=FileHistory(history_file), inputhook=pyplot.draw)
 except Exception as e:  # noqa: F841
     session = None
