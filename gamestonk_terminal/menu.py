0573a583be6ba2f99a2ad5bcc9c8bd19942ebf4a
clothespin <ian@unstu.cc>
2021-03-15 20:49:52 -0700

    Registers pyplot.draw as inputhook for prompt_toolkit

--- gamestonk_terminal/menu.py
+++ gamestonk_terminal/menu.py
@@ -1,11 +1,13 @@
 import os
 
+from matplotlib import pyplot
+
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 
 history_file = os.path.join(os.path.expanduser("~"), ".gamestonk_terminal.his")
 
 try:
-    session = PromptSession(history=FileHistory(history_file))
+    session = PromptSession(history=FileHistory(history_file), inputhook=pyplot.draw())
 except Exception as e:  # noqa: F841
     session = None
