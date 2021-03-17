0a3b91a49462a2d8dd74f92928ce53afb7d88fda
clothespin <ian@unstu.cc>
2021-03-16 21:23:18 -0700

    Tests string-valued env variables for truthiness in feature_flags.py

--- gamestonk_terminal/feature_flags.py
+++ gamestonk_terminal/feature_flags.py
@@ -1,12 +1,12 @@
 import os
 
-USE_COLOR = os.getenv("GTFF_USE_COLOR") or True
+USE_COLOR = os.getenv("GTFF_USE_COLOR").lower() not in ['false', '0'] or True
 USE_FLAIR = os.getenv("GTFF_USE_FLAIR") or "stars"
-USE_ION = os.getenv("GTFF_USE_ION") or True
-USE_PROMPT_TOOLKIT = os.getenv("GTFF_USE_PROMPT_TOOLKIT") or False
+USE_ION = os.getenv("GTFF_USE_ION").lower() not in ['false', '0'] or True
+USE_PROMPT_TOOLKIT = os.getenv("GTFF_USE_PROMPT_TOOLKIT").lower() not in ['false', '0'] or False
 
 # Enable Prediction features
-ENABLE_PREDICT = os.getenv("GTFF_ENABLE_PREDICT") or False
+ENABLE_PREDICT = os.getenv("GTFF_ENABLE_PREDICT").lower() not in ['false', '0'] or False
 
 # Enable plot autoscaling
-USE_PLOT_AUTOSCALING = os.getenv("GTFF_USE_PLOT_AUTOSCALING") or False
+USE_PLOT_AUTOSCALING = os.getenv("GTFF_USE_PLOT_AUTOSCALING").lower() not in ['false', '0'] or False
