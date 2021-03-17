e953b8b200b2bb969cd256ba96a149aab04f1aaa
Laurent Andriamifidy <lolrenx@gmail.com>
2021-03-17 20:44:21 +0100

    blacked

--- terminal.py
+++ terminal.py
@@ -1,4 +1,4 @@
-    #!/usr/bin/env python
+#!/usr/bin/env python
 
 import argparse
 
@@ -73,8 +73,7 @@ def main():
         "dd",
         "pred",
         "ca",
-        "op"
-        "fred",
+        "op" "fred",
     ]
     menu_parser.add_argument("opt", choices=choices)
     completer = NestedCompleter.from_nested_dict({c: None for c in choices})
@@ -184,7 +183,7 @@ def main():
 
         elif ns_known_args.opt == "op":
             b_quit = opm.opt_menu(df_stock, s_ticker, s_start, s_interval)
-            
+
         elif ns_known_args.opt == "fred":
             b_quit = fm.fred_menu()
 
