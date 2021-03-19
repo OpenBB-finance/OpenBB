482c032af702df33f218b74e50ae01e35621c276
didier <dro.lopes@campus.fct.unl.pt>
2021-03-19 19:12:01 +0000

    Thoughs of the day under feature flag

--- gamestonk_terminal/feature_flags.py
+++ gamestonk_terminal/feature_flags.py
@@ -1,9 +1,16 @@
 from distutils.util import strtobool
 import os
 
+# Use coloring features
 USE_COLOR = strtobool(os.getenv("GTFF_USE_COLOR", "True"))
+
+# Select console flair (choose from config_terminal.py list)
 USE_FLAIR = os.getenv("GTFF_USE_FLAIR") or "stars"
+
+# Enable interactive matplotlib mode
 USE_ION = strtobool(os.getenv("GTFF_USE_ION", "True"))
+
+# Enable Prompt Toolkit
 USE_PROMPT_TOOLKIT = strtobool(os.getenv("GTFF_USE_PROMPT_TOOLKIT", "False"))
 
 # Enable Prediction features
@@ -11,3 +18,6 @@ ENABLE_PREDICT = strtobool(os.getenv("GTFF_ENABLE_PREDICT", "False"))
 
 # Enable plot autoscaling
 USE_PLOT_AUTOSCALING = strtobool(os.getenv("GTFF_USE_PLOT_AUTOSCALING", "False"))
+
+# Enable thoughts of the day
+ENABLE_THOUGHTS_DAY = strtobool(os.getenv("GTFF_ENABLE_THOUGHTS_DAY", "False"))
