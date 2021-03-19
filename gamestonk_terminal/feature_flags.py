ae1a62a1d78dcfd7f8f7248e07a93a0838174c17
Artem Veremey <artem@veremey.net>
2021-03-18 17:43:14 -0700

    Fixing Feature Flag setting from environment

--- gamestonk_terminal/feature_flags.py
+++ gamestonk_terminal/feature_flags.py
@@ -1,12 +1,39 @@
 import os
 
-USE_COLOR = os.getenv("GTFF_USE_COLOR") or True
-USE_FLAIR = os.getenv("GTFF_USE_FLAIR") or "stars"
-USE_ION = os.getenv("GTFF_USE_ION") or True
-USE_PROMPT_TOOLKIT = os.getenv("GTFF_USE_PROMPT_TOOLKIT") or False
+
+def convert_to_bool(ins: str) -> bool:
+    return True if ins == "True" else False
+
+
+USE_COLOR = (
+    True
+    if os.getenv("GTFF_USE_COLOR") is None
+    else convert_to_bool(os.getenv("GTFF_USE_COLOR"))
+)
+USE_FLAIR = (
+    "stars" if os.getenv("GTFF_USE_FLAIR") is None else os.getenv("GTFF_USE_FLAIR")
+)
+USE_ION = (
+    True
+    if os.getenv("GTFF_USE_ION") is None
+    else convert_to_bool(os.getenv("GTFF_USE_ION"))
+)
+USE_PROMPT_TOOLKIT = (
+    False
+    if os.getenv("GTFF_USE_PROMPT_TOOLKIT") is None
+    else convert_to_bool(os.getenv("GTFF_USE_PROMPT_TOOLKIT"))
+)
 
 # Enable Prediction features
-ENABLE_PREDICT = os.getenv("GTFF_ENABLE_PREDICT") or False
+ENABLE_PREDICT = (
+    False
+    if os.getenv("GTFF_ENABLE_PREDICT") is None
+    else convert_to_bool(os.getenv("GTFF_ENABLE_PREDICT"))
+)
 
 # Enable plot autoscaling
-USE_PLOT_AUTOSCALING = os.getenv("GTFF_USE_PLOT_AUTOSCALING") or False
+USE_PLOT_AUTOSCALING = (
+    False
+    if os.getenv("GTFF_USE_PLOT_AUTOSCALING") is None
+    else convert_to_bool(os.getenv("GTFF_USE_PLOT_AUTOSCALING"))
+)
