25e6d3d282d12f634db7231a823dd934bcc2c8ce
Joao Vicente <65868008+JoaoVicente129@users.noreply.github.com>
2021-03-25 13:06:49 +0000

    config

---
+++ config_terminal.py
@@ -0,0 +1,22 @@
+import os
+
+# https://www.alphavantage.co
+API_KEY_ALPHAVANTAGE = os.getenv("GT_API_KEY_ALPHAVANTAGE") or "YG6O8II69KI4442A"
+
+# https://financialmodelingprep.com/developer
+API_KEY_FINANCIALMODELINGPREP = os.getenv("GT_API_KEY_FINANCIALMODELINGPREP") or "9367508cfa9057d92ed34833fb5ca391"
+
+# https://www.quandl.com/tools/api
+API_KEY_QUANDL = os.getenv("GT_API_KEY_QUANDL") or "H7jWnYzNZe8yLKYv4FqX"
+
+# https://www.reddit.com/prefs/apps
+API_REDDIT_CLIENT_ID = os.getenv("GT_API_REDDIT_CLIENT_ID") or "QONuVuzTVM7GMw"
+API_REDDIT_CLIENT_SECRET = os.getenv("GT_API_REDDIT_CLIENT_SECRET") or "JbE0qH33MG-cPXeLf7VUQHovDsk3Rw"
+API_REDDIT_USERNAME = os.getenv("GT_API_REDDIT_USERNAME") or "Zealousideal_Bet_166"
+API_REDDIT_USER_AGENT = os.getenv("GT_API_REDDIT_USER_AGENT") or "REPLACE_ME"
+API_REDDIT_PASSWORD = os.getenv("GT_API_REDDIT_PASSWORD") or "000000"
+
+# https://developer.twitter.com
+API_TWITTER_KEY = os.getenv("GT_API_TWITTER_KEY") or "bet_166"
+API_TWITTER_SECRET_KEY = os.getenv("GT_API_TWITTER_SECRET_KEY") or "000000jJ"
+API_TWITTER_BEARER_TOKEN = os.getenv("GT_API_TWITTER_BEARER_TOKEN") or "1362347503157473281-HewYr7c5VYPBSmbnqarhDmavnErU8A"
--- gamestonk_terminal/feature_flags.py
+++ gamestonk_terminal/feature_flags.py
@@ -11,13 +11,13 @@ USE_FLAIR = os.getenv("GTFF_USE_FLAIR") or "stars"
 USE_ION = strtobool(os.getenv("GTFF_USE_ION", "True"))
 
 # Enable Prompt Toolkit
-USE_PROMPT_TOOLKIT = strtobool(os.getenv("GTFF_USE_PROMPT_TOOLKIT", "False"))
+USE_PROMPT_TOOLKIT = strtobool(os.getenv("GTFF_USE_PROMPT_TOOLKIT", "True"))
 
 # Enable Prediction features
-ENABLE_PREDICT = strtobool(os.getenv("GTFF_ENABLE_PREDICT", "False"))
+ENABLE_PREDICT = strtobool(os.getenv("GTFF_ENABLE_PREDICT", "True"))
 
 # Enable plot autoscaling
-USE_PLOT_AUTOSCALING = strtobool(os.getenv("GTFF_USE_PLOT_AUTOSCALING", "False"))
+USE_PLOT_AUTOSCALING = strtobool(os.getenv("GTFF_USE_PLOT_AUTOSCALING", "True"))
 
 # Enable thoughts of the day
-ENABLE_THOUGHTS_DAY = strtobool(os.getenv("GTFF_ENABLE_THOUGHTS_DAY", "False"))
+ENABLE_THOUGHTS_DAY = strtobool(os.getenv("GTFF_ENABLE_THOUGHTS_DAY", "True"))
