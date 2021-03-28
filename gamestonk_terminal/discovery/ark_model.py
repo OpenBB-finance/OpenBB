8856fb7826b8376a8519b68f492f19bdc728007f
Artem Veremey <artem@veremey.net>
2021-03-28 13:09:22 -0700

    Refactoring ARK

---
+++ gamestonk_terminal/discovery/ark_model.py
@@ -0,0 +1,76 @@
+from datetime import timedelta
+import json
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+from pandas.core.frame import DataFrame
+import yfinance as yf
+
+from gamestonk_terminal.helper_funcs import get_user_agent
+
+
+def get_ark_orders() -> DataFrame:
+    url_orders = "https://cathiesark.com/ark-funds-combined/trades"
+
+    raw_page = requests.get(url_orders, headers={"User-Agent": get_user_agent()}).text
+
+    parsed_script = BeautifulSoup(raw_page, "lxml").find(
+        "script", {"id": "__NEXT_DATA__"}
+    )
+
+    parsed_json = json.loads(parsed_script.string)
+
+    df_orders = pd.json_normalize(parsed_json["props"]["pageProps"]["arkTrades"])
+
+    if df_orders.empty:
+        return pd.DataFrame()
+
+    df_orders.drop(
+        [
+            "everything",
+            "everything.profile.customThumbnail",
+            "hidden",
+            "images.thumbnail",
+        ],
+        axis=1,
+        inplace=True,
+    )
+
+    df_orders["date"] = pd.to_datetime(df_orders["date"], format="%Y-%m-%dZ").dt.date
+
+    return df_orders
+
+
+def add_order_total(df_orders: DataFrame) -> DataFrame:
+    start_date = df_orders["date"].iloc[-1] - timedelta(days=1)
+
+    tickers = " ".join(df_orders["ticker"].unique())
+
+    print("")
+
+    prices = yf.download(tickers, start=start_date, progress=False)
+
+    for i, candle in enumerate(["Volume", "Open", "Close", "High", "Low", "Total"]):
+        df_orders.insert(i + 3, candle.lower(), 0)
+
+    for i, _ in df_orders.iterrows():
+        if np.isnan(
+            prices["Open"][df_orders.loc[i, "ticker"]][
+                df_orders.loc[i, "date"].strftime("%Y-%m-%d")
+            ]
+        ):
+            for candle in ["Volume", "Open", "Close", "High", "Low", "Total"]:
+                df_orders.loc[i, candle.lower()] = 0
+            continue
+
+        for candle in ["Volume", "Open", "Close", "High", "Low"]:
+            df_orders.loc[i, candle.lower()] = prices[candle][
+                df_orders.loc[i, "ticker"]
+            ][df_orders.loc[i, "date"].strftime("%Y-%m-%d")]
+
+        df_orders.loc[i, "total"] = (
+            df_orders.loc[i, "close"] * df_orders.loc[i, "shares"]
+        )
+
+    return df_orders
--- gamestonk_terminal/discovery/ark_view.py
+++ gamestonk_terminal/discovery/ark_view.py
@@ -1,21 +1,15 @@
 import argparse
-from datetime import timedelta
-import json
-import requests
 from colorama import Fore, Style
-from bs4 import BeautifulSoup
-import numpy as np
 import pandas as pd
-from pandas.core.frame import DataFrame
-import yfinance as yf
 
 from gamestonk_terminal.helper_funcs import (
     check_positive,
-    get_user_agent,
     patch_pandas_text_adjustment,
     parse_known_args_and_warn,
 )
 from gamestonk_terminal import feature_flags as gtff
+
+from gamestonk_terminal.discovery import ark_model
 
 
 def direction_color_red_green(val: str) -> str:
@@ -27,72 +21,6 @@ def direction_color_red_green(val: str) -> str:
         ret = val
 
     return ret
-
-
-def get_ark_orders() -> DataFrame:
-    url_orders = "https://cathiesark.com/ark-funds-combined/trades"
-
-    raw_page = requests.get(url_orders, headers={"User-Agent": get_user_agent()}).text
-
-    parsed_script = BeautifulSoup(raw_page, "lxml").find(
-        "script", {"id": "__NEXT_DATA__"}
-    )
-
-    parsed_json = json.loads(parsed_script.string)
-
-    df_orders = pd.json_normalize(parsed_json["props"]["pageProps"]["arkTrades"])
-
-    if df_orders.empty:
-        return pd.DataFrame()
-
-    df_orders.drop(
-        [
-            "everything",
-            "everything.profile.customThumbnail",
-            "hidden",
-            "images.thumbnail",
-        ],
-        axis=1,
-        inplace=True,
-    )
-
-    df_orders["date"] = pd.to_datetime(df_orders["date"], format="%Y-%m-%dZ").dt.date
-
-    return df_orders
-
-
-def add_order_total(df_orders: DataFrame) -> DataFrame:
-    start_date = df_orders["date"].iloc[-1] - timedelta(days=1)
-
-    tickers = " ".join(df_orders["ticker"].unique())
-
-    print("")
-
-    prices = yf.download(tickers, start=start_date, progress=False)
-
-    for i, candle in enumerate(["Volume", "Open", "Close", "High", "Low", "Total"]):
-        df_orders.insert(i + 3, candle.lower(), 0)
-
-    for i, _ in df_orders.iterrows():
-        if np.isnan(
-            prices["Open"][df_orders.loc[i, "ticker"]][
-                df_orders.loc[i, "date"].strftime("%Y-%m-%d")
-            ]
-        ):
-            for candle in ["Volume", "Open", "Close", "High", "Low", "Total"]:
-                df_orders.loc[i, candle.lower()] = 0
-            continue
-
-        for candle in ["Volume", "Open", "Close", "High", "Low"]:
-            df_orders.loc[i, candle.lower()] = prices[candle][
-                df_orders.loc[i, "ticker"]
-            ][df_orders.loc[i, "date"].strftime("%Y-%m-%d")]
-
-        df_orders.loc[i, "total"] = (
-            df_orders.loc[i, "close"] * df_orders.loc[i, "shares"]
-        )
-
-    return df_orders
 
 
 def ark_orders(l_args):
@@ -118,14 +46,14 @@ def ark_orders(l_args):
     if not ns_parser:
         return
 
-    df_orders = get_ark_orders()
+    df_orders = ark_model.get_ark_orders()
 
     if df_orders.empty:
         print("The ARK orders aren't anavilable at the moment.\n")
         return
 
     pd.set_option("mode.chained_assignment", None)
-    df_orders = add_order_total(df_orders.head(ns_parser.n_num))
+    df_orders = ark_model.add_order_total(df_orders.head(ns_parser.n_num))
 
     if gtff.USE_COLOR:
         df_orders["direction"] = df_orders["direction"].apply(direction_color_red_green)
