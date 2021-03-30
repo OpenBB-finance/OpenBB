c3372ac0733d8534a69b089d73d5665aaa8fb044
Artem Veremey <artem@veremey.net>
2021-03-30 11:31:06 -0700

    Adding a unit test for the Seeking Alpha earnings calendar model

--- gamestonk_terminal/discovery/seeking_alpha_model.py
+++ gamestonk_terminal/discovery/seeking_alpha_model.py
@@ -9,6 +9,26 @@ from pandas.core.frame import DataFrame
 from gamestonk_terminal.helper_funcs import get_user_agent
 
 
+def get_earnings_html(url_next_earnings: str) -> str:
+    """Wraps HTTP requests.get for testibility
+
+    Parameters
+    ----------
+    url_next_earnings : str
+        Next earnings URL
+
+    Returns
+    -------
+    str
+        HTML page of next earnings
+    """
+    earnings_html = requests.get(
+        url_next_earnings, headers={"User-Agent": get_user_agent()}
+    ).text
+
+    return earnings_html
+
+
 def get_next_earnings(pages: int) -> DataFrame:
     """Returns a DataFrame with upcoming earnings
 
@@ -24,17 +44,11 @@ def get_next_earnings(pages: int) -> DataFrame:
     """
 
     earnings = list()
+    url_next_earnings = "https://seekingalpha.com/earnings/earnings-calendar"
+
     for idx in range(0, pages):
-        if idx == 0:
-            url_next_earnings = "https://seekingalpha.com/earnings/earnings-calendar"
-        else:
-            url_next_earnings = (
-                f"https://seekingalpha.com/earnings/earnings-calendar/{idx+1}"
-            )
         text_soup_earnings = BeautifulSoup(
-            requests.get(
-                url_next_earnings, headers={"User-Agent": get_user_agent()}
-            ).text,
+            get_earnings_html(url_next_earnings),
             "lxml",
         )
 
@@ -43,6 +57,10 @@ def get_next_earnings(pages: int) -> DataFrame:
             for a_stock in stock_rows.contents[:3]:
                 stocks.append(a_stock.text)
             earnings.append(stocks)
+
+        url_next_earnings = (
+            f"https://seekingalpha.com/earnings/earnings-calendar/{idx+1}"
+        )
 
     df_earnings = pd.DataFrame(earnings, columns=["Ticker", "Name", "Date"])
     df_earnings["Date"] = pd.to_datetime(df_earnings["Date"])
