eeb8bb6cefee32b902ca7af285d7459a3506fafe
Artem Veremey <artem@veremey.net>
2021-03-30 13:25:53 -0700

    Flake8

--- gamestonk_terminal/menu.py
+++ gamestonk_terminal/menu.py
@@ -12,7 +12,7 @@ def inputhook(inputhook_context):
         try:
             pyplot.pause(0.1)
         # pylint: disable=unused-variable
-        except Exception as e:
+        except Exception as e:  # noqa: F841
             continue
     return False
 
@@ -22,6 +22,7 @@ history_file = os.path.join(os.path.expanduser("~"), ".gamestonk_terminal.his")
 try:
     session = PromptSession(history=FileHistory(history_file))
     set_eventloop_with_inputhook(inputhook)
+# pylint: disable=unused-variable
 except Exception as e:  # noqa: F841
     print(
         "WARNING: Prompt toolkit is turned on but did not initialize successfully. Falling back to input()..."
