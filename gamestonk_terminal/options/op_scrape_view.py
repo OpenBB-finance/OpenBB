b7feb17d597335254476707f24b18b41675cd3a0
Jamie <jmaslek11@gmail.com>
2021-04-20 15:12:10 -0400

    scrape barchart to get options information,  added to poetry and reqs

---
+++ gamestonk_terminal/options/op_scrape_view.py
@@ -0,0 +1,102 @@
+"""Helper functions for scraping options data"""
+__docformat__ = "numpy"
+
+import argparse
+from typing import List
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options as cOpts
+from selenium.webdriver.firefox.options import Options as fOpts
+import pandas as pd
+from tabulate import tabulate
+from gamestonk_terminal.config_terminal import (
+    PATH_TO_SELENIUM_DRIVER as path_to_driver,
+    WEBDRIVER_TO_USE as web,
+)
+from gamestonk_terminal.helper_funcs import parse_known_args_and_warn
+
+browsers = ["chrome", "firefox"]
+
+
+def print_options_data(stock: str, other_args: List[str]):
+    """
+    Scrapes Barchart.com for the options information
+    Parameters
+    ----------
+    stock: str
+        Ticker to get options info for
+    other_args: List[str]
+        Other arguments.  Currently just accepts a browser flag for selenium
+
+    """
+
+    if path_to_driver is None:
+        print("Please specify your selenium driver path in config_terminal.py")
+        print("")
+        return
+
+    parser = argparse.ArgumentParser(
+        prog="info",
+        add_help=False,
+        description="Display option data [Source: Barchart.com]",
+    )
+
+    parser.add_argument(
+        "-b",
+        "--browser",
+        dest="browser",
+        help="selenium webdriver to use",
+        choices=browsers,
+        default=None,
+    )
+
+    try:
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return
+        if ns_parser.browser:
+            browser = ns_parser.browser
+        else:
+            browser = web
+
+    except Exception as e:
+        print(e)
+        print("")
+        return
+
+    try:
+        if browser == "chrome":
+            options = cOpts()
+            options.headless = True
+            driver = webdriver.Chrome(options=options, executable_path=path_to_driver)
+
+        elif browser == "firefox":
+            options = fOpts()
+            options.headless = True
+            driver = webdriver.Firefox(options=options, executable_path=path_to_driver)
+
+        page = f"https://www.barchart.com/stocks/quotes/{stock}/overview"
+        driver.get(page)
+
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        tags = soup.find(
+            "div",
+            attrs={
+                "class": "barchart-content-block symbol-fundamentals bc-cot-table-wrapper"
+            },
+        )
+        data = tags.find_all("li")
+        labels = []
+        values = []
+        print("")
+        for row in data:
+            labels.append(row.find_all("span")[0].getText())
+            values.append(row.find_all("span")[1].getText())
+
+        df = pd.DataFrame(data=[labels, values]).T
+        print(tabulate(df, tablefmt="fancy_grid", showindex=False))
+        print("")
+
+    except Exception as e:
+        print(e)
+        print("")
