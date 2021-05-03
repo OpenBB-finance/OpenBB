5cea53e46749c64b34d2ffe3baeda60cf6694cf3
didier <dro.lopes@campus.fct.unl.pt>
2021-05-03 15:04:29 +0100

    remove unused import

--- gamestonk_terminal/behavioural_analysis/finnhub_view.py
+++ gamestonk_terminal/behavioural_analysis/finnhub_view.py
@@ -1,7 +1,6 @@
 import argparse
 from typing import List, Dict
 import requests
-import pandas as pd
 from gamestonk_terminal import config_terminal as cfg
 from gamestonk_terminal.helper_funcs import (
     parse_known_args_and_warn,
