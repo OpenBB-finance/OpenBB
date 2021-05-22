dc25177c1bfb6d5acf40fcf51f0556d051990689
Jamie <jmaslek11@gmail.com>
2021-05-21 21:34:09 -0400

    pyupgrade upgraded

--- gamestonk_terminal/helper_funcs.py
+++ gamestonk_terminal/helper_funcs.py
@@ -326,10 +326,10 @@ def text_adjustment_join_unicode(self, lines, sep=""):
 def text_adjustment_adjoin(self, space, *lists, **kwargs):
     # Add space for all but the last column:
     pads = ([space] * (len(lists) - 1)) + [0]
-    max_col_len = max([len(col) for col in lists])
+    max_col_len = max(len(col) for col in lists)
     new_cols = []
     for col, pad in zip(lists, pads):
-        width = max([self.len(s) for s in col]) + pad
+        width = max(self.len(s) for s in col) + pad
         c = self.justify(col, width, mode="left")
         # Add blank cells to end of col if needed for different col lens:
         if len(col) < max_col_len:
