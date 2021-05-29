7d88b0e87425757d74db4b72ad723ed40ec6783e
didier <dro.lopes@campus.fct.unl.pt>
2021-05-29 01:03:16 +0100

    Improve finviz view implementation on ta controller

--- gamestonk_terminal/technical_analysis/finviz_view.py
+++ gamestonk_terminal/technical_analysis/finviz_view.py
@@ -2,11 +2,20 @@
 __docformat__ = "numpy"
 
 import argparse
+import io
 from typing import List
+import requests
+import matplotlib.pyplot as plt
 from finvizfinance.quote import finvizfinance
+from finvizfinance.util import headers
 from PIL import Image
 
-from gamestonk_terminal.helper_funcs import parse_known_args_and_warn
+from gamestonk_terminal.helper_funcs import (
+    parse_known_args_and_warn,
+    plot_autoscale,
+)
+from gamestonk_terminal.config_plot import PLOT_DPI
+from gamestonk_terminal import feature_flags as gtff
 
 
 def view(other_args: List[str], ticker: str):
@@ -33,11 +42,24 @@ def view(other_args: List[str], ticker: str):
             return
 
         stock = finvizfinance(ticker)
-        stock.TickerCharts()
+        image_url = stock.TickerCharts(urlonly=True)
 
-        img = Image.open(ticker + ".jpg")
-        img.show()
+        r = requests.get(image_url, stream=True, headers=headers, timeout=5)
+        r.raise_for_status()
+        r.raw.decode_content = True
 
+        dataBytesIO = io.BytesIO(r.content)
+        im = Image.open(dataBytesIO)
+
+        fig, ax = plt.subplots(figsize=plot_autoscale(), dpi=PLOT_DPI)
+        ax.set_axis_off()
+        fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
+
+        if gtff.USE_ION:
+            plt.ion()
+
+        plt.imshow(im)
+        plt.show()
         print("")
 
     except SystemExit:
