4d1dbc96a61d4db2b5b89f89a7bda65e42c5c304
didier <dro.lopes@campus.fct.unl.pt>
2021-06-17 01:45:28 +0100

    Refactor stockgrid.io commands on discovery

--- gamestonk_terminal/discovery/disc_controller.py
+++ gamestonk_terminal/discovery/disc_controller.py
@@ -55,7 +55,6 @@ class DiscoveryController:
         "latest",
         "trending",
         "darkpool",
-        "topdark",
         "darkshort",
         "shortvol",
     ]
@@ -103,10 +102,8 @@ class DiscoveryController:
         print("   latest         latest news [Seeking Alpha]")
         print("   trending       trending news [Seeking Alpha]")
         print("   ratings        top ratings updates [MarketBeat]")
-        print("   darkpool       dark pool tickers with growing activity [FINRA]")
-        print("   topdark        largest dark pool positions [Stockgrid.io]")
         print("   darkshort      dark pool short information [Stockgrid.io]")
-        print("   shortvol       short interest [Stockgrid.io]")
+        print("   shortvol       short interest and days to cover [Stockgrid.io]")
         print("")
 
     def switch(self, an_input: str):
@@ -226,10 +223,6 @@ class DiscoveryController:
     def call_darkpool(self, other_args: List[str]):
         """Process darkpool command"""
         finra_ats_view.dark_pool(other_args)
-
-    def call_topdark(self, other_args: List[str]):
-        """Process topdark command"""
-        stockgrid_view.top_dark_pools(other_args)
 
     def call_darkshort(self, other_args: List[str]):
         """Process shortvol command"""
