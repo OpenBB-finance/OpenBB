c1b0c95b67a69ca5777c6cb85ad6231799d9fd6d
jakub <plutakuba@gmail.com>
2021-06-26 15:14:09 +0200

    Now if ta is sperate mode, and load doesn't return data frame, change test in the way that ta with Coin object returns data frame

--- tests/test_coingecko_crypto.py
+++ tests/test_coingecko_crypto.py
@@ -1,7 +1,8 @@
 from unittest import mock, TestCase
 import json
 from pycoingecko import CoinGeckoAPI
-from gamestonk_terminal.cryptocurrency.coingecko.pycoingecko_view import load
+from gamestonk_terminal.cryptocurrency.coingecko.pycoingecko_view import load, ta
+from gamestonk_terminal.cryptocurrency.coingecko.pycoingecko_coin_model import Coin
 
 # pylint: disable=unused-import
 
@@ -20,9 +21,26 @@ class TestCoinGeckoAPI(TestCase):
             sample_return = json.load(f)
 
         mock_load.return_value = sample_return
-        coin, mock_return = load(["-c", "bitcoin"])
-        self.assertEqual(coin, "bitcoin")
-        self.assertTrue(mock_load.called)
+        coin = load(["-c", "bitcoin"])
+        self.assertEqual(coin.coin_symbol, "bitcoin")
+        self.assertIsInstance(coin, Coin)
+
+    @mock.patch(
+        "gamestonk_terminal.cryptocurrency.coingecko.pycoingecko_view.CoinGeckoAPI.get_coin_market_chart_by_id"
+    )
+    def test_coin_api_load_df_for_ta(self, mock_load):
+        """
+        Mock load function through get_coin_market_chart_by_id.
+        Mock returns a dict saved as .json
+
+        """
+
+        with open("tests/data/btc_usd_test_data.json") as f:
+            sample_return = json.load(f)
+
+        mock_load.return_value = sample_return
+        coin = load(["-c", "bitcoin"])
+        mock_return = ta(coin, ["--vs", "usd"])
         self.assertTrue(mock_return.shape == (722, 2))
 
     def test_coin(self):
