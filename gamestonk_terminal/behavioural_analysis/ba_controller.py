a0d463dde453d1f60c76631958c469fee26da069
didier <dro.lopes@campus.fct.unl.pt>
2021-07-05 00:08:45 +0100

    improve ba menu

--- gamestonk_terminal/behavioural_analysis/ba_controller.py
+++ gamestonk_terminal/behavioural_analysis/ba_controller.py
@@ -2,6 +2,7 @@
 __docformat__ = "numpy"
 
 import argparse
+import os
 from typing import List
 from datetime import datetime
 from prompt_toolkit.completion import NestedCompleter
@@ -23,6 +24,8 @@ class BehaviouralAnalysisController:
 
     # Command choices
     CHOICES = [
+        "?",
+        "cls",
         "help",
         "q",
         "quit",
@@ -62,7 +65,8 @@ class BehaviouralAnalysisController:
             "https://github.com/GamestonkTerminal/GamestonkTerminal/tree/main/gamestonk_terminal/behavioural_analysis"
         )
         print("\nBehavioural Analysis:")
-        print("   help          show this behavioural analysis menu again")
+        print("   cls           clear screen")
+        print("   ?/help        show this menu again")
         print("   q             quit this menu, and shows back to main menu")
         print("   quit          quit to abandon program")
         print("")
@@ -109,7 +113,22 @@ class BehaviouralAnalysisController:
             True - quit the program
             None - continue in the menu
         """
+        # Empty command
+        if not an_input:
+            print("")
+            return None
+
         (known_args, other_args) = self.ba_parser.parse_known_args(an_input.split())
+
+        # Help menu again
+        if known_args.cmd == "?":
+            self.print_help()
+            return None
+
+        # Clear screen
+        if known_args.cmd == "cls":
+            os.system("cls||clear")
+            return None
 
         return getattr(
             self, "call_" + known_args.cmd, lambda: "Command not recognized!"
