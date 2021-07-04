2e3ad76dd9e05c7e9bdb83059b72f01927a583af
didier <dro.lopes@campus.fct.unl.pt>
2021-07-05 00:49:19 +0100

    improve op menu

--- gamestonk_terminal/options/op_controller.py
+++ gamestonk_terminal/options/op_controller.py
@@ -2,6 +2,7 @@
 __docformat__ = "numpy"
 
 import argparse
+import os
 from typing import List
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -24,6 +25,8 @@ class OptionsController:
 
     # Command choices
     CHOICES = [
+        "cls",
+        "?",
         "help",
         "q",
         "quit",
@@ -114,7 +117,8 @@ class OptionsController:
             "https://github.com/GamestonkTerminal/GamestonkTerminal/tree/main/gamestonk_terminal/options"
         )
         print("\nOptions:")
-        print("   help          show this  menu again")
+        print("   cls           clear screen")
+        print("   ?/help        show this menu again")
         print("   q             quit this menu, and shows back to main menu")
         print("   quit          quit to abandon program")
         print("")
@@ -145,7 +149,23 @@ class OptionsController:
             True - quit the program
             None - continue in the menu
         """
+
+        # Empty command
+        if not an_input:
+            print("")
+            return None
+
         (known_args, other_args) = self.op_parser.parse_known_args(an_input.split())
+
+        # Help menu again
+        if known_args.cmd == "?":
+            self.print_help(self.expiry_date)
+            return None
+
+        # Clear screen
+        if known_args.cmd == "cls":
+            os.system("cls||clear")
+            return None
 
         return getattr(
             self, "call_" + known_args.cmd, lambda: "Command not recognized!"
