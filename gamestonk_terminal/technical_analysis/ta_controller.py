eb897c7ba689ff7213fb5e3e9296010392577c12
didier <dro.lopes@campus.fct.unl.pt>
2021-07-05 01:11:36 +0100

    improve ta menu

--- gamestonk_terminal/technical_analysis/ta_controller.py
+++ gamestonk_terminal/technical_analysis/ta_controller.py
@@ -2,6 +2,7 @@
 __docformat__ = "numpy"
 
 import argparse
+import os
 from typing import List
 from datetime import datetime
 import pandas as pd
@@ -27,6 +28,8 @@ class TechnicalAnalysisController:
 
     # Command choices
     CHOICES = [
+        "cls",
+        "?",
         "help",
         "q",
         "quit",
@@ -82,7 +85,8 @@ class TechnicalAnalysisController:
             print(f"\n{s_intraday} Stock: {self.ticker}")
 
         print("\nTechnical Analysis:")  # https://github.com/twopirllc/pandas-ta
-        print("   help        show this technical analysis menu again")
+        print("   cls         clear screen")
+        print("   ?/help      show this menu again")
         print("   q           quit this menu, and shows back to main menu")
         print("   quit        quit to abandon program")
         print("")
@@ -122,7 +126,23 @@ class TechnicalAnalysisController:
             True - quit the program
             None - continue in the menu
         """
+
+        # Empty command
+        if not an_input:
+            print("")
+            return None
+
         (known_args, other_args) = self.ta_parser.parse_known_args(an_input.split())
+
+        # Help menu again
+        if known_args.cmd == "?":
+            self.print_help()
+            return None
+
+        # Clear screen
+        if known_args.cmd == "cls":
+            os.system("cls||clear")
+            return None
 
         return getattr(
             self, "call_" + known_args.cmd, lambda: "Command not recognized!"
