e009d793b308a6fe543f1f72041f4d1d3b253b98
Colin Delahunty <colindelahunty@Colins-Air.attlocal.net>
2021-08-03 22:16:02 -0400

    Figured out testing with input and print

--- tests/test_fa/test_fa_controller.py
+++ tests/test_fa/test_fa_controller.py
@@ -1,5 +1,6 @@
 """ fundamental_analysis/fa_controller.py tests """
 from datetime import datetime, timedelta
+from contextlib import contextmanager
 import unittest
 import io
 import sys
@@ -8,9 +9,17 @@ import sys
 from gamestonk_terminal.fundamental_analysis import fa_controller
 
 
+@contextmanager
+def replace_stdin(target):
+    orig = sys.stdin
+    sys.stdin = target
+    yield
+    sys.stdin = orig
+
+
 class TestFaController(unittest.TestCase):
-    start = str(datetime.now() - timedelta(days=200))
-    menuClass = fa_controller.FundamentalAnalysisController("GME", start, "1440min")
+    start = datetime.now() - timedelta(days=200)
+    menuClass = fa_controller.FundamentalAnalysisController("GME", start, "1440min")  # type: ignore
     choices = fa_controller.FundamentalAnalysisController.CHOICES
 
     def test_fa_controller_help(self):
@@ -20,11 +29,22 @@ class TestFaController(unittest.TestCase):
         sys.stdout = sys.__stdout__
         capt = capturedOutput.getvalue()
         for item in self.choices:
-            assert item in capt
+            self.assertIn(item, capt)
 
-    def test_key_metrics_explained(self):
-        pass
+    def key_metrics_explained(self):
+        # Is this base function ever even used?
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        fa_controller.key_metrics_explained([""])
+        sys.stdout = sys.__stdout__
+        capt = capturedOutput.getvalue()
+        self.assertIn("RETURN ON EQUITY", capt)
 
     def test_menu(self):
-        # fa_controller.menu("GME", self.start, "1440min")
-        pass
+        with replace_stdin(io.StringIO("q")):
+            capturedOutput = io.StringIO()
+            sys.stdout = capturedOutput
+            fa_controller.menu("GME", self.start, "1440min")
+            sys.stdout = sys.__stdout__
+            capt = capturedOutput.getvalue()
+            self.assertIn("fraud", capt)
