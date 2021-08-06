e00ab6702d4e99e2cd80c98a1751744b37169e07
Colin Delahunty <colindelahunty@Colins-Air.attlocal.net>
2021-08-05 22:19:34 -0400

    Added main helper tests

--- tests/test_main/test_main_helper.py
+++ tests/test_main/test_main_helper.py
@@ -1,10 +1,33 @@
 from datetime import datetime, timedelta
+from contextlib import contextmanager
 import unittest
 from unittest.mock import patch
+import sys
+import io
 
 import pandas as pd
 
 from gamestonk_terminal import main_helper
+
+
+def return_val(x, shell, check):
+    # pylint: disable=unused-argument
+    class ReturnVal:
+        def __init__(self, code):
+            self.code = code
+
+        def returncode(self):
+            return self.code
+
+    return ReturnVal(2)
+
+
+@contextmanager
+def replace_stdin(target):
+    orig = sys.stdin
+    sys.stdin = target
+    yield
+    sys.stdin = orig
 
 
 class TestMainHelper(unittest.TestCase):
@@ -27,3 +50,65 @@ class TestMainHelper(unittest.TestCase):
     def test_candle(self, mock):
         # pylint: disable=unused-argument
         main_helper.candle("GME", ["GME"])
+
+    def test_quote(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        main_helper.quote(["GME"], "GME")
+        sys.stdout = sys.__stdout__
+        capt = capturedOutput.getvalue()
+        self.assertIn("Price", capt)
+
+    @patch("matplotlib.pyplot.show")
+    def test_view(self, mock):
+        # pylint: disable=unused-argument
+        main_helper.view(["GME"], "GME", "1440min", pd.DataFrame())
+
+    def test_check_api_keys(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        main_helper.check_api_keys()
+        sys.stdout = sys.__stdout__
+        capt = capturedOutput.getvalue()
+        self.assertIn("ALPHA", capt)
+
+    def test_print_goodbye(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        main_helper.print_goodbye()
+        sys.stdout = sys.__stdout__
+        capt = capturedOutput.getvalue()
+        self.assertTrue(len(capt) > 0)
+
+    @patch("subprocess.run", side_effect=return_val)
+    def test_update_terminal(self, mock):
+        # pylint: disable=unused-argument
+        value = main_helper.update_terminal()
+        print(f"Fail value: {value}")
+        self.assertEqual(value, 2)
+
+    def test_about_us(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        main_helper.about_us()
+        sys.stdout = sys.__stdout__
+        capt = capturedOutput.getvalue()
+        self.assertIn("Thanks for using Gamestonk Terminal.", capt)
+
+    def test_bootup(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        main_helper.bootup()
+        sys.stdout = sys.__stdout__
+        capt = capturedOutput.getvalue()
+        self.assertIn("Welcome to Gamestonk Terminal Beta", capt)
+
+    @patch("subprocess.run", side_effect=return_val)
+    def test_reset(self, mock):
+        # pylint: disable=unused-argument
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+        main_helper.reset()
+        sys.stdout = sys.__stdout__
+        capt = capturedOutput.getvalue()
+        self.assertIn("Unfortunately, resetting wasn't", capt)
