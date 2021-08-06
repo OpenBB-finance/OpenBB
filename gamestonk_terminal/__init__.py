9374828c3e85f8fe6456a4e93dce8f5891979f61
didier <dro.lopes@campus.fct.unl.pt>
2021-08-06 02:49:55 +0100

    remove init modules

--- gamestonk_terminal/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-# TODO: Needs to be fixed for papermill
-# from . import discovery
-# from . import due_diligence
-# from . import fundamental_analysis
-# from . import papermill
-# from . import prediction_techniques
-# from . import behavioural_analysis
-# from . import technical_analysis
