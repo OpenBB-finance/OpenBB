f47a6b2c366a953f122af2135c95e77acb85c2e5
Colin Delahunty <colindelahunty@Colins-Air.attlocal.net>
2021-08-14 10:56:43 -0400

    Switched import

--- tests/test_fa/test_fa_excel.py
+++ tests/test_fa/test_fa_excel.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 import vcr
 
 from gamestonk_terminal.stocks.fundamental_analysis.dcf_view import CreateExcelFA
-import gamestonk_terminal.stocks.fundamental_analysis.dcf_model as md
+from gamestonk_terminal.stocks.fundamental_analysis import dcf_model
 
 
 class TestExcelClass(TestCase):
@@ -12,17 +12,17 @@ class TestExcelClass(TestCase):
         "tests/cassettes/test_fa/test_fa_excel/test_covers_all_tickers.yaml"
     )
     def test_covers_all_tickers(self):
-        for ticker in md.tickers:
+        for ticker in dcf_model.tickers:
             excel = CreateExcelFA(ticker, False)
             df_is = excel.get_data("IS", 1, True)
-            items_is = md.non_gaap_is + md.gaap_is
+            items_is = dcf_model.non_gaap_is + dcf_model.gaap_is
             for item in df_is.index:
                 self.assertIn(item, items_is)
             df_bs = excel.get_data("BS", 1, True)
-            items_bs = md.non_gaap_bs + md.gaap_bs
+            items_bs = dcf_model.non_gaap_bs + dcf_model.gaap_bs
             for item in df_bs.index:
                 self.assertIn(item, items_bs)
             df_cf = excel.get_data("CF", 1, True)
-            items_cf = md.non_gaap_cf + md.gaap_cf
+            items_cf = dcf_model.non_gaap_cf + dcf_model.gaap_cf
             for item in df_cf.index:
                 self.assertIn(item, items_cf)
