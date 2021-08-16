a409873a8a435da46924a6716709abc17753552e
Jamie <jmaslek11@gmail.com>
2021-08-16 16:37:26 -0400

    Refactor trend indicators

--- gamestonk_terminal/stocks/technical_analysis/ta_controller.py
+++ gamestonk_terminal/stocks/technical_analysis/ta_controller.py
@@ -14,6 +14,7 @@ from gamestonk_terminal.helper_funcs import (
     get_flair,
     parse_known_args_and_warn,
     check_positive_list,
+    check_positive,
 )
 from gamestonk_terminal.menu import session
 from gamestonk_terminal.stocks.technical_analysis import (
@@ -26,7 +27,7 @@ from gamestonk_terminal.common.technical_analysis import (
     custom_indicators_view,
     momentum_model,
     overlap_view,
-    trend_indicators,
+    trend_indicators_view,
     volatility_model,
     volume_model,
 )
@@ -551,11 +552,142 @@ custom:
 
     def call_adx(self, other_args: List[str]):
         """Process adx command"""
-        trend_indicators.adx(other_args, self.s_stock, self.interval, self.stock)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            prog="adx",
+            description="""
+            The ADX is a Welles Wilder style moving average of the Directional Movement Index (DX).
+            The values range from 0 to 100, but rarely get above 60. To interpret the ADX, consider
+            a high number to be a strong trend, and a low number, a weak trend.
+        """,
+        )
+        parser.add_argument(
+            "-l",
+            "--length",
+            action="store",
+            dest="n_length",
+            type=check_positive,
+            default=14,
+            help="length",
+        )
+        parser.add_argument(
+            "-s",
+            "--scalar",
+            action="store",
+            dest="n_scalar",
+            type=check_positive,
+            default=100,
+            help="scalar",
+        )
+        parser.add_argument(
+            "-d",
+            "--drift",
+            action="store",
+            dest="n_drift",
+            type=check_positive,
+            default=1,
+            help="drift",
+        )
+        parser.add_argument(
+            "--export",
+            choices=["csv", "json", "xlsx"],
+            default="",
+            type=str,
+            dest="export",
+            help="Export dataframe data to csv,json,xlsx file",
+        )
+
+        try:
+            ns_parser = parse_known_args_and_warn(parser, other_args)
+            if not ns_parser:
+                return
+
+            trend_indicators_view.plot_adx(
+                s_ticker=self.s_stock,
+                s_interval=self.interval,
+                df_stock=self.stock,
+                length=ns_parser.n_length,
+                scalar=ns_parser.n_scalar,
+                drift=ns_parser.n_drift,
+                export=ns_parser.export,
+            )
+
+        except Exception as e:
+            print(e, "\n")
 
     def call_aroon(self, other_args: List[str]):
         """Process aroon command"""
-        trend_indicators.aroon(other_args, self.s_stock, self.interval, self.stock)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            prog="aroon",
+            description="""
+                The word aroon is Sanskrit for "dawn's early light." The Aroon
+                indicator attempts to show when a new trend is dawning. The indicator consists
+                of two lines (Up and Down) that measure how long it has been since the highest
+                high/lowest low has occurred within an n period range. \n \n When the Aroon Up is
+                staying between 70 and 100 then it indicates an upward trend. When the Aroon Down
+                is staying between 70 and 100 then it indicates an downward trend. A strong upward
+                trend is indicated when the Aroon Up is above 70 while the Aroon Down is below 30.
+                Likewise, a strong downward trend is indicated when the Aroon Down is above 70 while
+                the Aroon Up is below 30. Also look for crossovers. When the Aroon Down crosses above
+                the Aroon Up, it indicates a weakening of the upward trend (and vice versa).
+            """,
+        )
+
+        parser.add_argument(
+            "-l",
+            "--length",
+            action="store",
+            dest="n_length",
+            type=check_positive,
+            default=25,
+            help="length",
+        )
+        parser.add_argument(
+            "-s",
+            "--scalar",
+            action="store",
+            dest="n_scalar",
+            type=check_positive,
+            default=100,
+            help="scalar",
+        )
+        parser.add_argument(
+            "-o",
+            "--offset",
+            action="store",
+            dest="n_offset",
+            type=check_positive,
+            default=0,
+            help="offset",
+        )
+        parser.add_argument(
+            "--export",
+            choices=["csv", "json", "xlsx"],
+            default="",
+            type=str,
+            dest="export",
+            help="Export dataframe data to csv,json,xlsx file",
+        )
+
+        try:
+            ns_parser = parse_known_args_and_warn(parser, other_args)
+            if not ns_parser:
+                return
+
+            trend_indicators_view.plot_aroon(
+                s_ticker=self.s_stock,
+                s_interval=self.interval,
+                df_stock=self.stock,
+                length=ns_parser.n_length,
+                scalar=ns_parser.n_scalar,
+                export=ns_parser.export,
+            )
+
+        except Exception as e:
+            print(e, "\n")
 
     def call_bbands(self, other_args: List[str]):
         """Process bbands command"""
