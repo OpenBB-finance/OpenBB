b2f24deb3aa0b54bcc38e3e7008a3491c22dae3f
didier <dro.lopes@campus.fct.unl.pt>
2021-08-19 13:10:37 +0100

    fix test

--- gamestonk_terminal/stocks/due_diligence/fmp_view.py
+++ gamestonk_terminal/stocks/due_diligence/fmp_view.py
@@ -20,19 +20,22 @@ def rating(ticker: str, num: int):
     # TODO: This could be displayed in a nice rating plot over time
     # TODO: Add coloring to table
 
-    l_recoms = [col for col in df_fa.columns if "Recommendation" in col]
-    l_recoms_show = [
-        recom.replace("rating", "").replace("Details", "").replace("Recommendation", "")
-        for recom in l_recoms
-    ]
-    l_recoms_show[0] = "Rating"
-    print(
-        tabulate(
-            df_fa[l_recoms].head(num),
-            headers=l_recoms_show,
-            floatfmt=".2f",
-            showindex=True,
-            tablefmt="fancy_grid",
+    if not df_fa.empty:
+        l_recoms = [col for col in df_fa.columns if "Recommendation" in col]
+        l_recoms_show = [
+            recom.replace("rating", "")
+            .replace("Details", "")
+            .replace("Recommendation", "")
+            for recom in l_recoms
+        ]
+        l_recoms_show[0] = "Rating"
+        print(
+            tabulate(
+                df_fa[l_recoms].head(num),
+                headers=l_recoms_show,
+                floatfmt=".2f",
+                showindex=True,
+                tablefmt="fancy_grid",
+            )
         )
-    )
     print("")
--- tests/test_dd/test_dd_financial_modeling_prep_view.py
+++ tests/test_dd/test_dd_financial_modeling_prep_view.py
@@ -8,10 +8,13 @@ from tests.helpers import check_print
 
 
 class TestDdFinancialModelingPrepView(unittest.TestCase):
-    @check_print()
+    @check_print(assert_in="Invalid API KEY")
     @vcr.use_cassette(
         "tests/cassettes/test_dd/test_financial_model/test_rating.yaml",
         record_mode="new_episodes",
     )
     def test_rating(self):
-        rating(ticker="PLTR", num=10)
+        try:
+            rating(ticker="PLTR", num=10)
+        except ValueError as e:
+            print(e)
