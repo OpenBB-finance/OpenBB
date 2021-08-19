46f9e73ac365de3a150e676ce92dd694405a6cc2
didier <dro.lopes@campus.fct.unl.pt>
2021-08-19 19:07:27 +0100

    fix rating yaml

--- tests/test_dd/test_dd_financial_modeling_prep_view.py
+++ tests/test_dd/test_dd_financial_modeling_prep_view.py
@@ -8,13 +8,10 @@ from tests.helpers import check_print
 
 
 class TestDdFinancialModelingPrepView(unittest.TestCase):
-    @check_print(assert_in="Invalid API KEY")
+    @check_print(assert_in="Rating")
     @vcr.use_cassette(
         "tests/cassettes/test_dd/test_financial_model/test_rating.yaml",
         record_mode="new_episodes",
     )
     def test_rating(self):
-        try:
-            rating(ticker="PLTR", num=10)
-        except ValueError as e:
-            print(e)
+        rating(ticker="PLTR", num=10)
