e845828c5d75d934bec63815de56e3c97096ab07
didier <dro.lopes@campus.fct.unl.pt>
2021-08-20 16:03:35 +0100

    Refactor lins command in insider

--- gamestonk_terminal/stocks/insider/finviz_view.py
+++ gamestonk_terminal/stocks/insider/finviz_view.py
@@ -1,70 +1,55 @@
-""" FinViz View """
+""" Finviz View """
 __docformat__ = "numpy"
 
-import argparse
-from typing import List
-import finviz
+import os
 import pandas as pd
-from gamestonk_terminal.helper_funcs import (
-    check_positive,
-    parse_known_args_and_warn,
-)
+from tabulate import tabulate
+from gamestonk_terminal.stocks.insider import finviz_model
+from gamestonk_terminal.helper_funcs import export_data
 
 
-def last_insider_activity(other_args: List[str], ticker: str):
-    """Display insider activity for a given stock ticker
+def last_insider_activity(ticker: str, num: int, export: str):
+    """Display insider activity for a given stock ticker. [Source: Finviz]
 
     Parameters
     ----------
-    other_args : List[str]
-        argparse other args - ["-n", "10"]
     ticker : str
         Stock ticker
+    num : int
+        Number of latest insider activity to display
+    export : str
+        Export dataframe data to csv,json,xlsx file
     """
-    parser = argparse.ArgumentParser(
-        add_help=False,
-        prog="insider",
-        description="""
-            Prints information about inside traders. The following fields are expected: Date, Relationship,
-            Transaction, #Shares, Cost, Value ($), #Shares Total, Insider Trading, SEC Form 4. [Source: Finviz]
-        """,
-    )
-
-    parser.add_argument(
-        "-n",
-        "--num",
-        action="store",
-        dest="n_num",
-        type=check_positive,
-        default=10,
-        help="number of latest inside traders.",
-    )
-
-    try:
-        ns_parser = parse_known_args_and_warn(parser, other_args)
-        if not ns_parser:
-            return
-
-        d_finviz_insider = finviz.get_insider(ticker)
-        df_fa = pd.DataFrame.from_dict(d_finviz_insider)
-        df_fa.set_index("Date", inplace=True)
-        df_fa = df_fa[
-            [
-                "Relationship",
-                "Transaction",
-                "#Shares",
-                "Cost",
-                "Value ($)",
-                "#Shares Total",
-                "Insider Trading",
-                "SEC Form 4",
-            ]
+    d_finviz_insider = finviz_model.get_last_insider_activity(ticker)
+    df = pd.DataFrame.from_dict(d_finviz_insider)
+    df.set_index("Date", inplace=True)
+    df = df[
+        [
+            "Relationship",
+            "Transaction",
+            "#Shares",
+            "Cost",
+            "Value ($)",
+            "#Shares Total",
+            "Insider Trading",
+            "SEC Form 4",
         ]
-        print(df_fa.head(n=ns_parser.n_num))
+    ]
 
-        print("")
+    print(
+        tabulate(
+            df.head(num),
+            tablefmt="fancy_grid",
+            floatfmt=".2f",
+            headers=list(df.columns),
+            showindex=True,
+        )
+    )
+    print("")
 
-    except Exception as e:
-        print(e)
-        print("")
-        return
+    export_data(
+        export,
+        os.path.dirname(os.path.abspath(__file__)),
+        "lins",
+        df,
+    )
