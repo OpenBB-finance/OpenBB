0eaf1e3982888b070358d74334253db93a2702c5
Tep <devisioncapital@gmail.com>
2021-08-21 16:51:44 +1000

    add chaikin osc to ta menu

--- gamestonk_terminal/common/technical_analysis/volume_view.py
+++ gamestonk_terminal/common/technical_analysis/volume_view.py
@@ -105,6 +105,97 @@ def plot_ad(
     )
 
 
+def plot_adosc(
+    s_ticker: str,
+    s_interval: str,
+    df_stock: pd.DataFrame,
+    fast: int,
+    slow: int,
+    use_open: bool,
+    export: str,
+) -> pd.DataFrame:
+    """Display AD Osc Indicator
+
+    Parameters
+    ----------
+    s_ticker : str
+        Stock ticker
+    s_interval : str
+        Interval of stock data
+    df_stock : pd.DataFrame
+        Dataframe of prices
+    use_open : bool
+        Whether to use open prices in calculation
+    fast: int
+         Length of fast window
+    slow : int
+        Length of slow window
+    export : str
+        Format to export data
+    """
+    bar_colors = ["r" if x[1].Open < x[1].Close else "g" for x in df_stock.iterrows()]
+
+    if s_interval == "1440min":
+        bar_width = timedelta(days=1)
+    else:
+        bar_width = timedelta(minutes=int(s_interval.split("m")[0]))
+
+    df_ta = volume_model.adosc(df_stock, use_open, fast, slow)
+
+    fig, axes = plt.subplots(3, 1, figsize=plot_autoscale(), dpi=PLOT_DPI)
+    ax = axes[0]
+    ax.set_title(f"{s_ticker} AD Oscillator")
+    if s_interval == "1440min":
+        ax.plot(df_stock.index, df_stock["Adj Close"].values, "fuchsia", lw=1)
+    else:
+        ax.plot(df_stock.index, df_stock["Close"].values, "fuchsia", lw=1)
+    ax.set_xlim(df_stock.index[0], df_stock.index[-1])
+    ax.set_ylabel("Price ($)")
+    ax.grid(b=True, which="major", color="#666666", linestyle="-")
+
+    ax1 = axes[1]
+    ax1.set_ylabel("Volume")
+    if s_interval == "1440min":
+        ax1.bar(
+            df_stock.index,
+            df_stock["Volume"].values,
+            color=bar_colors,
+            alpha=0.8,
+            width=0.3,
+        )
+    else:
+        ax1.bar(
+            df_stock.index,
+            df_stock["Volume"].values,
+            color=bar_colors,
+            alpha=0.8,
+            width=bar_width,
+        )
+    ax1.set_xlim(df_stock.index[0], df_stock.index[-1])
+
+    ax2 = axes[2]
+    ax2.set_ylabel("AD osc")
+    ax2.plot(df_ta.index, df_ta.values, "b", lw=2, label="AD Osc")
+    ax2.set_xlim(df_stock.index[0], df_stock.index[-1])
+    ax2.grid(b=True, which="major", color="#666666", linestyle="-")
+
+    if gtff.USE_ION:
+        plt.ion()
+
+    plt.gcf().autofmt_xdate()
+    fig.tight_layout(pad=1)
+    plt.legend()
+    plt.show()
+    print("")
+
+    export_data(
+        export,
+        os.path.dirname(os.path.abspath(__file__)).replace("common", "stocks"),
+        "adosc",
+        df_ta,
+    )
+
+
 def plot_obv(s_ticker: str, s_interval: str, df_stock: pd.DataFrame, export: str):
     """Plot OBV tecnhical inticator
 
