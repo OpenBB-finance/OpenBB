fefdba4d16a721419e6287008b5f82fa6e71af8e
Chavithra PARANA <chavithra@gmail.com>
2021-08-26 19:13:43 +0200

    Add script load env from settings

---
+++ jupyterlab/scripts/jupyter_export_env.py
@@ -0,0 +1,17 @@
+import json
+import sys
+import os
+import subprocess
+
+settings = json.load(sys.stdin)
+
+print('settings', settings)
+
+for key, value in settings.items():
+    if not isinstance(value, dict) \
+        and not isinstance(value, list):
+        os.environ[key] = value
+
+from terminal import terminal
+
+terminal()
