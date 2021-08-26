5a20061b81bc6a8b90ec30d1c4b37820074625ae
Theodore Aptekarev <aptekarev@gmail.com>
2021-08-26 21:42:52 +0300

    Remove unused python API

--- jupyterlab/gst-settings/gamestonk_terminal_settings/__init__.py
+++ jupyterlab/gst-settings/gamestonk_terminal_settings/__init__.py
@@ -1,4 +1,3 @@
-from .handlers import TerminalSettingsHandler
 import json
 import os.path as osp
 
@@ -9,12 +8,6 @@ HERE = osp.abspath(osp.dirname(__file__))
 with open(osp.join(HERE, "labextension", "package.json")) as fid:
     data = json.load(fid)
 
-def _jupyter_labextension_paths():
-    return [{
-        'src': 'labextension',
-        'dest': data['name']
-    }]
 
-def load_jupyter_server_extension(server_app):
-    handlers = [("/gamestonk/settings", TerminalSettingsHandler)]
-    server_app.web_app.add_handlers(".*$", handlers)
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": data["name"]}]
