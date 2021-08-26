a280f421667e844bd8e068ee010351f2988f75e0
Theodore Aptekarev <aptekarev@gmail.com>
2021-08-26 21:42:33 +0300

    Lint python code black

--- jupyterlab/gst-settings/gamestonk_terminal_settings/__init__.py
+++ jupyterlab/gst-settings/gamestonk_terminal_settings/__init__.py
@@ -6,7 +6,7 @@ from ._version import __version__
 
 HERE = osp.abspath(osp.dirname(__file__))
 
-with open(osp.join(HERE, 'labextension', 'package.json')) as fid:
+with open(osp.join(HERE, "labextension", "package.json")) as fid:
     data = json.load(fid)
 
 def _jupyter_labextension_paths():
