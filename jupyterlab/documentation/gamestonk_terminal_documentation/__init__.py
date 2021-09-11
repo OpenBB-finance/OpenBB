af005e36b30292c957255b992fd8f6811d04d138
didier <dro.lopes@campus.fct.unl.pt>
2021-09-12 00:56:01 +0100

    Add documentation extension to Jupyter Lab

---
+++ jupyterlab/documentation/gamestonk_terminal_documentation/__init__.py
@@ -0,0 +1,13 @@
+import json
+import os.path as osp
+
+from ._version import __version__
+
+HERE = osp.abspath(osp.dirname(__file__))
+
+with open(osp.join(HERE, "labextension", "package.json")) as fid:
+    data = json.load(fid)
+
+
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": data["name"]}]
