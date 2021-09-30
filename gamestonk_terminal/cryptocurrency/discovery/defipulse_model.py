4dca72aafe63bd7809939fa713d30ca5130ee520
jakubpluta <plutakuba@gmail.com>
2021-09-30 18:46:19 +0200

    Add defipulse view

--- gamestonk_terminal/cryptocurrency/discovery/defipulse_model.py
+++ gamestonk_terminal/cryptocurrency/discovery/defipulse_model.py
@@ -7,14 +7,15 @@ import pandas as pd
 
 
 def get_defi_pulse_index() -> pd.DataFrame:
-    """Scrapes data from DeFi Pulse with all DeFi Pulse crypto protocols. [Source: https://defipulse.com/]
+    """Scrapes data from DeFi Pulse with all DeFi Pulse crypto protocols.
+    [Source: https://defipulse.com/]
 
     Returns
     -------
     pd.DataFrame
         List of DeFi Pulse protocols.
-
     """
+
     req = requests.get("https://defipulse.com/")
     result = req.content.decode("utf8")
     soup = BeautifulSoup(result, features="lxml")
@@ -32,14 +33,18 @@ def get_defi_pulse_index() -> pd.DataFrame:
         list_of_records,
         columns=[
             "x",
-            "Defi Pulse Rank",
+            "Rank",
             "Name",
             "Chain",
             "Category",
-            "Locked (USD)",
-            "1 Day % Change",
+            "TVL",
+            "Change_1D",
         ],
     )
+    try:
+        df["Rank"] = df["Rank"].apply(lambda x: int(x.replace(".", "")))
+    except Exception as e:
+        print(e, "\n")
+
     df.drop("x", axis=1, inplace=True)
-    print(df)
     return df
