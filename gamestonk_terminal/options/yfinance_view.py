6b09893bba8aedebbdc329b007d144f5bd5f6151
Colin Delahunty <72827203+colin99d@users.noreply.github.com>
2021-10-11 20:47:29 -0400

    Option Payoff Diagrams (#800)
    * Began creating payoff matrix
    
    * Updated parser abilities
    
    * Finalized ability to add, remove, and show options
    
    * Added graph generator
    
    * Cleaned code
    
    * Attempting to fix black
    
    * Attempting to fix black
    
    * Fixed typings
    
    * Added James' changes
    
    * Made changes, added documentation
    
    * Added changes
    
    * Fixed my silly mistake
    
    * Didier requests added
    
    Co-authored-by: Colin Delahunty <colindelahunty@Colins-MacBook-Air.local>
    Co-authored-by: Colin Delahunty <colindelahunty@Colins-Air.indiana.edu>

--- gamestonk_terminal/options/yfinance_view.py
+++ gamestonk_terminal/options/yfinance_view.py
@@ -3,6 +3,7 @@ __docformat__ = "numpy"
 
 import os
 from bisect import bisect_left
+from typing import List, Dict, Any
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,6 +15,7 @@ import gamestonk_terminal.config_plot as cfp
 import gamestonk_terminal.feature_flags as gtff
 from gamestonk_terminal.helper_funcs import export_data, plot_autoscale
 from gamestonk_terminal.options import op_helpers, yfinance_model
+from gamestonk_terminal.options.yfinance_model import generate_data
 
 
 def plot_oi(
@@ -421,3 +423,28 @@ def plot_smile(ticker: str, expiration: str, put: bool) -> None:
     ax.yaxis.set_major_formatter("{x:.2f}%")
     plt.legend()
     plt.show()
+
+
+def plot_payoff(
+    current_price: float,
+    options: List[Dict[Any, Any]],
+    underlying: int,
+    ticker: str,
+    expiration: str,
+) -> None:
+    """Generate a graph showing the option payoff diagram"""
+    x, yb, ya = generate_data(current_price, options, underlying)
+    _, ax = plt.subplots()
+    if ya:
+        ax.plot(x, yb, label="Payoff Before Premium")
+        ax.plot(x, ya, label="Payoff After Premium")
+    else:
+        ax.plot(x, yb, label="Payoff")
+    ax.set_title(f"Option Payoff Diagram for {ticker} on {expiration}")
+    ax.set_ylabel("Profit")
+    ax.set_xlabel("Underlying Asset Price at Expiration")
+    ax.xaxis.set_major_formatter("${x:.2f}")
+    ax.yaxis.set_major_formatter("${x:.2f}")
+    plt.legend()
+    plt.show()
+    print("")
