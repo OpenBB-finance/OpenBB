8ba938580580f5f5f92113858e0525c338da4514
Jamie <jmaslek11@gmail.com>
2021-10-13 11:26:19 -0400

    ARIMA models and poetry/req  updates

--- gamestonk_terminal/stocks/prediction_techniques/pred_controller.py
+++ gamestonk_terminal/stocks/prediction_techniques/pred_controller.py
@@ -427,7 +427,113 @@ Models:
 
     def call_arima(self, other_args: List[str]):
         """Process arima command"""
-        arima_view.arima(other_args, self.ticker, self.stock)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            prog="arima",
+            description="""
+                In statistics and econometrics, and in particular in time series analysis, an
+                autoregressive integrated moving average (ARIMA) model is a generalization of an
+                autoregressive moving average (ARMA) model. Both of these models are fitted to time
+                series data either to better understand the data or to predict future points in the
+                series (forecasting). ARIMA(p,d,q) where parameters p, d, and q are non-negative
+                integers, p is the order (number of time lags) of the autoregressive model, d is the
+                degree of differencing (the number of times the data have had past values subtracted),
+                and q is the order of the moving-average model.
+            """,
+        )
+        parser.add_argument(
+            "-d",
+            "--days",
+            action="store",
+            dest="n_days",
+            type=check_positive,
+            default=5,
+            help="prediction days.",
+        )
+        parser.add_argument(
+            "-i",
+            "--ic",
+            action="store",
+            dest="s_ic",
+            type=str,
+            default="aic",
+            choices=["aic", "aicc", "bic", "hqic", "oob"],
+            help="information criteria.",
+        )
+        parser.add_argument(
+            "-s",
+            "--seasonal",
+            action="store_true",
+            default=False,
+            dest="b_seasonal",
+            help="Use weekly seasonal data.",
+        )
+        parser.add_argument(
+            "-o",
+            "--order",
+            action="store",
+            dest="s_order",
+            default="",
+            type=str,
+            help="arima model order (p,d,q) in format: p,d,q.",
+        )
+        parser.add_argument(
+            "-r",
+            "--results",
+            action="store_true",
+            dest="b_results",
+            default=False,
+            help="results about ARIMA summary flag.",
+        )
+        parser.add_argument(
+            "-e",
+            "--end",
+            action="store",
+            type=valid_date,
+            dest="s_end_date",
+            default=None,
+            help="The end date (format YYYY-MM-DD) to select - Backtesting",
+        )
+
+        try:
+            ns_parser = parse_known_args_and_warn(
+                parser, other_args, export_allowed=EXPORT_ONLY_FIGURES_ALLOWED
+            )
+            if not ns_parser:
+                return
+
+            # BACKTESTING CHECK
+            if ns_parser.s_end_date:
+
+                if ns_parser.s_end_date < self.stock.index[0]:
+                    print(
+                        "Backtesting not allowed, since End Date is older than Start Date of historical data\n"
+                    )
+                    return
+
+                if ns_parser.s_end_date < get_next_stock_market_days(
+                    last_stock_day=self.stock.index[0],
+                    n_next_days=5 + ns_parser.n_days,
+                )[-1]:
+                    print(
+                        "Backtesting not allowed, since End Date is too close to Start Date to train model\n"
+                    )
+                    return
+
+            arima_view.display_arima(
+                dataset=self.ticker,
+                values=self.stock[self.target],
+                arima_order=ns_parser.s_order,
+                n_predict=ns_parser.n_days,
+                seasonal=ns_parser.b_seasonal,
+                ic=ns_parser.s_ic,
+                results=ns_parser.b_results,
+                s_end_date=ns_parser.s_end_date,
+                export=ns_parser.export,
+            )
+        except Exception as e:
+            print(e, "\n")
 
     def call_mlp(self, other_args: List[str]):
         """Process mlp command"""
