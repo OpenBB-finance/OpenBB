fb128ee2a46bb40b9251728a9b290c108a885583
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2021-10-20 12:51:34 +0100

    Improve discord docs (#836)
    * add relative path to the terminal
    
    * add troubleshoot message to README
    
    * minor fix
    
    * Update labels.yml

--- discordbot/config_discordbot.py
+++ discordbot/config_discordbot.py
@@ -8,9 +8,8 @@ env_files = [f for f in os.listdir() if f.endswith(".env")]
 if env_files:
     load_dotenv(env_files[0])
 
-# Path to the terminal
-GST_PATH = os.path.join("~", "Documents", "GamestonkTerminal")
-sys.path.append(GST_PATH)
+# Relative path to the terminal
+sys.path.append("..")
 
 # https://discord.com/developers/applications/
 DISCORD_BOT_TOKEN = os.getenv("GT_DISCORD_BOT_TOKEN") or "REPLACE_ME"
