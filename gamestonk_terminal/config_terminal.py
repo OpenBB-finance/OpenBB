8fae911c4ec1d05dd3e3e602444f8b6b53def0c6
Colin Delahunty <colindelahunty@Colins-Air.indiana.edu>
2021-10-25 21:53:12 -0400

    Updated port

--- gamestonk_terminal/config_terminal.py
+++ gamestonk_terminal/config_terminal.py
@@ -13,7 +13,7 @@ DEFAULT_CONTEXT = ""
 DEBUG_MODE = False
 
 # By default the jupyter notebook will be run on port 8888
-PAPERMILL_NOTEBOOK_REPORT_PORT = "8892"
+PAPERMILL_NOTEBOOK_REPORT_PORT = "8888"
 
 # https://www.alphavantage.co
 API_KEY_ALPHAVANTAGE = os.getenv("GT_API_KEY_ALPHAVANTAGE") or "REPLACE_ME"
