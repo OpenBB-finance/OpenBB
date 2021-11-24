e2b8d7d805f1e8907999b89ea7b79e7ce80f0026
James Maslek <james@macbook-pro.myfiosgateway.com>
2021-11-24 16:45:27 -0500

    First few

--- gamestonk_terminal/stocks/backtesting/bt_controller.py
+++ gamestonk_terminal/stocks/backtesting/bt_controller.py
@@ -2,6 +2,7 @@
 __docformat__ = "numpy"
 
 import argparse
+import difflib
 from typing import List
 
 import matplotlib as mpl
@@ -356,4 +357,11 @@ def menu(ticker: str, stock: pd.DataFrame):
 
         except SystemExit:
             print("The command selected doesn't exist\n")
+            similar_cmd = difflib.get_close_matches(
+                an_input, bt_controller.CHOICES, n=1, cutoff=0.7
+            )
+
+            if similar_cmd:
+                print(f"Did you mean '{similar_cmd[0]}'?\n")
+
             continue
