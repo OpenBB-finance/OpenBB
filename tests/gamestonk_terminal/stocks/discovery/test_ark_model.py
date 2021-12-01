28373e981108da21fc9a5e3210e3668b0da15ad4
Chavithra PARANA <chavithra@gmail.com>
2021-12-01 12:46:35 +0100

    Updating tests : test_discovery_ark_model

--- tests/gamestonk_terminal/stocks/discovery/test_ark_model.py
+++ tests/gamestonk_terminal/stocks/discovery/test_ark_model.py
@@ -10,7 +10,7 @@ from tests.helpers import check_print
 class TestDiscoveryArkModel(unittest.TestCase):
     @check_print(assert_in="direction")
     @vcr.use_cassette(
-        "tests/cassettes/test_discovery/test_discovery_ark/test_get_ark_order.yaml",
+        "tests/gamestonk_terminal/stocks/discovery/cassettes/test_ark_model/test_get_ark_order.yaml",
         record_mode="none",
     )
     def test_get_ark_orders(self):
@@ -19,7 +19,7 @@ class TestDiscoveryArkModel(unittest.TestCase):
 
     @check_print(assert_in="weight")
     @vcr.use_cassette(
-        "tests/cassettes/test_discovery/test_discovery_ark/test_get_add_order.yaml",
+        "tests/gamestonk_terminal/stocks/discovery/cassettes/test_ark_model/test_get_add_order.yaml",
         record_mode="none",
     )
     def test_add_order_total(self):
