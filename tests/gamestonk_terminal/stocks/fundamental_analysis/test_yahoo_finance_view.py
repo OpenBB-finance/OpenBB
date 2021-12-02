9850e872e491a3f1bb2d9c56007d45763570a95e
Chavithra PARANA <chavithra@gmail.com>
2021-12-02 02:56:36 +0100

    Updating tests : test_fa_yahoo_finance_view

--- tests/gamestonk_terminal/stocks/fundamental_analysis/test_yahoo_finance_view.py
+++ tests/gamestonk_terminal/stocks/fundamental_analysis/test_yahoo_finance_view.py
@@ -15,7 +15,7 @@ from tests.helpers import check_print
 class TestFaYahooFinanceApi(unittest.TestCase):
     @check_print(assert_in="Zip")
     @vcr.use_cassette(
-        "tests/cassettes/test_fa/test_fa_yahoo/test_info.yaml",
+        "tests/gamestonk_terminal/stocks/fundamental_analysis/cassettes/test_yahoo_finance_view/test_info.yaml",
         record_mode="new_episodes",
     )
     def test_info(self):
@@ -23,7 +23,7 @@ class TestFaYahooFinanceApi(unittest.TestCase):
 
     @check_print(assert_in="Military contract")
     @vcr.use_cassette(
-        "tests/cassettes/test_fa/test_fa_yahoo/test_sustainability.yaml",
+        "tests/gamestonk_terminal/stocks/fundamental_analysis/cassettes/test_yahoo_finance_view/test_sustainability.yaml",
         record_mode="new_episodes",
     )
     def test_sustainability(self):
@@ -31,7 +31,7 @@ class TestFaYahooFinanceApi(unittest.TestCase):
 
     @check_print(assert_in="Earnings Date")
     @vcr.use_cassette(
-        "tests/cassettes/test_fa/test_fa_yahoo/test_calendar_earnings.yaml",
+        "tests/gamestonk_terminal/stocks/fundamental_analysis/cassettes/test_yahoo_finance_view/test_calendar_earnings.yaml",
         record_mode="new_episodes",
     )
     def test_calendar_earnings(self):
