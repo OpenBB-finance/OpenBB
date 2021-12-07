2afb1bc6d35eb0a80f4413f8f977d1ba7cec4114
Chavithra <chavithra@gmail.com>
2021-12-07 13:34:45 +0100

    Fix stocks (#1035)
    * Fix Stocks > FA > Eclect US
    
    * Fix Stocks > FA > Market Watch
    
    * Fix Stocks > FA > Eclect US : black
    
    * Fixing linting

--- gamestonk_terminal/stocks/fundamental_analysis/eclect_us_model.py
+++ gamestonk_terminal/stocks/fundamental_analysis/eclect_us_model.py
@@ -1,6 +1,7 @@
 """Eclect.us model"""
 __docformat__ = "numpy"
 
+from collections import OrderedDict
 import requests
 from colorama import Style
 
@@ -20,29 +21,33 @@ def get_filings_analysis(ticker: str) -> str:
     str
         Analysis of filings text
     """
-    result = requests.get(f"https://api.eclect.us/symbol/{ticker.lower()}?page=1")
 
-    if result.status_code == 200:
-        if result.json():
-            rf_highlights = f"{Style.BRIGHT}\n\tRISK FACTORS:{Style.RESET_ALL}\n"
-            rf_highlights_txt = "\n\n".join(
-                {sentence["sentence"] for sentence in result.json()[0]["rf_highlights"]}
-            )
+    response = requests.get(f"https://api.eclect.us/symbol/{ticker.lower()}?page=1")
 
-            daa_highlights = (
-                f"{Style.BRIGHT}\n\tDISCUSSION AND ANALYSIS:{Style.RESET_ALL}\n"
-            )
-            daa_highlights += "\n\n".join(
-                {
-                    sentence["sentence"]
-                    for sentence in result.json()[0]["daa_highlights"]
-                }
-            )
+    if response.status_code != 200:
+        filings_analysis = ""
+    else:
+        response_dict = response.json()
 
-            return (
-                rf_highlights + rf_highlights_txt + "\n" + daa_highlights
-                if rf_highlights_txt
-                else daa_highlights
-            )
+        rf_highlights = f"{Style.BRIGHT}\n\tRISK FACTORS:{Style.RESET_ALL}\n"
+        rf_highlights_list = [
+            sentence["sentence"] for sentence in response_dict[0]["rf_highlights"]
+        ]
+        rf_highlights_list = list(OrderedDict.fromkeys(rf_highlights_list))
+        rf_highlights_txt = "\n\n".join(rf_highlights_list)
 
-    return ""
+        daa_highlights = (
+            f"{Style.BRIGHT}\n\tDISCUSSION AND ANALYSIS:{Style.RESET_ALL}\n"
+        )
+        daa_highlights_list = [
+            sentence["sentence"] for sentence in response_dict[0]["daa_highlights"]
+        ]
+        daa_highlights_list = list(OrderedDict.fromkeys(daa_highlights_list))
+        daa_highlights += "\n\n".join(daa_highlights_list)
+
+        if rf_highlights_txt:
+            filings_analysis = rf_highlights + rf_highlights_txt + "\n" + daa_highlights
+        else:
+            filings_analysis = daa_highlights
+
+    return filings_analysis
