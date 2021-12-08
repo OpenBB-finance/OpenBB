0b480eb4e09d9f11a5ab85864ed5eca7101c8527
Chavithra PARANA <chavithra@gmail.com>
2021-12-08 17:34:13 +0100

    Updating tests : stocks > due_diligence

--- tests/gamestonk_terminal/stocks/due_diligence/test_finviz_model.py
+++ tests/gamestonk_terminal/stocks/due_diligence/test_finviz_model.py
@@ -1,8 +1,6 @@
 # IMPORTATION STANDARD
 
 # IMPORTATION THIRDPARTY
-import numpy as np
-import pandas as pd
 import pytest
 
 # IMPORTATION INTERNAL
@@ -10,21 +8,14 @@ from gamestonk_terminal.stocks.due_diligence import finviz_model
 
 
 @pytest.mark.vcr
-def test_get_news(default_csv_path):
+def test_get_news(recorder):
     result_dict = finviz_model.get_news(ticker="TSLA")
-    result_df = pd.DataFrame(result_dict)
 
-    # result_df.to_csv(default_csv_path, index=True)
-    expected_df = pd.read_csv(default_csv_path)
-
-    np.array_equal(result_df.values, expected_df.values)
+    recorder.capture(result_dict)
 
 
 @pytest.mark.vcr
-def test_get_analyst_data(default_csv_path):
+def test_get_analyst_data(recorder):
     result_df = finviz_model.get_analyst_data(ticker="TSLA")
 
-    # result_df.to_csv(default_csv_path, index=True)
-    expected_df = pd.read_csv(default_csv_path, index_col="date")
-
-    pd.testing.assert_frame_equal(result_df, expected_df)
+    recorder.capture(result_df)
