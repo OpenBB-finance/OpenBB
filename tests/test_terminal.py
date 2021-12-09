a666747532e774c3bfcf21037fa82cf1ef2158b4
Chavithra PARANA <chavithra@gmail.com>
2021-12-09 10:01:44 +0100

    Updating tests : terminal

--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -22,9 +22,9 @@ def test_terminal_quit(mocker, monkeypatch):
     monkeypatch.setattr(terminal.gtff, "USE_PROMPT_TOOLKIT", True)
 
     mocker.patch("sys.stdin")
-    mocker.patch("builtins.input", return_value="quit")
+    mocker.patch("builtins.input", return_value="e")
     mocker.patch("terminal.session")
-    mocker.patch("terminal.session.prompt", return_value="quit")
+    mocker.patch("terminal.session.prompt", return_value="e")
     mocker.patch("terminal.print_goodbye")
     spy_print_goodbye = mocker.spy(terminal, "print_goodbye")
 
