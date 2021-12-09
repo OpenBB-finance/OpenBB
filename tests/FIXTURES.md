6c51a584e09ca2ac7c9755bfb029b7a675f42ce1
Chavithra <chavithra@gmail.com>
2021-12-09 06:48:13 +0100

    Chavithra/tests draft helpers (#1039)
    * Updating tests : fixtures
    
    * Updating tests : fixing pylint
    
    * Updating tests : fixtures
    
    * Updating tests : black
    
    * Updating tests : fixtures
    
    * Updating tests : doc
    
    * Updating tests : fixing mypy

---
+++ tests/FIXTURES.md
@@ -0,0 +1,296 @@
+# 1. Gamestonk Terminal : `Unit Testing` - `Fixtures`
+
+In this document we will list custom `pytest fixtures` built in GamestonkTerminal and explain how they works.
+
+
+## 1.1. What is a `fixture` ?
+This part is fully documented so you can take a look directly on `pytest` documentation right here :
+
+https://docs.pytest.org/en/6.2.x/fixture.html
+
+## 1.2. Which `fixtures` are available ?
+
+You can list all the available `pytest fixtures` using the following command :
+
+```
+pytest --fixtures
+```
+
+This will include the :
+- `fixtures` available by default in `pytest`
+- `fixtures` defined by installed `pytest plugins`
+- custom `fixtures` built specialy for this GamestonkTerminal
+
+## 1.3. Which are the custom `fixtures` ?
+
+The fixtures built for GamestonkTerminal are here to : make sure contributors are putting `test data file` in the right location.
+
+Here we call `test data file` the files that you might use to store data.
+
+For instance if you want to test a `function` generating a `Pandas DataFrame`, you might want to :
+- store expected `DataFrame` result inside a `csv`
+- then compare this expected result with the result of that `function`
+
+Here is a table listing the fixtures specifically built for this library :
+
+
+|**Fixture**|**Purpose**|
+|:-|:-|
+|default_csv_path|Builds a default location to store a `csv` file related to a test.|
+|default_txt_path|Builds a default location to store a `txt` file related to a test.|
+|default_json_path|Builds a default location to store a `json` file related to a test.|
+|record_stdout|Capture stdout in a test. And compare it to the last recording.|
+|recorder|Allow capturing one or multiple variable(s) in a test. And compare this captured variable(s) to the last recording|
+
+Note that you can't combine `record_stdout` and `recorder` fixtures in the same test.
+
+# 2. Path building `fixtures`
+## 2.1. What is the purpose of these `fixtures` ?
+These `fixtures` will provide a location to store your `data file`.
+
+The folder to store the `data file` will be generated automatically.
+
+The file will be store :
+- inside the same folder than the `test module`
+- inside a folder with the data type : for instance `txt` if you are storing text data
+- inside a file with the name of the `test function` and with the right extension : `.txt` if its a text file.
+
+Suppose `XXX` is your data type, the generated path will look like this :
+- module_path/`XXX`/test_module_name/test_function_name.`XXX`
+
+This mimic the default location for `vcrpy cassettes`.
+
+More on `vcrpy` here :
+https://vcrpy.readthedocs.io/en/latest/
+
+## 2.2. How to use `default_csv_path` ?
+
+```python
+import pandas as pd
+from tested_module import function_generating_a_dataframe
+
+def test_function(default_csv_path):
+    result_dataframe = function_generating_a_dataframe()
+
+    result_dataframe.to_csv(default_csv_path)
+
+    expected_dataframe = result_dataframe.read_csv(default_csv_path)
+    
+    pd.testing.assert_frame_equal(result_dataframe, expected_dataframe)
+```
+
+You might want to comment the file saving part after the first run.
+
+Or find a way to conditionnaly disable it.
+
+## 2.3. How to use `default_txt_path` ?
+
+```python
+from tested_module import function_generating_a_str
+
+def test_function(default_txt_path):
+    result_txt = function_generating_a_str()
+
+    with open(file=default_txt_path, mode="w", encoding="utf-8") as f:
+        f.write(result_txt)
+
+    with open(file=default_txt_path, encoding="utf-8") as f:
+        expected_txt = f.read()
+    
+    assert result_txt == expected_txt
+```
+
+## 2.4. How to use `default_json_path` ?
+
+```python
+import json
+from tested_module import function_generating_a_json
+
+def test_function(default_json_path):
+    result_json = function_generating_a_json()
+
+    with open(file=default_txt_path, mode="w", encoding="utf-8") as f:
+        json.dump(result_json, f)
+    
+    with open(file=default_json_path, encoding="utf-8") as f:
+        expected_json = json.load(f)
+    
+    assert result_json == expected_json
+```
+
+
+# 3. Recording fixtures
+
+
+## 3.1. How to use the `record_stdout fixture` ?
+
+**USAGE**
+
+Example of usage :
+```python
+@pytest.mark.record_stdout
+def test_function():
+    print("Something")
+```
+
+This will generate a text file to store the `printed` output.
+The content of this file will be compared to the `test_function` output at each execution of the test.
+
+**RECORD ONCE**
+
+You can run this command to generate the text file :
+
+```bash
+pytest --record-mode=once
+```
+
+This works if :
+- the text file was not generated yet
+- you remove the text file
+
+**RECORD REWRITE**
+
+You can run this command to regenerate the text file :
+
+```bash
+pytest --record-mode=rewrite
+```
+
+
+**VCR**
+
+You can combine `record_stdout` and `vcr` fixtures, like this :
+
+```python
+import requests
+
+@pytest.mark.vcr
+@pytest.mark.record_stdout
+def test_function():
+    response = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+    
+    print(response.status_code)
+    print(response.text)
+```
+
+**ASSERT IN LIST**
+
+You are not forced to save a text file, you can use a list of texts instead like this :
+
+```python
+@pytest.mark.record_stdout(
+    assert_in_list=["Some text", "Another text"],
+    save_record=False,
+)
+def test_function():
+    print("""
+        This text contains :
+            - Some text
+            - Another text
+    """)
+```
+
+
+**STRIP**
+
+If your `test_function` output a random number of blank before or after you can `strip` the captured data.
+
+Example :
+```python
+import random
+
+@pytest.mark.record_stdout(strip=True)
+def test_function():
+    some_text = "Some text output"
+    random_int = random.randint(0,10)
+
+    for _ in range(random_int):
+        text += " "
+
+    print(some_text)
+```
+
+## 3.1. How to use the `recorder fixture` ?
+
+**BEWARE**
+
+You can't combine these two fixtures :
+ - record_stdout
+ - recorder
+
+**USAGE**
+
+Example of usage :
+```python
+@pytest.mark.record_stdout
+def test_function(recorder):
+    some_dict = {1, 2, 3}
+    some_list = [7, 8, 9 ]
+    some_tuple = (4, 5, 6)
+    some_string = "Some string"
+
+    recorder.capture(some_dict)
+    recorder.capture(some_list)
+    recorder.capture(some_string)
+    recorder.capture(some_tuple)
+```
+
+
+This will generate one or multiple text file(s) to store the `printed` output.
+The content of the file(s) will be compared to the `test_function` output at each execution of the test.
+
+
+
+**RECORD ONCE**
+
+You can run this command to generate the text file :
+
+```bash
+pytest --record-mode=once
+```
+
+This works if :
+- the text file was not generated yet
+- you remove the text file
+
+**RECORD REWRITE**
+
+You can run this command to regenerate the text file :
+
+```bash
+pytest --record-mode=rewrite
+```
+
+**VCR**
+
+You can combine `recorder` and `vcr` fixtures, like this :
+
+```python
+import requests
+
+@pytest.mark.vcr
+@pytest.mark.record_stdout
+def test_function():
+    response = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+    
+    recorder.capture(response.status_code)
+    recorder.capture(response.text)
+```
+
+**STRIP**
+
+If your `test_function` output a random number of blank before or after you can `strip` the captured data.
+
+Example :
+```python
+import random
+
+def test_function(recorder):
+    some_text = "Some text output"
+    random_int = random.randint(0,10)
+
+    for _ in range(random_int):
+        text += " "
+
+    recorder.capture(some_text, strip=True)
+```
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 # IMPORTATION STANDARD
+import json
 import os
 import pathlib
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 # IMPORTATION THIRDPARTY
+import pandas as pd
 import pytest
 
 # from pytest_recording._vcr import merge_kwargs
@@ -17,65 +19,263 @@ from _pytest.mark.structures import Mark
 # pylint: disable=redefined-outer-name
 
 
-@pytest.fixture
-def default_csv_path(request):
-    module = request.node.fspath
-    path = os.path.join(
-        module.dirname,
-        "csv",
-        module.purebasename,
-        request.node.name,
+class Record:
+    @staticmethod
+    def extract_string(data: Any) -> str:
+        if isinstance(data, str):
+            string_value = data
+        elif isinstance(data, pd.DataFrame):
+            string_value = data.to_csv(encoding="utf-8", line_terminator="\n")
+        elif isinstance(data, (dict, list, tuple)):
+            string_value = json.dumps(data)
+        else:
+            raise AttributeError(f"Unsupported type : {type(data)}")
+
+        return string_value
+
+    @staticmethod
+    def load_string(path: str) -> Optional[str]:
+        if os.path.exists(path):
+            with open(file=path, encoding="utf-8") as f:
+                return f.read()
+        else:
+            return None
+
+    @property
+    def captured(self) -> str:
+        return self.__captured
+
+    @property
+    def strip(self) -> bool:
+        return self.__strip
+
+    @property
+    def record_changed(self) -> bool:
+        if self.__recorded is None:
+            changed = True
+        elif self.__strip and self.__recorded.strip() != self.__captured.strip():
+            changed = True
+        elif not self.__strip and self.__recorded != self.__captured:
+            changed = True
+        else:
+            changed = False
+
+        return changed
+
+    @property
+    def record_exists(self) -> bool:
+        return self.__recorded is not None
+
+    @property
+    def record_path(self) -> str:
+        return self.__record_path
+
+    @property
+    def recorded(self) -> Optional[str]:
+        return self.__recorded
+
+    def recorded_reload(self):
+        record_path = self.__record_path
+        self.__recorded = self.load_string(path=record_path)
+
+    def __init__(
+        self,
+        captured: Any,
+        record_path: str,
+        strip: bool = False,
+    ) -> None:
+        self.__captured = self.extract_string(data=captured)
+        self.__record_path = record_path
+        self.__strip = strip
+
+        self.__recorded = self.load_string(path=record_path)
+
+    def persist(self):
+        record_path = self.__record_path
+        captured = self.__captured
+        record_dir_name = os.path.dirname(record_path)
+
+        # CREATE FOLDER
+        if not os.path.exists(record_dir_name):
+            pathlib.Path(record_dir_name).mkdir(parents=True, exist_ok=True)
+
+        # SAVE FILE
+        with open(file=record_path, mode="w", encoding="utf-8") as f:
+            f.write(captured)
+
+        # RELOAD RECORDED CONTENT
+        self.recorded_reload()
+
+
+class PathTemplate:
+    EXTENSIONS_ALLOWED = ["csv", "json", "txt"]
+    EXTENSIONS_MATCHING = {
+        pd.DataFrame: "csv",
+        str: "txt",
+        tuple: "json",
+        list: "json",
+        dict: "json",
+    }
+
+    @classmethod
+    def find_extension(cls, data: Any):
+        for data_type, extension in cls.EXTENSIONS_MATCHING.items():
+            if isinstance(data, data_type):
+                return extension
+        raise Exception(f"No extension found for this type : {type(data)}")
+
+    def __init__(self, module_dir: str, module_name: str, test_name: str) -> None:
+        self.__module_dir = module_dir
+        self.__module_name = module_name
+        self.__test_name = test_name
+
+    def build_path_by_extension(self, extension: str, index: int = 0):
+        if extension not in self.EXTENSIONS_ALLOWED:
+            raise Exception(f"Unsupported extension : {extension}")
+
+        path = os.path.join(
+            self.__module_dir,
+            extension,
+            self.__module_name,
+            self.__test_name,
+        )
+        if index:
+            path += "_" + str(index)
+        path += "."
+        path += extension
+
+        return path
+
+    def build_path_by_data(self, data: Any, index: int = 0):
+        extension = self.find_extension(data=data)
+        return self.build_path_by_extension(extension=extension, index=index)
+
+
+class Recorder:
+    @property
+    def path_template(self) -> PathTemplate:
+        return self.__path_template
+
+    @property
+    def record_mode(self) -> str:
+        return self.__record_mode
+
+    @record_mode.setter
+    def record_mode(self, record_mode: str):
+        self.__record_mode = record_mode
+
+    def __init__(
+        self,
+        path_template: PathTemplate,
+        record_mode: str,
+    ) -> None:
+        self.__path_template = path_template
+        self.__record_mode = record_mode
+
+        self.__record_list: List[Record] = list()
+
+    def capture(self, captured: Any, strip: bool = False):
+        record_list = self.__record_list
+        record_path = self.__path_template.build_path_by_data(
+            data=captured,
+            index=len(record_list),
+        )
+        record = Record(
+            captured=captured,
+            record_path=record_path,
+            strip=strip,
+        )
+        self.__record_list.append(record)
+
+    def capture_list(self, captured_list: List[Any], strip: bool = False):
+        for captured in captured_list:
+            self.capture(captured=captured, strip=strip)
+
+    def assert_equal(self):
+        record_list = self.__record_list
+
+        for record in record_list:
+            assert not record.record_changed
+
+    def assert_in_list(self, in_list: List[str]):
+        record_list = self.__record_list
+
+        for record in record_list:
+            for string_value in in_list:
+                assert string_value in record.captured
+
+    def persist(self):
+        record_list = self.__record_list
+        record_mode = self.__record_mode
+
+        for record in record_list:
+            if record_mode == "all":
+                save = True
+            elif record_mode == "new_episodes":
+                save = record.record_changed
+            elif record_mode == "none":
+                if not record.record_exists:
+                    raise Exception("You are using `record-mode=none`.")
+
+                save = False
+            elif record_mode == "once":
+                save = not record.record_exists
+            elif record_mode == "rewrite":
+                save = True
+            else:
+                raise Exception(f"Unknown `record-mode` : {record_mode}")
+
+            if save:
+                record.persist()
+
+
+def build_path_by_extension(
+    request: SubRequest,
+    extension: str,
+    create_folder: bool = False,
+) -> str:
+    # SETUP PATH TEMPLATE
+    module_dir = request.node.fspath.dirname
+    module_name = request.node.fspath.purebasename
+    test_name = request.node.name
+    path_template = PathTemplate(
+        module_dir=module_dir,
+        module_name=module_name,
+        test_name=test_name,
     )
-    path += ".csv"
+
+    # BUILD PATH
+    path = path_template.build_path_by_extension(extension)
 
     # CREATE FOLDER
-    if not os.path.exists(path):
+    if create_folder:
         dir_name = os.path.dirname(path)
-        pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True)
-
-    return path
-
-
-@pytest.fixture
-def default_txt_path(request):
-    module = request.node.fspath
-    path = os.path.join(
-        module.dirname,
-        "txt",
-        module.purebasename,
-        request.node.name,
-    )
-    path += ".txt"
-
-    # CREATE FOLDER
-    if not os.path.exists(path):
-        dir_name = os.path.dirname(path)
-        pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True)
-
-    return path
-
-
-@pytest.fixture
-def default_json_path(request):
-    module = request.node.fspath
-    path = os.path.join(
-        module.dirname,
-        "json",
-        module.purebasename,
-        request.node.name,
-    )
-    path += ".json"
-
-    # CREATE FOLDER
-    if not os.path.exists(path):
-        dir_name = os.path.dirname(path)
-        pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True)
+        if not os.path.exists(dir_name):
+            dir_name = os.path.dirname(path)
+            pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True)
 
     return path
 
 
 def pytest_configure(config: Config) -> None:
     config.addinivalue_line("markers", "record_stdout: Mark the test as text record.")
+
+
+@pytest.fixture
+def default_csv_path(request: SubRequest) -> str:
+    return build_path_by_extension(request=request, extension="csv", create_folder=True)
+
+
+@pytest.fixture
+def default_txt_path(request: SubRequest) -> str:
+    return build_path_by_extension(request=request, extension="txt", create_folder=True)
+
+
+@pytest.fixture
+def default_json_path(request: SubRequest) -> str:
+    return build_path_by_extension(
+        request=request, extension="json", create_folder=True
+    )
 
 
 @pytest.fixture
@@ -92,68 +292,8 @@ def merge_markers_kwargs(markers: List[Mark]) -> Dict[str, Any]:
     return kwargs
 
 
-def record_stdout_cmp_content(
-    assert_in_list: List[str],
-    captured_content: str,
-    record_path: str,
-    strip: bool,
-):
-    # COMPARE RECORD
-    if os.path.exists(record_path):
-        with open(file=record_path, encoding="utf-8") as f:
-            record_content = f.read()
-        if strip:
-            assert captured_content.strip() == record_content.strip()
-        else:
-            assert captured_content == record_content
-
-    # COMPARE ASSERT IN LIST
-    for assert_in in assert_in_list:
-        assert assert_in in captured_content
-
-
-def record_stdout_save(
-    captured_content: str,
-    record_path: str,
-    record_mode: str,
-    save_record: bool,
-):
-    record_exists = os.path.exists(record_path)
-
-    if record_exists:
-        with open(file=record_path, encoding="utf-8") as f:
-            record_content = f.read()
-        unchanged = captured_content == record_content
-    else:
-        unchanged = False
-
-    if not save_record:
-        save = False
-    elif record_mode == "all":
-        save = True
-    elif record_mode == "new_episodes":
-        save = not record_exists or not unchanged
-    elif record_mode == "none":
-        save = False
-        if not record_exists:
-            raise Exception("You are using `record-mode=none`.")
-    elif record_mode == "once":
-        save = not record_exists
-    elif record_mode == "rewrite":
-        save = True
-    else:
-        raise Exception("Unknown `record-mode`.")
-
-    if save:
-        dir_name = os.path.dirname(record_path)
-        pathlib.Path(dir_name).mkdir(parents=True, exist_ok=True)
-
-        with open(file=record_path, mode="w", encoding="utf-8") as f:
-            f.write(captured_content)
-
-
 def record_stdout_format_kwargs(
-    default_txt_path: str,
+    test_name: str,
     record_mode: str,
     record_stdout_markers: List[Mark],
 ) -> Dict[str, Any]:
@@ -162,28 +302,15 @@ def record_stdout_format_kwargs(
     formatted_fields = dict()
     formatted_fields["assert_in_list"] = kwargs.get("assert_in_list", list())
     formatted_fields["record_mode"] = kwargs.get("record_mode", record_mode)
-    formatted_fields["record_path"] = kwargs.get("record_path", default_txt_path)
+    formatted_fields["record_name"] = kwargs.get("record_name", test_name)
     formatted_fields["save_record"] = kwargs.get("save_record", True)
     formatted_fields["strip"] = kwargs.get("strip", True)
-
-    # MERGE `RECORD_NAME`
-    if "record_name" in kwargs:
-        if "record_path" in kwargs:
-            raise Exception("You can't use both `record_name` and `record_path`.")
-
-        dir_name = os.path.dirname(default_txt_path)
-        formatted_fields["record_path"] = os.path.join(dir_name, kwargs["record_name"])
-
-    # FORMAT `RECORD_PATH` EXTENSION
-    if not formatted_fields["record_path"].endswith(".txt"):
-        formatted_fields["record_path"] += ".txt"
 
     return formatted_fields
 
 
 @pytest.fixture(autouse=True)
 def record_stdout(
-    default_txt_path: str,
     disable_recording: bool,
     record_stdout_markers: List[Mark],
     record_mode: str,
@@ -194,11 +321,26 @@ def record_stdout(
     if disable_recording:
         yield None
     elif marker:
-        # FORMAT MARKERS KEYWORD ARGUMENTS
+        # SETUP TEST DETAILS
+        module_dir = request.node.fspath.dirname
+        module_name = request.node.fspath.purebasename
+        test_name = request.node.name
+
+        # FORMAT MARKER'S KEYWORD ARGUMENTS
         formatted_kwargs = record_stdout_format_kwargs(
-            default_txt_path=default_txt_path,
+            test_name=test_name,
             record_mode=record_mode,
             record_stdout_markers=record_stdout_markers,
+        )
+
+        # SETUP RECORDER
+        path_template = PathTemplate(
+            module_dir=module_dir,
+            module_name=module_name,
+            test_name=formatted_kwargs["record_name"],
+        )
+        recorder = Recorder(
+            path_template=path_template, record_mode=formatted_kwargs["record_mode"]
         )
 
         # CAPTURE STDOUT
@@ -209,27 +351,52 @@ def record_stdout(
             )
             global_capturing.start_capturing()
             yield
-            captured_content = global_capturing.readouterr().out
+            recorder.capture(
+                captured=global_capturing.readouterr().out,
+                strip=formatted_kwargs["strip"],
+            )
             global_capturing.stop_capturing()
         else:
             capsys = request.getfixturevalue("capsys")
             yield
-            captured_content = capsys.readouterr().out
+            recorder.capture(
+                captured=capsys.readouterr().out, strip=formatted_kwargs["strip"]
+            )
 
-        # SAVE RECORD
-        record_stdout_save(
-            captured_content=captured_content,
-            record_path=formatted_kwargs["record_path"],
-            record_mode=formatted_kwargs["record_mode"],
-            save_record=formatted_kwargs["save_record"],
-        )
-
-        # ASSERT CONTENT
-        record_stdout_cmp_content(
-            assert_in_list=formatted_kwargs["assert_in_list"],
-            captured_content=captured_content,
-            record_path=formatted_kwargs["record_path"],
-            strip=formatted_kwargs["strip"],
-        )
+        # SAVE/CHECK RECORD
+        if formatted_kwargs["save_record"]:
+            recorder.persist()
+            recorder.assert_equal()
+            recorder.assert_in_list(in_list=formatted_kwargs["assert_in_list"])
+        else:
+            recorder.assert_in_list(in_list=formatted_kwargs["assert_in_list"])
     else:
         yield None
+
+
+@pytest.fixture
+def recorder(
+    disable_recording: bool,
+    record_mode: str,
+    request: SubRequest,
+):
+    marker_record_stdout = request.node.get_closest_marker("record_stdout")
+    module_dir = request.node.fspath.dirname
+    module_name = request.node.fspath.purebasename
+    test_name = request.node.name
+    path_template = PathTemplate(
+        module_dir=module_dir,
+        module_name=module_name,
+        test_name=test_name,
+    )
+    if disable_recording:
+        yield None
+    elif marker_record_stdout:
+        raise Exception(
+            "You can't combine both of these fixtures : `record_stdout marker`, `recorder`."
+        )
+    else:
+        recorder = Recorder(path_template, record_mode)
+        yield recorder
+        recorder.persist()
+        recorder.assert_equal()
