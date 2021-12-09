447d8ba7c7a92848a23972ab1f96f47ce8c74573
Chavithra PARANA <chavithra@gmail.com>
2021-12-09 06:25:53 +0100

    Updating tests : stocks/due_diligence/finviz_view

--- tests/gamestonk_terminal/stocks/due_diligence/test_finviz_model.py
+++ tests/gamestonk_terminal/stocks/due_diligence/test_finviz_model.py
@@ -1,6 +1,7 @@
 # IMPORTATION STANDARD
 
 # IMPORTATION THIRDPARTY
+import finviz.main_func
 import pytest
 
 # IMPORTATION INTERNAL
@@ -8,14 +9,18 @@ from gamestonk_terminal.stocks.due_diligence import finviz_model
 
 
 @pytest.mark.vcr
-def test_get_news(recorder):
+def test_get_news(mocker, recorder):
+    # REMOVE FINVIZ STOCK_PAGE CACHE
+    mocker.patch.object(target=finviz.main_func, attribute="STOCK_PAGE", new={})
     result_dict = finviz_model.get_news(ticker="TSLA")
 
     recorder.capture(result_dict)
 
 
 @pytest.mark.vcr
-def test_get_analyst_data(recorder):
+def test_get_analyst_data(mocker, recorder):
+    # REMOVE FINVIZ STOCK_PAGE CACHE
+    mocker.patch.object(target=finviz.main_func, attribute="STOCK_PAGE", new={})
     result_df = finviz_model.get_analyst_data(ticker="TSLA")
 
     recorder.capture(result_df)
