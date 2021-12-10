5ab7a7873d25c9e082d7c2d6be4c7d463c7475c8
Chavithra PARANA <chavithra@gmail.com>
2021-12-10 10:10:47 +0100

    Updating tests : stocks/discovery/seeking_alpha

--- tests/gamestonk_terminal/stocks/discovery/test_seeking_alpha_model.py
+++ tests/gamestonk_terminal/stocks/discovery/test_seeking_alpha_model.py
@@ -15,30 +15,32 @@ def vcr_config():
     }
 
 
-@pytest.mark.vcr()
+@pytest.mark.vcr
 def test_get_next_earnings(recorder):
     df_earnings = seeking_alpha_model.get_next_earnings(1)
     recorder.capture(df_earnings)
 
+
 @pytest.mark.skip("Broken ?")
-@pytest.mark.vcr()
+@pytest.mark.vcr
 def test_get_article_list():
     start_date = datetime.strptime("2020-12-10", "%Y-%m-%d")
     seeking_alpha_model.get_article_list(start_date=start_date, num=2)
 
 
-@pytest.mark.vcr()
+@pytest.mark.vcr
 def test_get_trending_list(recorder):
     trending_list = seeking_alpha_model.get_trending_list(num=2)
     recorder.capture(trending_list)
 
 
 @pytest.mark.skip("Broken ?")
-@pytest.mark.vcr()
+@pytest.mark.vcr
 def test_get_news_html():
     seeking_alpha_model.get_news_html()
 
+
 @pytest.mark.skip("Broken ?")
-@pytest.mark.vcr()
+@pytest.mark.vcr
 def test_get_news():
     seeking_alpha_model.get_news()
