00421464af1768beb2102aadf17c59aecf7c5327
Chavithra PARANA <chavithra@gmail.com>
2021-12-10 09:33:37 +0100

    Updating tests : stocks/discovery/shortinterest

--- tests/gamestonk_terminal/stocks/discovery/test_shortinterest_model.py
+++ tests/gamestonk_terminal/stocks/discovery/test_shortinterest_model.py
@@ -1,55 +1,25 @@
-""" dark_pool_shorts/short_interest_api.py tests """
-import unittest
-from unittest import mock
+# IMPORTATION STANDARD
 
-# from gamestonk_terminal.stocks.dark_pool_shorts.shortinterest_model import get_high_short_interest
-from gamestonk_terminal.stocks.discovery.shortinterest_model import get_low_float
+# IMPORTATION THIRDPARTY
+import pytest
 
-# pylint: disable=unused-import
-from tests.helpers.tools import (  # noqa: F401
-    parameterize_from_file,
-    pytest_generate_tests,
-)
-
-assertions = unittest.TestCase("__init__")
+# IMPORTATION INTERNAL
+from gamestonk_terminal.stocks.discovery import shortinterest_model
 
 
-class TestDiscoveryShortInterestApi:
-    """
-    # TODO
-    @mock.patch("gamestonk_terminal.stocks.discovery.shortinterest_model.requests")
-    @parameterize_from_file(
-        "test_get_high_short_interest",
-        "../tests/data/discovery_short_interest_model.yaml",
-    )
-    # pylint: disable=unused-argument
-    def test_get_high_short_interest(
-        self, mock_request_get, mock_high_short, expected_result
-    ):
-        mock_request_get.get().text = mock_high_short
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": [("User-Agent", None)],
+    }
 
-        df_high_short = get_high_short_interest()
+@pytest.mark.vcr()
+def test_get_low_float(recorder):
+    df_low_float = shortinterest_model.get_low_float()
+    recorder.capture(df_low_float)
 
-        assertions.assertEqual.__self__.maxDiff = None
-        assertions.assertEqual(
-            df_high_short.to_csv().replace("\r\n", "\n"),
-            expected_result.replace("\r\n", "\n"),
-        )
-    """
 
-    @mock.patch("gamestonk_terminal.stocks.discovery.shortinterest_model.requests")
-    @parameterize_from_file(
-        "test_get_low_float",
-        "../../tests/gamestonk_terminal/stocks/discovery/yaml/test_short_interest_model/model.yaml",
-    )
-    # pylint: disable=unused-argument
-    def test_get_low_float(self, mock_request_get, mock_low_float, expected_result):
-        mock_request_get.get().text = mock_low_float
-
-        df_low_float = get_low_float()
-
-        assertions.assertEqual.__self__.maxDiff = None
-        assertions.assertEqual(
-            df_low_float.to_csv().replace("\r\n", "\n"),
-            expected_result.replace("\r\n", "\n"),
-        )
+@pytest.mark.vcr()
+def test_get_today_hot_penny_stocks(recorder):
+    df_penny = shortinterest_model.get_today_hot_penny_stocks()
+    recorder.capture(df_penny)
