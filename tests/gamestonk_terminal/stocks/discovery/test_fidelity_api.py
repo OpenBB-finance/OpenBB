e7888664024e8df7a86eb332407d4610149381d9
Chavithra PARANA <chavithra@gmail.com>
2021-12-10 07:31:14 +0100

    Updating tests : stocks/discovery/fidelity

--- tests/gamestonk_terminal/stocks/discovery/test_fidelity_api.py
+++ tests/gamestonk_terminal/stocks/discovery/test_fidelity_api.py
@@ -1,48 +1,49 @@
-""" discovery/fidelity_api.py tests """
-import unittest
+# IMPORTATION STANDARD
 
-import vcr
+# IMPORTATION THIRDPARTY
+import pytest
 
-from gamestonk_terminal.stocks.discovery.fidelity_view import (
-    buy_sell_ratio_color_red_green,
-    orders_view,
-    price_change_color_red_green,
+# IMPORTATION INTERNAL
+from gamestonk_terminal.stocks.discovery import fidelity_view
+
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": [("User-Agent", None)],
+    }
+
+
+@pytest.mark.vcr(record_mode="none")
+@pytest.mark.parametrize(
+    "val",
+    [
+        "75% Buys, 25% Sells",
+        "25% Buys, 75% Sells",
+        "Mocked Value",
+    ],
 )
-from tests.helpers.helpers import check_print
+def test_buy_sell_ratio_color_red_green(val, recorder):
+    result_txt = fidelity_view.buy_sell_ratio_color_red_green(val=val)
+    recorder.capture(result_txt)
 
 
-class TestDiscoveryFidelityApi(unittest.TestCase):
-    @check_print(assert_in="Symbol")
-    @vcr.use_cassette(
-        "tests/gamestonk_terminal/stocks/discovery/cassettes/test_fidelity/test_orders.yaml",
-        record_mode="once",
-    )
-    def test_orders(self):
-        orders_view(5, "")
+@pytest.mark.vcr(record_mode="none")
+@pytest.mark.parametrize(
+    "val",
+    ["-8.20 (-18.3363%)", "+8.20 (+18.3363%)"],
+)
+def test_dprice_change_color_red_green(val, recorder):
+    result_txt = fidelity_view.price_change_color_red_green(val=val)
+    recorder.capture(result_txt)
 
-    @vcr.use_cassette(
-        "tests/gamestonk_terminal/stocks/discovery/cassettes/test_fidelity/test_buy_sell.yaml",
-        record_mode="new_episodes",
-    )
-    def test_buy_sell_ratio_color_red_green(self):
 
-        res = buy_sell_ratio_color_red_green("56% Buys, 44% Sells")
-
-        assert res == "\x1b[32m56%\x1b[0m Buys, 44% Sells"
-
-        res = buy_sell_ratio_color_red_green("44% Buys, 56% Sells")
-
-        assert res == "44% Buys, \x1b[31m56%\x1b[0m Sells"
-
-    @vcr.use_cassette(
-        "tests/cassettes/test_discovery/test_discovery_fidelity/test_price_change_color.yaml",
-        record_mode="new_episodes",
-    )
-    def test_price_change_color_red_green(self):
-        res = price_change_color_red_green("-6.99 (-6.4288%)")
-
-        assert res == "\x1b[31m-6.99 (-6.4288%)\x1b[0m"
-
-        res = price_change_color_red_green("2.40 (+124.3523%)")
-
-        assert res == "\x1b[32m2.40 (+124.3523%)\x1b[0m"
+@pytest.mark.vcr
+@pytest.mark.record_stdout
+@pytest.mark.parametrize(
+    "use_color",
+    [True, False],
+)
+def test_orders_view(mocker, use_color):
+    mocker.patch.object(target=fidelity_view.gtff, attribute="USE_COLOR", new=use_color)
+    fidelity_view.orders_view(num=1, export="")
