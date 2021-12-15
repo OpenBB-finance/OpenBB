fdbace7d29c92b6925b3c24e23941a232fb8bcd9
james <jmaslek11@gmail.com>
2021-12-14 20:47:26 -0500

    Clean up switch

--- gamestonk_terminal/etf/etf_controller.py
+++ gamestonk_terminal/etf/etf_controller.py
@@ -127,7 +127,7 @@ Finance Database:
                 an_input = actions[0]
 
             # Add all instructions to the queue
-            for cmd in actions[::-1]:
+            for cmd in actions[1:][::-1]:
                 if cmd:
                     self.queue.insert(0, cmd)
 
