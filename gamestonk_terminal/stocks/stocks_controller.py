afab585bb3867fa3bdc3b50d2690beb3f438922a
jose-donato <zmcdonato@gmail.com>
2021-12-16 18:20:11 +0000

    fix: refactor fa(and fmp) controllers

--- gamestonk_terminal/stocks/stocks_controller.py
+++ gamestonk_terminal/stocks/stocks_controller.py
@@ -648,12 +648,9 @@ Stocks Menus:
 
         from gamestonk_terminal.stocks.fundamental_analysis import fa_controller
 
-        ret = fa_controller.menu(self.ticker, self.start, self.interval, self.suffix)
-
-        if ret is False:
-            self.print_help()
-        else:
-            return True
+        return fa_controller.menu(
+            self.ticker, self.start, self.interval, self.suffix, self.queue
+        )
 
     def call_bt(self, _):
         """Process bt command"""
