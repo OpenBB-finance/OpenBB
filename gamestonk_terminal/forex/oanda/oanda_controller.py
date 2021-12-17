bf4035181151384cb73a7f7e597e43f7bd607c5b
Theodore Aptekarev <aptekarev@gmail.com>
2021-12-17 14:19:33 +0300

    Upgrade oanda controller

--- gamestonk_terminal/forex/oanda/oanda_controller.py
+++ gamestonk_terminal/forex/oanda/oanda_controller.py
@@ -19,6 +19,7 @@ from gamestonk_terminal.helper_funcs import (
     try_except,
     parse_known_args_and_warn,
     system_clear,
+    check_non_negative_float,
 )
 from gamestonk_terminal.menu import session
 
@@ -91,7 +92,7 @@ class OandaController:
         self.from_symbol = ""
         self.to_symbol = ""
 
-        self.instrument = None
+        self.instrument: Union[str, None] = None
 
         if queue:
             self.queue = queue
@@ -288,78 +289,367 @@ class OandaController:
     @try_except
     def call_price(self, other_args):
         """Process Price Command"""
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="price",
+            description="Get price for selected instrument.",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
         oanda_view.get_fx_price(account, self.instrument)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_summary(self, other_args):
         """Process account summary command"""
-        oanda_view.get_account_summary(account, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="summary",
+            description="Print some information about your account.",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        oanda_view.get_account_summary(account)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_orderbook(self, other_args):
         """Process Oanda Order Book"""
-        oanda_view.get_order_book(self.instrument, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="orderbook",
+            description="Plot an orderbook for an instrument if Oanda provides one.",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        oanda_view.get_order_book(account, self.instrument)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_positionbook(self, other_args):
         """Process Oanda Position Book"""
-        oanda_view.get_position_book(self.instrument, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="positionbook",
+            description="Plot a position book for an instrument if Oanda provides one.",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        oanda_view.get_position_book(account, self.instrument)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_list(self, other_args: List[str]):
         """Process list orders command"""
-        oanda_view.list_orders(account, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="list",
+            description="List order history",
+        )
+        parser.add_argument(
+            "-s",
+            "--state",
+            dest="state",
+            action="store",
+            default="ALL",
+            type=str,
+            required=False,
+            help="List orders that have a specific state.",
+        )
+        parser.add_argument(
+            "-l",
+            "--limit",
+            dest="limit",
+            action="store",
+            default=20,
+            type=int,
+            required=False,
+            help="Limit the number of orders to retrieve.",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        order_state = ns_parser.state.upper()
+        order_count = ns_parser.limit
+        oanda_view.list_orders(account, order_state, order_count)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_order(self, other_args: List[str]):
         """Place limit order"""
-        oanda_view.create_order(account, self.instrument, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="order",
+            description="Create order",
+        )
+        parser.add_argument(
+            "-u",
+            "--unit",
+            dest="units",
+            action="store",
+            type=int,
+            required="-h" not in other_args,
+            help="The number of units to place in the order request. Positive for "
+            + "a long position and negative for a short position.",
+        )
+        parser.add_argument(
+            "-p",
+            "--price",
+            dest="price",
+            action="store",
+            type=check_non_negative_float,
+            required="-h" not in other_args,
+            help="The price to set for the limit order.",
+        )
+
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        price = ns_parser.price
+        units = ns_parser.units
+        oanda_view.create_order(account, self.instrument, price, units)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_cancel(self, other_args: List[str]):
         """Cancel pending order by ID"""
-        oanda_view.cancel_pending_order(account, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="cancel",
+            description="Cancel a Pending Order.",
+        )
+        parser.add_argument(
+            "-i",
+            "--id",
+            dest="orderID",
+            action="store",
+            required="-h" not in other_args,
+            type=str,
+            help="The pending order ID to cancel.",
+        )
+        if other_args:
+            if "-" not in other_args[0]:
+                other_args.insert(0, "-i")
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        orderID = ns_parser.orderID
+        oanda_view.cancel_pending_order(account, orderID)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_positions(self, other_args):
         """Get Open Positions"""
-        oanda_view.get_open_positions(account, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="positions",
+            description="Get information about open positions.",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        oanda_view.get_open_positions(account)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_pending(self, other_args):
         """See up to 25 pending orders"""
-        oanda_view.get_pending_orders(account, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="pending",
+            description="Get information about pending orders.",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        oanda_view.get_pending_orders(account)
+        return self.queue if len(self.queue) > 0 else []
+
+    @try_except
+    def call_trades(self, other_args):
+        """List open trades."""
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="trades",
+            description="Get information about open trades.",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        oanda_view.get_open_trades(account)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_closetrade(self, other_args: List[str]):
         """Close a trade by id"""
-        oanda_view.close_trade(account, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="closetrade",
+            description="Close a trade by id.",
+        )
+        parser.add_argument(
+            "-i",
+            "--id",
+            dest="orderID",
+            action="store",
+            type=str,
+            required=False,
+            help="The Trade ID to close. ",
+        )
+        parser.add_argument(
+            "-u",
+            "--units",
+            dest="units",
+            action="store",
+            required=False,
+            help="The number of units on the trade to close. If not set it "
+            + "defaults to all units. ",
+        )
+        if other_args:
+            if "-i" not in other_args[0]:
+                other_args.insert(0, "-i")
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        orderID = ns_parser.orderID
+        units = ns_parser.units
+        oanda_view.close_trade(account, orderID, units)
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_candles(self, other_args: List[str]):
-        oanda_view.show_candles(account, self.instrument, other_args)
-        return self.queue if len(self.queue) > 0 else []
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="candles",
+            description="Display Candle Data",
+        )
+        parser.add_argument(
+            "-g",
+            "--granularity",
+            dest="granularity",
+            action="store",
+            type=str,
+            default="D",
+            required=False,
+            help="The timeframe to get for the candle chart (Seconds: S5, S10, S15, S30 "
+            + "Minutes: M1, M2, M4, M5, M10, M15, M30 Hours: H1, H2, H3, H4, H6, H8, H12 "
+            + "Day (default): D, Week: W Month: M",
+        )
+        parser.add_argument(
+            "-l",
+            "--limit",
+            dest="candlecount",
+            action="store",
+            default=180,
+            type=int,
+            required=False,
+            help="The number of candles to retrieve. Default:180 ",
+        )
+        parser.add_argument(
+            "-a",
+            "--ad",
+            dest="ad",
+            action="store_true",
+            help="Adds ad (Accumulation/Distribution Indicator) to the chart",
+        )
+        parser.add_argument(
+            "-b",
+            "--bbands",
+            dest="bbands",
+            action="store_true",
+            help="Adds Bollinger Bands to the chart",
+        )
+        parser.add_argument(
+            "-c",
+            "--cci",
+            dest="cci",
+            action="store_true",
+            help="Adds cci (Commodity Channel Index) to the chart",
+        )
+        parser.add_argument(
+            "-e",
+            "--ema",
+            dest="ema",
+            action="store_true",
+            help="Adds ema (Exponential Moving Average) to the chart",
+        )
+        parser.add_argument(
+            "-o",
+            "--obv",
+            dest="obv",
+            action="store_true",
+            help="Adds obv (On Balance Volume) to the chart",
+        )
+        parser.add_argument(
+            "-r",
+            "--rsi",
+            dest="rsi",
+            action="store_true",
+            help="Adds rsi (Relative Strength Index) to the chart",
+        )
+        parser.add_argument(
+            "-s",
+            "--sma",
+            dest="sma",
+            action="store_true",
+            help="Adds sma (Simple Moving Average) to the chart",
+        )
+        parser.add_argument(
+            "-v",
+            "--vwap",
+            dest="vwap",
+            action="store_true",
+            help="Adds vwap (Volume Weighted Average Price) to the chart",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
 
-    @try_except
-    def call_trades(self, other_args):
-        """List open trades"""
-        oanda_view.get_open_trades(account, other_args)
+        oanda_view.show_candles(
+            self.instrument,
+            granularity=ns_parser.granularity.upper(),
+            candlecount=ns_parser.candlecount,
+            additional_charts={
+                "ad": ns_parser.ad,
+                "bbands": ns_parser.bbands,
+                "cci": ns_parser.cci,
+                "ema": ns_parser.ema,
+                "obv": ns_parser.obv,
+                "rsi": ns_parser.rsi,
+                "sma": ns_parser.sma,
+                "vwap": ns_parser.vwap,
+            },
+        )
         return self.queue if len(self.queue) > 0 else []
 
     @try_except
     def call_calendar(self, other_args: List[str]):
         """Call calendar"""
-        oanda_view.calendar(self.instrument, other_args)
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="calendar",
+            description="Show Calendar Data",
+        )
+        parser.add_argument(
+            "-d",
+            "--days",
+            dest="days",
+            action="store",
+            type=int,
+            default=7,
+            required=False,
+            help="The number of days to search for, up to 30 forward or backward "
+            + "use negative numbers to search back. ",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not ns_parser:
+            return self.queue if len(self.queue) > 0 else []
+        days = ns_parser.days
+        oanda_view.calendar(self.instrument, days)
         return self.queue if len(self.queue) > 0 else []
 
 
