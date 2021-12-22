62be62e37603294dadd9490443edcc467b0fe8c3
Chavithra PARANA <chavithra@gmail.com>
2021-12-23 00:19:28 +0100

    Updating tests : stocks/comparison_analysis/polygon

--- tests/gamestonk_terminal/stocks/comparison_analysis/test_polygon_model.py
+++ tests/gamestonk_terminal/stocks/comparison_analysis/test_polygon_model.py
@@ -32,7 +32,7 @@ def test_get_similar_companies_invalid_status(mocker, recorder):
     mock_response = requests.Response()
     mock_response.status_code = 400
     # pylint: disable=protected-access
-    mock_response._content = """{"error":"MOCK_ERROR"}""".encode("utf-8")
+    mock_response._content = b"""{"error":"MOCK_ERROR"}"""
     mocker.patch(target="requests.get", new=mocker.Mock(return_value=mock_response))
     result_tuple = polygon_model.get_similar_companies(
         ticker="TSLA",
