0bae097de8096507c9994bf674bdef3397fbf0cb
teh_coderer <61340027+tehcoderer@users.noreply.github.com>
2021-12-24 23:32:18 -0500

    Update oi.py

--- discordbot/stocks/options/oi.py
+++ discordbot/stocks/options/oi.py
@@ -9,7 +9,7 @@ from discordbot.run_discordbot import gst_imgur
 
 from gamestonk_terminal import config_plot as cfp
 from gamestonk_terminal.helper_funcs import plot_autoscale
-from gamestonk_terminal.stocks.options import op_helpers, tradier_model
+from gamestonk_terminal.stocks.options import op_helpers, yfinance_model
 
 
 async def oi_command(ctx, ticker: str= None, expiry: str= None, min_sp: float= None, max_sp: float= None):
@@ -25,13 +25,15 @@ async def oi_command(ctx, ticker: str= None, expiry: str= None, min_sp: float= N
         if ticker is None:
             raise Exception("Stock ticker is required")    
         
-        dates = tradier_model.option_expirations(ticker)
+        dates = yfinance_model.option_expirations(ticker)
 
         if not dates:
             raise Exception("Stock ticker is invalid")
         
-        options = tradier_model.get_option_chains(ticker, expiry)
-        current_price = tradier_model.last_price(ticker)
+        options = yfinance_model.get_option_chain(ticker, expiry)
+        calls = options.calls
+        puts = options.puts        
+        current_price = yfinance_model.get_price(ticker)
 
 
         if min_sp is None:
@@ -43,25 +45,22 @@ async def oi_command(ctx, ticker: str= None, expiry: str= None, min_sp: float= N
             max_strike = 1.90 * current_price
         else:
             max_strike = max_sp
-
-        calls = options[options.option_type == "call"][["strike", "open_interest"]]
-        puts = options[options.option_type == "put"][["strike", "open_interest"]]
-        call_oi = calls.set_index("strike")["open_interest"] / 1000
-        put_oi = puts.set_index("strike")["open_interest"] / 1000
+            
+        call_oi = calls.set_index("strike")["openInterest"] / 1000
+        put_oi = puts.set_index("strike")["openInterest"] / 1000
 
         df_opt = pd.merge(call_oi, put_oi, left_index=True, right_index=True)
         df_opt = df_opt.rename(
-              columns={"open_interest_x": "OI_call", "open_interest_y": "OI_put"}
+            columns={"openInterest_x": "OI_call", "openInterest_y": "OI_put"}
         )
 
         max_pain = op_helpers.calculate_max_pain(df_opt)
         plt.style.use("seaborn")
         fig, ax = plt.subplots(figsize=plot_autoscale(), dpi=cfp.PLOT_DPI)
 
-
         put_oi.plot(
             x="strike",
-            y="open_interest",
+            y="openInterest",
             label="Puts",
             ax=ax,
             marker="o",
@@ -70,27 +69,28 @@ async def oi_command(ctx, ticker: str= None, expiry: str= None, min_sp: float= N
         )
         call_oi.plot(
             x="strike",
-            y="open_interest",
+            y="openInterest",
             label="Calls",
             ax=ax,
             marker="o",
             ls="-",
             c="g",
         )
-        ax.axvline(
-            current_price, lw=2, c="k", ls="--", label="Current Price", alpha=0.7
-        )
+        ax.axvline(current_price, lw=2, c="k", ls="--", label="Current Price", alpha=0.7)
         ax.axvline(max_pain, lw=3, c="k", label=f"Max Pain: {max_pain}", alpha=0.7)
         ax.grid("on")
         ax.set_xlabel("Strike Price")
         ax.set_ylabel("Open Interest (1k) ")
         ax.set_xlim(min_strike, max_strike)
 
+
         ax.set_title(f"Open Interest for {ticker.upper()} expiring {expiry}")
         plt.legend(loc=0)
         fig.tight_layout(pad=1)
 
+        imagefile = "opt_oi.png"
         plt.savefig("opt_oi.png")
+        
         uploaded_image = gst_imgur.upload_image("opt_oi.png", title="something")
         image_link = uploaded_image.link
         if cfg.DEBUG:
