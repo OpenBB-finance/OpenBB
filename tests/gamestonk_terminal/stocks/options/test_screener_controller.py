8215326bb96156df97686dced79bd6b1bc355684
Chavithra PARANA <chavithra@gmail.com>
2022-01-02 18:58:42 +0100

    Updating tests : skip

--- tests/gamestonk_terminal/stocks/options/test_screener_controller.py
+++ tests/gamestonk_terminal/stocks/options/test_screener_controller.py
@@ -1,5 +1,5 @@
 # IMPORTATION STANDARD
-import os
+# import os
 
 # IMPORTATION THIRDPARTY
 import pandas as pd
@@ -12,7 +12,7 @@ from gamestonk_terminal.stocks.options import screener_controller
 # pylint: disable=W0603
 # pylint: disable=E1111
 
-pytest.skip(allow_module_level=True)
+# pytest.skip(allow_module_level=True)
 
 PRICES = pd.DataFrame(data={"Price": [11.0, 12.0], "Chance": [0.2, 0.8]})
 
