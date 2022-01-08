a8d6c2b4a07b6fc18dc6e08c52bd53f42b8c6405
jose-donato <zmcdonato@gmail.com>
2022-01-08 13:27:09 +0000

    trying to fix tests

--- tests/gamestonk_terminal/cryptocurrency/due_diligence/test_glassnode_view.py
+++ tests/gamestonk_terminal/cryptocurrency/due_diligence/test_glassnode_view.py
@@ -3,6 +3,16 @@ import pytest
 from gamestonk_terminal.cryptocurrency.due_diligence import glassnode_view
 
 
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": [("User-Agent", None)],
+        "filter_query_parameters": [
+            ("api_key", "MOCK_API_KEY"),
+        ],
+    }
+
+
 @pytest.mark.vcr
 @pytest.mark.record_stdout
 def test_display_btc_rainbow():
