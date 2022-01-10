7de480465fd5bda7dcb3c993c732135886a71177
jose-donato <zmcdonato@gmail.com>
2022-01-10 15:03:20 +0000

    black lint

--- gamestonk_terminal/cryptocurrency/due_diligence/glassnode_view.py
+++ gamestonk_terminal/cryptocurrency/due_diligence/glassnode_view.py
@@ -115,9 +115,9 @@ def display_btc_rainbow(since: int, until: int, export: str = ""):
 
         ax.grid(alpha=0.2)
         ax.minorticks_off()
-        ax.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(
-            lambda x, _: int(x) if x >= 1 else x
-        ))
+        ax.yaxis.set_major_formatter(
+            matplotlib.ticker.FuncFormatter(lambda x, _: int(x) if x >= 1 else x)
+        )
         ax.yaxis.set_major_locator(
             matplotlib.ticker.LogLocator(base=100, subs=[1.0, 2.0, 5.0, 10.0])
         )
