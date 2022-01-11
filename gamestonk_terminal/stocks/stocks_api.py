c20e2801b071f9d60cc57c618a4b75ffc990a83e
Theodore Aptekarev <aptekarev@gmail.com>
2022-01-12 00:11:50 +0300

    Add backtesting api wrapper

--- gamestonk_terminal/stocks/stocks_api.py
+++ gamestonk_terminal/stocks/stocks_api.py
@@ -8,4 +8,5 @@ from .stocks_helper import load, process_candle, search, quote
 from .stocks_helper import display_candle as candle
 
 # Context menus
+from .backtesting import bt_api as bt
 from .discovery import disc_api as disc
