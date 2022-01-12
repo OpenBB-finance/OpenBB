4191a67c5957fe63ad1df705f85525f81ace787c
Theodore Aptekarev <aptekarev@gmail.com>
2022-01-12 19:27:51 +0300

    Add ta api wrapper

--- gamestonk_terminal/stocks/stocks_api.py
+++ gamestonk_terminal/stocks/stocks_api.py
@@ -21,3 +21,4 @@ from .prediction_techniques import pred_api as pred
 from .quantitative_analysis import qa_api as qa
 from .screener import screener_api as screener
 from .sector_industry_analysis import sia_api as sia
+from .technical_analysis import ta_api as ta
