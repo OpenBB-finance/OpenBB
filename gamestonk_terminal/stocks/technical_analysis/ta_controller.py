42b1acf5c3d2f9e9922ac8c4e956fffc08a5d7ae
Theodore Aptekarev <aptekarev@gmail.com>
2022-01-13 15:50:28 +0300

    Add an API wrapper for terminal functions to be used in python (#1170)
    * Initial implementation of the API wrapper for stocks/disc
    
    * Create a file for helper classes
    
    * Move models namespace class where it belongs... TO THE HELPER_CLASSES🚀
    
    * Add backtesting api wrapper
    
    * Add ba api wrapper
    
    * Add dps api wrapper
    
    * Fix flake8 warnings in command help strings
    
    * Add comparison analysis models
    
    * Add dd api wrapper
    
    * Add fa api wrapper
    
    * Add gov api wrapper
    
    * Add ins api wrapper *note the note
    
    * Add options api wrapper *note the note
    
    * Add prediction api wrapper
    
    * Add api support for importing models from both local and common contexts
    
    * Add qa api wrapper
    
    * Add screener api wrapper *note the note
    
    * Fix typos in sia controller
    
    * Add sia api wrapper
    
    * Add ta api wrapper
    
    * Disable linting for specific lines that fail in CI
    
    * Remove deprecated parameter
    
    * Add exception handling when prediction packages are not installed

--- gamestonk_terminal/stocks/technical_analysis/ta_controller.py
+++ gamestonk_terminal/stocks/technical_analysis/ta_controller.py
@@ -156,9 +156,10 @@ Custom:
             add_help=False,
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
             prog="load",
-            description="Load stock ticker to perform analysis on. When the data source is 'yf', an Indian ticker can be"
-            " loaded by using '.NS' at the end, e.g. 'SBIN.NS'. See available market in"
-            " https://help.yahoo.com/kb/exchanges-data-providers-yahoo-finance-sln2310.html.",
+            description="Load stock ticker to perform analysis on. When the data source"
+            + " is 'yf', an Indian ticker can be loaded by using '.NS' at the end,"
+            + " e.g. 'SBIN.NS'. See available market in"
+            + " https://help.yahoo.com/kb/exchanges-data-providers-yahoo-finance-sln2310.html.",
         )
         parser.add_argument(
             "-t",
