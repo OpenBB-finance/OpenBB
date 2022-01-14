6111b744cef8c28c816eb043ac6da835a13b69e4
jose-donato <zmcdonato@gmail.com>
2022-01-14 09:10:15 +0000

    added mocking to llama tests

--- tests/gamestonk_terminal/cryptocurrency/defi/test_llama_view.py
+++ tests/gamestonk_terminal/cryptocurrency/defi/test_llama_view.py
@@ -12,18 +12,30 @@ def test_display_defi_protocols():
 @pytest.mark.skip
 @pytest.mark.vcr
 @pytest.mark.record_stdout
-def test_display_defi_tvl():
+def test_display_defi_tvl(mocker):
+    # MOCK CHARTS
+    mocker.patch.object(target=llama_view.gtff, attribute="USE_ION", new=True)
+    mocker.patch(target="gamestonk_terminal.stocks.options.yfinance_view.plt.ion")
+    mocker.patch(target="gamestonk_terminal.stocks.options.yfinance_view.plt.show")
     llama_view.display_defi_tvl(20)
 
 
 @pytest.mark.vcr
 @pytest.mark.record_stdout
-def test_display_grouped_defi_protocols():
+def test_display_grouped_defi_protocols(mocker):
+    # MOCK CHARTS
+    mocker.patch.object(target=llama_view.gtff, attribute="USE_ION", new=True)
+    mocker.patch(target="gamestonk_terminal.stocks.options.yfinance_view.plt.ion")
+    mocker.patch(target="gamestonk_terminal.stocks.options.yfinance_view.plt.show")
     llama_view.display_grouped_defi_protocols(20)
 
 
 @pytest.mark.skip
 @pytest.mark.vcr
 @pytest.mark.record_stdout
-def test_display_historical_tvl():
+def test_display_historical_tvl(mocker):
+    # MOCK CHARTS
+    mocker.patch.object(target=llama_view.gtff, attribute="USE_ION", new=True)
+    mocker.patch(target="gamestonk_terminal.stocks.options.yfinance_view.plt.ion")
+    mocker.patch(target="gamestonk_terminal.stocks.options.yfinance_view.plt.show")
     llama_view.display_historical_tvl("sushiswap,uniswap")
