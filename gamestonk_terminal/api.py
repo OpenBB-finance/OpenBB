0ebc438c1b6d236cad4ad148c0fdcdf723cdd582
Colin Delahunty <colin99delahunty@gmail.com>
2022-01-19 14:34:50 -0500

    Added ETF API

--- gamestonk_terminal/api.py
+++ gamestonk_terminal/api.py
@@ -1,3 +1,4 @@
+"""Gamestonk Terminal API."""
 # flake8: noqa
 # pylint: disable=unused-import
 from .stocks import stocks_api as stocks
@@ -5,3 +6,4 @@ from .alternative import alt_api as alt
 from .cryptocurrency import crypto_api as crypto
 from .custom import custom_api as custom
 from .economy import economy_api as economy
+from .etf import etf_api as etf
