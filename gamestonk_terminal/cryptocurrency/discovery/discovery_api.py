a67a52affdc96f5d3f11f439fa6e6ff3cc00523f
jose-donato <zmcdonato@gmail.com>
2022-01-20 16:15:18 +0000

    fix linting

--- gamestonk_terminal/cryptocurrency/discovery/discovery_api.py
+++ gamestonk_terminal/cryptocurrency/discovery/discovery_api.py
@@ -8,8 +8,6 @@ from gamestonk_terminal.helper_classes import ModelsNamespace as _models
 # Menu commands
 from .pycoingecko_view import display_gainers as cggainers
 from .pycoingecko_view import display_losers as cglosers
-from .pycoingecko_view import display_top_volume_coins as cgvolume
-from .pycoingecko_view import display_top_defi_coins as cgdefi
 from .coinmarketcap_view import display_cmc_top_coins as cmctop
 from .coinpaprika_view import display_search_results as cpsearch
 
