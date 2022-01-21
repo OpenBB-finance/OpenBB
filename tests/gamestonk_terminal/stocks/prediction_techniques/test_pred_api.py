843c9fb7c545cef0cb6946ce0541417b43d57d9d
Chavithra PARANA <chavithra@gmail.com>
2022-01-21 19:48:49 +0100

    Updating tests : stocks/prediction_techniques

--- tests/gamestonk_terminal/stocks/prediction_techniques/test_pred_api.py
+++ tests/gamestonk_terminal/stocks/prediction_techniques/test_pred_api.py
@@ -1,11 +1,15 @@
 # IMPORTATION STANDARD
 
 # IMPORTATION THIRDPARTY
+import pytest
 
 # IMPORTATION INTERNAL
 from gamestonk_terminal.helper_classes import ModelsNamespace as _models
-from gamestonk_terminal.stocks.prediction_techniques import pred_api
 
+try:
+    from gamestonk_terminal.stocks.prediction_techniques import pred_api
+except ImportError:
+    pytest.skip(allow_module_level=True)
 
 def test_models():
     assert isinstance(pred_api.models, _models)
