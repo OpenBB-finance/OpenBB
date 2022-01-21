bf144b3dc73e4b2b706423b88f6a8bf7cc85d27f
Chavithra PARANA <chavithra@gmail.com>
2022-01-21 04:59:30 +0100

    Updating tests : stocks/sector_industry_analysis

---
+++ tests/gamestonk_terminal/stocks/sector_industry_analysis/test_sia_api.py
@@ -0,0 +1,13 @@
+# IMPORTATION STANDARD
+import os
+
+# IMPORTATION THIRDPARTY
+import pytest
+
+# IMPORTATION INTERNAL
+from gamestonk_terminal.helper_classes import ModelsNamespace as _models
+from gamestonk_terminal.stocks.sector_industry_analysis import sia_api
+
+
+def test_models():
+    assert isinstance(sia_api.models, _models)
