0b01f8e8b8453160a8f78059f8e2bf7c8367b5b1
northern-64bit <75195383+northern-64bit@users.noreply.github.com>
2022-01-22 05:20:54 +0100

    Allocation Command (#1204)
    * Adding the allocation (al) command to the portfolio menu
    
    * Fix linting
    
    * Fixing errors
    
    * More fixes
    
    * Requested changes
    
    * More fixes
    
    Co-authored-by: jmaslek <jmaslek11@gmail.com>

--- gamestonk_terminal/portfolio/portfolio_controller.py
+++ gamestonk_terminal/portfolio/portfolio_controller.py
@@ -16,6 +16,7 @@ from gamestonk_terminal import feature_flags as gtff
 from gamestonk_terminal.helper_funcs import (
     valid_date,
     check_positive_float,
+    check_positive,
 )
 from gamestonk_terminal.menu import session
 
@@ -42,6 +43,7 @@ class PortfolioController(BaseController):
         "rmv",
         "ar",
         "rmr",
+        "al",
     ]
     CHOICES_MENUS = [
         "bro",
@@ -90,6 +92,7 @@ class PortfolioController(BaseController):
 
 [info]Graphs:[/info][cmds]
     rmr         graph your returns versus the market's returns
+    al          displays the allocation of the portfolio
         """
         console.print(text=help_text, menu="Portfolio")
 
@@ -350,3 +353,52 @@ class PortfolioController(BaseController):
                     console.print("Cannot generate a graph from an empty dataframe\n")
             else:
                 console.print("Please add items to the portfolio\n")
+
+    def call_al(self, other_args: List[str]):
+        """Process al command"""
+        parser = argparse.ArgumentParser(
+            add_help=False,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            prog="al",
+            description="Gives allocation",
+        )
+        parser.add_argument(
+            "-l",
+            "--limit",
+            help="Number to show",
+            type=check_positive,
+            default=20,
+            dest="limit",
+        )
+        parser.add_argument(
+            "-n",
+            "--noetf",
+            action="store_true",
+            default=False,
+            dest="no_etf_positions",
+            help="If etf positions should not be added to the portfolio",
+        )
+        parser.add_argument(
+            "-g",
+            "--graph",
+            action="store_true",
+            default=False,
+            dest="graph",
+            help="Display table or pi chart of holdings",
+        )
+        ns_parser = parse_known_args_and_warn(parser, other_args)
+        if not self.portfolio.empty:
+            val, hist = portfolio_model.convert_df(self.portfolio)
+            if not val.empty:
+                df = portfolio_model.get_allocation(
+                    val,
+                    hist,
+                    self.portfolio,
+                    ns_parser.limit,
+                    ns_parser.no_etf_positions,
+                )
+                portfolio_view.display_allocation(df, ns_parser.graph)
+            else:
+                console.print("Cannot generate a graph from an empty dataframe\n")
+        else:
+            console.print("Please add items to the portfolio\n")
