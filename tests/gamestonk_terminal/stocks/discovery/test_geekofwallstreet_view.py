964ad975934f10923d69dd3ef3ec360823c1c747
Colin Delahunty <colin99delahunty@gmail.com>
2022-01-26 09:31:56 -0500

    Fixed tests

--- tests/gamestonk_terminal/stocks/discovery/test_geekofwallstreet_view.py
+++ tests/gamestonk_terminal/stocks/discovery/test_geekofwallstreet_view.py
@@ -5,6 +5,7 @@ import pytest
 
 # IMPORTATION INTERNAL
 from gamestonk_terminal.stocks.discovery import geekofwallstreet_view
+from gamestonk_terminal import helper_funcs
 
 
 @pytest.mark.default_cassette("test_get_realtime_earnings")
@@ -12,6 +13,6 @@ from gamestonk_terminal.stocks.discovery import geekofwallstreet_view
 @pytest.mark.parametrize("use_tab", [True, False])
 def test_get_realtime_earnings(mocker, use_tab):
     mocker.patch.object(
-        target=geekofwallstreet_view.gtff, attribute="USE_TABULATE_DF", new=use_tab
+        target=helper_funcs.gtff, attribute="USE_TABULATE_DF", new=use_tab
     )
     geekofwallstreet_view.display_realtime_earnings(export="")
--- tests/gamestonk_terminal/stocks/fundamental_analysis/test_av_view.py
+++ tests/gamestonk_terminal/stocks/fundamental_analysis/test_av_view.py
@@ -7,6 +7,7 @@ import pytest
 
 # IMPORTATION INTERNAL
 from gamestonk_terminal.stocks.fundamental_analysis import av_view
+from gamestonk_terminal import helper_funcs
 
 
 @pytest.fixture(scope="module")
@@ -71,7 +72,7 @@ def vcr_config():
     [True, False],
 )
 def test_check_output(func, kwargs_dict, monkeypatch, use_tab):
-    monkeypatch.setattr(av_view.gtff, "USE_TABULATE_DF", use_tab)
+    monkeypatch.setattr(helper_funcs.gtff, "USE_TABULATE_DF", use_tab)
     getattr(av_view, func)(**kwargs_dict)
 
 
