1b515d911030f326cf63a172a46477784fc330ae
Minh Hoang <minh@Minhs-MBP.Home>
2022-01-27 17:09:40 +0000

    Add test for Sentiment Investor View & Model

---
+++ tests/gamestonk_terminal/common/behavioural_analysis/test_sentimentinvestor_view.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import pytest
+
+from gamestonk_terminal.common.behavioural_analysis import sentimentinvestor_view
+
+
+@pytest.mark.vcr
+@pytest.mark.record_stdout
+def test_display_historical():
+    sentimentinvestor_view.display_historical(
+        ticker="AAPL", start="2021-12-12", end="2021-12-15", export="", raw=True
+    )
+
+
+@pytest.mark.vcr
+@pytest.mark.record_stdout
+def test_display_trending():
+    sentimentinvestor_view.display_trending(
+        start=datetime(2021, 12, 21),
+        hour=9,
+        number=10,
+        limit=10,
+        export="",
+    )
