17aa934bcc7c51e2817820446cc2807899584589
Chavithra PARANA <chavithra@gmail.com>
2022-01-29 14:25:01 +0100

    Updating tests : linting

--- tests/gamestonk_terminal/cryptocurrency/defi/test_graph_model.py
+++ tests/gamestonk_terminal/cryptocurrency/defi/test_graph_model.py
@@ -30,17 +30,19 @@ def test_query_graph_status_400(mocker):
         ("get_last_uni_swaps", dict(limit=5)),
     ],
 )
-def test_call_func(func, kwargs, recorder):
+def test_call_func(func, kwargs):
     df = getattr(graph_model, func)(**kwargs)
+
     assert isinstance(df, pd.DataFrame)
     assert not df.empty
 
 
 @pytest.mark.vcr(filter_post_data_parameters=[("query", "MOCK_QUERY")])
-def test_get_uniswap_pool_recently_added(recorder):
+def test_get_uniswap_pool_recently_added():
     df = graph_model.get_uniswap_pool_recently_added(
         last_days=14, min_volume=100, min_liquidity=0, min_tx=100
     )
+
     assert isinstance(df, pd.DataFrame)
     assert not df.empty
 
