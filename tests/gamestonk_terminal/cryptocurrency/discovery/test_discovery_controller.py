dff161266d554c985c70648fc1340532922eee62
Chavithra PARANA <chavithra@gmail.com>
2022-01-30 11:43:24 +0100

    Updating tests : cryptocurrency/discovery

--- tests/gamestonk_terminal/cryptocurrency/discovery/test_discovery_controller.py
+++ tests/gamestonk_terminal/cryptocurrency/discovery/test_discovery_controller.py
@@ -126,7 +126,7 @@ def test_print_help():
     [
         ("", []),
         ("/help", ["quit", "quit", "help"]),
-        ("help/help", ["help"]),
+        ("help/help", ["help", "help"]),
         ("q", ["quit"]),
         ("h", []),
         (
