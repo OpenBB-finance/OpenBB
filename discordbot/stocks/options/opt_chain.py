20402f4b2e9313f759da13522aed0f2df5a64d80
teh_coderer <me@tehcoderer.com>
2022-01-31 15:10:46 -0500

    switch to yfinance
    and fix indention on min/max strike

--- discordbot/stocks/options/opt_chain.py
+++ discordbot/stocks/options/opt_chain.py
@@ -1,15 +1,15 @@
 import os
-import disnake
-from PIL import Image
-import df2img
-import numpy as np
-from discordbot.helpers import autocrop_image
 
-from gamestonk_terminal.stocks.options import tradier_model
+import df2img
 import discordbot.config_discordbot as cfg
-from discordbot.config_discordbot import gst_imgur
-from discordbot.config_discordbot import logger
+import disnake
+import numpy as np
+import pandas as pd
+from discordbot.config_discordbot import gst_imgur, logger
+from discordbot.helpers import autocrop_image
+from gamestonk_terminal.stocks.options import yfinance_model
 from menus.menu import Menu
+from PIL import Image
 
 
 async def chain_command(
@@ -18,44 +18,56 @@ async def chain_command(
     expiry: str = None,
     opt_type: str = None,
     min_sp: float = None,
-    max_sp: float = None
+    max_sp: float = None,
 ):
     """Show calls/puts for given ticker and expiration"""
     try:
         # Debug
         if cfg.DEBUG:
-            logger.debug("opt-chain %s %s %s %s %s", ticker, expiry, opt_type, min_sp, max_sp)
+            logger.debug(
+                "opt-chain %s %s %s %s %s", ticker, expiry, opt_type, min_sp, max_sp
+            )
 
         # Check for argument
         if not ticker:
             raise Exception("Stock ticker is required")
 
-        chains_df = tradier_model.get_option_chains(ticker, expiry)
+        dates = yfinance_model.option_expirations(ticker)
 
-        column_map = {"open_interest": "oi", "volume": "vol"}
-        columns = ["strike", "bid", "ask", "volume", "open_interest", "option_type"]
-        chains_df = chains_df[columns].rename(columns=column_map)
-        min_strike = np.percentile(chains_df["strike"], 1)
-        max_strike = np.percentile(chains_df["strike"], 100)
+        if not dates:
+            raise Exception("Stock ticker is invalid")
+
+        options = yfinance_model.get_option_chain(ticker, expiry)
+        calls_df = options.calls
+        puts_df = options.puts
+
+        column_map = {"openInterest": "oi", "volume": "vol", "impliedVolatility": "iv"}
+        columns = ["strike", "bid", "ask", "volume", "openInterest", "impliedVolatility"]
+
+        if opt_type == "Calls":
+            df = calls_df[columns].rename(columns=column_map)
+        if opt_type == "Puts":
+            df = puts_df[columns].rename(columns=column_map)
+
+        min_strike = np.percentile(df["strike"], 1)
+        max_strike = np.percentile(df["strike"], 100)
 
         if min_sp:
             min_strike = min_sp
         if max_sp:
             max_strike = max_sp
-        if min_sp > max_sp:  # type: ignore
-            min_sp, max_sp = max_strike, min_strike
+            if min_sp > max_sp:  # type: ignore
+                min_sp, max_sp = max_strike, min_strike
 
-        chains_df = chains_df[chains_df["strike"] >= min_strike]
-        chains_df = chains_df[chains_df["strike"] <= max_strike]
+        df = df[df["strike"] >= min_strike]
+        df = df[df["strike"] <= max_strike]
 
-        calls_df = chains_df[chains_df.option_type == "call"].drop(columns=["option_type"])
-        puts_df = chains_df[chains_df.option_type == "put"].drop(columns=["option_type"])
-        calls_df.set_index("strike", inplace=True)
-        puts_df.set_index("strike", inplace=True)
-        if opt_type == "Calls":
-            df = calls_df
-        if opt_type == "Puts":
-            df = puts_df
+        df["iv"] = pd.to_numeric(df["iv"].astype(float))
+
+        formats = {"iv": "{:.2f}"}
+        for col, f in formats.items():
+            df[col] = df[col].map(lambda x: f.format(x))
+        df.set_index("strike", inplace=True)
 
         title = f"Stocks: {opt_type} Option Chain for {ticker.upper()} on {expiry} [yfinance]"
 
@@ -64,8 +76,8 @@ async def chain_command(
         i, i2, end = 0, 0, 20
         df_pg = []
         embeds_img = []
-        dindex = len(calls_df.index)
-        while i <= dindex:
+        dindex = len(df.index)
+        while i < dindex:
             df_pg = df.iloc[i:end]
             df_pg.append(df_pg)
             figp = df2img.plot_dataframe(
@@ -80,7 +92,7 @@ async def chain_command(
                     size=20,
                 ),
                 template="plotly_dark",
-                paper_bgcolor="rgba(0, 0, 0, 0)"
+                paper_bgcolor="rgba(0, 0, 0, 0)",
             )
             imagefile = f"opt-chain{i}.png"
 
@@ -88,6 +100,7 @@ async def chain_command(
             image = Image.open(imagefile)
             image = autocrop_image(image, 0)
             image.save(imagefile, "PNG", quality=100)
+
             uploaded_image = gst_imgur.upload_image(imagefile, title="something")
             image_link = uploaded_image.link
             embeds_img.append(
@@ -123,7 +136,7 @@ async def chain_command(
             i += 1
         embeds[0].set_footer(text=f"Page 1 of {len(embeds)}")
         options = [
-            disnake.SelectOption(label="Home", value='0', emoji="🟢"),
+            disnake.SelectOption(label="Home", value="0", emoji="🟢"),
         ]
 
         await ctx.send(embed=embeds[0], view=Menu(embeds, options))
