5a43898858f48f1ae89c9eff7d3d2ab0ec1e74f9
Colin Delahunty <colin99delahunty@gmail.com>
2022-01-31 10:27:20 -0500

    Fixed some tests

--- tests/gamestonk_terminal/cryptocurrency/discovery/test_dappradar_model.py
+++ tests/gamestonk_terminal/cryptocurrency/discovery/test_dappradar_model.py
@@ -38,6 +38,7 @@ def test__make_request_status_400(mocker):
     # MOCK GET
     attrs = {
         "status_code": 400,
+        "text": "MOCK_TEXT",
     }
     mock_response = mocker.Mock(**attrs)
     mocker.patch(target="requests.get", new=mocker.Mock(return_value=mock_response))
@@ -46,10 +47,12 @@ def test__make_request_status_400(mocker):
 
 
 @pytest.mark.vcr(record_mode="none")
+@pytest.mark.record_stdout
 def test__make_request_value_error(mocker):
     # MOCK GET
     attrs = {
         "status_code": 200,
+        "text": "MOCK_TEXT",
         "json.side_effect": UnicodeDecodeError,
     }
     mock_response = mocker.Mock(**attrs)
