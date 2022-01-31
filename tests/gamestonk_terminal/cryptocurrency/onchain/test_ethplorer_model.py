9aa843be21606c4eb119359c60460e667802e505
Chavithra PARANA <chavithra@gmail.com>
2022-02-01 00:54:15 +0100

    Updating tests : cryptocurrency/onchain

--- tests/gamestonk_terminal/cryptocurrency/onchain/test_ethplorer_model.py
+++ tests/gamestonk_terminal/cryptocurrency/onchain/test_ethplorer_model.py
@@ -31,12 +31,6 @@ from gamestonk_terminal.cryptocurrency.onchain import ethplorer_model
         ),
         ("get_token_info", dict(address="0x1f9840a85d5af5bf1d1762f925bdaddc4201f984")),
         (
-            "get_tx_info",
-            dict(
-                tx_hash="0x9dc7b43ad4288c624fdd236b2ecb9f2b81c93e706b2ffd1d19b112c1df7849e6"
-            ),
-        ),
-        (
             "get_token_history",
             dict(address="0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"),
         ),
@@ -56,3 +50,13 @@ def test_call_func(func, kwargs, recorder):
         recorder.capture_list(result)
     else:
         recorder.capture(result)
+
+
+@pytest.mark.vcr
+def test_get_tx_info():
+    df = ethplorer_model.get_tx_info(
+        tx_hash="0x9dc7b43ad4288c624fdd236b2ecb9f2b81c93e706b2ffd1d19b112c1df7849e6",
+    )
+
+    assert isinstance(df, pd.DataFrame)
+    assert not df.empty
