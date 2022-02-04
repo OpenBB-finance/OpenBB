b189ffa45dff708d172044e588fafbf8e5bcbc22
Colin Delahunty <72827203+colin99d@users.noreply.github.com>
2022-02-04 09:15:38 -0500

    Fix bugs (#1310)
    * Added model fix
    
    * Fixed nasdaq view issue
    
    * Fixed fred_view bug
    
    * Fixed screener view bug
    
    * Improved oanda_model excption handling
    
    * Fixed scripts
    
    * Improved run_scripts

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,5 +1,7 @@
 for f in scripts/*.gst; do
-  python terminal.py "$f" || break  # execute successfully or break
-  # Or more explicitly: if this execution fails, then stop the `for`:
-  # if ! bash "$f"; then break; fi
+  if [[ "$f" == *"$1"*  ]] || [ -z "$1" ]; then
+    python terminal.py "$f" || break  # execute successfully or break
+    # Or more explicitly: if this execution fails, then stop the `for`:
+    # if ! bash "$f"; then break; fi
+  fi
 done
