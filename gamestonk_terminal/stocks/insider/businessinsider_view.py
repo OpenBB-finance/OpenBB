36e8dac4e81e4842de2063cacade960531dd176f
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-02-05 18:13:26 +0000

    fix issue with empty dataframe and formatting

--- gamestonk_terminal/stocks/insider/businessinsider_view.py
+++ gamestonk_terminal/stocks/insider/businessinsider_view.py
@@ -54,128 +54,134 @@ def insider_activity(
     """
     df_ins = businessinsider_model.get_insider_activity(ticker)
 
-    if start:
-        df_insider = df_ins[start:].copy()  # type: ignore
+    if df_ins.empty:
+        console.print("[red]The insider activity on the ticker does not exist.\n[/red]")
     else:
-        df_insider = df_ins.copy()
+        if start:
+            df_insider = df_ins[start:].copy()  # type: ignore
+        else:
+            df_insider = df_ins.copy()
 
-    if raw:
-        df_insider.index = pd.to_datetime(df_insider.index).date
+        if raw:
+            df_insider.index = pd.to_datetime(df_insider.index).date
 
-        print_rich_table(
-            df_insider.sort_index(ascending=False)
-            .head(n=num)
-            .applymap(lambda x: x.replace(".00", "").replace(",", "")),
-            headers=list(df_insider.columns),
-            show_index=True,
-            title="Insider Activity",
-        )
-
-    else:
-        _, ax = plt.subplots()
-
-        if interval == "1440min":
-            plt.plot(stock.index, stock["Adj Close"].values, lw=3)
-        else:  # Intraday
-            plt.plot(stock.index, stock["Close"].values, lw=3)
-
-        plt.title(f"{ticker.upper()} (Time Series) and Price Target")
-
-        plt.xlabel("Time")
-        plt.ylabel("Share Price")
-
-        df_insider["Trade"] = df_insider.apply(
-            lambda row: (1, -1)[row.Type == "Sell"]
-            * float(row["Shares Traded"].replace(",", "")),
-            axis=1,
-        )
-        plt.xlim(df_insider.index[0], stock.index[-1])
-        min_price, max_price = ax.get_ylim()
-
-        price_range = max_price - min_price
-        shares_range = (
-            df_insider[df_insider["Type"] == "Buy"]
-            .groupby(by=["Date"])
-            .sum()["Trade"]
-            .max()
-            - df_insider[df_insider["Type"] == "Sell"]
-            .groupby(by=["Date"])
-            .sum()["Trade"]
-            .min()
-        )
-        n_proportion = price_range / shares_range
-
-        for ind in (
-            df_insider[df_insider["Type"] == "Sell"].groupby(by=["Date"]).sum().index
-        ):
-            if ind in stock.index:
-                ind_dt = ind
-            else:
-                ind_dt = get_next_stock_market_days(ind, 1)[0]
-
-            n_stock_price = 0
-            if interval == "1440min":
-                n_stock_price = stock["Adj Close"][ind_dt]
-            else:
-                n_stock_price = stock["Close"][ind_dt]
-
-            plt.vlines(
-                x=ind_dt,
-                ymin=n_stock_price
-                + n_proportion
-                * float(
-                    df_insider[df_insider["Type"] == "Sell"]
-                    .groupby(by=["Date"])
-                    .sum()["Trade"][ind]
-                ),
-                ymax=n_stock_price,
-                colors="red",
-                ls="-",
-                lw=5,
+            print_rich_table(
+                df_insider.sort_index(ascending=False)
+                .head(n=num)
+                .applymap(lambda x: x.replace(".00", "").replace(",", "")),
+                headers=list(df_insider.columns),
+                show_index=True,
+                title="Insider Activity",
             )
 
-        for ind in (
-            df_insider[df_insider["Type"] == "Buy"].groupby(by=["Date"]).sum().index
-        ):
-            if ind in stock.index:
-                ind_dt = ind
-            else:
-                ind_dt = get_next_stock_market_days(ind, 1)[0]
+        else:
+            _, ax = plt.subplots()
 
-            n_stock_price = 0
             if interval == "1440min":
-                n_stock_price = stock["Adj Close"][ind_dt]
-            else:
-                n_stock_price = stock["Close"][ind_dt]
+                plt.plot(stock.index, stock["Adj Close"].values, lw=3)
+            else:  # Intraday
+                plt.plot(stock.index, stock["Close"].values, lw=3)
 
-            plt.vlines(
-                x=ind_dt,
-                ymin=n_stock_price,
-                ymax=n_stock_price
-                + n_proportion
-                * float(
-                    df_insider[df_insider["Type"] == "Buy"]
-                    .groupby(by=["Date"])
-                    .sum()["Trade"][ind]
-                ),
-                colors="green",
-                ls="-",
-                lw=5,
+            plt.title(f"{ticker.upper()} (Time Series) and Price Target")
+
+            plt.xlabel("Time")
+            plt.ylabel("Share Price")
+
+            df_insider["Trade"] = df_insider.apply(
+                lambda row: (1, -1)[row.Type == "Sell"]
+                * float(row["Shares Traded"].replace(",", "")),
+                axis=1,
             )
+            plt.xlim(df_insider.index[0], stock.index[-1])
+            min_price, max_price = ax.get_ylim()
 
-        plt.grid(b=True, which="major", color="#666666", linestyle="-")
-        plt.gcf().autofmt_xdate()
+            price_range = max_price - min_price
+            shares_range = (
+                df_insider[df_insider["Type"] == "Buy"]
+                .groupby(by=["Date"])
+                .sum()["Trade"]
+                .max()
+                - df_insider[df_insider["Type"] == "Sell"]
+                .groupby(by=["Date"])
+                .sum()["Trade"]
+                .min()
+            )
+            n_proportion = price_range / shares_range
 
-        if gtff.USE_ION:
-            plt.ion()
+            for ind in (
+                df_insider[df_insider["Type"] == "Sell"]
+                .groupby(by=["Date"])
+                .sum()
+                .index
+            ):
+                if ind in stock.index:
+                    ind_dt = ind
+                else:
+                    ind_dt = get_next_stock_market_days(ind, 1)[0]
 
-        plt.show()
+                n_stock_price = 0
+                if interval == "1440min":
+                    n_stock_price = stock["Adj Close"][ind_dt]
+                else:
+                    n_stock_price = stock["Close"][ind_dt]
 
-    console.print("")
+                plt.vlines(
+                    x=ind_dt,
+                    ymin=n_stock_price
+                    + n_proportion
+                    * float(
+                        df_insider[df_insider["Type"] == "Sell"]
+                        .groupby(by=["Date"])
+                        .sum()["Trade"][ind]
+                    ),
+                    ymax=n_stock_price,
+                    colors="red",
+                    ls="-",
+                    lw=5,
+                )
 
-    export_data(
-        export,
-        os.path.dirname(os.path.abspath(__file__)),
-        "act",
-        df_insider,
-    )
+            for ind in (
+                df_insider[df_insider["Type"] == "Buy"].groupby(by=["Date"]).sum().index
+            ):
+                if ind in stock.index:
+                    ind_dt = ind
+                else:
+                    ind_dt = get_next_stock_market_days(ind, 1)[0]
+
+                n_stock_price = 0
+                if interval == "1440min":
+                    n_stock_price = stock["Adj Close"][ind_dt]
+                else:
+                    n_stock_price = stock["Close"][ind_dt]
+
+                plt.vlines(
+                    x=ind_dt,
+                    ymin=n_stock_price,
+                    ymax=n_stock_price
+                    + n_proportion
+                    * float(
+                        df_insider[df_insider["Type"] == "Buy"]
+                        .groupby(by=["Date"])
+                        .sum()["Trade"][ind]
+                    ),
+                    colors="green",
+                    ls="-",
+                    lw=5,
+                )
+
+            plt.grid(b=True, which="major", color="#666666", linestyle="-")
+            plt.gcf().autofmt_xdate()
+
+            if gtff.USE_ION:
+                plt.ion()
+
+            plt.show()
+
+        console.print("")
+
+        export_data(
+            export,
+            os.path.dirname(os.path.abspath(__file__)),
+            "act",
+            df_insider,
+        )
