3ce08deed3fceeceeaf210fc89c8bd6f1d2f19b0
Theodore Aptekarev <aptekarev@gmail.com>
2022-02-09 01:18:03 +0300

    Plot updates in common BA (#1335)
    * Add external axes support to common/ba/finbrain
    
    * Add external axes support to common/ba/twitter
    
    * Add external axes support to common/ba/google
    
    * Add external axes support to common/ba/sentimentinvestor
    
    * Add sentimentinvestor to the notebooks API
    
    * Fix tests

--- gamestonk_terminal/common/behavioural_analysis/twitter_view.py
+++ gamestonk_terminal/common/behavioural_analysis/twitter_view.py
@@ -3,6 +3,7 @@ __docformat__ = "numpy"
 
 import logging
 import os
+from typing import Optional, List
 from datetime import datetime, timedelta
 
 import matplotlib.pyplot as plt
@@ -11,7 +12,7 @@ import pandas as pd
 from dateutil import parser as dparse
 
 import gamestonk_terminal.config_plot as cfg_plot
-from gamestonk_terminal import feature_flags as gtff
+from gamestonk_terminal.config_terminal import theme
 from gamestonk_terminal.common.behavioural_analysis import twitter_model
 from gamestonk_terminal.decorators import log_start_end
 from gamestonk_terminal.helper_funcs import export_data, plot_autoscale
@@ -69,7 +70,13 @@ def display_inference(ticker: str, num: int, export: str = ""):
 
 
 @log_start_end(log=logger)
-def display_sentiment(ticker: str, n_tweets: int, n_days_past: int, export: str = ""):
+def display_sentiment(
+    ticker: str,
+    n_tweets: int,
+    n_days_past: int,
+    export: str = "",
+    external_axes: Optional[List[plt.Axes]] = None,
+):
     """Plot sentiments from ticker
 
     Parameters
@@ -84,10 +91,10 @@ def display_sentiment(ticker: str, n_tweets: int, n_days_past: int, export: str 
         Format to export tweet dataframe
     """
     # Date format string required by twitter
-    dtformat = "%Y-%m-%dT%H:%M:%SZ"
+    dt_format = "%Y-%m-%dT%H:%M:%SZ"
 
     # Algorithm to extract
-    dt_recent = datetime.now() - timedelta(seconds=20)
+    dt_recent = datetime.utcnow() - timedelta(seconds=20)
     dt_old = dt_recent - timedelta(days=n_days_past)
     console.print(
         f"From {dt_recent.date()} retrieving {n_tweets*24} tweets ({n_tweets} tweets/hour)"
@@ -113,8 +120,8 @@ def display_sentiment(ticker: str, n_tweets: int, n_days_past: int, export: str 
         temp = twitter_model.load_analyze_tweets(
             ticker,
             n_tweets,
-            start_time=dt_past.strftime(dtformat),
-            end_time=dt_recent.strftime(dtformat),
+            start_time=dt_past.strftime(dt_format),
+            end_time=dt_recent.strftime(dt_format),
         )
 
         if temp.empty:
@@ -144,44 +151,56 @@ def display_sentiment(ticker: str, n_tweets: int, n_days_past: int, export: str 
     df_tweets["date"] = pd.to_datetime(df_tweets["created_at"])
     df_tweets = df_tweets.sort_values(by="date")
     df_tweets["cumulative_compound"] = df_tweets["sentiment"].cumsum()
-    _, ax = plt.subplots(2, 1, figsize=plot_autoscale(), dpi=cfg_plot.PLOT_DPI)
-    ax[0].plot(
+
+    # This plot has 2 axis
+    if external_axes is None:
+        _, axes = plt.subplots(
+            2, 1, sharex=True, figsize=plot_autoscale(), dpi=cfg_plot.PLOT_DPI
+        )
+        ax1, ax2 = axes
+    else:
+        if len(external_axes) != 2:
+            console.print("[red]Expected list of one axis item./n[/red]")
+            return
+        (ax1, ax2) = external_axes
+
+    ax1.plot(
         pd.to_datetime(df_tweets["created_at"]),
         df_tweets["cumulative_compound"].values,
-        lw=3,
-        c="cyan",
     )
-    ax[0].set_ylabel("Cumulative VADER Sentiment")
-    xlocations = []
-    xlabels = []
+    ax1.set_ylabel("\nCumulative\nVADER Sentiment")
     for _, day_df in df_tweets.groupby(by="Day"):
         day_df["time"] = pd.to_datetime(day_df["created_at"])
         day_df = day_df.sort_values(by="time")
-        ax[0].plot(day_df["time"], day_df["sentiment"].cumsum(), c="tab:blue")
-        xlocations.append(day_df.time.values[0])
-        xlabels.append(day_df["time"].apply(lambda x: x.strftime("%m-%d")).values[0])
-
-        ax[1].bar(df_tweets["date"], df_tweets["positive"], color="green", width=0.02)
-    ax[1].bar(df_tweets["date"], -1 * df_tweets["negative"], color="red", width=0.02)
-    ax[0].grid(b=True, which="major", color="#666666", linestyle="-", lw=1.5, alpha=0.5)
-    ax[0].minorticks_on()
-    ax[0].grid(b=True, which="minor", color="#999999", linestyle="-", alpha=0.2)
-    ax[0].set_xticks(xlocations)
-    ax[0].set_xticklabels(xlabels)
-
-    ax[1].grid(b=True, which="major", color="#666666", linestyle="-", lw=1.5, alpha=0.5)
-    ax[1].minorticks_on()
-    ax[1].grid(b=True, which="minor", color="#999999", linestyle="-", alpha=0.2)
-    ax[1].set_ylabel("VADER Polarity Scores")
-    ax[1].set_xticks(xlocations)
-    ax[1].set_xticklabels(xlabels)
-    plt.suptitle(
+        ax1.plot(
+            day_df["time"],
+            day_df["sentiment"].cumsum(),
+            label=pd.to_datetime(day_df["date"]).iloc[0].strftime("%Y-%m-%d"),
+        )
+        ax2.bar(
+            df_tweets["date"],
+            df_tweets["positive"],
+            color=theme.up_color,
+            width=theme.volume_bar_width / 100,
+        )
+    ax2.bar(
+        df_tweets["date"],
+        -1 * df_tweets["negative"],
+        color=theme.down_color,
+        width=theme.volume_bar_width / 100,
+    )
+    ax1.set_title(
         f"Twitter's {ticker} total compound sentiment over time is {round(np.sum(df_tweets['sentiment']), 2)}"
     )
-    if gtff.USE_ION:
-        plt.ion()
-    plt.show()
-    console.print("")
+    ax1.legend()
+    theme.style_primary_axis(ax1)
+
+    ax2.set_ylabel("VADER Polarity Scores")
+    theme.style_primary_axis(ax2)
+
+    if external_axes is None:
+        theme.visualize_output()
+
     export_data(
         export, os.path.dirname(os.path.abspath(__file__)), "sentiment", df_tweets
     )
