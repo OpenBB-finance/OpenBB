bbc842315426195021449d4d42c5a5b2fc23e188
teh_coderer <me@tehcoderer.com>
2022-02-09 15:37:26 -0500

    ta + image border helper

--- bots/stocks/technical_analysis/recom.py
+++ bots/stocks/technical_analysis/recom.py
@@ -1,51 +1,26 @@
-import disnake
-
 import bots.config_discordbot as cfg
 from bots.config_discordbot import logger
 from gamestonk_terminal.stocks.technical_analysis import tradingview_model
 
 
-async def recom_command(ctx, ticker=""):
+async def recom_command(ticker=""):
     """Displays text of a given stocks recommendation based on ta [Tradingview API]"""
 
-    try:
+    # Debug
+    if cfg.DEBUG:
+        logger.debug("!stocks.ta.recom %s", ticker)
 
-        # Debug
-        if cfg.DEBUG:
-            logger.debug("!stocks.ta.recom %s", ticker)
+    # Check for argument
+    if ticker == "":
+        raise Exception("Stock ticker is required")
 
-        # Check for argument
-        if ticker == "":
-            raise Exception("Stock ticker is required")
+    recom = tradingview_model.get_tradingview_recommendation(ticker, "america", "", "")
 
-        recom = tradingview_model.get_tradingview_recommendation(
-            ticker, "america", "", ""
-        )
+    recom = recom[["BUY", "NEUTRAL", "SELL", "RECOMMENDATION"]]
 
-        recom = recom[["BUY", "NEUTRAL", "SELL", "RECOMMENDATION"]]
+    report = "```" + recom.to_string() + "```"
 
-        report = "```" + recom.to_string() + "```"
-        embed = disnake.Embed(
-            title="Stocks: [Tradingview API] Recommendation based on TA",
-            description=report,
-            colour=cfg.COLOR,
-        )
-        embed.set_author(
-            name=cfg.AUTHOR_NAME,
-            icon_url=cfg.AUTHOR_ICON_URL,
-        )
-
-        await ctx.send(embed=embed)
-
-    except Exception as e:
-        embed = disnake.Embed(
-            title="ERROR Stocks: [Tradingview API] Recommendation based on TA",
-            colour=cfg.COLOR,
-            description=e,
-        )
-        embed.set_author(
-            name=cfg.AUTHOR_NAME,
-            icon_url=cfg.AUTHOR_ICON_URL,
-        )
-
-        await ctx.send(embed=embed, delete_after=30.0)
+    return {
+        "title": "Stocks: [Tradingview API] Recommendation based on TA",
+        "description": report,
+    }
--- bots/stocks/technical_analysis/summary.py
+++ bots/stocks/technical_analysis/summary.py
@@ -1,47 +1,24 @@
-import disnake
-
 import bots.config_discordbot as cfg
 from bots.config_discordbot import logger
 from gamestonk_terminal.stocks.technical_analysis import finbrain_model
 
 
-async def summary_command(ctx, ticker=""):
+async def summary_command(ticker=""):
     """Displays text of a given stocks ta summary [FinBrain API]"""
 
-    try:
+    # Debug
+    if cfg.DEBUG:
+        logger.debug("!stocks.ta.summary %s", ticker)
 
-        # Debug
-        if cfg.DEBUG:
-            logger.debug("!stocks.ta.summary %s", ticker)
+    # Check for argument
+    if ticker == "":
+        raise Exception("Stock ticker is required")
 
-        # Check for argument
-        if ticker == "":
-            raise Exception("Stock ticker is required")
+    report = finbrain_model.get_technical_summary_report(ticker)
+    if report:
+        report = "```" + report.replace(". ", ".\n") + "```"
 
-        report = finbrain_model.get_technical_summary_report(ticker)
-        if report:
-            report = "```" + report.replace(". ", ".\n") + "```"
-        embed = disnake.Embed(
-            title="Stocks: [FinBrain API] Summary",
-            description=report,
-            colour=cfg.COLOR,
-        )
-        embed.set_author(
-            name=cfg.AUTHOR_NAME,
-            icon_url=cfg.AUTHOR_ICON_URL,
-        )
-
-        await ctx.send(embed=embed)
-
-    except Exception as e:
-        embed = disnake.Embed(
-            title="ERROR Stocks: [FinBrain API] Summary",
-            colour=cfg.COLOR,
-            description=e,
-        )
-        embed.set_author(
-            name=cfg.AUTHOR_NAME,
-            icon_url=cfg.AUTHOR_ICON_URL,
-        )
-
-        await ctx.send(embed=embed, delete_after=30.0)
+    return {
+        "title": "Stocks: [FinBrain API] Summary",
+        "description": report,
+    }
