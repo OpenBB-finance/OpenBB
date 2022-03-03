7e6eaa394537010f53173a6db81555e17114da3b
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-03-03 14:13:47 +0000

    add tests for stocks/ba/snews

--- gamestonk_terminal/stocks/behavioural_analysis/ba_api.py
+++ gamestonk_terminal/stocks/behavioural_analysis/ba_api.py
@@ -66,6 +66,6 @@ from gamestonk_terminal.common.behavioural_analysis.sentimentinvestor_view impor
     display_trending as trend,
 )
 
-from gamestonk_terminal.common.behavioural_analysis.finnhub_view import (
-    display_sentiment_stats as snews,
+from gamestonk_terminal.stocks.behavioural_analysis.finnhub_view import (
+    display_stock_price_headlines_sentiment as snews,
 )
