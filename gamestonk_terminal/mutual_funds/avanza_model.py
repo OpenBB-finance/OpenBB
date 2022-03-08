100818a1b2f1d8e63525022207f908e2657cd5eb
northern-64bit <75195383+northern-64bit@users.noreply.github.com>
2022-03-08 19:08:02 +0100

    Adding info_se and al_swe commands

---
+++ gamestonk_terminal/mutual_funds/avanza_model.py
@@ -0,0 +1,33 @@
+"""Avanza Model"""
+__docformat__ = "numpy"
+
+import logging
+import requests
+import os
+
+import pandas as pd
+
+from gamestonk_terminal.decorators import log_start_end
+from gamestonk_terminal.rich_config import console
+
+logger = logging.getLogger(__name__)
+
+
+@log_start_end(log=logger)
+def get_data(fund_name: str):
+    """Gets the data from Avanza
+
+    Parameters
+    ----------
+    fund_name: str
+        Full name of the fund
+    """
+    ava_fund = pd.read_csv(
+        os.path.join("gamestonk_terminal", "mutual_funds", "avanza_fund_ID.csv"),
+        index_col=0,
+    )
+    fund_id = ava_fund.loc[fund_name, "ID"]
+    url = f"https://www.avanza.se/_api/fund-guide/guide/{fund_id}"
+    response = requests.get(url)
+    fund_data = response.json()
+    return fund_data
