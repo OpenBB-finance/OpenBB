efdaaa1cf31025b0b9f33de9bb0ec2541f5f2935
Jeroen Bouma <jer.bouma@gmail.com>
2022-03-21 12:05:19 +0100

    Improve labelling and some minor fixes

--- tests/gamestonk_terminal/economy/test_econdb_view.py
+++ tests/gamestonk_terminal/economy/test_econdb_view.py
@@ -11,15 +11,10 @@ from gamestonk_terminal.economy import econdb_view, econdb_model
     [
         [["RGDP"], ["United States", "Germany"], "2020-01-01", "2020-10-02", False],
         [["EMP", "PPI"], ["France"], "2010-01-01", "2019-01-01", False],
-        [["GDP, RGDP"], ["Italy", "Netherlands"], "2016-01-01", "2016-10-10", False],
+        [["GDP", "RGDP"], ["Italy", "Netherlands"], "2016-01-01", "2016-10-10", False],
     ],
 )
-def test_show_macro_data(
-    parameters, mocker, countries, start_date, end_date, convert_currency
-):
-    mocker.patch(target="gamestonk_terminal.economy.econdb_view.theme.visualize_output")
-    # MOCK EXPORT_DATA
-    mocker.patch(target="gamestonk_terminal.economy.econdb_view.export_data")
+def test_show_macro_data(parameters, countries, start_date, end_date, convert_currency):
     econdb_view.show_macro_data(
         parameters, countries, start_date, end_date, convert_currency
     )
@@ -35,12 +30,7 @@ def test_show_macro_data(
         [["average", "inflation"], ["3y", "5y"], "weekly", "2018-06-05", "2018-07-06"],
     ],
 )
-def test_show_treasuries(
-    instruments, mocker, maturities, frequency, start_date, end_date
-):
-    mocker.patch(target="gamestonk_terminal.economy.econdb_view.theme.visualize_output")
-    # MOCK EXPORT_DATA
-    mocker.patch(target="gamestonk_terminal.economy.econdb_view.export_data")
+def test_show_treasuries(instruments, maturities, frequency, start_date, end_date):
     econdb_view.show_treasuries(
         instruments, maturities, frequency, start_date, end_date
     )
