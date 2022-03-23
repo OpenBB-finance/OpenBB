cc665e76bd5bdc33ae848ecc09d155c68c79c9f5
Chavithra PARANA <chavithra@gmail.com>
2022-03-23 11:29:20 +0100

    Logging removing : unlink

--- gamestonk_terminal/log/collection/s3_sender.py
+++ gamestonk_terminal/log/collection/s3_sender.py
@@ -80,7 +80,8 @@ def send_to_s3(archives_file: Path, file: Path, object_key: str, tmp_file: Path)
     if not file.stat().st_size > 0:
         raise AttributeError(f"File is empty : {file}")
     else:
-        file.unlink(missing_ok=True)
+        # file.unlink(missing_ok=True)
+        pass
 
     os.makedirs(tmp_file.parent, exist_ok=True)
     file = file.rename(tmp_file)
