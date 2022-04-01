53a87876a8f9db41ea00baf56185b64b321f7b9b
Chavithra PARANA <chavithra@gmail.com>
2022-04-01 16:39:37 +0200

    Bots : test_run_telegram

--- tests/bots/telegram/test_run_telegram.py
+++ tests/bots/telegram/test_run_telegram.py
@@ -5,15 +5,9 @@ class MockTelegram:
     def set_my_commands(self, commands):
         print(commands)
 
-
-# TODO: Ask Chavi for help
-@pytest.mark.skip
-@pytest.mark.record_stdout
 def test_detect_valid_command(mocker):
     # mocker.patch.object(telebot.TeleBot, MockTelegram())
-    mocker.patch(
-        "bots.telegram.run_telegram.telebot.set_my_commands", return_value=True
-    )
+    mocker.patch("telebot.TeleBot")
     from bots.telegram.run_telegram import detect_valid_command
 
     assert detect_valid_command("hello") is False
