35be8a97f3ed58ae554aef04aab062aa1fbd3439
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-04-02 22:19:12 +0100

    black formatter

--- tests/openbb_terminal/stocks/test_stocks_helper.py
+++ tests/openbb_terminal/stocks/test_stocks_helper.py
@@ -40,7 +40,15 @@ def test_search(mocker, use_tab):
     mocker.patch.object(
         target=helper_funcs.obbff, attribute="USE_TABULATE_DF", new=use_tab
     )
-    stocks_helper.search(query="sonae", country="Portugal", sector="", industry="", exchange_country="", amount=5, export="")
+    stocks_helper.search(
+        query="sonae",
+        country="Portugal",
+        sector="",
+        industry="",
+        exchange_country="",
+        amount=5,
+        export="",
+    )
 
 
 @pytest.mark.vcr
