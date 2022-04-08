7dc0dcd3d063dc3616794408cd4a3ed4a4fa4f2f
colin99d <colin99delahunty@gmail.com>
2022-04-08 11:52:05 -0400

    Basic tests

--- tests/bots/test_run_discordbot.py
+++ tests/bots/test_run_discordbot.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 # try:
@@ -5,16 +7,28 @@ import pytest
 #    pytest.skip(allow_module_level=True)
 
 
+@pytest.fixture(autouse=True)
+def change_test_dir(monkeypatch):
+    cwd = os.getcwd()
+    print(cwd)
+    monkeypatch.chdir(cwd + "/bots")
+    print(os.getcwd())
+
+
 @pytest.mark.bots
-def test_read_root(mocker):
-    mocker.patch("bots.run_discordbot.gst_bot")
+def test_read_root(mocker, request):
+    mocker.patch("bots.run_discordbot.GSTBot", return_value=None)
+    cwd = os.getcwd()
+    os.chdir(cwd + "/bots")
     from bots import run_discordbot as rdbot
 
     value = rdbot.read_root()
 
     assert "Hello" in value
+    os.chdir(request.config.invocation_dir)
 
 
+@pytest.mark.skip
 @pytest.mark.bots
 def test_hash_user_id(mocker):
     mocker.patch("bots.run_discordbot.gst_bot")
