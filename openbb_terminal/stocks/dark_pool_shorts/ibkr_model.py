850dc139050e8474d81251a819b67db02b5f35bd
spartan737 <tanguanquan@yahoo.com>
2022-04-09 13:15:26 +0800

    add ctb to dps

---
+++ openbb_terminal/stocks/dark_pool_shorts/ibkr_model.py
@@ -0,0 +1,38 @@
+""" Interactive Broker Model """
+__docformat__ = "numpy"
+
+import logging
+
+import ftplib
+from io import BytesIO
+import pandas as pd
+
+from openbb_terminal.decorators import log_start_end
+
+logger = logging.getLogger(__name__)
+
+
+@log_start_end(log=logger)
+def get_cost_to_borrow() -> pd.DataFrame:
+    """Get stocks with highest cost to borrow [Source: Interactive Broker]
+
+    Returns
+    -------
+    pd.DataFrame
+        Cost to borrow
+    """
+    ftp = ftplib.FTP("ftp3.interactivebrokers.com", "shortstock")
+
+    flo = BytesIO()
+    ftp.retrbinary("RETR usa.txt", flo.write)
+    flo.seek(0)
+
+    data = pd.read_csv(flo, sep="|", skiprows=1)
+    data = data[["#SYM", "FEERATE", "AVAILABLE"]]
+    data["AVAILABLE"] = data["AVAILABLE"].replace(">10000000", 10000000)
+    data.fillna(0, inplace=True)
+    data["AVAILABLE"] = data["AVAILABLE"].astype(int)
+    data.sort_values(by=["FEERATE"], ascending=False, inplace=True)
+    data["FEERATE"] = data["FEERATE"].apply(lambda x: str(x) + "%")
+    data.columns = ["Symbol", "Fees", "Available"]
+    return data
---
+++ openbb_terminal/stocks/dark_pool_shorts/ibkr_view.py
@@ -0,0 +1,44 @@
+""" Interactive Broker View """
+__docformat__ = "numpy"
+
+import logging
+import os
+
+from openbb_terminal.decorators import log_start_end
+from openbb_terminal.helper_funcs import export_data, print_rich_table
+from openbb_terminal.rich_config import console
+from openbb_terminal.stocks.dark_pool_shorts import ibkr_model
+
+logger = logging.getLogger(__name__)
+
+
+@log_start_end(log=logger)
+def display_cost_to_borrow(num_stocks: int, export: str):
+    """Display stocks with highest cost to borrow. [Source: Interactive Broker]
+
+    Parameters
+    ----------
+    num_stocks: int
+        Number of stocks to display
+    export : str
+        Export dataframe data to csv,json,xlsx file
+    """
+    df = ibkr_model.get_cost_to_borrow().head(num_stocks)
+
+    if df.empty:
+        console.print("No data found.")
+    else:
+        print_rich_table(
+            df,
+            headers=list(df.columns),
+            show_index=False,
+            title="Highest Cost to Borrow",
+        )
+    console.print("")
+
+    export_data(
+        export,
+        os.path.dirname(os.path.abspath(__file__)),
+        "cost_to_borrow",
+        df,
+    )
