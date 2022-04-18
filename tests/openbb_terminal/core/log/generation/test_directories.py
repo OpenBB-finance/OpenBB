6e512beb5f0ef60fd5462100a4c76c1de8793c38
colin99d <colin99delahunty@gmail.com>
2022-04-17 21:30:53 -0400

    Finished test_directories

--- tests/openbb_terminal/core/log/generation/test_directories.py
+++ tests/openbb_terminal/core/log/generation/test_directories.py
@@ -2,15 +2,17 @@ import os
 from pathlib import Path
 from openbb_terminal.core.log.generation import directories
 
+# pylint: disable=too-few-public-methods
+
 
 class MockFilePath:
     def __init__(self, _):
         self.parent = Path(__file__)
 
 
-
-
 def test_get_log_dir(mocker):
     mocker.patch("openbb_terminal.core.log.generation.directories.Path", MockFilePath)
     path = directories.get_log_dir()
+    os.rmdir(path)
+    os.remove(path.parent.joinpath(".logid"))
     os.rmdir(path.parent)
