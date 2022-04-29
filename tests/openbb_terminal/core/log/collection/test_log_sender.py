8bcd37c53f38cf91501cf12b2c77788326c13087
colin99d <colin99delahunty@gmail.com>
2022-04-29 10:43:30 -0400

    refactored get settings to use tmp_path

--- tests/openbb_terminal/core/log/collection/test_log_sender.py
+++ tests/openbb_terminal/core/log/collection/test_log_sender.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from openbb_terminal.core.log.collection import log_sender
 from openbb_terminal.core.log.generation.settings import (
     Settings,
@@ -7,41 +6,44 @@ from openbb_terminal.core.log.generation.settings import (
     LogSettings,
 )
 
-settings = Settings(
-    app_settings=AppSettings(
-        commit_hash="MOCK_COMMIT_HASH",
-        name="MOCK_COMMIT_HASH",
-        identifier="MOCK_COMMIT_HASH",
-        session_id="MOCK_SESSION_ID",
-    ),
-    aws_settings=AWSSettings(
-        aws_access_key_id="MOCK_AWS_ACCESS_KEY_ID",
-        aws_secret_access_key="MOCK_AWS",  # pragma: allowlist secret
-    ),
-    log_settings=LogSettings(
-        directory=Path("."),
-        frequency="H",
-        handler_list="file",
-        rolling_clock=False,
-        verbosity=20,
-    ),
-)
+
+def get_settings(path):
+    settings = Settings(
+        app_settings=AppSettings(
+            commit_hash="MOCK_COMMIT_HASH",
+            name="MOCK_COMMIT_HASH",
+            identifier="MOCK_COMMIT_HASH",
+            session_id="MOCK_SESSION_ID",
+        ),
+        aws_settings=AWSSettings(
+            aws_access_key_id="MOCK_AWS_ACCESS_KEY_ID",
+            aws_secret_access_key="MOCK_AWS",  # pragma: allowlist secret
+        ),
+        log_settings=LogSettings(
+            directory=path,
+            frequency="H",
+            handler_list="file",
+            rolling_clock=False,
+            verbosity=20,
+        ),
+    )
+    return settings
 
 
-def test_queue_str():
-    log_sender.QueueItem(Path(".")).__str__()
+def test_queue_str(tmp_path):
+    log_sender.QueueItem(tmp_path).__str__()
 
 
-def test_sender_settings():
-    value = log_sender.LogSender(settings).settings
+def test_sender_settings(tmp_path):
+    value = log_sender.LogSender(get_settings(tmp_path)).settings
     assert value is not None
 
 
-def test_sender_fails():
-    value = log_sender.LogSender(settings).fails
+def test_sender_fails(tmp_path):
+    value = log_sender.LogSender(get_settings(tmp_path)).fails
     assert value is not None
 
 
-def test_sender_queue():
-    value = log_sender.LogSender(settings).queue
+def test_sender_queue(tmp_path):
+    value = log_sender.LogSender(get_settings(tmp_path)).queue
     assert value is not None
