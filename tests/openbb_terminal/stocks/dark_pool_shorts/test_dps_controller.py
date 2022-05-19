dbf5b7ab99b1e96d0e32bebdf41cff232b50887b
james <jmaslek11@gmail.com>
2022-05-19 13:42:52 -0400

    lint

--- tests/openbb_terminal/stocks/dark_pool_shorts/test_dps_controller.py
+++ tests/openbb_terminal/stocks/dark_pool_shorts/test_dps_controller.py
@@ -407,7 +407,7 @@ def test_call_func_expect_queue(expected_queue, queue, func):
         #         mpl=True,
         #         export="csv",
         #     ),
-        #),
+        # ),
     ],
 )
 def test_call_func(tested_func, mocked_func, other_args, called_with, mocker):
@@ -445,7 +445,7 @@ def test_call_func(tested_func, mocked_func, other_args, called_with, mocker):
         "call_dpotc",
         "call_ftd",
         "call_spos",
-#        "call_volexch",
+        #        "call_volexch",
     ],
 )
 def test_call_func_no_parser(func, mocker):
@@ -473,7 +473,7 @@ def test_call_func_no_parser(func, mocker):
         "call_ftd",
         "call_spos",
         "call_psi",
-#        "call_volexch",
+        #        "call_volexch",
     ],
 )
 def test_call_func_no_ticker(func, mocker):
