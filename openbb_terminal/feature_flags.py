09a921f2c5edf7b337770f72fd8d14e42e5cd780
Theodore Aptekarev <aptekarev@gmail.com>
2022-06-06 13:58:13 +0200

    Bump project version and update deps

--- openbb_terminal/feature_flags.py
+++ openbb_terminal/feature_flags.py
@@ -102,5 +102,5 @@ LOGGING_COMMIT_HASH = str(os.getenv("OPENBB_LOGGING_COMMIT_HASH", "REPLACE_ME"))
 try:
     version = pkg_resources.get_distribution("OpenBBTerminal").version
 except Exception:
-    version = "1.3.0m"
+    version = "1.4.0m"
 VERSION = str(os.getenv("OPENBB_VERSION", version))
