8b6ea1fb80fafd8553c7b0d66432ecc5bfd30bf6
Chavithra PARANA <chavithra@gmail.com>
2022-06-13 17:59:52 +0200

    Comparison Analysis controller

--- tests/openbb_terminal/stocks/comparison_analysis/test_ca_controller.py
+++ tests/openbb_terminal/stocks/comparison_analysis/test_ca_controller.py
@@ -471,7 +471,7 @@ def test_call_func(tested_func, mocked_func, other_args, called_with, mocker):
 )
 def test_call_func_no_parser(func, mocker):
     mocker.patch(
-        "openbb_terminal.stocks.comparison_analysis.ca_controller.parse_known_args_and_warn",
+        "openbb_terminal.stocks.comparison_analysis.ca_controller.ComparisonAnalysisController.parse_known_args_and_warn",
         return_value=None,
     )
     controller = ca_controller.ComparisonAnalysisController(
