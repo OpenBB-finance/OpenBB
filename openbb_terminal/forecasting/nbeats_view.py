974491a72edb2930bb6c410d044bd356a69e90d7
martinb-bb <martin.bufi@openbb.finance>
2022-06-21 13:12:35 -0400

    fix naming scheme

---
+++ openbb_terminal/forecasting/nbeats_view.py
@@ -0,0 +1,142 @@
+"""NBEATS View"""
+__docformat__ = "numpy"
+
+import logging
+from typing import Union
+
+import pandas as pd
+
+from openbb_terminal.forecasting import nbeats_model
+from openbb_terminal.decorators import log_start_end
+from openbb_terminal.forecasting import helpers
+
+logger = logging.getLogger(__name__)
+# pylint: disable=too-many-arguments
+
+
+@log_start_end(log=logger)
+def display_nbeats_forecast(
+    data: Union[pd.DataFrame, pd.Series],
+    ticker_name: str,
+    n_predict: int = 5,
+    target_col: str = "close",
+    past_covariates: str = None,
+    train_split: float = 0.85,
+    forecast_horizon: int = 5,
+    input_chunk_length: int = 14,
+    output_chunk_length: int = 5,
+    num_stacks: int = 10,
+    num_blocks: int = 3,
+    num_layers: int = 4,
+    layer_widths: int = 512,
+    n_epochs: int = 100,
+    learning_rate: float = 1e-3,
+    batch_size: int = 800,
+    model_save_name: str = "nbeats_model",
+    force_reset: bool = True,
+    save_checkpoints: bool = True,
+    export: str = "",
+    residuals: bool = False,
+):
+    """Display NBEATS forecast
+
+    Parameters
+    ----------
+        data (Union[pd.Series, pd.DataFrame]):
+            Input Data
+        ticker_name str
+            The name of the ticker to be predicted
+        n_predict (int, optional):
+            Days to predict. Defaults to 5.
+        target_col (str, optional):
+            Target column to forecast. Defaults to "close".
+        train_split (float, optional):
+            Train/val split. Defaults to 0.85.
+        past_covariates (str, optional):
+            Multiple secondary columns to factor in when forecasting. Defaults to None.
+        forecast_horizon (int, optional):
+            Forecast horizon when performing historical forecasting. Defaults to 5.
+        input_chunk_length (int, optional):
+            Number of past time steps that are fed to the forecasting module at prediction time. Defaults to 14.
+        output_chunk_length (int, optional):
+            The length of the forecast of the model. Defaults to 5.
+        num_stacks (int, optional):
+            The number of stacks that make up the whole model. Defaults to 10.
+        num_blocks (int, optional):
+            The number of blocks making up every stack. Defaults to 3.
+        num_layers (int, optional):
+            The number of fully connected layers preceding the final forking layers in each block
+            of every stack. Defaults to 4.
+        layer_widths (int, optional):
+            Determines the number of neurons that make up each fully connected layer in each block
+            of every stack. Defaults to 512.
+        batch_size (int, optional):
+            Number of time series (input and output sequences) used in each training pass. Defaults
+            to 32.
+        n_epochs (int, optional):
+            Number of epochs over which to train the model. Defaults to 100.
+        learning_rate (float, optional):
+            Defaults to 1e-3.
+        model_save_name (str, optional):
+            Name for model. Defaults to "brnn_model".
+        force_reset (bool, optional):
+            If set to True, any previously-existing model with the same name will be reset (all
+            checkpoints will be discarded). Defaults to True.
+        save_checkpoints (bool, optional):
+            Whether or not to automatically save the untrained model and checkpoints from training.
+            Defaults to True.
+        export: str
+            Format to export data
+        external_axes : Optional[List[plt.Axes]], optional
+            External axes (2 axis is expected in the list), by default None
+        residuals: bool
+            Whether to show residuals for the model. Defaults to False.
+    """
+
+    # reformat the date column to remove any hour/min/sec
+    data["date"] = data["date"].apply(helpers.dt_format)
+
+    (
+        ticker_series,
+        historical_fcast,
+        predicted_values,
+        precision,
+        _model,
+    ) = nbeats_model.get_NBEATS_data(
+        data,
+        n_predict,
+        target_col,
+        past_covariates,
+        train_split,
+        forecast_horizon,
+        input_chunk_length,
+        output_chunk_length,
+        num_stacks,
+        num_blocks,
+        num_layers,
+        layer_widths,
+        batch_size,
+        n_epochs,
+        learning_rate,
+        model_save_name,
+        force_reset,
+        save_checkpoints,
+    )
+    probabilistic = False
+    helpers.plot_forecast(
+        "NBEATS",
+        target_col,
+        historical_fcast,
+        predicted_values,
+        ticker_series,
+        ticker_name,
+        data,
+        n_predict,
+        forecast_horizon,
+        past_covariates,
+        precision,
+        probabilistic,
+        export,
+    )
+    if residuals:
+        helpers.plot_residuals(_model, past_covariates, ticker_series)
---
+++ openbb_terminal/forecasting/tcn_view.py
@@ -0,0 +1,139 @@
+"""TCN  View"""
+__docformat__ = "numpy"
+
+import logging
+from typing import Union
+
+import pandas as pd
+
+from openbb_terminal.forecasting import tcn_model
+from openbb_terminal.decorators import log_start_end
+from openbb_terminal.forecasting import helpers
+
+logger = logging.getLogger(__name__)
+# pylint: disable=too-many-arguments
+
+
+@log_start_end(log=logger)
+def display_tcn_forecast(
+    data: Union[pd.DataFrame, pd.Series],
+    ticker_name: str,
+    n_predict: int = 5,
+    target_col: str = "close",
+    past_covariates: str = None,
+    train_split: float = 0.85,
+    forecast_horizon: int = 5,
+    input_chunk_length: int = 14,
+    output_chunk_length: int = 5,
+    dropout: float = 0.1,
+    num_filters: int = 6,
+    weight_norm: bool = True,
+    dilation_base: int = 2,
+    n_epochs: int = 100,
+    learning_rate: float = 1e-3,
+    batch_size: int = 800,
+    model_save_name: str = "tcn_model",
+    force_reset: bool = True,
+    save_checkpoints: bool = True,
+    export: str = "",
+    residuals: bool = False,
+):
+    """Display TCN forecast
+
+    Parameters
+    ----------
+        data (Union[pd.Series, pd.DataFrame]):
+            Input Data
+        ticker_name str
+            The name of the ticker to be predicted
+        n_predict (int, optional):
+            Days to predict. Defaults to 5.
+        target_col (str, optional):
+            Target column to forecast. Defaults to "close".
+        train_split (float, optional):
+            Train/val split. Defaults to 0.85.
+        past_covariates (str, optional):
+            Multiple secondary columns to factor in when forecasting. Defaults to None.
+        forecast_horizon (int, optional):
+            Forecast horizon when performing historical forecasting. Defaults to 5.
+        input_chunk_length (int, optional):
+            Number of past time steps that are fed to the forecasting module at prediction time. Defaults to 14.
+        output_chunk_length (int, optional):
+            The length of the forecast of the model. Defaults to 5.
+        dropout (float, optional):
+            Fraction of neurons afected by Dropout. Defaults to 0.1.
+        num_filters (int, optional):
+            The number of filters in a convolutional layer of the TCN. Defaults to 6.
+        weight_norm (bool, optional):
+            Boolean value indicating whether to use weight normalization. Defaults to True.
+        dilation_base (int, optional):
+            The base of the exponent that will determine the dilation on every level. Defaults to 2.
+        batch_size (int, optional):
+            Number of time series (input and output sequences) used in each training pass. Defaults to 32.
+        n_epochs (int, optional):
+            Number of epochs over which to train the model. Defaults to 100.
+        learning_rate (float, optional):
+            Defaults to 1e-3.
+        model_save_name (str, optional):
+            Name for model. Defaults to "brnn_model".
+        force_reset (bool, optional):
+            If set to True, any previously-existing model with the same name will be reset
+            (all checkpoints will be discarded). Defaults to True.
+        save_checkpoints (bool, optional):
+            Whether or not to automatically save the untrained model and checkpoints from training. Defaults to True.
+        export: str
+            Format to export data
+        external_axes : Optional[List[plt.Axes]], optional
+            External axes (2 axis is expected in the list), by default None
+        residuals: bool
+            Whether to show residuals for the model. Defaults to False.
+    """
+
+    # reformat the date column to remove any hour/min/sec
+    data["date"] = data["date"].apply(helpers.dt_format)
+
+    (
+        ticker_series,
+        historical_fcast,
+        predicted_values,
+        precision,
+        _model,
+    ) = tcn_model.get_tcn_data(
+        data=data,
+        n_predict=n_predict,
+        target_col=target_col,
+        past_covariates=past_covariates,
+        train_split=train_split,
+        forecast_horizon=forecast_horizon,
+        input_chunk_length=input_chunk_length,
+        output_chunk_length=output_chunk_length,
+        dropout=dropout,
+        # kernel_size=kernel_size,
+        num_filters=num_filters,
+        weight_norm=weight_norm,
+        dilation_base=dilation_base,
+        batch_size=batch_size,
+        n_epochs=n_epochs,
+        learning_rate=learning_rate,
+        model_save_name=model_save_name,
+        force_reset=force_reset,
+        save_checkpoints=save_checkpoints,
+    )
+    probabilistic = False
+    helpers.plot_forecast(
+        "TCN",
+        target_col,
+        historical_fcast,
+        predicted_values,
+        ticker_series,
+        ticker_name,
+        data,
+        n_predict,
+        forecast_horizon,
+        past_covariates,
+        precision,
+        probabilistic,
+        export,
+    )
+    if residuals:
+        helpers.plot_residuals(_model, past_covariates, ticker_series)
