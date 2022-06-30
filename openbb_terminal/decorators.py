f398f228700113f9a283992b082018736c581806
Sam Kerr <kerr.sam@gmail.com>
2022-06-30 13:44:12 -0400

    Return None instead of empty list when exception is caught

--- openbb_terminal/decorators.py
+++ openbb_terminal/decorators.py
@@ -69,7 +69,7 @@ def log_start_end(func=None, log=None):
                     str(e),
                     extra={"func_name_override": func.__name__},
                 )
-                return []
+                return None
 
         return wrapper
 
