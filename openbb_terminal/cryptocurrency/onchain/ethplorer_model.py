0017b5aa2e5438638200eaa9a9271040b653823c
J. Farid <jrab227@gmail.com>
2022-07-15 20:34:06 -0400

    Run previous df generation.

--- openbb_terminal/cryptocurrency/onchain/ethplorer_model.py
+++ openbb_terminal/cryptocurrency/onchain/ethplorer_model.py
@@ -250,7 +250,9 @@ def get_address_info(address: str) -> pd.DataFrame:
         "tokenAddress",
         "balance",
     ]
-    df = pd.DataFrame(tokens + [eth_row], columns=cols)
+    df = pd.DataFrame(tokens)[cols]
+    eth_row_df = pd.DataFrame([eth_row], columns=cols)
+    df = pd.concat([eth_row_df, df], ignore_index=True)
     df = df[df["tokenName"].notna()]
     create_df_index(df, "index")
     return df
