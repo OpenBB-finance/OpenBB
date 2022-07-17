5a2ff6107c97386f682c03b1d31a1261d71565cb
northern-64bit <75195383+northern-64bit@users.noreply.github.com>
2022-07-17 21:41:41 +0200

    Revert "Merge branch 'API-Factory' of github.com:northern-64bit/GamestonkTerminal into API-Factory"
    This reverts commit 4e5cc6c83f7a27af040224068cfc68bc9243d738, reversing
    changes made to 96cfc467384ac648bddcefdf4e8003538097748f.

--- openbb_terminal/cryptocurrency/onchain/ethplorer_model.py
+++ openbb_terminal/cryptocurrency/onchain/ethplorer_model.py
@@ -209,9 +209,10 @@ def get_address_info(address: str) -> pd.DataFrame:
     """
 
     response = make_request("getAddressInfo", address)
-    tokens = []
+
     if "tokens" in response:
         tokens = response.pop("tokens")
+
         for token in tokens:
             token_info = token.pop("tokenInfo")
             token.update(
@@ -223,7 +224,7 @@ def get_address_info(address: str) -> pd.DataFrame:
                     / (10 ** int(token_info.get("decimals"))),
                 }
             )
-    elif "token_info" in response:
+    else:
         token_info = response.get("tokenInfo") or {}
         tokens = [
             {
@@ -235,7 +236,7 @@ def get_address_info(address: str) -> pd.DataFrame:
             }
         ]
 
-    eth = response.get("ETH") or {}
+    eth = response["ETH"] or {}
     eth_balance = eth.get("balance")
     eth_row = [
         "Ethereum",
@@ -243,16 +244,17 @@ def get_address_info(address: str) -> pd.DataFrame:
         "0x0000000000000000000000000000000000000000",
         eth_balance,
     ]
+
     cols = [
         "tokenName",
         "tokenSymbol",
         "tokenAddress",
         "balance",
     ]
-    df = pd.DataFrame(tokens)
+    df = pd.DataFrame(tokens)[cols]
     eth_row_df = pd.DataFrame([eth_row], columns=cols)
     df = pd.concat([eth_row_df, df], ignore_index=True)
-    df = df[df["tokenName"].notna()][cols]
+    df = df[df["tokenName"].notna()]
     create_df_index(df, "index")
     return df
 
