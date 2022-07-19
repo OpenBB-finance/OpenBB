7f0019b1be0429aec26dab6032d2bdb40c780a48
montezdesousa <montezdesousa@gmail.com>
2022-07-19 23:14:22 +0100

    display stored datasets in one line

--- openbb_terminal/economy/economy_helpers.py
+++ openbb_terminal/economy/economy_helpers.py
@@ -5,7 +5,13 @@ __docformat__ = "numpy"
 def update_stored_datasets_string(datasets) -> str:
     stored_datasets_string = ""
     for key in datasets:
-        for col in datasets[key].columns:
-            stored_datasets_string += f"\n  {key}    : {col}"
+        stored_datasets_string += (
+            "\n" + " " * (len("Stored datasets") - len(key)) + key + ": "
+        )
+        for i, col in enumerate(datasets[key].columns):
+            if i == 0:
+                stored_datasets_string += col
+            else:
+                stored_datasets_string += ", " + col
 
     return stored_datasets_string
