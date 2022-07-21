2618ecfab53d244461691c941bd68b2866d517e2
northern-64bit <75195383+northern-64bit@users.noreply.github.com>
2022-07-21 16:57:16 +0200

    Updating api tests

--- tests/openbb_terminal/api/test_api.py
+++ tests/openbb_terminal/api/test_api.py
@@ -8,4 +8,4 @@
 def test_import_api():
     # pylint: disable=import-outside-toplevel, unused-import
     # flake8: noqa
-    from openbb_terminal import api
+    from openbb_terminal.api import openbb
