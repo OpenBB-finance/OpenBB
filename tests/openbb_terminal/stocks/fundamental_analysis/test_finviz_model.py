e09a29513ac97a95a00a27d5f69cf5f9056ba457
montezdesousa <montezdesousa@gmail.com>
2022-08-10 17:38:13 +0100

    fix more fa tests

--- tests/openbb_terminal/stocks/fundamental_analysis/test_finviz_model.py
+++ tests/openbb_terminal/stocks/fundamental_analysis/test_finviz_model.py
@@ -13,7 +13,7 @@ from openbb_terminal.stocks.fundamental_analysis import finviz_model
 def test_get_data(mocker):
     # REMOVE FINVIZ STOCK_PAGE CACHE
     mocker.patch.object(target=finviz.main_func, attribute="STOCK_PAGE", new={})
-    result_df = finviz_model.get_data(ticker="AAPL")
+    result_df = finviz_model.get_data(symbol="AAPL")
 
     assert isinstance(result_df, pd.DataFrame)
     assert result_df.empty is False
