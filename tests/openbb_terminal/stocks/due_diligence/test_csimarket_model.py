6d82270a0e5193788254dcae761a983f9f6ae91e
montezdesousa <montezdesousa@gmail.com>
2022-08-10 15:12:08 +0100

    fix tests csi market model

--- tests/openbb_terminal/stocks/due_diligence/test_csimarket_model.py
+++ tests/openbb_terminal/stocks/due_diligence/test_csimarket_model.py
@@ -9,23 +9,23 @@ from openbb_terminal.stocks.due_diligence import csimarket_model
 
 @pytest.mark.vcr
 def test_get_suppliers(recorder):
-    result_txt = csimarket_model.get_suppliers(ticker="TSLA")
+    result_txt = csimarket_model.get_suppliers(symbol="TSLA")
     recorder.capture(result_txt)
 
 
 @pytest.mark.vcr
 def test_get_suppliers_invalid(recorder):
-    result_txt = csimarket_model.get_suppliers(ticker="INVALID_TICKER")
+    result_txt = csimarket_model.get_suppliers(symbol="INVALID_TICKER")
     recorder.capture(result_txt)
 
 
 @pytest.mark.vcr
 def test_get_customers(recorder):
-    result_txt = csimarket_model.get_customers(ticker="TSLA")
+    result_txt = csimarket_model.get_customers(symbol="TSLA")
     recorder.capture(result_txt)
 
 
 @pytest.mark.vcr
 def test_get_customers_invalid(recorder):
-    result_txt = csimarket_model.get_customers(ticker="INVALID_TICKER")
+    result_txt = csimarket_model.get_customers(symbol="INVALID_TICKER")
     recorder.capture(result_txt)
