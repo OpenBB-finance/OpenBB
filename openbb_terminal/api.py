2f49c135e6e376d7b4b292dbe3d7641090b02c15
colin99d <colin99delahunty@gmail.com>
2022-08-25 10:47:29 -0400

    Added fixes

--- openbb_terminal/api.py
+++ openbb_terminal/api.py
@@ -6,11 +6,15 @@ from .alternative import alt_api as alt
 from .cryptocurrency import crypto_api as crypto
 from .economy import economy_api as economy
 from .econometrics import econometrics_api as econometrics
-from .forecast import forecast_api as forecast
 from .etf import etf_api as etf
 from .forex import forex_api as forex
 from .mutual_funds import mutual_fund_api as funds
 from .portfolio import portfolio_api as portfolio
 from .reports import widget_helpers as widgets
+
+try:
+    from .forecast import forecast_api as forecast
+except ImportError:
+    pass
 
 from .config_terminal import theme
