93220879a9e7e668ebcb1a7a8eb1663b80dc12e8
colin99d <colin99delahunty@gmail.com>
2022-08-25 13:02:23 -0400

    Remove bad tests

--- tests/openbb_terminal/forecast/test_mc_model.py
+++
@@ -1,6 +0,0 @@
-from openbb_terminal.forecast.mc_model import get_mc_brownian
-
-
-def test_get_mc_brownian(tsla_csv):
-    # This uses non standard test because its from the old system
-    get_mc_brownian(tsla_csv["close"], 5, 5)
