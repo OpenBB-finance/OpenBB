ccc3b9bd4e0c254c8d715591ff7391334fe180d6
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-08-28 00:09:48 +0100

    fix tests

--- tests/openbb_terminal/economy/test_econdb_model.py
+++ tests/openbb_terminal/economy/test_econdb_model.py
@@ -8,16 +8,18 @@ from openbb_terminal.economy import econdb_model
 
 @pytest.mark.vcr
 @pytest.mark.parametrize(
-    "parameter, country, start_date, end_date, convert_currency",
+    "parameter, country, transform, start_date, end_date, convert_currency",
     [
-        ["RGDP", "United States", "2005-05-05", "2006-01-01", False],
-        ["EMP", "Germany", "2020-01-01", "2020-10-10", False],
-        ["GDP", "France", "2015-01-01", "2020-10-10", False],
+        ["RGDP", "United States", "", "2005-05-05", "2006-01-01", False],
+        ["EMP", "Germany", "", "2020-01-01", "2020-10-10", False],
+        ["GDP", "France", "", "2015-01-01", "2020-10-10", False],
     ],
 )
-def test_get_macro_data(parameter, country, start_date, end_date, convert_currency):
+def test_get_macro_data(
+    parameter, country, transform, start_date, end_date, convert_currency
+):
     result_df, units = econdb_model.get_macro_data(
-        parameter, country, start_date, end_date, convert_currency
+        parameter, country, transform, start_date, end_date, convert_currency
     )
 
     assert isinstance(result_df, pd.Series)
@@ -27,18 +29,25 @@ def test_get_macro_data(parameter, country, start_date, end_date, convert_curren
 
 @pytest.mark.vcr
 @pytest.mark.parametrize(
-    "parameters, countries, start_date, end_date, convert_currency",
+    "parameters, countries, transform, start_date, end_date, convert_currency",
     [
-        [["RGDP"], ["United States", "Germany"], "2020-01-01", "2020-10-10", False],
-        [["EMP", "PPI"], ["France"], "2018-01-01", "2019-01-01", False],
-        [["RGDP", "GDP"], ["Italy", "Netherlands"], "2010-01-01", "2010-10-08", False],
+        [["RGDP"], ["United States", "Germany"], "", "2020-01-01", "2020-10-10", False],
+        [["EMP", "PPI"], ["France"], "", "2018-01-01", "2019-01-01", False],
+        [
+            ["RGDP", "GDP"],
+            ["Italy", "Netherlands"],
+            "",
+            "2010-01-01",
+            "2010-10-08",
+            False,
+        ],
     ],
 )
 def test_get_aggregated_macro_data(
-    parameters, countries, start_date, end_date, convert_currency
+    parameters, countries, transform, start_date, end_date, convert_currency
 ):
     result_df, units, denomination = econdb_model.get_aggregated_macro_data(
-        parameters, countries, start_date, end_date, convert_currency
+        parameters, countries, transform, start_date, end_date, convert_currency
     )
 
     assert isinstance(result_df, pd.DataFrame)
