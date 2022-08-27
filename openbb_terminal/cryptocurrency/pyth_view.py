da1af29471f02fb1a3fe16ed40ad9a1f62fda9b1
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2022-08-27 18:17:32 +0300

    some improvements to this pyth command (#2433)
    * some improvements to this pyth command
    
    * minor improv

--- openbb_terminal/cryptocurrency/pyth_view.py
+++ openbb_terminal/cryptocurrency/pyth_view.py
@@ -2,45 +2,30 @@
 
 import asyncio
 import logging
-import os
 
 from openbb_terminal.cryptocurrency.pyth_model import get_price
-from openbb_terminal.decorators import check_api_key
 from openbb_terminal.decorators import log_start_end
-from openbb_terminal.helper_funcs import (
-    export_data,
-)
 from openbb_terminal.rich_config import console
 
 logger = logging.getLogger(__name__)
 
 
 @log_start_end(log=logger)
-@check_api_key(["API_COINGLASS_KEY"])
-def display_price(symbol: str, export: str = "") -> None:
-    """Displays live price from pyth live feed
-    [Source: https://pyth.network/]
+def display_price(symbol: str) -> None:
+    """Displays live price from pyth live feed [Source: https://pyth.network/]
 
     Parameters
     ----------
     symbol : str
         Symbol of the asset to get price for
-    export : str
-        Export dataframe data to csv,json,xlsx file"""
-
+    """
     try:
         while True:
             price, confidence, previous_price = asyncio.run(get_price(symbol))
             console.print(
-                f"Price of {symbol} is {'[red]' if previous_price >= price else '[green]'}{price}{'[/red]' if previous_price >= price else '[/green]'} ± {confidence}",  # noqa: E501
+                f"{symbol} = {'[red]' if previous_price >= price else '[green]'}"
+                f"{price}{'[/red]' if previous_price >= price else '[/green]'} ± {confidence}",
                 end="\r",
             )
     except KeyboardInterrupt:
-        print(f"\nStopped watching {symbol} price\n")
-
-    export_data(
-        export,
-        os.path.dirname(os.path.abspath(__file__)),
-        "price",
-        "",
-    )
+        print(f"\n\nStopped watching {symbol} price and confidence interval\n")
