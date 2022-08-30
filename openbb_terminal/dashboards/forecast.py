06952bc1407bc1f042731e8c8175d0abb0535d12
colin99d <colin99delahunty@gmail.com>
2022-08-30 07:48:38 -0400

    Fixed issues

--- openbb_terminal/dashboards/forecast.py
+++ openbb_terminal/dashboards/forecast.py
@@ -1,7 +1,32 @@
 import streamlit as st
 import pandas as pd
+import yfinance as yf
+from openbb_terminal.forecast import expo_model
+from openbb_terminal.forecast import helpers
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(
-    pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
-)
+data = yf.download("TSLA")
+target_column = "Close"
+
+data = helpers.clean_data(data, None, None)
+# TODO: refactor this command to allow for printing the actual error message
+if helpers.check_data(data, target_column):
+    (
+        ticker_series,
+        historical_fcast,
+        predicted_values,
+        precision,
+        _model,
+    ) = expo_model.get_expo_data(
+        data=data,
+        target_column=target_column,
+        trend="A",
+        seasonal="A",
+        seasonal_periods=7,
+        dampen="F",
+        n_predict=30,
+        start_window=0.85,
+        forecast_horizon=5,
+    )
+    st.write(pd.DataFrame(predicted_values))
+else:
+    st.write("There was an error with the data")
