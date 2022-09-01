2d9e27c85e17aa1da5e6e99533af5037797f5dc4
Om Gupta <guptagom@gmail.com>
2022-09-01 17:40:13 +0400

    load styles, log, env

--- openbb_terminal/core/config/constants.py
+++ openbb_terminal/core/config/constants.py
@@ -4,15 +4,15 @@ from pathlib import Path
 
 REPO_DIR = Path(__file__).parent.parent.parent.parent
 
-FILE_DIR = Path(str(Path.home()) + "/OpenBB")
-if not os.path.exists(FILE_DIR):
-    os.mkdir(FILE_DIR)
+USER_DATA_DIR = Path(str(Path.home()) + "/OpenBBUserData")
+if not os.path.exists(USER_DATA_DIR):
+    os.mkdir(USER_DATA_DIR)
 
 USER_HOME = Path(os.path.expanduser("~"))
-ENV_FILE = FILE_DIR.joinpath(".env")
-
+ENV_FILE_DIR = Path(str(Path.home()) + "/.openbb_terminal")
+ENV_FILE = ENV_FILE_DIR.joinpath(".env")
 if not ENV_FILE.is_file():
-    Path(str(FILE_DIR) + ".env")
+    open(str(ENV_FILE),"w")
 
 folders = [
     "custom_imports",
@@ -20,28 +20,28 @@ folders = [
     "custom_imports/stocks/options",
     "exports",
     "routines",
+    "logs",
     "stored",
-    "styles",
+    "settings",
+    "settings/styles",
     "portfolio",
     "portfolio/holdings/",
     "portfolio/optimization",
     "portfolio/allocation",
-    "stocks",
-    "stocks/presets",
-    "options",
-    "options/presets",
-    "insider",
-    "insider/presets",
-    "etf",
-    "etf/screen",
+    "presets",
+    "presets/stocks",
+    "presets/stocks/options",
+    "presets/etf",
+    "presets/etf/screen",
+    "presets/insider",
 ]
 folder_paths = {}
 
 for folder in folders:
-    full_folder = str(FILE_DIR) + "/" + folder
+    full_folder = str(USER_DATA_DIR) + "/" + folder
     if not os.path.exists(full_folder):
         os.mkdir(full_folder)
     folder_paths[folder] = full_folder
 
 
-CUSTOM_IMPORTS = Path(str(FILE_DIR) + "/custom_imports")
+CUSTOM_IMPORTS = Path(str(USER_DATA_DIR) + "/custom_imports")
