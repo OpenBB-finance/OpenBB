bb5df41676e2cd9279c2f03fef7ab13e63ac1ece
Chavithra Parana <chavithra@gmail.com>
2022-09-07 11:51:38 +0200

    Adding paths

---
+++ openbb_terminal/core/config/paths.py
@@ -0,0 +1,11 @@
+# IMPORTATION STANDARD
+from pathlib import Path
+
+
+HOME_DIRECTORY = Path.home()
+REPOSITORY_DIRECTORY = Path(__file__).parent.parent.parent.parent
+USER_DATA_DIRECTORY = HOME_DIRECTORY / "OpenBBUserData"
+SETTINGS_DIRECTORY = HOME_DIRECTORY / ".openbb_terminal"
+
+REPOSITORY_ENV_FILE = REPOSITORY_DIRECTORY / ".env"
+USER_ENV_FILE = SETTINGS_DIRECTORY / ".env"
