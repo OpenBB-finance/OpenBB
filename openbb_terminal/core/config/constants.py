8478b053cb9dc2d3c52e4ab2a8bad1ea2992334f
Om Gupta <guptagom@gmail.com>
2022-09-07 13:47:34 +0400

    small change

--- openbb_terminal/core/config/constants.py
+++ openbb_terminal/core/config/constants.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import os
 
 REPO_DIR = Path(__file__).parent.parent.parent.parent
-USER_DATA_DIR = Path(str(Path.home()) + "/OpenBBUserData")
+USER_DATA_DIR = Path.home() / "OpenBBUserData"
 USER_HOME = Path(os.path.expanduser("~"))
 if not os.path.exists(USER_DATA_DIR):
     os.mkdir(USER_DATA_DIR)
