847a60f556f41832d4163b3de635b2a9da0cb887
colin99d <colin99delahunty@gmail.com>
2022-09-09 14:28:58 +0200

    Added options

--- openbb_terminal/dashboards/stream/forecast.py
+++ openbb_terminal/dashboards/stream/forecast.py
@@ -37,21 +37,9 @@ feat_engs = {
     "signal": api.forecast.signal,
 }
 
-interval_opts = [
-    "1m",
-    "2m",
-    "5m",
-    "15m",
-    "30m",
-    "60m",
-    "90m",
-    "1h",
-    "1d",
-    "5d",
-    "1wk",
-    "1mo",
-    "3mo",
-]
+# Add these: "1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h",
+
+interval_opts = ["1d", "5d", "1wk", "1mo", "3mo"]
 
 
 def format_df(df: pd.DataFrame) -> pd.DataFrame:
