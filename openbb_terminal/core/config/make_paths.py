93ee8de7d0aaa3165f5fa890cc9292b0a0ef385d
Om Gupta <guptagom@gmail.com>
2022-09-15 17:37:34 +0400

    fix error in logic

--- openbb_terminal/core/config/make_paths.py
+++ openbb_terminal/core/config/make_paths.py
@@ -12,7 +12,7 @@ from openbb_terminal.core.config.paths import (
 
 def create_paths(list_dirs: List):
     """
-    Creates dirs/files for user data and .env files outside the
+    Creates dirs for user data outside the
     terminal if they don't exist
     """
     for dirs in list_dirs:
@@ -20,11 +20,21 @@ def create_paths(list_dirs: List):
             os.mkdir(dirs)
 
 
+def create_files(list_files: List):
+    """
+    Creates files outside the terminal if they don't exist
+    """
+    for filename in list_files:
+        if not filename.is_file():
+            with open(str(filename), "w"):
+                pass
+
+
 dirs_list = [
     SETTINGS_DIRECTORY,
     USER_DATA_DIRECTORY,
-    USER_ENV_FILE,
-    REPOSITORY_ENV_FILE,
     USER_STYLES_DIRECTORY,
 ]
+dirs_files = [USER_ENV_FILE, REPOSITORY_ENV_FILE]
 create_paths(dirs_list)
+create_files(dirs_files)
