d6e232ee8c53e7c7becf1124920dcf484f675e3a
Om Gupta <guptagom@gmail.com>
2022-09-20 16:30:58 +0400

    fresh reports fix

--- openbb_terminal/reports/reports_controller.py
+++ openbb_terminal/reports/reports_controller.py
@@ -5,14 +5,19 @@ import logging
 
 # pylint: disable=R1732
 import os
+from pathlib import Path
+import re
 import webbrowser
 from ast import literal_eval
 from datetime import datetime
 from typing import List
+import importlib
 
-import papermill as pm
+from nbconvert.nbconvertapp import NbConvertApp
+from nbconvert.exporters import ScriptExporter
+from nbconvert.writers import FilesWriter
+
 from prompt_toolkit.completion import NestedCompleter
-
 from openbb_terminal import feature_flags as obbff
 from openbb_terminal.decorators import log_start_end
 from openbb_terminal.menu import session
@@ -50,7 +55,8 @@ class ReportController(BaseController):
         )
 
         # Open notebook with report template
-        notebook_content = open(notebook_file + ".ipynb").read()
+        with open(notebook_file + ".ipynb") as n_file:
+            notebook_content = n_file.read()
 
         # Look for the metadata cell to understand if there are parameters required by the report
         metadata_cell = """"metadata": {\n    "tags": [\n     "parameters"\n    ]\n   },\n   "outputs":"""
@@ -60,34 +66,39 @@ class ReportController(BaseController):
             notebook_content.find(metadata_cell) :  # noqa: E203
         ]
         cell_start = 'source": '
-        cell_end = '"report_name ='
-        params = (
-            notebook_metadata_content[
-                notebook_metadata_content.find(
-                    cell_start
-                ) : notebook_metadata_content.find(  # noqa: E203
-                    cell_end
-                )
-            ]
-            + "]"
-        )
-
+        cell_end = "]"
+        params = notebook_metadata_content[
+            notebook_metadata_content.find(
+                cell_start
+            ) : notebook_metadata_content.find(  # noqa: E203
+                cell_end, notebook_metadata_content.find(cell_start)
+            )
+            + 1
+        ]
         # Make sure that the parameters provided are relevant
         if "parameters" in notebook_content:
             l_params = [
-                param.split("=")[0]
+                param.split("=")[0][:-1]
                 for param in literal_eval(params.strip('source": '))
                 if param[0] not in ["#", "\n"]
             ]
-        else:
-            l_params = []
-        d_params[report_to_run] = l_params
+            def_params = [
+                param.split("=")[1][2:-1]
+                for param in literal_eval(params.strip('source": '))
+                if param[0] not in ["#", "\n"]
+            ]
+        # to ensure default value is correctly selected
+        for param in range(len(def_params) - 1):
+            def_params[param] = def_params[param][:-1]
 
+        d_params[report_to_run] = [l_params, def_params]
+        l_params_copy = list(l_params)
+        l_params_copy.remove("report_name")
         # On the menu of choices add the parameters necessary for each template report
-        if len(l_params) > 1 or not l_params:
-            args = f"<{'> <'.join(l_params)}>"
+        if len(l_params_copy) > 1 or not l_params_copy:
+            args = f"<{'> <'.join(l_params_copy)}>"
         else:
-            args = f"<{l_params[0]}>"
+            args = f"<{l_params_copy[0]}>"
 
         reports_opts += (
             f"    {k}. {report_to_run}"
@@ -177,13 +188,13 @@ class ReportController(BaseController):
             else:
                 report_to_run = known_args.cmd
 
-            params = self.d_params[report_to_run]
+            params = self.d_params[report_to_run][0]
 
             # Check that the number of arguments match. We can't check validity of the
             # argument used because this depends on what the user will use it for in
             # the notebook. This is a downside of allowing the user to have this much
             # flexibility.
-            if len(other_args) != len(params):
+            if len(other_args) != len(params) - 1:  # excluding report name
                 console.print("Wrong number of arguments provided!")
                 if len(params):
                     console.print("Provide, in order:")
@@ -194,9 +205,6 @@ class ReportController(BaseController):
                 console.print("")
                 return []
 
-            notebook_template = os.path.join(
-                "openbb_terminal", "reports", report_to_run
-            )
             args_to_output = f"_{'_'.join(other_args)}" if "_".join(other_args) else ""
             report_output_name = (
                 f"{datetime.now().strftime('%Y%m%d_%H%M%S')}"
@@ -209,27 +217,73 @@ class ReportController(BaseController):
                 "stored",
                 report_output_name,
             )
+            # only a single backslash appears in the .py file otherwise
+            notebook_output = notebook_output.replace("\\", "\\\\")
+            notebook_output = notebook_output.replace("\\", "\\\\")
 
+            # gather params from user
             d_report_params = {}
-            for idx, args in enumerate(params):
-                d_report_params[args] = other_args[idx]
+            for idx in range(len(params) - 1):
+                d_report_params[params[idx]] = other_args[idx]
 
             d_report_params["report_name"] = notebook_output
 
-            result = pm.execute_notebook(
-                notebook_template + ".ipynb",
-                notebook_output + ".ipynb",
-                parameters=d_report_params,
-                kernel_name="python3",
+            notebook_file = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), report_to_run
             )
+            with open(notebook_file + ".ipynb") as n_file:
+                notebook_content = n_file.read()
 
-            if not result["metadata"]["papermill"]["exception"]:
-                if obbff.OPEN_REPORT_AS_HTML:
-                    report_output_path = os.path.join(
-                        os.path.abspath(os.path.join(".")), notebook_output + ".html"
+            # replace params in notebook
+            if "parameters" in notebook_content:
+                params_to_change = self.d_params[report_to_run][1]
+                for idx, param in enumerate(params_to_change):
+                    notebook_content = notebook_content.replace(
+                        param, d_report_params[params[idx]], 1
                     )
+
+                notebook_file_copy = notebook_file + "copy"
+
+                with open(notebook_file_copy + ".ipynb", "w") as file:
+                    file.write(notebook_content)
+
+            # use nbconvert to switch to .py
+            converter = NbConvertApp()
+            converter.export_format = "script"
+            converter.exporter = ScriptExporter()
+            converter.writer = FilesWriter()
+
+            converter.convert_single_notebook(notebook_file_copy + ".ipynb")
+
+            python_file = Path(notebook_file_copy + ".py")
+            python_text = python_file.read_text().split("\n")
+
+            # remove lines with get_ipython
+            lines = [
+                line
+                for line in python_text
+                if len(line) == 0 or "get_ipython()" not in line
+            ]
+            clean_code = "\n".join(lines)
+            clean_code = re.sub(r"\n{2,}", "\n\n", clean_code)
+            python_file.write_text(clean_code.strip())
+
+            # run the report
+            importlib.import_module(f"openbb_terminal.reports.{report_to_run}copy")
+
+            # TO DO: modify report output path to exports folder in user data
+            report_output_path = Path(
+                os.path.join(
+                    os.path.abspath(os.path.join(".")), notebook_output + ".html"
+                )
+            )
+            os.remove(notebook_file_copy + ".ipynb")
+            os.remove(notebook_file_copy + ".py")
+
+            if report_output_path.is_file():
+                if obbff.OPEN_REPORT_AS_HTML:
                     print(report_output_path)
-                    webbrowser.open(f"file://{report_output_path}")
+                    webbrowser.open(f"file://{str(report_output_path)}")
 
                 console.print("")
                 console.print(
@@ -241,4 +295,5 @@ class ReportController(BaseController):
                 )
             else:
                 console.print("[red]\nParameter provided is not valid.\n[/red]")
+
         return self.queue
