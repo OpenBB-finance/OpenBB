35b21cee39c33532d146192982adb09b158c55cf
Uzair Aftab <48220549+Uzaaft@users.noreply.github.com>
2022-09-23 21:59:12 +0200

    refactor: convert for loops into list and dict comprehensions (#2493)

--- openbb_terminal/econometrics/econometrics_view.py
+++ openbb_terminal/econometrics/econometrics_view.py
@@ -367,11 +367,6 @@ def display_cointegration_test(
         External axes to plot on
     """
 
-    if isinstance(datasets, pd.DataFrame):
-        new_datasets = {}
-        for column in datasets.columns:
-            new_datasets[column] = datasets[column]
-
     pairs = list(combinations(datasets.keys(), 2))
     result: Dict[str, list] = {}
     z_values: Dict[str, pd.Series] = {}
