85371580569a390f090a2990001dfa5e33658d85
colin99d <colin99delahunty@gmail.com>
2022-09-26 08:54:50 +0200

    Fixes tests

--- tests/openbb_terminal/forecast/test_forecast_api.py
+++ tests/openbb_terminal/forecast/test_forecast_api.py
@@ -1,8 +1,9 @@
 import pytest
 from openbb_terminal.helper_classes import ModelsNamespace
 
+# pylint: enable=E1101
 try:
-    from openbb_terminal.forecast import forecast_api
+    from openbb_terminal.forecast import forecast_api  # type: ignore
 except ImportError:
     pytest.skip(allow_module_level=True)
 
