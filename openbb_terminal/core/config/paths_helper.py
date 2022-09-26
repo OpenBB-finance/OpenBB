5a3a9f94e55dc6c7e444f2ab87beba1e71e74d89
Om Gupta <85685255+soggyomelette@users.noreply.github.com>
2022-09-26 12:28:58 +0100

    New path for portfolio data (#2611)
    * start portfolio path change
    
    * portfolio path changes
    
    * some more changes
    
    * fix
    
    * allow default files to be imported
    
    * portfolio changes
    
    * allow default portfolios to be imported
    
    * black
    
    * fix pylint
    
    * add files in terminal spec for reports
    
    Co-authored-by: minhhoang1023 <40023817+minhhoang1023@users.noreply.github.com>

--- openbb_terminal/core/config/paths_helper.py
+++ openbb_terminal/core/config/paths_helper.py
@@ -9,6 +9,7 @@ from openbb_terminal.core.config.paths import (
     REPOSITORY_ENV_FILE,
     CUSTOM_IMPORTS_DIRECTORY,
     USER_EXPORTS_DIRECTORY,
+    PORTFOLIO_DATA_DIRECTORY,
 )
 
 
@@ -42,6 +43,11 @@ dirs_list = [
     CUSTOM_IMPORTS_DIRECTORY / "dashboards",
     USER_EXPORTS_DIRECTORY,
     USER_EXPORTS_DIRECTORY / "reports",
+    PORTFOLIO_DATA_DIRECTORY,
+    PORTFOLIO_DATA_DIRECTORY / "views",
+    PORTFOLIO_DATA_DIRECTORY / "holdings",
+    PORTFOLIO_DATA_DIRECTORY / "allocation",
+    PORTFOLIO_DATA_DIRECTORY / "optimization",
 ]
 dirs_files = [USER_ENV_FILE, REPOSITORY_ENV_FILE]
 create_paths(dirs_list)
