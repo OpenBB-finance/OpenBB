241a9f49b76ad7be920508fe4682cae0defa4ce8
colin99d <colin99delahunty@gmail.com>
2022-09-29 11:39:20 +0200

    Tells you when a bad source is sent

--- openbb_terminal/cryptocurrency/cryptocurrency_helpers.py
+++ openbb_terminal/cryptocurrency/cryptocurrency_helpers.py
@@ -310,8 +310,8 @@ def load(
     exchange: str = "binance",
     vs_currency: str = "usdt",
     end_date: datetime = datetime.now(),
-    source: str = "ccxt",
-):
+    source: str = "CCXT",
+) -> pd.DataFrame:
     """Load crypto currency to perform analysis on CoinGecko is used as source for price and
     YahooFinance for volume.
 
@@ -410,6 +410,8 @@ def load(
             console.print(f"\nPair {pair} not found in Yahoo Finance\n")
             return pd.DataFrame()
         df.index.name = "date"
+    else:
+        console.print("[red]Invalid source sent[/red]\n")
     return df
 
 
