bae58a93918a123ee37d6c2fd56e31281cb54d3d
Damian Guenzing <65827185+gnzng@users.noreply.github.com>
2022-09-29 12:18:20 +0200

    fixed linters

--- openbb_terminal/stocks/discovery/disc_helpers.py
+++ openbb_terminal/stocks/discovery/disc_helpers.py
@@ -10,6 +10,6 @@ def get_df(url: str, header: int = None) -> pd.DataFrame:
     headers = {"User-Agent": get_user_agent()}
     html = requests.get(url, headers=headers).text
     # use regex to replace radio button html entries
-    html_clean = re.sub(r"(<span class=\"Fz\(0\)\">).*?(</span>)","",html)
+    html_clean = re.sub(r"(<span class=\"Fz\(0\)\">).*?(</span>)", "", html)
     dfs = pd.read_html(html_clean, header=header)
     return dfs
