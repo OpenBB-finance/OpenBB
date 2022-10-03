011539ce619199e92c6c10380bb5bca631ed4283
montezdesousa <montezdesousa@gmail.com>
2022-10-03 16:03:17 +0100

    add si unittest

--- tests/openbb_terminal/test_keys_model.py
+++ tests/openbb_terminal/test_keys_model.py
@@ -66,10 +66,12 @@ def test_get_keys():
 )
 def test_set_av_key(key, persist, show_output, expected):
 
+    # Pop environment variable from os if it's already there -> naive os.environ
     env_var_name = "OPENBB_API_KEY_ALPHAVANTAGE"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
-    # Route .env file location
-    # Just for safety, persist=False should not change it
+    # Route .env file location to temporary empty file -> naive .env
     keys_model.USER_ENV_FILE = Path(".tmp")
 
     # Test
@@ -117,6 +119,8 @@ def test_set_av_key(key, persist, show_output, expected):
 def test_set_fmp_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_KEY_FINANCIALMODELINGPREP"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
@@ -160,10 +164,14 @@ def test_set_fmp_key(key, persist, show_output, expected):
 def test_set_quandl_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_KEY_QUANDL"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
-    status = keys_model.set_quandl_key(key=key, persist=persist, show_output=show_output)
+    status = keys_model.set_quandl_key(
+        key=key, persist=persist, show_output=show_output
+    )
 
     dotenv_key = keys_model.dotenv.get_key(
         str(keys_model.USER_ENV_FILE), key_to_get=env_var_name
@@ -203,10 +211,14 @@ def test_set_quandl_key(key, persist, show_output, expected):
 def test_set_polygon_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_POLYGON_KEY"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
-    status = keys_model.set_polygon_key(key=key, persist=persist, show_output=show_output)
+    status = keys_model.set_polygon_key(
+        key=key, persist=persist, show_output=show_output
+    )
 
     dotenv_key = keys_model.dotenv.get_key(
         str(keys_model.USER_ENV_FILE), key_to_get=env_var_name
@@ -246,6 +258,8 @@ def test_set_polygon_key(key, persist, show_output, expected):
 def test_set_fred_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_FRED_KEY"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
@@ -289,6 +303,8 @@ def test_set_fred_key(key, persist, show_output, expected):
 def test_set_news_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_NEWS_TOKEN"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
@@ -332,10 +348,14 @@ def test_set_news_key(key, persist, show_output, expected):
 def test_set_tradier_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_TRADIER_TOKEN"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
-    status = keys_model.set_tradier_key(key=key, persist=persist, show_output=show_output)
+    status = keys_model.set_tradier_key(
+        key=key, persist=persist, show_output=show_output
+    )
 
     dotenv_key = keys_model.dotenv.get_key(
         str(keys_model.USER_ENV_FILE), key_to_get=env_var_name
@@ -375,6 +395,8 @@ def test_set_tradier_key(key, persist, show_output, expected):
 def test_set_cmc_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_CMC_KEY"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
@@ -418,10 +440,14 @@ def test_set_cmc_key(key, persist, show_output, expected):
 def test_set_finnhub_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_FINNHUB_KEY"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
-    status = keys_model.set_finnhub_key(key=key, persist=persist, show_output=show_output)
+    status = keys_model.set_finnhub_key(
+        key=key, persist=persist, show_output=show_output
+    )
 
     dotenv_key = keys_model.dotenv.get_key(
         str(keys_model.USER_ENV_FILE), key_to_get=env_var_name
@@ -461,6 +487,8 @@ def test_set_finnhub_key(key, persist, show_output, expected):
 def test_set_iex_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_IEX_KEY"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
@@ -489,6 +517,7 @@ def test_set_iex_key(key, persist, show_output, expected):
 
 ### def test_set_reddit_key
 
+
 @patch.dict(os.environ, {})
 @pytest.mark.vcr
 @pytest.mark.record_stdout
@@ -506,10 +535,14 @@ def test_set_iex_key(key, persist, show_output, expected):
 def test_set_bitquery_key(key, persist, show_output, expected):
 
     env_var_name = "OPENBB_API_BITQUERY_KEY"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
 
     keys_model.USER_ENV_FILE = Path(".tmp")
 
-    status = keys_model.set_bitquery_key(key=key, persist=persist, show_output=show_output)
+    status = keys_model.set_bitquery_key(
+        key=key, persist=persist, show_output=show_output
+    )
 
     dotenv_key = keys_model.dotenv.get_key(
         str(keys_model.USER_ENV_FILE), key_to_get=env_var_name
@@ -542,6 +575,50 @@ def test_set_bitquery_key(key, persist, show_output, expected):
 
 ### def test_set_binance_key
 
-### def test_set_si_key
+
+@patch.dict(os.environ, {})
+@pytest.mark.vcr
+@pytest.mark.record_stdout
+@pytest.mark.parametrize(
+    "key, persist, show_output, expected",
+    [
+        ("test_key", False, True, -1),
+        ("test_key", False, False, -1),
+        ("test_key", True, False, -1),
+        ("test_key", False, False, -1),
+        ("REPLACE_ME", False, True, 0),
+        ("REPLACE_ME", False, False, 0),
+    ],
+)
+def test_set_si_key(key, persist, show_output, expected):
+
+    env_var_name = "OPENBB_API_SENTIMENTINVESTOR_TOKEN"
+    if env_var_name in os.environ:
+        os.environ.pop(env_var_name)
+
+    keys_model.USER_ENV_FILE = Path(".tmp")
+
+    status = keys_model.set_si_key(key=key, persist=persist, show_output=show_output)
+
+    dotenv_key = keys_model.dotenv.get_key(
+        str(keys_model.USER_ENV_FILE), key_to_get=env_var_name
+    )
+
+    os_key = os.getenv(env_var_name)
+
+    if env_var_name.startswith("OPENBB_"):
+        env_var_name = env_var_name[7:]
+        cfg_key = getattr(keys_model.cfg, env_var_name)
+
+    if keys_model.USER_ENV_FILE.is_file():
+        os.remove(keys_model.USER_ENV_FILE)
+
+    if persist is True:
+        assert dotenv_key == os_key == cfg_key == key
+    else:
+        assert (dotenv_key is None) and (os_key is None) and (cfg_key == key)
+
+    assert status == expected
+
 
 ### def test_set_coinbase_key
