c51d9586d2f5adf5652fc204d786d33563c2309f
montezdesousa <montezdesousa@gmail.com>
2022-10-03 12:15:18 +0100

    first unit test for set api key

--- tests/openbb_terminal/test_keys_model.py
+++ tests/openbb_terminal/test_keys_model.py
@@ -8,12 +8,7 @@ from openbb_terminal import keys_model
 
 # pylint: disable=R0902,R0903,W1404
 
-
-def test_get_keys():
-    df = keys_model.get_keys()
-    assert isinstance(df, pd.DataFrame)
-
-
+# Test persist
 @patch.dict(os.environ, {}, clear=True)
 @pytest.mark.parametrize(
     "env_var_name, env_var_value, persist",
@@ -21,35 +16,62 @@ def test_get_keys():
 )
 def test_set_key(env_var_name: str, env_var_value: str, persist: bool):
 
-    # Route env file location
+    # Route .env file location
     keys_model.USER_ENV_FILE = Path(".tmp")
+
+    # Test
     keys_model.set_key(env_var_name, env_var_value, persist)
 
-    # Check if key was exported to .env
+    # Get key from .env
     dotenv_key = keys_model.dotenv.get_key(
         str(keys_model.USER_ENV_FILE), key_to_get=env_var_name
     )
 
-    # Remove temporary env file
+    # Remove temp .env
     if keys_model.USER_ENV_FILE.is_file():
         os.remove(keys_model.USER_ENV_FILE)
 
-    # Check if key was exported to os
+    # Get key from patched os.environ
     os_key = os.getenv(env_var_name)
 
-    # Check if key was exported to config_terminal.py
+    # Get key from config_terminal.py
     if env_var_name.startswith("OPENBB_"):
         env_var_name = env_var_name[7:]
         cfg_key = getattr(keys_model.cfg, env_var_name)
 
     if persist is True:
-        assert (
-            (dotenv_key == env_var_value)
-            and (os_key == env_var_value)
-            and (cfg_key == env_var_value)
-        )
+        assert dotenv_key == os_key == cfg_key == env_var_value
     else:
         assert (dotenv_key is None) and (os_key is None) and (cfg_key == env_var_value)
 
 
-# TODO: Test each set method
+def test_get_keys():
+    df = keys_model.get_keys()
+    assert isinstance(df, pd.DataFrame)
+
+
+# Test set_[api]_key with persist=False and dummy key
+@patch.dict(os.environ, {})
+@pytest.mark.vcr
+@pytest.mark.record_stdout
+@pytest.mark.parametrize(
+    "key, persist, show_output",
+    [
+        ("test_key", False, True),
+        ("test_key", False, False),
+    ],
+)
+def test_set_fred_key(key, persist, show_output):
+
+    # Route .env file location
+    # Just for safety, persist=False should not change it
+    keys_model.USER_ENV_FILE = Path(".tmp")
+
+    # Test
+    status = keys_model.set_fred_key(key=key, persist=persist, show_output=show_output)
+
+    # Remove temp .env
+    if keys_model.USER_ENV_FILE.is_file():
+        os.remove(keys_model.USER_ENV_FILE)
+
+    assert status == -1
