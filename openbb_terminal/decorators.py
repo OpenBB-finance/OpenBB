2bd480cbf902f3128bd0f80df10192f65c01da0d
hjoaquim <h.joaquim@campus.fct.unl.pt>
2022-10-10 13:48:00 +0100

    fixing pylint

--- openbb_terminal/decorators.py
+++ openbb_terminal/decorators.py
@@ -107,10 +107,8 @@ def check_api_key(api_keys):
                         f"[red]{undefined_apis_name} not defined. "
                         "Set API Keys in config_terminal.py or under keys menu.[/red]\n"
                     )  # pragma: allowlist secret
-                else:
-                    return func(*args, **kwargs)
-            else:
-                return func(*args, **kwargs)
+                    return None
+            return func(*args, **kwargs)
 
         return wrapper_decorator
 
