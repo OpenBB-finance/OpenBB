1cc76a22a385d4eef217f9dd7012d5dc1272eea8
montezdesousa <79287829+montezdesousa@users.noreply.github.com>
2022-10-13 20:22:09 +0100

    Interest rates and spread matrix command - economy menu (#2799)
    * add matrix command
    
    * start heatmap
    
    * connect to investpy
    
    * add heatmap to command
    
    * add title
    
    * finetune decimals
    
    * add maturity and change options
    
    * 1 decimal on change matrix
    
    * be explicit about images location
    
    * colorize warning
    
    * add docs to website
    
    * prettify
    
    * specify decimal places
    
    * fix issue with controller
    
    * remove frame
    
    * remove comments
    
    * add more groups
    
    * wrong group name fix
    
    * change command name to spread
    
    * missed website
    
    * add docstrings
    
    * change api path to spread
    
    * ignore flake8
    
    * add some colors
    
    * codespell
    
    * func name
    
    * rewrite expected
    
    * website help
    
    * add unit tests
    
    * flake8
    
    * fix tests
    
    * mypy
    
    * rewrite test
    
    * pylint
    
    * fix integration tests
    
    * fix integration tests
    
    * fix ycrv website info
    
    * add doctring to create matrix
    
    * openbb to default
    
    * clean warning
    
    * update defaults
    
    * add spread to main yml
    
    * forgot openbb here
    
    Co-authored-by: James Maslek <jmaslek11@gmail.com>

--- tests/openbb_terminal/economy/test_investingcom_view.py
+++ tests/openbb_terminal/economy/test_investingcom_view.py
@@ -4,6 +4,7 @@
 # IMPORTATION THIRDPARTY
 # import pandas as pd
 
+from typing import List, Union
 import pytest
 
 # IMPORTATION INTERNAL
@@ -25,4 +26,58 @@ def test_display_economic_calendar():
         category="Employment",
         start_date="2022-7-7",
         end_date="2022-7-8",
+    )
+
+
+@pytest.mark.vcr
+@pytest.mark.record_stdout
+@pytest.mark.parametrize(
+    "countries, maturity, change, color, raw",
+    [
+        (
+            "EZ",
+            "10Y",
+            False,
+            "rgb",
+            True,
+        ),
+        (
+            ["Portugal", "Spain"],
+            "5Y",
+            True,
+            "rgb",
+            True,
+        ),
+        (
+            "PIIGS",
+            "10Y",
+            False,
+            "binary",
+            True,
+        ),
+        (
+            "AMERICAS",
+            "10Y",
+            False,
+            "openbb",
+            True,
+        ),
+        (
+            "G7",
+            "20Y",
+            True,
+            "openbb",
+            True,
+        ),
+    ],
+)
+def test_display_spread_matrix(
+    countries: Union[str, List[str]],
+    maturity: str,
+    change: bool,
+    color: str,
+    raw: bool,
+):
+    investingcom_view.display_spread_matrix(
+        countries=countries, maturity=maturity, change=change, color=color, raw=raw
     )
