e211c49155590b555a867433e35982f555528927
Chavithra PARANA <chavithra@gmail.com>
2022-10-14 15:16:58 +0200

    Move i18n

--- openbb_terminal/feature_flags.py
+++ openbb_terminal/feature_flags.py
@@ -10,9 +10,10 @@ import i18n
 
 # IMPORTATION INTERNAL
 from openbb_terminal.core.config.paths import (
-    USER_ENV_FILE,
+    MISCELLANEOUS_DIRECTORY,
     REPOSITORY_ENV_FILE,
     USER_DATA_SOURCES_DEFAULT_FILE,
+    USER_ENV_FILE,
 )
 from openbb_terminal.core.config import paths_helper
 
@@ -20,12 +21,7 @@ paths_helper.init_userdata()
 
 # pylint: disable=no-member,c-extension-no-member
 
-i18n_dict_location = (
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "i18n")
-    if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), "i18n"))
-    else os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "i18n")
-)
-i18n.load_path.append(i18n_dict_location)
+i18n.load_path.append(MISCELLANEOUS_DIRECTORY / "i18n")
 i18n.set("locale", "en")
 i18n.set("filename_format", "{locale}.{format}")
 
