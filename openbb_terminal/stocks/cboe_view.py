10213f6b03a188dbb28fc667b8d87ed15cd30925
james <jmaslek11@gmail.com>
2022-10-14 11:13:08 -0400

    test + merge 1 print table

--- openbb_terminal/stocks/cboe_view.py
+++ openbb_terminal/stocks/cboe_view.py
@@ -1,6 +1,7 @@
 """CBOE View"""
 __docformat__ = "numpy"
 
+import pandas as pd
 from openbb_terminal.stocks import cboe_model
 from openbb_terminal.helper_funcs import print_rich_table
 from openbb_terminal.rich_config import console
@@ -19,10 +20,15 @@ def display_top_of_book(symbol: str, exchange: str = "BZX"):
     bids, asks = cboe_model.get_top_of_book(symbol, exchange)
 
     if not bids.empty and not asks.empty:
+        asks = asks[["Price", "Size"]]
+        bids.columns = ["Bid " + col for col in bids.columns]
+        asks.columns = ["Ask " + col for col in asks.columns]
+        merged = pd.concat([bids, asks], axis=1)
         print_rich_table(
-            bids, title=f"{symbol} Bids", show_index=False, headers=bids.columns
+            merged,
+            title=f"{symbol} Top of Book",
+            show_index=False,
+            headers=merged.columns,
         )
-        print_rich_table(
-            asks, title=f"{symbol} Asks", show_index=False, headers=asks.columns
-        )
+
     console.print()
