3d2ca54af3554ac45b4fb90c51dace547e74ab2f
James Simmons <simmonsj330@gmail.com>
2022-10-19 22:49:55 -0400

    making pretty

--- openbb_terminal/cryptocurrency/defi/defipulse_model.py
+++ openbb_terminal/cryptocurrency/defi/defipulse_model.py
@@ -13,6 +13,23 @@ logger = logging.getLogger(__name__)
 
 
 @log_start_end(log=logger)
+def num_to_int(num):
+    if num[-1] == "M":
+        return int(float(num[1:-1])) * 1000000
+    if num[-1] == "B":
+        return int(float(num[1:-1])) * 1000000000
+    if num[-1] == "k":
+        return int(float(num[1:-1])) * 1000
+
+    return int(num)
+
+
+@log_start_end(log=logger)
+def percent_to_float(num):
+    return float(num[0:-1])
+
+
+@log_start_end(log=logger)
 def get_defipulse_index(sortby: str = "TVL_$", ascend: bool = False) -> pd.DataFrame:
     """Scrapes data from DeFi Pulse with all DeFi Pulse crypto protocols.
     [Source: https://defipulse.com/]
@@ -51,21 +68,20 @@ def get_defipulse_index(sortby: str = "TVL_$", ascend: bool = False) -> pd.DataF
 
     df["Rank"] = df.index
     df["30D_Users"] = df["30D_Users"].str.replace(",", "").astype(int)
-    for i, row in df.iterrows():
-        value = row["TVL_$"]
-        residue = len(value.partition(".")[2]) - 1
-        value = value.replace("$", "")
-        value = value.replace("M", "000000")
-        value = value.replace("B", "000000000")
-        value = value[0:-residue]
-        value = value.replace(".", "")
-        df.at[i, "TVL_$"] = int(value)
-
-        percent = row["1_Day_%"]
-        percent = percent.replace("%", "")
-        percent = percent.replace("+", "")
-        df.at[i, "1_Day_%"] = float(percent)
-    df = df.sort_values(by=sortby, ascending=ascend)
+    if sortby == "TVL_$":
+        df = df.sort_values(
+            by=sortby,
+            key=lambda x: df["TVL_$"].apply(num_to_int),
+            ascending=ascend,
+        )
+    elif sortby == "1_Day_%":
+        df = df.sort_values(
+            by=sortby,
+            key=lambda x: df["1_Day_%"].apply(percent_to_float),
+            ascending=ascend,
+        )
+    else:
+        df = df.sort_values(by=sortby, ascending=ascend)
     df.rename(
         columns={"30D_Users": "30D Users", "TVL_$": "TVL $", "1_Day_%": "1 Day %"},
         inplace=True,
