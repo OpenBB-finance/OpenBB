f749bcc903c357d439c939d70c83b8444404d7d0
james <jmaslek11@gmail.com>
2022-10-22 16:23:10 -0400

    #2948

--- openbb_terminal/stocks/screener/screener_controller.py
+++ openbb_terminal/stocks/screener/screener_controller.py
@@ -58,13 +58,13 @@ class ScreenerController(BaseController):
 
     PRESETS_PATH_DEFAULT = Path(__file__).parent / "presets"
     preset_choices = {
-        filepath.name: filepath
+        filepath.name.strip(".ini"): filepath
         for filepath in PRESETS_PATH.iterdir()
         if filepath.suffix == ".ini"
     }
     preset_choices.update(
         {
-            filepath.name: filepath
+            filepath.name.strip(".ini"): filepath
             for filepath in PRESETS_PATH_DEFAULT.iterdir()
             if filepath.suffix == ".ini"
         }
@@ -175,6 +175,11 @@ class ScreenerController(BaseController):
             other_args.insert(0, "-p")
         ns_parser = self.parse_known_args_and_warn(parser, other_args)
         if ns_parser:
+            if ns_parser.preset:
+                if ns_parser.preset in finviz_model.d_signals:
+                    console.print("This is a Finviz preset.\n")
+                    return
+                ns_parser.preset += ".ini"
             screener_view.display_presets(ns_parser.preset)
             console.print("")
 
@@ -201,7 +206,7 @@ class ScreenerController(BaseController):
             other_args.insert(0, "-p")
         ns_parser = self.parse_known_args_and_warn(parser, other_args)
         if ns_parser:
-            self.preset = ns_parser.preset
+            self.preset = ns_parser.preset + ".ini"
         console.print("")
 
     @log_start_end(log=logger)
@@ -318,12 +323,17 @@ class ScreenerController(BaseController):
         )
 
         if ns_parser:
+            if self.preset.strip(".ini") in finviz_model.d_signals:
+                preset = self.preset.strip(".ini")
+            else:
+                preset = self.preset
+
             if ns_parser.sort:
                 if ns_parser.sort not in finviz_view.d_cols_to_sort["overview"]:
                     console.print(f"{ns_parser.sort} not a valid sort choice.\n")
                 else:
                     self.screen_tickers = finviz_view.screener(
-                        loaded_preset=self.preset,
+                        loaded_preset=preset,
                         data_type="overview",
                         limit=ns_parser.limit,
                         ascend=ns_parser.ascend,
@@ -334,7 +344,7 @@ class ScreenerController(BaseController):
             else:
 
                 self.screen_tickers = finviz_view.screener(
-                    loaded_preset=self.preset,
+                    loaded_preset=preset,
                     data_type="overview",
                     limit=ns_parser.limit,
                     ascend=ns_parser.ascend,
