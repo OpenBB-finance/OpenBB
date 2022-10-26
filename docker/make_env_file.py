9b160742248786e8ddc46f633a4497d13106cfc7
Chavithra PARANA <chavithra@gmail.com>
2022-10-26 12:02:21 +0200

    Docker : remove script

--- docker/make_env_file.py
+++
@@ -1,9 +0,0 @@
-from openbb_terminal.core.config.paths import PACKAGE_ENV_FILE
-from openbb_terminal.loggers import get_commit_hash
-
-commit_hash = get_commit_hash(use_env=False)
-
-with PACKAGE_ENV_FILE.open("w") as f:
-    f.write(f"OPENBB_LOGGING_COMMIT_HASH='{commit_hash}'")
-    f.write("\n")
-    f.write("OPENBB_LOGGING_APP_NAME='gst_docker'")
