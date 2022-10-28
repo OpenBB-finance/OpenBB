90c0d40106ace4903b7421bbfb596c4c8d7a5b8f
hjoaquim <h.joaquim@campus.fct.unl.pt>
2022-10-28 17:27:31 +0100

    fix the exaple

--- openbb_terminal/economy/economy_controller.py
+++ openbb_terminal/economy/economy_controller.py
@@ -1760,8 +1760,9 @@ class EconomyController(BaseController):
             description="""Create custom data column from loaded datasets.  Can be mathematical expressions supported
             by pandas.eval() function.
 
-            Example.  If I have loaded `fred dgs2 dgs5` and I want to create a new column that is the difference
-            between these two, I can create a new column by doing `eval spread = dgs2 - dgs5`
+            Example.  If I have loaded `fred DGS2,DGS5` and I want to create a new column that is the difference
+            between these two, I can create a new column by doing `eval spread = DGS2 - DGS5`.
+            Notice that the command is case sensitive, i.e., `DGS2` is not the same as `dgs2`.
             """,
         )
         parser.add_argument(
