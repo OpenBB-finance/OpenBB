fbef59cc5625a17c1b5d019be5e4dd26f2be7132
martinb-bb <105685594+martinb-bb@users.noreply.github.com>
2022-10-31 10:59:09 -0400

    fix depr. warning

--- openbb_terminal/forecast/forecast_model.py
+++ openbb_terminal/forecast/forecast_model.py
@@ -306,9 +306,9 @@ def add_rsi(
     d = u.copy()
     u[delta > 0] = delta[delta > 0]
     d[delta < 0] = -delta[delta < 0]
-    u[u.index[period - 1]] = np.mean(u[:period])  # first value is sum of avg gains
+    u[u.index[period - 1]] = np.mean(u.iloc[:period])  # first value is sum of avg gains
     u = u.drop(u.index[: (period - 1)])
-    d[d.index[period - 1]] = np.mean(d[:period])  # first value is sum of avg losses
+    d[d.index[period - 1]] = np.mean(d.iloc[:period])  # first value is sum of avg losses
     d = d.drop(d.index[: (period - 1)])
     rs = (
         u.ewm(com=period - 1, adjust=False).mean()
