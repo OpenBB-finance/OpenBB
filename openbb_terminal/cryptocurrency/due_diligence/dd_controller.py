37762af87fd5266b6f99d9a246a2a5fce3a59048
jose-donato <zmcdonato@gmail.com>
2022-11-01 13:33:12 +0000

    fix: tweets params and descend

--- openbb_terminal/cryptocurrency/due_diligence/dd_controller.py
+++ openbb_terminal/cryptocurrency/due_diligence/dd_controller.py
@@ -175,7 +175,13 @@ class DueDiligenceController(CryptoBaseController):
                 "-l": "--limit",
                 "--descend": {},
             }
-            choices["events"]["-s"] = {c: None for c in coinpaprika_view.EVENTS_FILTERS}
+            choices["events"] = {
+                "--sort": {c: {} for c in coinpaprika_view.EVENTS_FILTERS},
+                "-s": "--sort",
+                "--limit": {str(c): {} for c in range(1, 100)},
+                "-l": "--limit",
+                "--descend": {},
+            }
             choices["twitter"] = {
                 "--sort": {c: {} for c in coinpaprika_view.TWEETS_FILTERS},
                 "-s": "--sort",
@@ -1183,7 +1189,7 @@ class DueDiligenceController(CryptoBaseController):
 
         parser.add_argument(
             "--descend",
-            action="store_false",
+            action="store_true",
             help="Flag to sort in descending order (lowest first)",
             dest="descend",
             default=False,
