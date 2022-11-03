be3f95a92c6ac16ba15c62e0caea98e4189fdf33
Colin Delahunty <72827203+colin99d@users.noreply.github.com>
2022-11-03 15:48:29 +0100

    Added handling for bad env files (#3285)

--- openbb_terminal/config_plot.py
+++ openbb_terminal/config_plot.py
@@ -3,18 +3,12 @@ import os
 import dotenv
 
 from openbb_terminal.core.config.paths import USER_ENV_FILE, REPOSITORY_ENV_FILE
-from openbb_terminal.rich_config import console
+from openbb_terminal import base_helpers
 
 dotenv.load_dotenv(USER_ENV_FILE)
 dotenv.load_dotenv(REPOSITORY_ENV_FILE, override=True)
 
-try:
-    PLOT_DPI = int(os.getenv("OPENBB_PLOT_DPI", "100"))
-except ValueError:
-    PLOT_DPI = 100
-    console.print(
-        f"[red]OPENBB_PLOT_DPI is not an integer, using default value of {PLOT_DPI}[/red]"
-    )
+PLOT_DPI = base_helpers.load_env_vars("OPENBB_PLOT_DPI", int, 100)
 
 # Backend to use for plotting
 BACKEND = os.getenv("OPENBB_BACKEND", "None")
@@ -28,40 +22,20 @@ if BACKEND == "None":
 # See more: https://matplotlib.org/stable/tutorials/introductory/usage.html#the-builtin-backends
 
 # Used when USE_PLOT_AUTOSCALING is set to False
-try:
-    PLOT_HEIGHT = int(os.getenv("OPENBB_PLOT_HEIGHT", "500"))
-except ValueError:
-    PLOT_HEIGHT = 500
-    console.print(
-        "[red] Invalid value for OPENBB_PLOT_HEIGHT. Please use a number.[/red]\n"
-    )
-try:
-    PLOT_WIDTH = int(os.getenv("OPENBB_PLOT_WIDTH", "800"))
-except ValueError:
-    PLOT_WIDTH = 800
-    console.print(
-        "[red] Invalid value for OPENBB_PLOT_WIDTH. Please use a number.[/red]\n"
-    )
+PLOT_HEIGHT = base_helpers.load_env_vars("OPENBB_PLOT_HEIGHT", int, 500)
+PLOT_WIDTH = base_helpers.load_env_vars("OPENBB_PLOT_WIDTH", int, 800)
 
 # Used when USE_PLOT_AUTOSCALING is set to True
-try:
-    PLOT_HEIGHT_PERCENTAGE = float(os.getenv("OPENBB_PLOT_HEIGHT_PERCENTAGE", "50.00"))
-except ValueError:
-    PLOT_HEIGHT_PERCENTAGE = 50.00
-    console.print(
-        "[red] Invalid value for OPENBB_PLOT_HEIGHT_PERCENTAGE. Please use a number.[/red]\n"
-    )
-try:
-    PLOT_WIDTH_PERCENTAGE = float(os.getenv("OPENBB_PLOT_WIDTH_PERCENTAGE", "70.00"))
-except ValueError:
-    PLOT_WIDTH_PERCENTAGE = 70.00
-    console.print(
-        "[red] Invalid value for OPENBB_PLOT_WIDTH_PERCENTAGE. Please use a number.[/red]\n"
-    )
+PLOT_HEIGHT_PERCENTAGE = base_helpers.load_env_vars(
+    "OPENBB_PLOT_HEIGHT_PERCENTAGE", float, 50.0
+)
+PLOT_WIDTH_PERCENTAGE = base_helpers.load_env_vars(
+    "OPENBB_PLOT_WIDTH_PERCENTAGE", float, 70.0
+)
 
 # When autoscaling is True, choose which monitor to scale to
 # Primary monitor = 0, secondary monitor use 1
-MONITOR = int(os.getenv("OPENBB_MONITOR", "0"))
+MONITOR = base_helpers.load_env_vars("OPENBB_MONITOR", int, 0)
 
 # Color for `view` command data.  All pyplot colors listed at:
 # https://matplotlib.org/stable/gallery/color/named_colors.html
