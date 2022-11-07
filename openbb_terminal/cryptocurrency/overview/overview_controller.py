d234780adcde67ff7c8cc57f7a8c82a8233ab8b4
James Simmons <simmonsj330@gmail.com>
2022-11-07 16:03:32 -0500

    fixing cgstables bug

--- openbb_terminal/cryptocurrency/overview/overview_controller.py
+++ openbb_terminal/cryptocurrency/overview/overview_controller.py
@@ -141,8 +141,21 @@ class OverviewController(BaseController):
                 "-l": "--limit",
                 "--pie": {},
             }
+
             choices["stables"] = {
-                "--sortby": {c: {} for c in pycoingecko_model.COINS_COLUMNS},
+                "--sortby": {
+                    c: {}
+                    for c in [
+                        "Symbol",
+                        "Name",
+                        "Price_[$]",
+                        "Market_Cap_[$]",
+                        "Market_Cap_Rank",
+                        "Change_7d_[%]",
+                        "Change_24h_[%]",
+                        "Volume_[$]",
+                    ]
+                },
                 "-s": "--sortby",
                 "--limit": {str(c): {} for c in range(1, 100)},
                 "-l": "--limit",
@@ -765,8 +778,8 @@ class OverviewController(BaseController):
             dest="sortby",
             type=str,
             help="Sort by given column. Default: market_cap_desc",
-            default=pycoingecko_model.SORT_VALUES[0],
-            choices=pycoingecko_model.SORT_VALUES,
+            # default=pycoingecko_model.SORT_VALUES[0],
+            # choices=pycoingecko_model.SORT_VALUES,
         )
 
         parser.add_argument(
@@ -818,13 +831,12 @@ class OverviewController(BaseController):
             dest="sortby",
             type=str,
             help="Sort by given column. Default: market_cap",
-            default="market_cap",
-            choices=pycoingecko_model.COINS_COLUMNS,
+            default="Market_Cap_[$]",
         )
 
         parser.add_argument(
             "--descend",
-            action="store_false",
+            action="store_true",
             help="Flag to sort in descending order (lowest first)",
             dest="descend",
             default=False,
