92a42ca058b212845b03195c55435ab8f1ba8bd8
Chavithra PARANA <chavithra@gmail.com>
2022-11-09 18:16:07 +0100

    SDK : forecasting

--- openbb_terminal/core/library/trail_map.py
+++ openbb_terminal/core/library/trail_map.py
@@ -1,76 +1,53 @@
-import pickle
-from enum import Enum
+from pathlib import Path
 from typing import Dict
 
 import pandas as pd
 
 from openbb_terminal.core.config.paths import MISCELLANEOUS_DIRECTORY
 
+try:
+    import darts  # pyright: reportMissingImports=false
 
-class PERSISTENCE_FORMAT(Enum):
-    PICKLE = 1
-    CSV = 2
+    # If you just import darts this will pass during pip install, this creates
+    # Failures later on, also importing utils ensures that darts is installed correctly
+    from darts import utils
+
+    FORECASTING = True
+except ImportError:
+    FORECASTING = False
 
 
 class TrailMap:
-    MAP_PICKLE_PATH = MISCELLANEOUS_DIRECTORY / "library" / "trail_map.pickle"
-    MAP_CSV_PATH = MISCELLANEOUS_DIRECTORY / "library" / "trail_map.csv"
+    MAP_PATH = MISCELLANEOUS_DIRECTORY / "library" / "trail_map.csv"
+    MAP_FORECASTING_PATH = (
+        MISCELLANEOUS_DIRECTORY / "library" / "trail_map_forecasting.csv"
+    )
 
     @classmethod
-    def load_pickle(
-        cls,
-    ) -> Dict[str, Dict[str, str]]:
-        map_dict = {}
-        with cls.MAP_PICKLE_PATH.open("rb") as f:
-            map_dict = pickle.load(f)
-        return map_dict
-
-    @classmethod
-    def save_pickle(cls, map_dict: Dict[str, Dict[str, str]]) -> None:
-        cls.MAP_PICKLE_PATH.parent.mkdir(parents=True, exist_ok=True)
-        with cls.MAP_PICKLE_PATH.open("wb") as f:
-            pickle.dump(map_dict, f)
-
-    @classmethod
-    def load_csv(cls) -> Dict[str, Dict[str, str]]:
-        df = pd.read_csv(cls.MAP_CSV_PATH, keep_default_na=False)
+    def load_csv(cls, path: Path = None) -> Dict[str, Dict[str, str]]:
+        path = path or cls.MAP_PATH
+        df = pd.read_csv(path, keep_default_na=False)
         df.set_index("trail", inplace=True)
         return df.to_dict(orient="index")
 
     @classmethod
-    def save_csv(cls, map_dict: Dict[str, Dict[str, str]]) -> None:
+    def save_csv(cls, map_dict: Dict[str, Dict[str, str]], path: Path = None) -> None:
+        path = path or cls.MAP_PATH
         df = pd.DataFrame.from_dict(data=map_dict, orient="index")
         df.index.name = "trail"
-        df.to_csv(path_or_buf=cls.MAP_CSV_PATH)
+        df.to_csv(path_or_buf=path)
 
     @property
     def map_dict(self) -> Dict[str, Dict[str, str]]:
         return self.__map_dict
 
     def load(self):
-        persitence_format = self.__persistence_format
-
-        if persitence_format == PERSISTENCE_FORMAT.CSV:
-            map_dict = self.load_csv()
-        elif persitence_format == PERSISTENCE_FORMAT.PICKLE:
-            map_dict = self.load_pickle()
-        else:
-            raise AttributeError("Incorrect 'persitence_format'")
+        map_dict = self.load_csv(path=self.MAP_PATH)
+        if FORECASTING:
+            map_forecasting_dict = self.load_csv(path=self.MAP_FORECASTING_PATH)
+            map_dict = {**map_dict, **map_forecasting_dict}
 
         self.__map_dict = map_dict
 
-    def persist(self):
-        map_dict = self.__map_dict
-        persitence_format = self.__persistence_format
-
-        if persitence_format == PERSISTENCE_FORMAT.CSV:
-            self.save_csv(map_dict=map_dict)
-        elif persitence_format == PERSISTENCE_FORMAT.PICKLE:
-            self.save_pickle(map_dict=map_dict)
-        else:
-            raise AttributeError("Incorrect 'persitence_format'")
-
-    def __init__(self, persistence_format: PERSISTENCE_FORMAT = None):
-        self.__persistence_format = persistence_format or PERSISTENCE_FORMAT.CSV
-
+    def __init__(self):
         self.load()
