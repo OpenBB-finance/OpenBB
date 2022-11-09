637a35f419c699db751b1030afd65a98f38f90d1
Chavithra PARANA <chavithra@gmail.com>
2022-11-09 18:25:46 +0100

    SDK

--- openbb_terminal/sdk.py
+++ openbb_terminal/sdk.py
@@ -1,7 +1,16 @@
+from openbb_terminal.config_terminal import theme  # noqa # pylint: disable=unused-import
+from openbb_terminal.helper_classes import TerminalStyle  # noqa # pylint: disable=unused-import
+from openbb_terminal import helper_funcs as helper  # noqa # pylint: disable=unused-import
+from openbb_terminal.reports import widget_helpers as widgets  # noqa # pylint: disable=unused-import
+from openbb_terminal.portfolio.portfolio_model import PortfolioModel as Portfolio  # noqa # pylint: disable=unused-import
+from openbb_terminal.cryptocurrency.due_diligence.pycoingecko_model import Coin  # noqa # pylint: disable=unused-import
+
+
 from openbb_terminal.core.library.breadcrumb import Breadcrumb
 from openbb_terminal.core.library.trail_map import TrailMap
 from openbb_terminal.core.library.breadcrumb import MetadataBuilder
 
+TerminalStyle().applyMPLstyle()
 trail = ""
 trail_map = TrailMap()
 metadata = MetadataBuilder.build(trail=trail, trail_map=trail_map)
