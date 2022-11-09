e3da73e029813401582a4bd5a5bb8599cb5b7840
Chavithra PARANA <chavithra@gmail.com>
2022-11-09 20:40:00 +0100

    Linting

--- openbb_terminal/sdk.py
+++ openbb_terminal/sdk.py
@@ -1,22 +1,10 @@
-from openbb_terminal.config_terminal import (
-    theme,
-)  # noqa # pylint: disable=unused-import
-from openbb_terminal.helper_classes import (
-    TerminalStyle,
-)  # noqa # pylint: disable=unused-import
-from openbb_terminal import (
-    helper_funcs as helper,
-)  # noqa # pylint: disable=unused-import
-from openbb_terminal.reports import (
-    widget_helpers as widgets,
-)  # noqa # pylint: disable=unused-import
-from openbb_terminal.portfolio.portfolio_model import (
-    PortfolioModel as Portfolio,
-)  # noqa # pylint: disable=unused-import
-from openbb_terminal.cryptocurrency.due_diligence.pycoingecko_model import (
-    Coin,
-)  # noqa # pylint: disable=unused-import
-
+# pylint: disable=unused-import
+from openbb_terminal.config_terminal import theme
+from openbb_terminal.helper_classes import TerminalStyle
+from openbb_terminal import helper_funcs as helper
+from openbb_terminal.reports import widget_helpers as widgets
+from openbb_terminal.portfolio.portfolio_model import PortfolioModel as Portfolio
+from openbb_terminal.cryptocurrency.due_diligence.pycoingecko_model import Coin
 
 from openbb_terminal.core.library.breadcrumb import Breadcrumb
 from openbb_terminal.core.library.trail_map import TrailMap
@@ -32,13 +20,3 @@ openbb = Breadcrumb(
     trail=trail,
     trail_map=trail_map,
 )
-
-__all__ = [
-    theme,
-    TerminalStyle,
-    helper,
-    widgets,
-    Portfolio,
-    Coin,
-    openbb,
-]
