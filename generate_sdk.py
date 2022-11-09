5791bac5f5accf2f93f95d6ef1657186586dc08f
teh_coderer <me@tehcoderer.com>
2022-11-09 13:25:23 -0500

    sdk auto gen modules done

---
+++ generate_sdk.py
@@ -0,0 +1,261 @@
+import csv
+import glob
+import inspect
+import os
+
+import importlib
+from typing import Any, Dict, List, Optional, TextIO
+
+from openbb_terminal.sdk_core.sdk_helpers import clean_attr_desc
+
+
+sub_names = {
+    "defi": "DeFi",
+    "disc": "Discovery",
+    "dd": "Due Diligence",
+    "onchain": "OnChain",
+    "ov": "Overview",
+    "keys": "Keys",
+    "forecast": "Forecasting",
+    "alt": "Alternative",
+    "crypto": "Cryptocurrency",
+    "ba": "Behavioral Analysis",
+    "ca": "Comparison Analysis",
+    "dps": "Darkpool Shorts",
+    "po": "Portfolio Optimization",
+    "qa": "Quantitative Analysis",
+    "screener": "Screener",
+    "sia": "Sector Industry Analysis",
+    "ta": "Technical Analysis",
+    "th": "Trading Hours",
+    "fa": "Fundamental Analysis",
+    "Oss": "Open Source Software",
+    "funds": "Mutual Funds",
+    "gov": "Government",
+    "ins": "Insiders",
+    "nft": "NFT",
+}
+
+
+class Trailmap:
+    def __init__(self, trailmap: str, model: str, view: Optional[str] = None):
+        tmap = trailmap.split(".")
+        self.class_attr: str = tmap.pop(-1)
+        self.category = tmap[0]
+        self.location_path = [c for c in tmap]
+        self.model = model
+        self.view = view if view else None
+        self.model_func: Optional[str] = f"lib.{model}" if model else None
+        self.view_func: Optional[str] = f"lib.{view}" if view else None
+        self.short_doc: Dict[str, str] = {}
+        self.long_doc: Dict[str, str] = {}
+        self.params: Dict[str, Dict[str, inspect.Parameter]] = {}
+        self.get_docstrings()
+
+    def get_docstrings(self) -> None:
+        """Gets the function docstrings. We get the short and long docstrings."""
+
+        for func in [self.model, self.view]:
+            if func:
+                key = "model" if func == self.model else "view"
+                attr = getattr(
+                    importlib.import_module("openbb_terminal.sdk_core.sdk_init"),
+                    func.split(".")[0],
+                )
+                self.long_doc[key] = attr.__doc__
+                self.short_doc[key] = clean_attr_desc(getattr(attr, func.split(".")[1]))
+
+
+class BuildCategoryModelClasses:
+    def __init__(self, trailmaps: List[Trailmap], sdk_folder: str) -> None:
+        self.trailmaps = trailmaps
+        self.categories: Dict[str, Any] = {}
+        self.import_modules: Dict[str, Any] = {}
+        self.sdk_folder = sdk_folder
+        self.root_modules: Dict[str, Any] = {}
+        self.import_cat_class = (
+            "from openbb_terminal.sdk_core.sdk_helpers import Category\r"
+        )
+
+        for tmap in self.trailmaps:
+            local = tmap.location_path
+            self.categories = self.add_todict(self.categories, local, tmap)
+
+        if not os.path.exists(f"openbb_terminal/sdk_core/{sdk_folder}/categories"):
+            os.makedirs(f"openbb_terminal/sdk_core/{sdk_folder}/categories")
+
+        if not os.path.exists(
+            f"openbb_terminal/sdk_core/{sdk_folder}/categories/__init__.py"
+        ):
+            with open(
+                f"openbb_terminal/sdk_core/{sdk_folder}/categories/__init__.py", "w"
+            ) as f:
+                f.write("")
+
+        if not os.path.exists(f"openbb_terminal/sdk_core/{sdk_folder}/__init__.py"):
+            with open(f"openbb_terminal/sdk_core/{sdk_folder}/__init__.py", "w") as f:
+                f.write("")
+
+    def add_todict(self, d: dict, location_path: list, tmap: Trailmap) -> dict:
+        """Adds the trailmap to the dictionary. A trailmap is a path to a function
+        in the sdk. This function creates the dictionary paths to the function."""
+
+        if location_path[0] not in d:
+            d[location_path[0]] = {}
+
+        if len(location_path) > 1:
+            self.add_todict(d[location_path[0]], location_path[1:], tmap)
+        else:
+            d[location_path[0]][tmap.class_attr] = tmap
+
+        return d
+
+    def get_nested_dict(self, d: dict) -> dict:
+        """Gets the nested dictionary of the category."""
+        nested_dict = {}
+        for k, v in d.items():
+            if isinstance(v, dict):
+                nested_dict[k] = self.get_nested_dict(v)
+        return nested_dict
+
+    def get_subcategory(self, cat: str) -> str:
+        """Gets the full category name from the shortened category name."""
+        if cat in sub_names:
+            return sub_names[cat]
+        return cat.title().replace(" ", "")
+
+    def write_class_attr_docs(self, f: TextIO, d: dict) -> None:
+        """Writes the attributes to the category file."""
+        f.write("\r    Attributes:\r")
+        for v in d.values():
+            if isinstance(v, Trailmap):
+                if v.model_func and "model" in v.short_doc:
+                    f.write(f"        `{v.class_attr}`: {v.short_doc['model']}\r")
+                if v.view_func and "view" in v.short_doc:
+                    f.write(f"        `{v.class_attr}_view`: {v.short_doc['view']}\r")
+        f.write('    """\r\r    def __init__(self):\r        super().__init__()\r')
+
+    def write_class_attributes(self, f: TextIO, d: dict) -> None:
+        """Writes the attributes to the category file."""
+        for v in d.values():
+            if isinstance(v, Trailmap):
+                if v.model_func:
+                    f.write(f"        self.{v.class_attr} = {v.model_func}\r")
+                if v.view_func:
+                    f.write(f"        self.{v.class_attr}_view = {v.view_func}\r")
+        f.write("\r\r")
+
+    def write_category(self, category: str, d: dict, f: TextIO) -> None:
+        """Writes the category class to the file
+
+        Parameters
+        ----------
+        category : str
+            The category name
+        d : dict
+            The category dictionary
+        f : TextIO
+            The file to write to
+        """
+        subname = self.get_subcategory(category)
+        self.root_modules[category] = f"{category.title().replace(' ', '')}Root"
+
+        # If this catergory has no attributes, then we don't write it to the file.
+        if not any(isinstance(v, Trailmap) for v in d.values()):
+            return
+
+        category = f"{category.title().replace(' ', '')}Root"
+
+        f.write(f"class {category}(Category):\r")
+        f.write(f'    """OpenBB SDK {subname.title()} Module\r')
+
+        self.write_class_attr_docs(f, d)
+        self.write_class_attributes(f, d)
+
+    def write_nested_category(self, category: str, d: dict, f: TextIO) -> None:
+        """Writes the nested category classes.
+
+        Parameters
+        ----------
+        category : str
+            The category name
+        d : dict
+            The category dictionary
+        f : TextIO
+            The file to write to
+        """
+        added_submodules = False
+        for nested_category, nested_dict in d.items():
+            if isinstance(nested_dict, Trailmap):
+                continue
+            subname = self.get_subcategory(nested_category)
+            f.write(f"class {category.title()}{subname.replace(' ', '')}(Category):\r")
+            f.write(f'    """OpenBB SDK {category.title()} {subname} Module\r')
+
+            nested_cat = self.get_nested_dict(nested_dict)
+            if nested_cat:
+                if not added_submodules:
+                    f.write("\r    Submodules:\r")
+                    added_submodules = True
+                for k in nested_cat:
+                    subcat_name = self.get_subcategory(k)
+                    f.write(f"        `{k}`: {subcat_name} Module\r")
+
+            if isinstance(nested_dict, dict):
+                self.write_class_attr_docs(f, nested_dict)
+                self.write_class_attributes(f, nested_dict)
+
+    def write_category_file(self, category: str, d: dict) -> None:
+        """Writes the category file. This is the file that contains the categories
+        and subcategories of the sdk."""
+        with open(
+            f"openbb_terminal/sdk_core/{self.sdk_folder}/categories/{category}_sdk_model.py",
+            "w",
+        ) as f:
+            import_cat_class = self.import_cat_class
+            if len(self.categories[category]) == 1:
+                import_cat_class = ""
+                self.root_modules[category] = f"{category.title().replace(' ', '')}Root"
+                category = f"{category.title().replace(' ', '')}Root"
+
+            f.write(
+                f"# flake8: noqa\r{import_cat_class}import openbb_terminal.sdk_init as lib\r\r\r"
+            )
+
+            self.write_category(category, d, f)
+            self.write_nested_category(category, d, f)
+            f.seek(f.tell() - 2, os.SEEK_SET)
+            f.truncate()
+
+    def build(self) -> None:
+        """Builds the sdk model files."""
+        for category, d in self.categories.items():
+            if isinstance(d, Trailmap):
+                continue
+            self.write_category_file(category, d)
+
+        for file in glob.glob(
+            f"openbb_terminal/sdk_core/{self.sdk_folder}/categories/*.py"
+        ):
+            with open(file, "rb") as f:
+                content = f.read()
+            with open(file, "wb") as f:
+                f.write(content.replace(b"\r", b"\n"))
+        for file in glob.glob(f"openbb_terminal/sdk_core/{self.sdk_folder}/*.py"):
+            with open(file, "rb") as f:
+                content = f.read()
+            with open(file, "wb") as f:
+                f.write(content.replace(b"\r", b"\n"))
+
+
+def generate():
+    trailmaps = []
+    with open("openbb_terminal/sdk_core/trail_map.csv", "r") as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            trail, model, view = row
+            trail_map = Trailmap(trail, model, view)
+            trailmaps.append(trail_map)
+
+    BuildCategoryModelClasses(trailmaps, "sdk_modules").build()
---
+++ openbb_terminal/sdk_core/sdk_helpers.py
@@ -0,0 +1,117 @@
+"""OpenBB Terminal SDK Helpers."""
+from traceback import format_stack
+import types
+import functools
+from typing import (
+    Any,
+    Callable,
+)
+import logging
+from openbb_terminal.decorators import log_start_end, sdk_arg_logger
+
+
+def check_suppress_logging(suppress_dict: dict) -> bool:
+    """
+    Check if logging should be suppressed.
+    If the dict contains a value that is found in the stack trace,
+    the logging should be suppressed.
+
+    Parameters
+    ----------
+    supress_dict: dict
+        Dictionary with values that trigger log suppression
+
+    Returns
+    -------
+    bool
+        True if logging shall be suppressed, False otherwise
+    """
+    for _, value in suppress_dict.items():
+        for ele in format_stack():
+            if value in ele:
+                return True
+    return False
+
+
+def copy_func(
+    f: Callable,
+    logging_decorator: bool = False,
+    chart: bool = False,
+) -> Callable:
+    """Copy the contents and attributes of the entered function.
+
+    Based on https://stackoverflow.com/a/13503277
+
+    Parameters
+    ----------
+    f: Callable
+        Function to be copied
+    logging_decorator: bool
+        If True, the copied function will be decorated with the logging decorator
+    chart: bool
+        If True, the copied function will log info on whether it is a view (chart)
+
+    Returns
+    -------
+    g: Callable
+        New function
+    """
+    # Removing the logging decorator
+    if hasattr(f, "__wrapped__"):
+        f = f.__wrapped__  # type: ignore
+
+    g = types.FunctionType(
+        f.__code__,
+        f.__globals__,  # type: ignore
+        name=f.__name__,
+        argdefs=f.__defaults__,  # type: ignore
+        closure=f.__closure__,  # type: ignore
+    )
+    g = functools.update_wrapper(g, f)
+    g.__kwdefaults__ = f.__kwdefaults__  # type: ignore
+
+    if logging_decorator:
+        log_name = logging.getLogger(g.__module__)
+        g = sdk_arg_logger(func=g, log=log_name, chart=chart)
+        g = log_start_end(func=g, log=log_name)
+
+    return g
+
+
+def clean_attr_desc(attr: Any) -> str:
+    """Clean the attribute description."""
+    if attr.__doc__ is None:
+        return ""
+    return (
+        attr.__doc__.splitlines()[1].lstrip()
+        if not attr.__doc__.splitlines()[0]
+        else attr.__doc__.splitlines()[0].lstrip()
+        if attr.__doc__
+        else ""
+    )
+
+
+class Category:
+    """The base class that all categories must inherit from."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the class"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, attr: str, value: Any) -> None:
+        """We override setattr to apply logging decorator to Catergory attributes."""
+        chart = False
+        if "_view" in attr:
+            chart = True
+        if callable(value) and not attr.startswith("_"):
+            value = copy_func(value, logging_decorator=True, chart=chart)
+        super().__setattr__(attr, value)
+
+    def __repr__(self):
+        """Return the representation of the class."""
+        repr_docs = [
+            f"    {k}: {clean_attr_desc(v)}\n"
+            for k, v in self.__dict__.items()
+            if v.__doc__
+        ]
+        return f"{self.__class__.__name__}(\n{''.join(repr_docs)}\n)"
