0297b6d06f032737834590b2bf704b63449c0911
james <jmaslek11@gmail.com>
2022-11-10 11:25:56 -0500

    flake

--- openbb_terminal/portfolio/portfolio_controller.py
+++ openbb_terminal/portfolio/portfolio_controller.py
@@ -157,8 +157,6 @@ class PortfolioController(BaseController):
 
         choices: dict = {c: {} for c in self.controller_choices}
 
-        zero_to_hundred_detailed = {str(c): {} for c in np.arange(0.0, 100.0, 0.1)}
-        one_to_hundred: dict = {str(c): {} for c in range(1, 100)}
         choices["load"] = {
             "--file": {c: {} for c in self.DATA_HOLDINGS_FILES},
             "-f": "--file",
