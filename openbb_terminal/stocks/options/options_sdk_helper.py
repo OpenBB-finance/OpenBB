f2bff67008603f0f5b14302d0e581f54045c3b36
james <jmaslek11@gmail.com>
2022-11-14 13:56:15 -0500

    Allow chains to specify an expiry

--- openbb_terminal/stocks/options/options_sdk_helper.py
+++ openbb_terminal/stocks/options/options_sdk_helper.py
@@ -9,7 +9,9 @@ from openbb_terminal.stocks.options import yfinance_model, tradier_model, nasdaq
 logger = logging.getLogger(__name__)
 
 
-def get_full_option_chain(symbol: str, source: str = "Nasdaq"):
+def get_full_option_chain(
+    symbol: str, source: str = "Nasdaq", expiration: str = None
+) -> pd.DataFrame:
     """Get Option Chain For A Stock.  No greek data is returned
 
     Parameters
@@ -18,23 +20,38 @@ def get_full_option_chain(symbol: str, source: str = "Nasdaq"):
         Symbol to get chain for
     source : str, optional
         Source to get data from, by default "Nasdaq"
+    expiration : str, optional
+        Date to get chain for.  By default returns all dates
 
     Returns
     -------
     pd.DataFrame
-        Dataframe of full option chain across all expirations
+        Dataframe of full option chain.
 
     Examples
     --------
     >>> from openbb_terminal.sdk import openbb
     >>> aapl_option_chain = openbb.stocks.options.chains("AAPL", source = "Nasdaq")
+
+    To get a specific expiration date, use the expiration parameter
+
+    >>> aapl_chain_date = openbb.stocks.options.chains("AAPL", expiration="2023-07-21", source="Nasdaq)
     """
 
     if source == "Tradier":
-        return tradier_model.get_full_option_chain(symbol)
+        df = tradier_model.get_full_option_chain(symbol)
+        if expiration:
+            return df[df.expiration == expiration]
+        return df
     if source == "YahooFinance":
-        return yfinance_model.get_full_option_chain(symbol)
+        df = yfinance_model.get_full_option_chain(symbol)
+        if expiration:
+            return df[df.expiration == expiration]
+        return df
     if source == "Nasdaq":
+        # Nasdaq handles these slightly differently
+        if expiration:
+            return nasdaq_model.get_chain_given_expiration(symbol, expiration)
         return nasdaq_model.get_full_option_chain(symbol)
     logger.info("Invalid Source")
     return pd.DataFrame()
