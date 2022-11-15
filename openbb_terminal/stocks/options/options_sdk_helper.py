da316401d66c02f1b63facf21ef0213d475ec77d
james <jmaslek11@gmail.com>
2022-11-14 20:41:05 -0500

    Fixes #3325

--- openbb_terminal/stocks/options/options_sdk_helper.py
+++ openbb_terminal/stocks/options/options_sdk_helper.py
@@ -1,16 +1,18 @@
 """Options Functions For OpenBB SDK"""
 
 import logging
-
+from typing import List, Optional
 import pandas as pd
 
 from openbb_terminal.stocks.options import nasdaq_model, tradier_model, yfinance_model
+from openbb_terminal.decorators import log_start_end
 
 logger = logging.getLogger(__name__)
 
 
+@log_start_end(log=logger)
 def get_full_option_chain(
-    symbol: str, source: str = "Nasdaq", expiration: str = None
+    symbol: str, source: str = "Nasdaq", expiration: Optional[str] = None
 ) -> pd.DataFrame:
     """Get Option Chain For A Stock.  No greek data is returned
 
@@ -53,5 +55,41 @@ def get_full_option_chain(
         if expiration:
             return nasdaq_model.get_chain_given_expiration(symbol, expiration)
         return nasdaq_model.get_full_option_chain(symbol)
+    logger.info("Invalid Source")
+    return pd.DataFrame()
+
+
+@log_start_end(log=logger)
+def get_option_expirations(symbol: str, source: str = "Nasdaq") -> List:
+    """Get Option Chain Expirations
+
+    Parameters
+    ----------
+    symbol : str
+        Symbol to get chain for
+    source : str, optional
+        Source to get data from, by default "Nasdaq"
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe of full option chain.
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> aapl_option_chain = openbb.stocks.options.expirations("SPX", source = "Nasdaq")
+    """
+
+    if source == "Tradier":
+        df = tradier_model.get_full_option_chain(symbol)
+        return list(df.expiration.unique())
+    if source == "YahooFinance":
+        df = yfinance_model.get_full_option_chain(symbol)
+        return list(df.expiration.unique())
+    if source == "Nasdaq":
+        # Nasdaq handles these slightly differently
+        return nasdaq_model.get_expirations(symbol)
+
     logger.info("Invalid Source")
     return pd.DataFrame()
