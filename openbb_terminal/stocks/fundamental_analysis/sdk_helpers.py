ffbb0d4cba27f1782d2ba27c2f24143da5dee80a
james <jmaslek11@gmail.com>
2022-11-16 18:57:59 -0500

    Balance Sheet SDK

--- openbb_terminal/stocks/fundamental_analysis/sdk_helpers.py
+++ openbb_terminal/stocks/fundamental_analysis/sdk_helpers.py
@@ -72,3 +72,66 @@ def get_income_statement(
         df = eodhd_model.get_financials(symbol, "income", quarterly, ratios)
         return df
     return pd.DataFrame()
+
+
+def get_balance_sheet(
+    symbol: str,
+    quarterly: bool = False,
+    ratios: bool = False,
+    source: str = "YahooFinance",
+    limit: int = 10,
+) -> pd.DataFrame:
+    """Get balance sheet.
+
+    Parameters
+    ----------
+    symbol : str
+        Symbol to get balance sheet for
+    source : str, optional
+        Data source for balance sheet, by default "YahooFinance"
+    quarterly : bool, optional
+        Flag to get quarterly data
+    ratios : bool, optional
+       Flag to return data as a percent change.
+    limit : int
+        Number of statements to return (free tiers may be limited to 5 years)
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe of income statement
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> balance_sheet = openbb.stocks.fa.balance("AAPL", source="YahooFinance)
+
+    If you have a premium AlphaVantage key, you can use the quarterly flag to get quarterly statements
+    >>> quarterly_income_statement = openbb.stocks.fa.balance("AAPL", source="AlphaVantage", quarterly=True)
+    """
+    if source == "YahooFinance":
+        if quarterly:
+            print(
+                "Quarterly income statement not available from Yahoo Finance.  Returning annual"
+            )
+        df = yahoo_finance_model.get_financials(
+            symbol=symbol, statement="balance-sheet", ratios=ratios
+        )
+        return df
+    if source == "AlphaVantage":
+        df = av_model.get_balance_sheet(
+            symbol=symbol, quarterly=quarterly, ratios=ratios, limit=limit
+        )
+        return df
+    if source == "FinancialModelingPrep":
+        df = fmp_model.get_balance(
+            symbol=symbol, limit=limit, quarterly=quarterly, ratios=ratios
+        )
+        return df
+    if source == "Polygon":
+        df = polygon_model.get_financials(symbol, "balance", quarterly, ratios)
+        return df
+    if source == "EODHD":
+        df = eodhd_model.get_financials(symbol, "balance", quarterly, ratios)
+        return df
+    return pd.DataFrame()
