bb4e04116a6e8e14bd5cfbc38afaf08da33464af
Chavithra PARANA <chavithra@gmail.com>
2022-11-21 12:37:33 +0100

    Update stocks/sia

--- openbb_terminal/stocks/sector_industry_analysis/sia_controller.py
+++ openbb_terminal/stocks/sector_industry_analysis/sia_controller.py
@@ -680,7 +680,42 @@ class SectorIndustryAnalysisController(BaseController):
             add_help=False,
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
             prog="metric",
-            description="Visualize a particular metric with the filters selected",
+            description=
+            " Visualize a particular metric with the filters selected"
+            " Available Metrics:"
+            "    roa           return on assets"
+            "    roe           return on equity"
+            "    cr            current ratio"
+            "    qr            quick ratio"
+            "    de            debt to equity"
+            "    tc            total cash"
+            "    tcs           total cash per share"
+            "    tr            total revenue"
+            "    rps           revenue per share"
+            "    rg            revenue growth"
+            "    eg            earnings growth"
+            "    pm            profit margins"
+            "    gp            gross profits"
+            "    gm            gross margins"
+            "    ocf           operating cash flow"
+            "    om            operating margins"
+            "    fcf           free cash flow"
+            "    td            total debt"
+            "    ebitda        earnings before interest, taxes, depreciation and amortization"
+            "    ebitdam       ebitda margins"
+            "    rec           recommendation mean"
+            "    mc            market cap"
+            "    fte           full time employees"
+            "    er            enterprise to revenue"
+            "    bv            book value"
+            "    ss            shares short"
+            "    pb            price to book"
+            "    beta          beta"
+            "    fs            float shares"
+            "    sr            short ratio"
+            "    peg           peg ratio"
+            "    ev            enterprise value"
+            "    fpe           forward P/E,",
         )
         parser.add_argument(
             "-m",
@@ -708,45 +743,6 @@ class SectorIndustryAnalysisController(BaseController):
         )
         if other_args and "-" not in other_args[0][0]:
             other_args.insert(0, "-m")
-
-        if not other_args:
-            help_text = """Available Metrics:
-    roa           return on assets
-    roe           return on equity
-    cr            current ratio
-    qr            quick ratio
-    de            debt to equity
-    tc            total cash
-    tcs           total cash per share
-    tr            total revenue
-    rps           revenue per share
-    rg            revenue growth
-    eg            earnings growth
-    pm            profit margins
-    gp            gross profits
-    gm            gross margins
-    ocf           operating cash flow
-    om            operating margins
-    fcf           free cash flow
-    td            total debt
-    ebitda        earnings before interest, taxes, depreciation and amortization
-    ebitdam       ebitda margins
-    rec           recommendation mean
-    mc            market cap
-    fte           full time employees
-    er            enterprise to revenue
-    bv            book value
-    ss            shares short
-    pb            price to book
-    beta          beta
-    fs            float shares
-    sr            short ratio
-    peg           peg ratio
-    ev            enterprise value
-    fpe           forward P/E
-            """
-            console.print(help_text)
-            return
 
         ns_parser = self.parse_known_args_and_warn(
             parser, other_args, EXPORT_BOTH_RAW_DATA_AND_FIGURES
@@ -787,11 +783,24 @@ class SectorIndustryAnalysisController(BaseController):
     @log_start_end(log=logger)
     def call_vis(self, other_args: List[str]):
         """Process vis command"""
+
+        statement_string = {
+            "BS": "Balance Sheet Statement",
+            "IS": "Income Statement",
+            "CF": "Cash Flow Statement",
+        }
+        help_text = "Visualize a particular metric with the filters selected\n"
+
+        for statement, statement_value in stockanalysis_model.SA_KEYS.items():
+            help_text += f"\n{statement_string[statement]}\n"
+            for k, v in statement_value.items():
+                help_text += f"  {k} {(10 - len(k)) * ' '} {v} \n"
+
         parser = argparse.ArgumentParser(
             add_help=False,
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
             prog="vis",
-            description="Visualize a particular metric with the filters selected",
+            description=help_text,
         )
         parser.add_argument(
             "-m",
@@ -820,22 +829,6 @@ class SectorIndustryAnalysisController(BaseController):
 
         if other_args and "-" not in other_args[0][0]:
             other_args.insert(0, "-m")
-
-        if not other_args:
-            statement_string = {
-                "BS": "Balance Sheet Statement",
-                "IS": "Income Statement",
-                "CF": "Cash Flow Statement",
-            }
-            help_text = ""
-
-            for statement, statement_value in stockanalysis_model.SA_KEYS.items():
-                help_text += f"\n{statement_string[statement]}\n"
-                for k, v in statement_value.items():
-                    help_text += f"  {k} {(10 - len(k)) * ' '} {v} \n"
-
-            console.print(help_text)
-            return
 
         ns_parser = self.parse_known_args_and_warn(
             parser, other_args, EXPORT_BOTH_RAW_DATA_AND_FIGURES, limit=10, raw=True
