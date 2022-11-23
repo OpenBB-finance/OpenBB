55f6eba5e55a6df02a127be53299e6cfaab76a64
Diogo Sousa <montezdesousa@gmail.com>
2022-11-23 18:42:42 +0000

    warn users if no extras

--- openbb_terminal/core/library/trail_map.py
+++ openbb_terminal/core/library/trail_map.py
@@ -4,21 +4,47 @@ from typing import Dict, Optional
 import pandas as pd
 
 from openbb_terminal.core.config.paths import MISCELLANEOUS_DIRECTORY
+from openbb_terminal.rich_config import console
 
 try:
     import darts  # pylint: disable=W0611 # noqa: F401
-
     from darts import utils  # pylint: disable=W0611 # noqa: F401
 
-    FORECASTING = True
-except ImportError:
-    FORECASTING = False
+    FORECASTING_TOOLKIT_ENABLED = True
+except ModuleNotFoundError:
+    FORECASTING_TOOLKIT_ENABLED = False
+    console.print(
+        "[yellow]"
+        "Forecasting Toolkit is disabled. "
+        "To use the Forecasting features please install the toolkit following the "
+        "instructions here: https://docs.openbb.co/sdk/quickstart/installation/"
+        "\n"
+        "[/yellow]"
+    )
+
+try:
+    import riskfolio  # pylint: disable=W0611 # noqa: F401
+
+    OPTIMIZATION_TOOLKIT_ENABLED = True
+except ModuleNotFoundError:
+    OPTIMIZATION_TOOLKIT_ENABLED = False
+    console.print(
+        "[yellow]"
+        "Portfolio Optimization Toolkit is disabled. "
+        "To use the Optimization features please install the toolkit following the "
+        "instructions here: https://docs.openbb.co/sdk/quickstart/installation/"
+        "\n"
+        "[/yellow]"
+    )
 
 
 class TrailMap:
     MAP_PATH = MISCELLANEOUS_DIRECTORY / "library" / "trail_map.csv"
     MAP_FORECASTING_PATH = (
         MISCELLANEOUS_DIRECTORY / "library" / "trail_map_forecasting.csv"
+    )
+    MAP_OPTIMIZATION_PATH = (
+        MISCELLANEOUS_DIRECTORY / "library" / "trail_map_optimization.csv"
     )
 
     @classmethod
@@ -84,9 +110,12 @@ class TrailMap:
 
     def load(self):
         map_dict = self.load_csv(path=self.MAP_PATH)
-        if FORECASTING:
+        if FORECASTING_TOOLKIT_ENABLED:
             map_forecasting_dict = self.load_csv(path=self.MAP_FORECASTING_PATH)
             map_dict = {**map_dict, **map_forecasting_dict}
+        if OPTIMIZATION_TOOLKIT_ENABLED:
+            map_optimization_dict = self.load_csv(path=self.MAP_OPTIMIZATION_PATH)
+            map_dict = {**map_dict, **map_optimization_dict}
 
         self.__map_dict = map_dict
 
