2a290273b6716de6b9a2d502f9670be2c39033bb
Chavithra PARANA <chavithra@gmail.com>
2022-11-28 23:05:06 +0100

    Update etf/ta

--- openbb_terminal/etf/technical_analysis/ta_controller.py
+++ openbb_terminal/etf/technical_analysis/ta_controller.py
@@ -44,7 +44,6 @@ class TechnicalAnalysisController(BaseController):
     """Technical Analysis Controller class"""
 
     CHOICES_COMMANDS = [
-        "load",
         "ema",
         "sma",
         "wma",
