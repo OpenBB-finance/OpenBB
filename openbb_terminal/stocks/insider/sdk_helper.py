b7eacd948038c468ba8ca070d9fd1fad00fa2f23
james <jmaslek11@gmail.com>
2022-12-08 18:08:06 -0500

    Add filter function

--- openbb_terminal/stocks/insider/sdk_helper.py
+++ openbb_terminal/stocks/insider/sdk_helper.py
@@ -26,3 +26,28 @@ def stats(symbol: str) -> pd.DataFrame:
     """
     link = f"http://openinsider.com/screener?s={symbol}"
     return openinsider_model.get_open_insider_data(link, has_company_name=False)
+
+
+def insider_filter(preset: str) -> pd.DataFrame:
+    """GEt insider trades based on preset filter
+
+    Parameters
+    ----------
+    preset : str
+       Name of preset filter
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of filtered insider data
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+
+    In order to filter, we pass one of the predefined .ini filters from OpenBBUserData/presets/stocks/insider
+    >>> filter = "Gold-Silver"
+    >>> insider_trades = openbb.stocks.ins.filter(filter)
+    """
+    url = openinsider_model.get_open_insider_link(preset)
+    return openinsider_model.get_open_insider_data(url, has_company_name=True)
