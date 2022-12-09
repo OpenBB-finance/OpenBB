4df05f5a3cbc5814b2f17b98522a0b2968548939
james <jmaslek11@gmail.com>
2022-12-09 18:42:19 -0500

    add 10 insider functions

--- openbb_terminal/stocks/insider/sdk_helper.py
+++ openbb_terminal/stocks/insider/sdk_helper.py
@@ -51,3 +51,179 @@ def insider_filter(preset: str) -> pd.DataFrame:
     """
     url = openinsider_model.get_open_insider_link(preset)
     return openinsider_model.get_open_insider_data(url, has_company_name=True)
+
+
+def lcb() -> pd.DataFrame:
+    """Get latest cluster buys
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.lcb()
+    """
+    return openinsider_model.get_print_insider_data("lcb")
+
+
+def lpsb() -> pd.DataFrame:
+    """Get latest penny stock buys
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.lpsb()
+    """
+    return openinsider_model.get_print_insider_data("lpsb")
+
+
+def lit() -> pd.DataFrame:
+    """Get latest insider trades
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.lit()
+    """
+    return openinsider_model.get_print_insider_data("lit")
+
+
+def lip() -> pd.DataFrame:
+    """Get latest insider purchases
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.lip()
+    """
+    return openinsider_model.get_print_insider_data("lip")
+
+
+def blip() -> pd.DataFrame:
+    """Get latest insider purchases > 25k
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.blip()
+    """
+    return openinsider_model.get_print_insider_data("blip")
+
+
+def blop() -> pd.DataFrame:
+    """Get latest officer purchases > 25k
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.blop()
+    """
+    return openinsider_model.get_print_insider_data("blop")
+
+
+def blcp() -> pd.DataFrame:
+    """Get latest CEO/CFO purchases > 25k
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.bclp()
+    """
+    return openinsider_model.get_print_insider_data("blcp")
+
+
+def lis() -> pd.DataFrame:
+    """Get latest insider sales
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.lis()
+    """
+    return openinsider_model.get_print_insider_data("lis")
+
+
+def blis() -> pd.DataFrame:
+    """Get latest insider sales > 100k
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.blis()
+    """
+    return openinsider_model.get_print_insider_data("blis")
+
+
+def blos() -> pd.DataFrame:
+    """Get latest officer sales > 100k
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.blos()
+    """
+    return openinsider_model.get_print_insider_data("blos")
+
+
+def blcs() -> pd.DataFrame:
+    """Get latest CEO/CFO sales > 100k
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latest insider trades
+
+    Examples
+    --------
+    >>> from openbb_terminal.sdk import openbb
+    >>> openbb.stocks.ins.blcs()
+    """
+    return openinsider_model.get_print_insider_data("blcs")
