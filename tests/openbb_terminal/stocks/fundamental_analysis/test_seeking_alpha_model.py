6430a5108ceaf6a06c030b461ba6b992d8dd71af
kajdo <juergen.kajdocsy@gmail.com>
2022-12-12 01:10:38 +0100

    change to match linter rules

--- tests/openbb_terminal/stocks/fundamental_analysis/test_seeking_alpha_model.py
+++ tests/openbb_terminal/stocks/fundamental_analysis/test_seeking_alpha_model.py
@@ -4,6 +4,7 @@ import pytest
 # IMPORTATION INTERNAL
 from openbb_terminal.stocks.fundamental_analysis import seeking_alpha_model
 
+
 @pytest.fixture(scope="module")
 # might not be necessary
 def vcr_config():
@@ -21,10 +22,11 @@ def test_get_seekingalpha_id(recorder):
 
 @pytest.mark.vcr
 def test_check_get_estimates_eps(recorder):
-    df = seeking_alpha_model.get_estimates_eps(ticker="JNJ") 
+    df = seeking_alpha_model.get_estimates_eps(ticker="JNJ")
     recorder.capture(df)
+
 
 @pytest.mark.vcr
 def test_check_get_estimates_rev(recorder):
-    df = seeking_alpha_model.get_estimates_eps(ticker="JNJ") 
+    df = seeking_alpha_model.get_estimates_eps(ticker="JNJ")
     recorder.capture(df)
