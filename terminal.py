c132dc8cf3087059c736f5980be8dd7421b3d3c9
Diogo Sousa <montezdesousa@gmail.com>
2022-12-15 17:02:55 +0000

    rename test controller

--- terminal.py
+++ terminal.py
@@ -1,10 +1,10 @@
 import sys
 from openbb_terminal import terminal_controller
-from openbb_terminal.integration_tests import test
+from openbb_terminal.integration_tests import integration_controller
 
 if __name__ == "__main__":
     sent_args = sys.argv[1:]
     if "-t" in sent_args or "--test" in sent_args:
-        test.main()
+        integration_controller.main()
     else:
         terminal_controller.parse_args_and_run()
