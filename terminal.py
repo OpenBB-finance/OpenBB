336182b524517be39a18ea6301526cf7857b3e9a
Diogo Sousa <montezdesousa@gmail.com>
2022-12-20 16:25:59 +0000

    must be up here

--- terminal.py
+++ terminal.py
@@ -4,9 +4,9 @@ from openbb_terminal import terminal_controller
 from openbb_terminal.integration_tests import integration_controller
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     sent_args = sys.argv[1:]
     if "-t" in sent_args or "--test" in sent_args:
-        multiprocessing.freeze_support()
         integration_controller.main()
     else:
         terminal_controller.parse_args_and_run()
