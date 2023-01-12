7d93f8ec86e64b8a028e54397b16ae0cd8a2fab1
Diogo Sousa <montezdesousa@gmail.com>
2023-01-12 14:24:25 +0000

    first version for login prompt

--- openbb_terminal/account/login_controller.py
+++ openbb_terminal/account/login_controller.py
@@ -1,44 +1,224 @@
 import argparse
-from typing import List
+from typing import List, Optional
+import webbrowser
+from openbb_terminal.helper_funcs import get_flair
 from openbb_terminal.rich_config import console, MenuText
+from openbb_terminal import terminal_controller
+from enum import Enum
+
+
+class Action(Enum):
+    """Class to handle status"""
+
+    EXIT = -1
+    DISPLAY_MENU = 1
+    LAUNCH_TERMINAL = 420
 
 
 class LoginController:
+    """Login Controller"""
+
+    CHOICES = ["login", "register", "h", "?", "help", "q", "quit", "e", "exit"]
+
     def __init__(self):
-        self.print_help()
+        self.controller_parser = argparse.ArgumentParser(
+            add_help=False,
+            prog="controller",
+        )
+        self.controller_parser.exit_on_error = False  # type: ignore
+        self.controller_parser.add_argument("cmd", choices=self.CHOICES)
+        self.call_help()
 
-    def print_help(self):
+    def call_help(self) -> Action:
         """Print help"""
-
         mt = MenuText("login/", 100)
         mt.add_cmd("login")
         mt.add_cmd("register")
         console.print(text=mt.menu_text, menu="Login")
+        return Action.DISPLAY_MENU
 
-    def call_login(self, other_args: List[str]):
-        parser = argparse.ArgumentParser(
+    def switch(self, an_input: str) -> Action:
+        """Process and dispatch input
+
+        Parameters
+        ----------
+        an_input: str
+            Input string
+
+        Returns
+        -------
+        Action
+            Action of the controller
+        """
+
+        try:
+            (
+                controller_known_args,
+                other_args,
+            ) = self.controller_parser.parse_known_args(an_input.split())
+
+            if controller_known_args.cmd in ("h", "help", "?"):
+                return self.call_help()
+            elif controller_known_args.cmd in ("e", "exit", "q", "quit"):
+                return Action.EXIT
+            elif controller_known_args.cmd == "register":
+                return self.call_register(cmd_args=other_args)
+            elif controller_known_args.cmd == "login":
+                return self.call_login(cmd_args=other_args)
+            else:
+                console.print("Command not recognized!")
+                return Action.DISPLAY_MENU
+        except Exception:
+            console.print("Command not recognized!")
+            return Action.DISPLAY_MENU
+
+    def call_register(self, cmd_args: List[str]) -> Action:
+        """Call register
+
+        Parameters
+        ----------
+        cmd_args: List[str]
+            Other arguments
+
+        Returns
+        -------
+        Action
+            Action of the controller
+        """
+        cmd_parser = argparse.ArgumentParser(
+            add_help=False,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            prog="register",
+            description="Register openbb account",
+        )
+        cmd_parser.add_argument(
+            "-h", "--help", action="store_true", help="show this help message"
+        )
+        ns_parser = self.parse_simple_args(cmd_parser, cmd_args)
+
+        if ns_parser:
+            webbrowser.open("https://my.openbb.co/register")
+
+        return Action.DISPLAY_MENU
+
+    def call_login(self, cmd_args: List[str]):
+        """Call login
+
+        Parameters
+        ----------
+        cmd_args: List[str]
+            Other arguments
+
+        Returns
+        -------
+        Action
+            Action of the controller
+        """
+
+        cmd_parser = argparse.ArgumentParser(
             add_help=False,
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
             prog="login",
             description="Login to your openbb account",
         )
-        parser.add_argument(
+        cmd_parser.add_argument(
             "-e",
             "--email",
             type=str,
             dest="email",
             help="The email for the user",
-            required="-h" not in other_args,
+            required="-h" not in cmd_args and "--help" not in cmd_args,
         )
-        parser.add_argument(
+        cmd_parser.add_argument(
             "-p",
             "--password",
             type=str,
             dest="password",
             help="The password for the user",
-            required="-h" not in other_args,
+            required="-h" not in cmd_args and "--help" not in cmd_args,
         )
+        cmd_parser.add_argument(
+            "-h", "--help", action="store_true", help="show this help message"
+        )
+
+        ns_parser = self.parse_simple_args(cmd_parser, cmd_args)
+        if ns_parser:
+            print("Login tried")
+            return Action.LAUNCH_TERMINAL
+        else:
+            print("Failed to attempt login")
+            return Action.DISPLAY_MENU
+
+    @staticmethod
+    def parse_simple_args(
+        parser: argparse.ArgumentParser, other_args: List[str]
+    ) -> Optional[argparse.Namespace]:
+        """Parse list of arguments into the supplied parser.
+
+        Parameters
+        ----------
+        parser: argparse.ArgumentParser
+            Parser with predefined arguments
+        other_args: List[str]
+            List of arguments to parse
+
+        Returns
+        -------
+        Optional[argparse.Namespace]:
+            Namespace with parsed arguments
+        """
+
+        try:
+            (ns_parser, unknown_args) = parser.parse_known_args(other_args)
+        except SystemExit:
+            return None
+
+        if ns_parser.help:
+            txt_help = parser.format_help()
+            console.print(f"[help]{txt_help}[/help]")
+            return None
+
+        if unknown_args:
+            console.print(
+                f"The following args couldn't be interpreted: {unknown_args}\n"
+            )
+
+        return ns_parser
 
 
 if __name__ == "__main__":
-    LoginController()
+    login_controller = LoginController()
+
+    action = Action.DISPLAY_MENU
+    while action == Action.DISPLAY_MENU:
+        _input = input(f"{get_flair()} $ ")
+        action = login_controller.switch(an_input=_input)
+
+        if action == Action.LAUNCH_TERMINAL:
+            break
+
+    if action == Action.LAUNCH_TERMINAL:
+        terminal_controller.parse_args_and_run()
+
+
+# login_controller
+# login
+# args
+# email
+# password
+# help
+# subcommands always available
+# exit/e
+# quit/q
+# help/h/?
+# register
+# subcommands always available
+# exit/e
+# quit/q
+# help/h/?
+
+
+# We might rebuild controllers with Command class
+# We are creating the command at each call
+# And then we call it
+# We could improve performance if we just create it once and then reuse it to call
