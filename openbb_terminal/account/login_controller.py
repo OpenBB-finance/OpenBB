f98f928cfdc81a9158cb627224de798a950bb102
Diogo Sousa <montezdesousa@gmail.com>
2023-01-12 14:29:18 +0000

    this var was unecessary

--- openbb_terminal/account/login_controller.py
+++ openbb_terminal/account/login_controller.py
@@ -29,6 +29,42 @@ class LoginController:
         self.controller_parser.add_argument("cmd", choices=self.CHOICES)
         self.call_help()
 
+    @staticmethod
+    def parse_simple_args(
+        parser: argparse.ArgumentParser, other_args: List[str]
+    ) -> Optional[argparse.Namespace]:
+        """Parse list of arguments into the supplied parser.
+
+        Parameters
+        ----------
+        parser: argparse.ArgumentParser
+            Parser with predefined arguments
+        other_args: List[str]
+            List of arguments to parse
+
+        Returns
+        -------
+        Optional[argparse.Namespace]:
+            Namespace with parsed arguments
+        """
+
+        try:
+            (ns_parser, unknown_args) = parser.parse_known_args(other_args)
+        except SystemExit:
+            return None
+
+        if ns_parser.help:
+            txt_help = parser.format_help()
+            console.print(f"[help]{txt_help}[/help]")
+            return None
+
+        if unknown_args:
+            console.print(
+                f"The following args couldn't be interpreted: {unknown_args}\n"
+            )
+
+        return ns_parser
+
     def call_help(self) -> Action:
         """Print help"""
         mt = MenuText("login/", 100)
@@ -149,42 +185,6 @@ class LoginController:
             print("Failed to attempt login")
             return Action.DISPLAY_MENU
 
-    @staticmethod
-    def parse_simple_args(
-        parser: argparse.ArgumentParser, other_args: List[str]
-    ) -> Optional[argparse.Namespace]:
-        """Parse list of arguments into the supplied parser.
-
-        Parameters
-        ----------
-        parser: argparse.ArgumentParser
-            Parser with predefined arguments
-        other_args: List[str]
-            List of arguments to parse
-
-        Returns
-        -------
-        Optional[argparse.Namespace]:
-            Namespace with parsed arguments
-        """
-
-        try:
-            (ns_parser, unknown_args) = parser.parse_known_args(other_args)
-        except SystemExit:
-            return None
-
-        if ns_parser.help:
-            txt_help = parser.format_help()
-            console.print(f"[help]{txt_help}[/help]")
-            return None
-
-        if unknown_args:
-            console.print(
-                f"The following args couldn't be interpreted: {unknown_args}\n"
-            )
-
-        return ns_parser
-
 
 if __name__ == "__main__":
     login_controller = LoginController()
@@ -193,9 +193,6 @@ if __name__ == "__main__":
     while action == Action.DISPLAY_MENU:
         _input = input(f"{get_flair()} $ ")
         action = login_controller.switch(an_input=_input)
-
-        if action == Action.LAUNCH_TERMINAL:
-            break
 
     if action == Action.LAUNCH_TERMINAL:
         terminal_controller.parse_args_and_run()
