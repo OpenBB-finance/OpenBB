01c9d094a379ce7fdb80c3072bc14d639e66ef8c
martinb-bb <105685594+martinb-bb@users.noreply.github.com>
2023-01-16 11:59:02 -0500

    large commit

--- openbb_terminal/forecast/forecast_controller.py
+++ openbb_terminal/forecast/forecast_controller.py
@@ -3403,3 +3403,98 @@ class ForecastController(BaseController):
                 breaklines=ns_parser.breaklines,
                 output_dir=ns_parser.save,
             )
+
+    @log_start_end(log=logger)
+    def call_whisper(self, other_args: List[str]):
+        """Utilize Whisper Model to transcribe a video. Currently only supports Youtube URLS"""
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            add_help=False,
+            prog="whisper",
+            description="""
+                Utilize Whisper Model to transcribe a video. Currently only supports Youtube URLS:
+                https://github.com/openai/whisper
+            """,
+        )
+        parser.add_argument(
+            "--video",
+            dest="video",
+            type=str,
+            default="",
+            help="video URLs to transcribe",
+        )
+        parser.add_argument(
+            "--model_name",
+            dest="model_name",
+            choices=whisper.available_models(),
+            default="small",
+            help="name of the Whisper model to use",
+        )
+        parser.add_argument(
+            "--subtitles_format",
+            dest="subtitles_format",
+            type=str,
+            choices=["vtt", "srt"],
+            help="the subtitle format to output",
+        )
+        parser.add_argument(
+            "--verbose",
+            dest="verbose",
+            type=str2bool,
+            default=False,
+            help="Whether to print out the progress and debug messages",
+        )
+        parser.add_argument(
+            "--task",
+            dest="task",
+            type=str,
+            choices=["transcribe", "translate"],
+            help="whether to perform X->X speech recognition ('transcribe') or X->English translation ('translate')",
+        )
+        parser.add_argument(
+            "--language",
+            dest="language",
+            type=str,
+            default=None,
+            choices=sorted(LANGUAGES.keys())
+            + sorted([k.title() for k in TO_LANGUAGE_CODE.keys()]),
+            help="language spoken in the audio, skip to perform language detection",
+        )
+        parser.add_argument(
+            "--breaklines",
+            dest="breaklines",
+            type=int,
+            default=0,
+            help="Whether to break lines into a bottom-heavy pyramid shape if line length exceeds N characters. 0 disables line breaking.",
+        )
+        parser.add_argument(
+            "--save",
+            dest="save",
+            type=str,
+            default=USER_FORECAST_WHISPER_DIRECTORY,
+            help="Whether to break lines into a bottom-heavy pyramid shape if line length exceeds N characters. 0 disables line breaking.",
+        )
+
+        parser = self.add_standard_args(
+            parser,
+        )
+        ns_parser = self.parse_known_args_and_warn(
+            parser,
+            other_args,
+            export_allowed=EXPORT_ONLY_FIGURES_ALLOWED,
+        )
+
+        if ns_parser:
+            if not helpers.check_parser_input(ns_parser, self.datasets):
+                return
+
+            whisper_view.transcribe_and_summarize(
+                video=ns_parser.video,
+                model_name=ns_parser.model_name,
+                subtitles_format=ns_parser.subtitles_format,
+                verbose=ns_parser.verbose,
+                task=ns_parser.task,
+                language=ns_parser.language,
+                breaklines=ns_parser.breaklines,
+                export=ns_parser.save,
+            )
--- openbb_terminal/forecast/whisper_view.py
+++ openbb_terminal/forecast/whisper_view.py
@@ -12,9 +12,96 @@ from openbb_terminal.decorators import log_start_end
 from openbb_terminal.forecast import helpers
 from openbb_terminal.rich_config import console
 
+import warnings
+import os
+import whisper
+from openbb_terminal.forecast.whisper_utils import (
+    slugify,
+    str2bool,
+    write_srt,
+    write_vtt,
+)
+import yt_dlp
+
+# from .utils import slugify, str2bool, write_srt, write_vtt
+import tempfile
+
 logger = logging.getLogger(__name__)
 # pylint: disable=too-many-arguments
 
 
-# @log_start_end(log=logger)
-# def transcribe_and_summarize():
+def get_audio(urls):
+    temp_dir = tempfile.gettempdir()
+
+    ydl = yt_dlp.YoutubeDL(
+        {
+            "quiet": True,
+            "verbose": False,
+            "format": "bestaudio",
+            "outtmpl": os.path.join(temp_dir, "%(id)s.%(ext)s"),
+            "postprocessors": [
+                {
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                    "key": "FFmpegExtractAudio",
+                }
+            ],
+        }
+    )
+
+    paths = {}
+
+    for url in urls:
+        result = ydl.extract_info(url, download=True)
+        print(f"Downloaded video \"{result['title']}\". Generating subtitles...")
+        paths[result["title"]] = os.path.join(temp_dir, f"{result['id']}.mp3")
+
+    return paths
+
+
+@log_start_end(log=logger)
+def transcribe_and_summarize(
+    video: str = "",
+    model_name: str = "small",
+    subtitles_format: str = "vtt",
+    export: str = "",
+    verbose: bool = False,
+    task: str = "transcribe",
+    language: str = None,
+    breaklines: int = 0,
+    output_dir: str = "/Users/martinbufi/OpenBBTerminal/openbb_terminal/forecast/whisper_output",
+):
+    console.print("Transcribing and summarizing...")
+    if model_name.endswith(".en"):
+        warnings.warn(
+            f"{model_name} is an English-only model, forcing English detection."
+        )
+    os.makedirs(output_dir, exist_ok=True)
+
+    model = whisper.load_model(model_name)
+    audios = get_audio(video)
+
+    for title, audio_path in audios.items():
+        warnings.filterwarnings("ignore")
+
+        result = whisper.transcribe(
+            audio_path,
+            model,
+            language=language,
+            verbose=verbose,
+            task=task,
+        )
+        warnings.filterwarnings("default")
+
+        if subtitles_format == "vtt":
+            vtt_path = os.path.join(output_dir, f"{slugify(title)}.vtt")
+            with open(vtt_path, "w", encoding="utf-8") as vtt:
+                write_vtt(result["segments"], file=vtt, line_length=breaklines)
+
+            print("Saved VTT to", os.path.abspath(vtt_path))
+        else:
+            srt_path = os.path.join(output_dir, f"{slugify(title)}.srt")
+            with open(srt_path, "w", encoding="utf-8") as srt:
+                write_srt(result["segments"], file=srt, line_length=breaklines)
+
+            print("Saved SRT to", os.path.abspath(srt_path))
