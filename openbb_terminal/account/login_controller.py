5a0012ddcf0c656566689b9549a8b7bf973a9512
Diogo Sousa <montezdesousa@gmail.com>
2023-01-19 18:49:33 +0000

    handle possible errors

--- openbb_terminal/account/login_controller.py
+++ openbb_terminal/account/login_controller.py
@@ -1,20 +1,31 @@
 from openbb_terminal.rich_config import console
 from openbb_terminal.account.login_model import (
+    Status,
+    Error,
     get_login_info,
-    is_login_valid,
+    get_login_status,
     request_login_info,
     launch_terminal,
 )
 
 
 def display_welcome_message():
-    console.print(
-        "You need to be connected to use the installer version of OpenBB."
-        "\nTo register please go on: https://my.openbb.co/register"
-    )
+    """Display welcome message"""
+    console.print("You need to be connected to use the installer version of OpenBB.\n")
 
 
 def get_user_input():
+    """Get user input
+
+    Returns
+    -------
+    email : str
+        User email
+    password : str
+        User password
+    save : bool
+        Save login info
+    """
     console.print("\nPlease enter your credentials:")
     email = console.input("> Email: ", style="blue")
     password = console.getpass("> Password: ", style="blue")
@@ -30,23 +41,38 @@ def get_user_input():
     return email, password, save
 
 
-def login_prompt():
-    display_welcome_message()
+def login_prompt(welcome=True):
+    """Login prompt
+
+    Parameters
+    ----------
+    welcome : bool, optional
+        Display welcome message, by default True
+    """
+    if welcome:
+        display_welcome_message()
+
     while True:
         email, password, save = get_user_input()
         login_info = request_login_info(email, password, save)
-        if login_info:
+        if isinstance(login_info, dict) and login_info:
             break
 
-    return launch_terminal(login_info)
+    launch_terminal(login_info)
 
 
 def main():
     """Main function"""
     login_info = get_login_info()
-    if is_login_valid(login_info):
-        return launch_terminal(login_info)
-    return login_prompt()
+    if login_info == Error.READ_ERROR:
+        return login_prompt()
+
+    status = get_login_status(login_info=login_info)
+    if status == Status.VALID_LOGIN:
+        launch_terminal(login_info=login_info)
+
+    if status == Status.INVALID_LOGIN:
+        login_prompt(welcome=False)
 
 
 if __name__ == "__main__":
--- openbb_terminal/account/login_model.py
+++ openbb_terminal/account/login_model.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
 import os.path
 import json
+from typing import Union
 import requests
 from openbb_terminal import terminal_controller
 from openbb_terminal.core.config.paths import SETTINGS_DIRECTORY
@@ -9,7 +12,20 @@ from openbb_terminal import feature_flags
 import jwt
 
 BASE_URL = "http://127.0.0.1:8000/terminal/"
-SUCCESS_MSG = "[green]\nLogin successful[/green]"
+
+
+class Status(Enum):
+    VALID_LOGIN = "[green]\nLogin successful.[/green]"
+    INVALID_LOGIN = "[red]\nInvalid login.[/red]"
+
+
+class Error(Enum):
+    CONNECTION_ERROR = "[red]\nConnection error.[/red]"
+    WRONG_CREDENTIALS = "[red]\nWrong credentials.[/red]"
+    UNVERIFIED_EMAIL = "[red]\nUnverified email.[/red]"
+    UNKNOWN_ERROR = "[red]\nUnknown error.[/red]"
+    SAVE_ERROR = "[red]\nError saving login info.[/red]"
+    READ_ERROR = "[red]\nError reading login info.[/red]"
 
 
 @dataclass(frozen=True)
@@ -19,6 +35,13 @@ class User:
 
 
 def launch_terminal(login_info: dict):
+    """Launch the terminal.
+
+    Parameters
+    ----------
+    login_info : dict
+        The login info.
+    """
     token = login_info.get("access_token", "")
     User.uuid = login_info.get("uuid", "")
     if token:
@@ -30,47 +53,135 @@ def launch_terminal(login_info: dict):
         terminal_controller.parse_args_and_run()
 
 
-def request_login_info(email: str, password: str, save: bool) -> dict:
+def write_to_file(data: dict, file_path: Path):
+    """Write data to a file.
+
+    Parameters
+    ----------
+    data : dict
+        The data to write.
+    file_path : Path
+        The file path.
+    """
+    try:
+        with open(file_path, "w") as outfile:
+            outfile.write(json.dumps(data))
+    except Exception:
+        console.print(Error.SAVE_ERROR.value)
+
+
+def process_response(response: requests.Response, save: bool) -> Union[dict, Error]:
+    """Process the response from the server.
+
+    Parameters
+    ----------
+    response : requests.Response
+        The response from the server.
+    save : bool
+        Save login info.
+
+    Returns
+    -------
+    Union[dict, Error]
+        The login info, or an error.
+    """
+    if response.status_code == 200:
+        console.print(Status.VALID_LOGIN.value)
+        login = response.json()
+        if save:
+            write_to_file(login, SETTINGS_DIRECTORY / "login.json")
+        return login
+
+    if response.status_code == 401:
+        console.print(Error.WRONG_CREDENTIALS.value)
+        return Error.WRONG_CREDENTIALS
+    elif response.status_code == 403:
+        console.print(Error.UNVERIFIED_EMAIL.value)
+        return Error.UNVERIFIED_EMAIL
+    else:
+        console.print(Error.UNKNOWN_ERROR.value)
+        return Error.UNKNOWN_ERROR
+
+
+def request_login_info(email: str, password: str, save: bool) -> Union[dict, Error]:
+    """Request login info from the server.
+
+    Parameters
+    ----------
+    email : str
+        The user email.
+    password : str
+        The user password.
+    save : bool
+        Save login info.
+
+    Returns
+    -------
+    Union[dict, Error]
+        The login info, or an error.
+    """
     data = {
         "email": email,
         "password": password,
         "remember": True,
     }
-    response = requests.post(BASE_URL + "login", json=data)
-    code = response.status_code
-    if code == 200:
-        console.print(SUCCESS_MSG)
-        login = response.json()
-        if save:
-            with open(SETTINGS_DIRECTORY / "login.json", "w") as outfile:
-                outfile.write(json.dumps(login))
-        return login
-    if code == 401:
-        console.print(f"[red]\n{str(response.json()['detail']).capitalize()}[/red]\n")
-    elif code == 403:
-        console.print(f"[red]\n{str(response.json()['message']).capitalize()}[/red]\n")
-    else:
-        console.print("[red]\nUnknown error.[/red]\n")
-    return {}
+
+    try:
+        response = requests.post(BASE_URL + "login", json=data)
+    except requests.exceptions.ConnectionError:
+        console.print(Error.CONNECTION_ERROR.value)
+        return Error.CONNECTION_ERROR
+
+    return process_response(response, save)
 
 
-def get_login_info() -> dict:
+def get_login_info() -> Union[dict, Error]:
+    """Get the login info from the file.
+
+    Returns
+    -------
+    dict
+        The login info.
+    """
     file_path = SETTINGS_DIRECTORY / "login.json"
-    if os.path.isfile(file_path):
-        with open(file_path, "r") as file:
-            return json.load(file)
+
+    try:
+        if os.path.isfile(file_path):
+            with open(file_path, "r") as file:
+                return json.load(file)
+    except Exception:
+        console.print(Error.READ_ERROR.value)
+        return Error.READ_ERROR
     return {}
 
 
-def is_login_valid(login_info: dict) -> bool:
+def get_login_status(login_info: dict) -> Union[Status, Error]:
+    """Check if the login info is valid.
+
+    Parameters
+    ----------
+    login_info : dict
+        The login info to check.
+
+    Returns
+    -------
+    Union[Status, Error]
+        The status of the login info, or an error.
+    """
     if "access_token" in login_info and "token_type" in login_info:
-        response = requests.get(
-            url=BASE_URL + "user",
-            headers={
-                "Authorization": f"{login_info['token_type'].title()} {login_info['access_token']}"
-            },
-        )
-        if response.status_code == 200:
-            console.print(SUCCESS_MSG)
-            return True
-    return False
+        try:
+            if (
+                requests.get(
+                    url=BASE_URL + "user",
+                    headers={
+                        "Authorization": f"{login_info['token_type'].title()} {login_info['access_token']}"
+                    },
+                ).status_code
+                == 200
+            ):
+                console.print(Status.VALID_LOGIN.value)
+                return Status.VALID_LOGIN
+        except requests.exceptions.ConnectionError:
+            console.print(Error.CONNECTION_ERROR.value)
+            return Error.CONNECTION_ERROR
+    return Status.INVALID_LOGIN
