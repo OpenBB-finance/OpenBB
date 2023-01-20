673db3b2cae16233b00a2fa5da3731edd5912dc2
Diogo Sousa <montezdesousa@gmail.com>
2023-01-20 15:33:44 +0000

    unecessary dataclass

--- openbb_terminal/account/user.py
+++ openbb_terminal/account/user.py
@@ -1,9 +1,6 @@
-# Will something like this for the logging
-from dataclasses import dataclass
 from typing import Optional
 
 
-@dataclass(frozen=True)
 class User:
     token_type: str = ""
     token: str = ""
