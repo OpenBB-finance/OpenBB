e1828411613d0c9420e04c2d69bf29f66a10a8a4
Diogo Sousa <montezdesousa@gmail.com>
2023-01-20 12:38:48 +0000

    remove user from statics

--- openbb_terminal/account/statics.py
+++ openbb_terminal/account/statics.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from openbb_terminal.rich_config import console
 
 BASE_URL = "http://127.0.0.1:8000/"
@@ -14,11 +13,3 @@ class Failure:
     def __init__(self, msg):
         self.msg = msg
         console.print(msg)
-
-
-@dataclass(frozen=True)
-class User:
-    token_type: str = ""
-    token: str = ""
-    email: str = ""
-    uuid: str = ""
