d517a1813826c7aac1f728f2c0b9f2972e9f37b7
northern-64bit <75195383+northern-64bit@users.noreply.github.com>
2023-01-22 17:07:29 +0100

    Add api paths + better descriptions

--- openbb_terminal/fixedincome/nyfed_view.py
+++ openbb_terminal/fixedincome/nyfed_view.py
@@ -120,7 +120,11 @@ def plot_fftr(
     export: str = "",
     external_axes: Optional[List[plt.Axes]] = None,
 ):
-    """Plot Federal Funding Target Range (upper and lower)
+    """Plot Federal Funding Target Range (upper and lower).
+
+    A bank rate is the interest rate a nation's central bank charges to its domestic banks to borrow money. The rates
+    central banks charge are set to stabilize the economy. In the United States, the Federal Reserve System's Board
+    of Governors set the bank rate, also known as the discount rate.
 
     Parameters
     ----------
@@ -175,7 +179,11 @@ def plot_effr(
     export: str = "",
     external_axes: Optional[List[plt.Axes]] = None,
 ):
-    """Plot Effective Federal Funds Rate
+    """Plot Effective Federal Funds Rate.
+
+    A bank rate is the interest rate a nation's central bank charges to its domestic banks to borrow money. The rates
+    central banks charge are set to stabilize the economy. In the United States, the Federal Reserve System's Board
+    of Governors set the bank rate, also known as the discount rate.
 
     Parameters
     ----------
@@ -233,7 +241,11 @@ def plot_obfr(
     export: str = "",
     external_axes: Optional[List[plt.Axes]] = None,
 ):
-    """Plot Overnight Bank Funding Rate
+    """Plot Overnight Bank Funding Rate.
+
+    A bank rate is the interest rate a nation's central bank charges to its domestic banks to borrow money. The rates
+    central banks charge are set to stabilize the economy. In the United States, the Federal Reserve System's Board
+    of Governors set the bank rate, also known as the discount rate.
 
     Parameters
     ----------
