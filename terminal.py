106ee5a37325e9bf6a982c0a61c43aa0cfd992ae
Diogo Sousa <montezdesousa@gmail.com>
2023-01-25 17:16:25 +0000

    pull imports up

--- terminal.py
+++ terminal.py
@@ -1,15 +1,15 @@
 import multiprocessing
 import sys
+from openbb_terminal.core.integration_tests import integration_controller
+from openbb_terminal.session import session_controller
+from openbb_terminal.terminal_helper import is_packaged_application
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     sent_args = sys.argv[1:]
     if "-t" in sent_args or "--test" in sent_args:
-        from openbb_terminal.core.integration_tests import integration_controller
 
         integration_controller.main()
     else:
-        from openbb_terminal.session import session_controller
-        from openbb_terminal.terminal_helper import is_packaged_application
 
         session_controller.main(guest_allowed=not is_packaged_application())
