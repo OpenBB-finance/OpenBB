46e16b17a4003f8414199eeeefa35c6aa0338d1e
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-01-26 11:13:47 +0000

    minor fix

--- openbb_terminal/session/sdk_session.py
+++ openbb_terminal/session/sdk_session.py
@@ -14,9 +14,9 @@ def get_session(email: str, password: str, save: bool):
 
 
 def login(email: str = "", password: str = "", save: bool = False):
-    local_session = Local.get_session()
+    session = Local.get_session()
 
-    if not local_session:
+    if not session:
         session = get_session(email, password, save)
 
     User.load_user_info(session)
@@ -24,6 +24,6 @@ def login(email: str = "", password: str = "", save: bool = False):
 
     if response and response.status_code == 200:
         Local.apply_configs(configs=json.loads(response.content))
-        return (response.text, response.status_code)
+        return ("Login successful", response.status_code)
 
     raise Exception("Failed to fetch user configs")
