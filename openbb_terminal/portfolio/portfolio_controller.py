7a374d52f453ac6ed1e65fca2c42b3bacafe1e8c
Jeroen Bouma <jeroenbouma@Jeroens-MacBook-Pro.local>
2023-01-26 16:37:15 +0100

    Some spelling

--- openbb_terminal/portfolio/portfolio_controller.py
+++ openbb_terminal/portfolio/portfolio_controller.py
@@ -214,7 +214,7 @@ class PortfolioController(BaseController):
 >   bro              brokers holdings, \t\t supports: robinhood, ally, degiro, coinbase
 >   po               portfolio optimization, \t optimize your portfolio weights efficiently[/menu]
 [cmds]
-    load             load transactions into the portfolio (use 'load --example' for an example)
+    load             load transactions into the portfolio (use load --example for an example)
     show             show existing transactions
     bench            define the benchmark
 [/cmds]
