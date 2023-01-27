f86a80af7ac9e17d0c59fee36a0a8ac1c766b671
kulbinder dio <kulbinderdio@gmail.com>
2023-01-27 18:44:52 +0000

    Updated tests with new limit argument

--- tests/openbb_terminal/alternative/realestate/test_landRegistry_model.py
+++ tests/openbb_terminal/alternative/realestate/test_landRegistry_model.py
@@ -7,18 +7,18 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def test_get_estate_sales():
-    assert len(landRegistry_model.get_estate_sales("DA119NF")) > 0
+    assert len(landRegistry_model.get_estate_sales("DA119NF", 10)) > 0
 
 
 def test_invalid_postcode_get_estate_sales():
-    assert len(landRegistry_model.get_estate_sales("DA11")) == 0
+    assert len(landRegistry_model.get_estate_sales("DA11", 10)) == 0
 
 
 def test_get_towns_sold_prices():
     assert (
         len(
             landRegistry_model.get_towns_sold_prices(
-                "DARTFORD", "2019-01-01", "2022-01-01"
+                "DARTFORD", "2019-01-01", "2022-01-01", 10
             )
         )
         > 0
