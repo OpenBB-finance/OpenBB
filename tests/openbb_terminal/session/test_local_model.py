57b4ba8bd1a05b652f0c651e062ef7187463ff8d
Diogo Sousa <montezdesousa@gmail.com>
2023-02-02 12:06:23 +0000

    data

--- tests/openbb_terminal/session/test_local_model.py
+++ tests/openbb_terminal/session/test_local_model.py
@@ -23,9 +23,14 @@ def test_save_good_session():
 
 
 def test_get_session():
-    open_mock = mock_open(read_data=json.dumps({"test": "test"}))
+    data = {
+        "access_token": "test_token",
+        "token_type": "bearer",
+        "uuid": "test_uuid",
+    }
+    open_mock = mock_open(read_data=json.dumps(data))
     with patch("openbb_terminal.session.local_model.open", open_mock, create=True):
-        assert local_model.get_session() == {"test": "test"}
+        assert local_model.get_session() == data
 
     open_mock.assert_called_with(local_model.SESSION_FILE_PATH)
     open_mock.return_value.read.assert_called_once()
