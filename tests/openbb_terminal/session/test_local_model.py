656e10ed387cfff8b6ffdbd574ab55daf9440e1b
Diogo Sousa <montezdesousa@gmail.com>
2023-02-02 11:59:34 +0000

    save session test

---
+++ tests/openbb_terminal/session/test_local_model.py
@@ -0,0 +1,22 @@
+# IMPORTATION STANDARD
+
+# IMPORTATION THIRDPARTY
+import json
+from unittest.mock import patch, mock_open
+
+# IMPORTATION INTERNAL
+from openbb_terminal.session import local_model
+
+
+def test_save_good_session():
+    open_mock = mock_open()
+    with patch("openbb_terminal.session.local_model.open", open_mock, create=True):
+        data = {
+            "access_token": "test_token",
+            "token_type": "bearer",
+            "uuid": "test_uuid",
+        }
+        local_model.save_session(data=data)
+
+    open_mock.assert_called_with(local_model.SESSION_FILE_PATH, "w")
+    open_mock.return_value.write.assert_called_once_with(json.dumps(data))
