2fca2c0ec3a0517ebf9166efcce7f124bc48bc78
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-02-09 11:52:16 +0000

    resolve conflicting tests

--- openbb_terminal/session/hub_model.py
+++ openbb_terminal/session/hub_model.py
@@ -4,7 +4,7 @@ import requests
 
 from openbb_terminal.rich_config import console
 
-BASE_URL = "https://payments.openbb.dev/"
+BASE_URL = "http://127.0.0.1:8000/"
 TIMEOUT = 15
 
 CONNECTION_ERROR_MSG = "[red]Connection error.[/red]"
