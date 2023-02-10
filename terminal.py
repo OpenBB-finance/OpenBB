96e31c3e82e1594c5c8ba8f814af96e60cf31807
Diogo Sousa <montezdesousa@gmail.com>
2023-02-10 15:02:22 +0000

    move env load

--- terminal.py
+++ terminal.py
@@ -1,7 +1,6 @@
 import multiprocessing
 import sys
 
-from openbb_terminal.core.config.paths import load_dotenv_with_priority
 from openbb_terminal.core.integration_tests import integration_controller
 from openbb_terminal.session import session_controller
 from openbb_terminal.terminal_helper import is_auth_enabled, is_installer
@@ -9,10 +8,6 @@ from openbb_terminal.terminal_helper import is_auth_enabled, is_installer
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     sent_args = sys.argv[1:]
-
-    # When authentication is fully implemented
-    # Remove the line below and references to is_auth_enabled
-    load_dotenv_with_priority()
 
     if "-t" in sent_args or "--test" in sent_args:
         integration_controller.main()
