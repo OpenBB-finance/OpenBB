93e22a5925c386050951e8a7dcc41dbb50ec1381
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-02-13 10:31:56 +0000

    tests

--- tests/openbb_terminal/test_keys_model.py
+++ tests/openbb_terminal/test_keys_model.py
@@ -1659,3 +1659,53 @@ def test_set_shroom_key(
     )
 
     assert_keys_and_status(args, persist, expected, env_var_name_list, status)
+
+
+@patch.dict(os.environ, {})
+@pytest.mark.vcr
+@pytest.mark.record_stdout
+@pytest.mark.parametrize(
+    "args, persist, show_output, expected",
+    [
+        (
+            ["test_key"],
+            False,
+            True,
+            keys_model.KeyStatus.DEFINED_TEST_FAILED,
+        ),
+        (
+            ["test_key"],
+            False,
+            False,
+            keys_model.KeyStatus.DEFINED_TEST_FAILED,
+        ),
+        (
+            ["test_key"],
+            True,
+            True,
+            keys_model.KeyStatus.DEFINED_TEST_FAILED,
+        ),
+        (
+            ["REPLACE_ME"],
+            False,
+            True,
+            keys_model.KeyStatus.NOT_DEFINED,
+        ),
+    ],
+)
+def test_set_openbb_key(
+    args: List[str], persist: bool, show_output: bool, expected: str
+):
+    env_var_name_list = [
+        "OPENBB_OPENBB_PERSONAL_ACCESS_TOKEN",
+    ]
+
+    set_naive_environment(env_var_name_list)
+
+    status = keys_model.set_openbb_personal_access_token(
+        key=args[0],
+        persist=persist,
+        show_output=show_output,
+    )
+
+    assert_keys_and_status(args, persist, expected, env_var_name_list, status)
