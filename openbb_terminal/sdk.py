000499e951d9663e289ff3b6c3c2a9eda68be687
Diogo Sousa <montezdesousa@gmail.com>
2023-02-13 18:31:38 +0000

    ruff

--- openbb_terminal/sdk.py
+++ openbb_terminal/sdk.py
@@ -2,7 +2,6 @@
 from openbb_terminal import helper_funcs as helper  # noqa: F401
 from openbb_terminal.base_helpers import load_dotenv_and_reload_configs
 from openbb_terminal.config_terminal import theme  # noqa: F401
-from openbb_terminal.core.config.paths import load_dotenv_and_reload
 from openbb_terminal.core.library.breadcrumb import Breadcrumb, MetadataBuilder
 from openbb_terminal.core.library.trail_map import TrailMap
 from openbb_terminal.cryptocurrency.due_diligence.pycoingecko_model import (
