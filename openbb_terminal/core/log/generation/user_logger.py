65633cdf029efe42d36a4c2a2fb9ca113bf797ac
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-02-20 13:50:02 +0000

    get_user_uuid function

--- openbb_terminal/core/log/generation/user_logger.py
+++ openbb_terminal/core/log/generation/user_logger.py
@@ -1,11 +1,8 @@
 # IMPORTATION STANDARD
-import json
 import logging
-from typing import Optional
 
 # IMPORTATION THIRDPARTY
 # IMPORTATION INTERNAL
-from openbb_terminal.core.log.generation.common import do_rollover
 from openbb_terminal.session.user import User
 
 logger = logging.getLogger(__name__)
@@ -13,25 +10,10 @@ logger = logging.getLogger(__name__)
 NO_USER_PLACEHOLDER = "NA"
 
 
-def log_user(with_rollover: bool = True):
-    """Log user"""
-    if not User.is_guest():
-        _log_user_info()
-
-    if with_rollover:
-        do_rollover()
-
-
-def _log_user_info():
-    """Log user info"""
-    user_info = {"user_uuid": User.get_uuid()}
-    logger.info("USER: %s ", json.dumps(user_info))
-
-
-def get_user_uuid() -> Optional[str]:
+def get_user_uuid() -> str:
     """Get user UUID"""
 
     if not User.is_guest():
         return User.get_uuid()
 
-    return None
+    return NO_USER_PLACEHOLDER
