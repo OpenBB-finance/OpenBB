e374d38e6eadf3465e7032f61759b0530005013c
Diogo Sousa <montezdesousa@gmail.com>
2023-02-24 12:35:26 +0000

    start path refactor

--- openbb_terminal/core/session/local_model.py
+++ openbb_terminal/core/session/local_model.py
@@ -10,7 +10,7 @@ from openbb_terminal import (
 from openbb_terminal.base_helpers import strtobool
 from openbb_terminal.core.config import paths
 from openbb_terminal.core.config.paths import (
-    HIST_FILE_PATH,
+    USER_HIST_FILE_PATH,
     SETTINGS_DIRECTORY,
     USER_ROUTINES_DIRECTORY,
 )
@@ -83,7 +83,7 @@ def remove_session_file(file_path: Path = SESSION_FILE_PATH) -> bool:
         return False
 
 
-def remove_cli_history_file(file_path: Path = HIST_FILE_PATH) -> bool:
+def remove_cli_history_file(file_path: Path = USER_HIST_FILE_PATH) -> bool:
     """Remove the cli history file.
 
     Parameters
