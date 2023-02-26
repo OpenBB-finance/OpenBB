b5bcaf98301092dbf6f31572d4f3a8408d1f3a5a
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-02-26 13:12:06 -0800

    adds unit testing for cones

---
+++ tests/openbb_terminal/stocks/technical_analysis/test_volatility_model.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+import pandas as pd
+
+
+from openbb_terminal.common.technical_analysis import volatility_model
+
+MODELS = volatility_model.VOLATILITY_MODELS
+MOCK_DATA = pd.read_csv(
+    "tests/openbb_terminal/stocks/technical_analysis/csv/test_volatility_model/test_cones_df.csv",
+    index_col=0,
+)
+
+model = MODELS[0]
+lower_q = 0.05
+upper_q = 0.95
+is_crypto: bool = False
+
+
+@pytest.mark.parametrize(
+    "model, lower_q, upper_q, is_crypto",
+    [
+        (MODELS[0], 0.05, 0.95, False),
+        (MODELS[1], 0.05, 0.95, False),
+        (MODELS[2], 0.05, 0.95, False),
+        (MODELS[3], 0.05, 0.95, False),
+        (MODELS[4], 0.05, 0.95, False),
+        (MODELS[3], 0.10, 0.90, True),
+        (MODELS[2], 15, 85, True),
+    ],
+)
+@pytest.mark.vcr()
+def test_cones(recorder, model, lower_q, upper_q, is_crypto):
+    result = volatility_model.cones(data=MOCK_DATA, upper_q=0.95, lower_q=0.05)
+    recorder.capture(result)
