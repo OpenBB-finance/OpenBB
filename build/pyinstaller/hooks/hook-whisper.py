081defcf96d74f0e8c9bc4ebe8845749be8e5930
teh_coderer <me@tehcoderer.com>
2023-02-27 11:20:19 -0500

    Update hook-whisper.py

--- build/pyinstaller/hooks/hook-whisper.py
+++ build/pyinstaller/hooks/hook-whisper.py
@@ -11,7 +11,6 @@ datas += copy_metadata("requests")
 datas += copy_metadata("packaging")
 datas += copy_metadata("filelock")
 datas += copy_metadata("numpy")
-datas += copy_metadata("dataclasses")
 datas += copy_metadata("torch")
 
 candidates = [
