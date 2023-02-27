b71df4c9371c2009c3fa9b7762539ad813bbbe47
teh_coderer <me@tehcoderer.com>
2023-02-27 12:30:04 -0500

    merge killed it

--- openbb_terminal/core/sdk/controllers/stocks_sdk_controller.py
+++ openbb_terminal/core/sdk/controllers/stocks_sdk_controller.py
@@ -271,6 +271,7 @@ class StocksController(model.StocksRoot):
             `eodchain`: Get full EOD option date across all expirations\n
             `expirations`: Get Option Chain Expirations\n
             `generate_data`: Gets x values, and y values before and after premiums\n
+            `greeks`: Gets the greeks for a given option\n
             `grhist`: Get historical option greeks\n
             `grhist_chart`: Plots historical greeks for a given option. [Source: Syncretism]\n
             `hist`: Get historical option pricing.\n
