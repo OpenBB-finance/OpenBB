8686f38e43e693d55d634bd7fe5bcd0deb1177fb
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-03-01 10:18:56 +0000

    ruff

--- openbb_terminal/stocks/options/options_controller.py
+++ openbb_terminal/stocks/options/options_controller.py
@@ -8,8 +8,8 @@ from typing import List, Optional
 
 import pandas as pd
 
-from openbb_terminal import feature_flags as obbff
 import openbb_terminal.config_terminal as cfg
+from openbb_terminal import feature_flags as obbff
 from openbb_terminal.custom_prompt_toolkit import NestedCompleter
 from openbb_terminal.decorators import log_start_end
 from openbb_terminal.helper_funcs import (
