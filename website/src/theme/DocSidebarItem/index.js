6bf45816bccd662eed9e8f1683e9f4b3c8cd5249
jose-donato <zmcdonato@gmail.com>
2023-03-07 19:41:18 +0000

    fix: made it so from iframe only shows relevant sidebar

--- website/src/theme/DocSidebarItem/index.js
+++ website/src/theme/DocSidebarItem/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import DocSidebarItemCategory from '@theme/DocSidebarItem/Category';
-import DocSidebarItemLink from '@theme/DocSidebarItem/Link';
-import DocSidebarItemHtml from '@theme/DocSidebarItem/Html';
-export default function DocSidebarItem({item, ...props}) {
+import React from "react";
+import DocSidebarItemCategory from "@theme/DocSidebarItem/Category";
+import DocSidebarItemLink from "@theme/DocSidebarItem/Link";
+import DocSidebarItemHtml from "@theme/DocSidebarItem/Html";
+import { useIFrameContext } from "../Root";
+import { useLocation } from "@docusaurus/router";
+
+function shouldHideItem(item, productPath) {
+  const { href, items } = item;
+  if (!href) {
+    return items.some((item) => shouldHideItem(item, productPath));
+  }
+  return !href.startsWith(productPath);
+}
+
+export default function DocSidebarItem({ item, ...props }) {
+  const { isIFrame } = useIFrameContext();
+  const { pathname } = useLocation();
+
+  if (isIFrame) {
+    const firstTwoPathSegments = pathname.split("/").slice(0, 3).join("/");
+
+    if (shouldHideItem(item, firstTwoPathSegments)) {
+      return null;
+    }
+  }
+
   switch (item.type) {
-    case 'category':
+    case "category":
       return <DocSidebarItemCategory item={item} {...props} />;
-    case 'html':
+    case "html":
       return <DocSidebarItemHtml item={item} {...props} />;
-    case 'link':
+    case "link":
     default:
       return <DocSidebarItemLink item={item} {...props} />;
   }
