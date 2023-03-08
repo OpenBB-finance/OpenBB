4b199b570ebd3909729a11423e621e53d057dee6
jose-donato <zmcdonato@gmail.com>
2023-03-08 15:47:46 +0000

    fix: small fixes

--- frontend-components/live-options/src/App.vue
+++ frontend-components/live-options/src/App.vue
@@ -1,16 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
