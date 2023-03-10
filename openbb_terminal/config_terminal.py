78fa13046b8dcd4db48578398903121dfc2db899
teh_coderer <me@tehcoderer.com>
2023-03-10 05:35:15 -0600

    fix: Edge case of temp html files deleted when multiple terminals open and one is closed (#4448)
    Co-authored-by: montezdesousa <79287829+montezdesousa@users.noreply.github.com>

--- openbb_terminal/config_terminal.py
+++ openbb_terminal/config_terminal.py
@@ -22,9 +22,9 @@ from openbb_terminal.core.session.current_user import get_current_user
 from openbb_terminal.core.config.paths import MISCELLANEOUS_DIRECTORY
 from openbb_terminal.base_helpers import load_env_vars, strtobool
 from openbb_terminal.core.config.paths import MISCELLANEOUS_DIRECTORY
+from openbb_terminal.core.config.paths_helper import init_userdata
 from openbb_terminal.core.plots.backend import plots_backend
 from openbb_terminal.core.session.current_user import get_current_user
-from openbb_terminal.core.config.paths_helper import init_userdata
 
 from .helper_classes import TerminalStyle as _TerminalStyle
 
