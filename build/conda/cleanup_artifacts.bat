dac9d5c7102cf9f0f3ea8cb81c63c84c66bd00bf
andrewkenreich <andrew.kenreich@gmail.com>
2023-03-20 15:02:23 -0400

    working!!

--- build/conda/cleanup_artifacts.bat
+++ build/conda/cleanup_artifacts.bat
@@ -10,6 +10,7 @@ if errorlevel 1 (
     exit /b
 )
 
+
 :: Ensure that the obb conda environment exists
 for /f %%A in ('conda env list ^| findstr /c:"obb"') do set env_exists=1
 if not defined env_exists (
@@ -19,26 +20,31 @@ if not defined env_exists (
     exit /b
 )
 
+
 :: Ensure that the obb conda environment is activated
-set "conda_prefix=!CONDA_PREFIX!"
+set "path=!CONDA_PREFIX!"
+set "conda_prefix=%CONDA_DEFAULT_ENV%"
 if not defined conda_prefix (
-    echo The obb conda environment is not activated
+    echo The obb conda environment is not activated2
     echo Activate it by running 'conda activate obb'
     exit /b
 )
 
-if not "!conda_prefix!" == "obb" (
-    echo The obb conda environment is not activated
+if not !conda_prefix! == %CONDA_DEFAULT_ENV% (
+    echo The obb conda environment is not activated3
     echo Activate it by running 'conda activate obb'
     exit /b
 )
 
 :: Remove build artifacts
-for /f "delims=" %%A in ('where python') do set python_path=%%A
-for /f "delims=" %%B in ('dirname "!python_path!"') do set dir=%%B
-for /f "delims=" %%C in ('dirname "!dir!"') do set parent_dir=%%C
-set site_packages_dir=!parent_dir!\lib\python*\site-packages
-for /r "!site_packages_dir!" %%D in (direct_url.json) do if %%~zD LEQ 2 del /f /q "%%D"
+set site_packages_dir=!path!\Lib\site-packages
+cd !site_packages_dir!
+echo !site_packages_dir!
+@REM For /r %%~1f IN (direct_url.json) do Echo "%~1f"
+
+set "cwd=!site_packages_dir!"
+for /f "delims=" %%a in ('dir /b /s /a-d direct_url.json') do (del /f /q %%a)
+
 
 :: Say goodbye
-echo Done
+echo Done1
