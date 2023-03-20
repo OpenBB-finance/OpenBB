545b10ff4393fe8fd76b552f7861ffc62c8db368
jose-donato <zmcdonato@gmail.com>
2023-03-20 20:16:02 +0000

    feat: added imageurl for examples

--- website/src/theme/CodeBlock/Content/String.js
+++ website/src/theme/CodeBlock/Content/String.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import clsx from "clsx";
 import { useThemeConfig, usePrismTheme } from "@docusaurus/theme-common";
 import {
@@ -11,50 +11,33 @@ import {
 import Highlight, { defaultProps } from "prism-react-renderer";
 import Line from "@theme/CodeBlock/Line";
 import CopyButton from "@theme/CodeBlock/CopyButton";
-import PlayButton from "@site/src/components/General/PlayButton";
 import WordWrapButton from "@theme/CodeBlock/WordWrapButton";
 import Container from "@theme/CodeBlock/Container";
 import styles from "./styles.module.css";
 import { useLocation } from "react-router-dom";
 
-
-function checkIfImageLoads (url, callback) {
-  const img = new Image();
-  img.onload = function () {
-    callback(true);
-  };
-  img.onerror = function () {
-    callback(false);
-  };
-}
-
-function getImageUrl(pathname, text, setImageUrl) {
-const pathvalue = pathname.split("/")[4];
-  console.log(pathvalue)
+function getImageUrl(pathname, text) {
+  if (!pathname.startsWith("/bot/")) {
+    return null;
+  }
+  const pathvalue = pathname.split("/")[4];
   const platform = pathname.split("/")[3];
-  console.log(platform)
-  var imgname = "c3m"
 
-  // TODO - Check these - make sure they all work - So far need to do a special case for these 2
-  // menus as we don't do the same thing for them as we do for the other pages
-  if ((pathvalue == "charts" || pathvalue == "general") && platform == "discord") {
-    console.log(pathvalue + " " + platform)
+  let imgname = "c3m";
+  if (
+    (pathvalue == "charts" || pathvalue == "general") &&
+    platform == "discord"
+  ) {
     imgname = text.split(" ")[0].toLowerCase().replace("/", "");
-    console.log("here1")
   } else if (platform == "telegram") {
     if (pathvalue.toString() == "etfs") {
       imgname = text.split(" ")[1].toLowerCase().replace("/", "");
-      console.log("here2")
-    }else{
-      console.log(pathvalue + " " + platform)
+    } else {
       imgname = text.split(" ")[0].toLowerCase().replace("/", "");
-      console.log("here122")
     }
-
   } else {
-    try{
+    try {
       imgname = text.split(" ")[1].toLowerCase().replace("/", "");
-      // special case for defi - i know its not great :D
       if (imgname == "defi") {
         imgname = text.split(" ")[2].toLowerCase().replace("/", "");
       }
@@ -63,23 +46,8 @@ const pathvalue = pathname.split("/")[4];
     }
   }
 
-  //console.log(platform)
-  //console.log(pathvalue)
-  //console.log(imgname)
-
-  const img5 = `https://openbb-assets.s3.amazonaws.com/${platform}/${pathvalue}/${imgname}.png`;
-  console.log("final url",img5)
-  setImageUrl(img5)
-
-  /*checkIfImageLoads(img5, function (exists) {
-    if (exists) {
-      console.log("exists")
-      setImageUrl(img5)
-    } else {
-      console.log("doesnt exist")
-      setImageUrl(null)
-    }
-  })*/
+  const finalImage = `https://openbb-assets.s3.amazonaws.com/${platform}/${pathvalue}/${imgname}.png`;
+  return finalImage;
 }
 
 export default function CodeBlockString({
@@ -116,79 +84,101 @@ export default function CodeBlockString({
   const newDate = getThirdFriday();
 
   const newCode = code.replace("2022-07-29", newDate);
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
 
-  useEffect(() => {getImageUrl(pathname, newCode, setImageUrl)}, [pathname, newCode]);
+  // get Container sibling
+
+  useEffect(() => {
+    if (ref.current && pathname.startsWith("/bot/")) {
+      // get ref.current sibling above
+      const container = ref.current.parentElement;
+      const containerSibling = container.previousElementSibling;
+      if (containerSibling) {
+        if (containerSibling.id.includes("examples")) {
+          const finalImage = getImageUrl(pathname, newCode);
+          setImageUrl(finalImage);
+        }
+      }
+    }
+  }, []);
+
+  const ref = useRef(null);
 
   return (
     <>
-    <Container
-      as="div"
-      className={clsx(
-        blockClassName,
-        language &&
-          !blockClassName.includes(`language-${language}`) &&
-          `language-${language}`
-      )}
-    >
-      {title && <div className={styles.codeBlockTitle}>{title}</div>}
-      <div className={styles.codeBlockContent}>
-        <Highlight
-          {...defaultProps}
-          theme={prismTheme}
-          code={newCode}
-          language={language ?? "text"}
-        >
-          {({ className, tokens, getLineProps, getTokenProps }) => (
-            <pre
-              /* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */
-              tabIndex={0}
-              ref={wordWrap.codeBlockRef}
-              className={clsx(className, styles.codeBlock, "thin-scrollbar")}
-            >
-              <code
-                style={
-                  shouldWordwrapByDefault
-                    ? {
-                        whiteSpace: "pre-wrap",
-                        overflowWrap: "anywhere",
-                      }
-                    : {}
-                }
-                className={clsx(
-                  styles.codeBlockLines,
-                  showLineNumbers && styles.codeBlockLinesWithNumbering
-                )}
+      <Container
+        as="div"
+        className={clsx(
+          blockClassName,
+          language &&
+            !blockClassName.includes(`language-${language}`) &&
+            `language-${language}`
+        )}
+      >
+        {title && <div className={styles.codeBlockTitle}>{title}</div>}
+        <div className={styles.codeBlockContent} ref={ref}>
+          <Highlight
+            {...defaultProps}
+            theme={prismTheme}
+            code={newCode}
+            language={language ?? "text"}
+          >
+            {({ className, tokens, getLineProps, getTokenProps }) => (
+              <pre
+                /* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */
+                tabIndex={0}
+                ref={wordWrap.codeBlockRef}
+                className={clsx(className, styles.codeBlock, "thin-scrollbar")}
               >
-                {tokens.map((line, i) => (
-                  <Line
-                    key={i}
-                    line={line}
-                    getLineProps={getLineProps}
-                    getTokenProps={getTokenProps}
-                    classNames={lineClassNames[i]}
-                    showLineNumbers={showLineNumbers}
-                  />
-                ))}
-              </code>
-            </pre>
-          )}
-        </Highlight>
-        <div className={styles.buttonGroup}>
-          {(wordWrap.isEnabled || wordWrap.isCodeScrollable) && (
-            <WordWrapButton
-              className={styles.codeButton}
-              onClick={() => wordWrap.toggle()}
-              isEnabled={wordWrap.isEnabled}
-            />
-          )}
-          <CopyButton className={styles.codeButton} code={newCode} />
+                <code
+                  style={
+                    shouldWordwrapByDefault
+                      ? {
+                          whiteSpace: "pre-wrap",
+                          overflowWrap: "anywhere",
+                        }
+                      : {}
+                  }
+                  className={clsx(
+                    styles.codeBlockLines,
+                    showLineNumbers && styles.codeBlockLinesWithNumbering
+                  )}
+                >
+                  {tokens.map((line, i) => (
+                    <Line
+                      key={i}
+                      line={line}
+                      getLineProps={getLineProps}
+                      getTokenProps={getTokenProps}
+                      classNames={lineClassNames[i]}
+                      showLineNumbers={showLineNumbers}
+                    />
+                  ))}
+                </code>
+              </pre>
+            )}
+          </Highlight>
+          <div className={styles.buttonGroup}>
+            {(wordWrap.isEnabled || wordWrap.isCodeScrollable) && (
+              <WordWrapButton
+                className={styles.codeButton}
+                onClick={() => wordWrap.toggle()}
+                isEnabled={wordWrap.isEnabled}
+              />
+            )}
+            <CopyButton className={styles.codeButton} code={newCode} />
+          </div>
         </div>
-      </div>
-    </Container>
-    {imageUrl && (
-      <img src={imageUrl} alt="example" />
-    )}
+      </Container>
+      {imageUrl && (
+        <img
+          onError={() => {
+            setImageUrl(null);
+          }}
+          src={imageUrl}
+          alt="example"
+        />
+      )}
     </>
   );
 }
