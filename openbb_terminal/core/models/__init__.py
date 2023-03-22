675144d8bcda57b54e377613a581a14ded980003
Diogo Sousa <montezdesousa@gmail.com>
2023-03-22 15:39:10 +0000

    Add sources model

--- openbb_terminal/core/models/__init__.py
+++ openbb_terminal/core/models/__init__.py
@@ -1,11 +1,13 @@
 from openbb_terminal.core.models.credentials_model import CredentialsModel
 from openbb_terminal.core.models.preferences_model import PreferencesModel
 from openbb_terminal.core.models.profile_model import ProfileModel
+from openbb_terminal.core.models.sources_model import SourcesModel
 from openbb_terminal.core.models.user_model import UserModel
 
 __all__ = [
     "CredentialsModel",
     "PreferencesModel",
     "ProfileModel",
+    "SourcesModel",
     "UserModel",
 ]
