90daf70cad56f2c12b5fd76e4eef9c6b6e354c9a
Diogo Sousa <montezdesousa@gmail.com>
2023-03-27 17:02:23 +0100

    ruff

--- openbb_terminal/core/models/preferences_model.py
+++ openbb_terminal/core/models/preferences_model.py
@@ -1,5 +1,5 @@
 import os
-from typing import Literal, Optional
+from typing import Optional
 
 from pydantic import NonNegativeInt, PositiveFloat, PositiveInt
 from pydantic.dataclasses import dataclass
