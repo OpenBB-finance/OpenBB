08ff0993ebacf92aa237cabdf40649aab6f3a95e
jose-donato <zmcdonato@gmail.com>
2023-03-28 13:29:59 +0000

    fix: improved light mode and not found page

--- website/src/theme/NotFound.js
+++ website/src/theme/NotFound.js
@@ -1,62 +1,49 @@
 import React from "react";
 import LetteringLogo from "@site/src/components/Icons/LetteringLogo";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import DarkBlueRadialGradient from "../components/Icons/RadialGradients/DarkBlue";
+import RubyRedRadialGradient from "../components/Icons/RadialGradients/RubyRed";
 
 export default function NotFound() {
   return (
-    <div className="h-screen overflow-hidden">
-      <div className="flex flex-col items-center justify-center mt-24 px-4">
-        <LetteringLogo width={260} />
-      </div>
-      <div className="flex flex-col items-center justify-center h-[80%] px-4">
+    <Layout title="404 | OpenBB Docs">
+      <div className="flex flex-col items-center justify-center my-auto p-10 relative overflow-hidden min-h-[60vh] 2xl:min-h-[70vh]">
+        <DarkBlueRadialGradient className="absolute opacity-40 -left-[1000px] w-[1600px] h-[1600px] lg:-left-[1000px] lg:opacity-40 -top-20 xl:-top-60 xl:-left-[800px] 2xl:opacity-50" />
+        <RubyRedRadialGradient className="absolute opacity-40 -right-[1000px] w-[1600px] h-[1600px] lg:-right-[1000px] lg:opacity-40 -top-20 xl:-top-60 xl:-right-[800px] 2xl:opacity-50" />
+
         <img
-          // TODO - fix this Z-index
-          alt="background"
-          className="absolute -z-5 w-[1100px] h-[980px] object-cover"
-          src="img/background404.png"
+          src="img/404.png"
+          className="shadow-none"
+          width={612}
+          height={271}
+          alt="not found"
         />
-        <div className="mt-20">
-          <img
-            src={
-              // TODO - add 500 image and how to get status here
-              status == 404
-                ? "img/404.png"
-                : "img/404.png"
-            }
-            width={612}
-            height={271}
-            alt="not found"
-          />
-          <div className="mt-8 flex-center flex-col gap-0">
-            <h1 className="uppercase text-center tracking-widest font-bold text-2xl">
-              OOPS! PAGE NOT FOUND!
-            </h1>
-            <p className="text-center text-xl mt-6">
-              Sorry the page you’re looking for doesn’t exist.
-              <br />
-              If you think something is broken, please report a problem.
-            </p>
-            <div className="flex flex-col md:flex-row gap-6 mt-10 justify-center w-full">
+        <div className="mt-8 flex-center flex-col gap-0">
+          <h1 className="uppercase text-center tracking-widest font-bold text-2xl">
+            OOPS! PAGE NOT FOUND!
+          </h1>
+          <p className="text-center text-xl mt-6">
+            Sorry the page you’re looking for doesn’t exist.
+            <br />
+            If you think something is broken, please report a problem.
+          </p>
+          <div className="flex flex-col md:flex-row gap-6 mt-10 justify-center w-full">
+            <Link to="/" className="_btn">
+              Return Home
+            </Link>
             <a
-                href="https://docs.openbb.co"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="_btn-secondary"
-              >
-                Return Home
-              </a>
-              <a
-                // TODO - add link to report problem
-                href="https://openbb.co/support"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="_btn-secondary"
-              >
-                Report problem
-              </a>
-            </div>
+              // TODO - add link to report problem
+              href="https://openbb.co/support"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="_btn-secondary"
+            >
+              Report problem
+            </a>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
