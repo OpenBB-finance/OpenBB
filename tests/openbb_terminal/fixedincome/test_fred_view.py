b4b64f30642f84b78899cb08a43236641735ad22
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-03-30 22:15:56 +0200

    Re-record unit test

--- tests/openbb_terminal/fixedincome/test_fred_view.py
+++ tests/openbb_terminal/fixedincome/test_fred_view.py
@@ -5,34 +5,52 @@ import pytest
 from openbb_terminal.fixedincome import fred_view
 
 
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": [("User-Agent", None)],
+        "filter_query_parameters": [
+            ("api_key", "MOCK_API_KEY"),
+        ],
+    }
+
+
+@pytest.mark.record_http
 def test_plot_sofr():
     fred_view.plot_sofr()
 
 
+@pytest.mark.record_http
 def test_plot_sonia():
     fred_view.plot_sonia()
 
 
+@pytest.mark.record_http
 def test_plot_ameribor():
     fred_view.plot_ameribor()
 
 
+@pytest.mark.record_http
 def test_plot_fed():
     fred_view.plot_fed()
 
 
+@pytest.mark.record_http
 def test_plot_iorb():
     fred_view.plot_iorb()
 
 
+@pytest.mark.record_http
 def test_plot_fftr():
     fred_view.plot_fftr()
 
 
+@pytest.mark.record_http
 def test_plot_projection():
     fred_view.plot_projection()
 
 
+@pytest.mark.record_http
 @pytest.mark.parametrize(
     "parameter, start_date, end_date, kwargs",
     [
@@ -43,18 +61,22 @@ def test_plot_dwcpcr(parameter, start_date, end_date, kwargs):
     fred_view.plot_dwpcr(parameter, start_date, end_date, **kwargs)
 
 
+@pytest.mark.record_http
 def test_plot_ecb():
     fred_view.plot_ecb()
 
 
+@pytest.mark.record_http
 def test_plot_tmc():
     fred_view.plot_tmc()
 
 
+@pytest.mark.record_http
 def test_plot_ffrmc():
     fred_view.plot_ffrmc()
 
 
+@pytest.mark.record_http
 @pytest.mark.parametrize(
     "date, kwargs",
     [
@@ -65,6 +87,7 @@ def test_display_yield_curve(date, kwargs):
     fred_view.display_yield_curve(date=date, **kwargs)
 
 
+@pytest.mark.record_http
 @pytest.mark.parametrize(
     "parameter, maturity, start_date, end_date, kwargs",
     [
@@ -75,6 +98,7 @@ def test_plot_usrates(parameter, maturity, start_date, end_date, kwargs):
     fred_view.plot_usrates(parameter, maturity, start_date, end_date, **kwargs)
 
 
+@pytest.mark.record_http
 @pytest.mark.parametrize(
     "parameter, start_date, end_date, kwargs",
     [
@@ -85,21 +109,26 @@ def test_plot_tbffr(parameter, start_date, end_date, kwargs):
     fred_view.plot_tbffr(parameter, start_date, end_date, **kwargs)
 
 
+@pytest.mark.record_http
 def test_plot_icebofa():
     fred_view.plot_icebofa()
 
 
+@pytest.mark.record_http
 def test_plot_moody():
     fred_view.plot_moody()
 
 
+@pytest.mark.record_http
 def test_plot_cp():
     fred_view.plot_cp()
 
 
+@pytest.mark.record_http
 def test_plot_spot():
     fred_view.plot_spot()
 
 
+@pytest.mark.record_http
 def test_plot_hqm():
     fred_view.plot_hqm()
