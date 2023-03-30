585f2ade91c682ff8190be752bfbf347e091db8b
Adi Sai <adithya.sairam1@gmail.com>
2023-03-30 18:20:13 -0400

    add unit tests

--- tests/openbb_terminal/common/test_ultima_newsmonitor_model.py
+++ tests/openbb_terminal/common/test_ultima_newsmonitor_model.py
@@ -4,10 +4,11 @@
 import pytest
 
 # IMPORTATION INTERNAL
-from openbb_terminal.common.ultima_newsmonitor_model import get_news
+from openbb_terminal.common.ultima_newsmonitor_model import get_news, supported_terms, get_company_info
 
 
-@pytest.mark.vcr
+# @pytest.mark.default_cassette("test_get_news.yaml")
+# @pytest.mark.vcr
 @pytest.mark.parametrize(
     "term, sources",
     [
@@ -20,4 +21,19 @@ from openbb_terminal.common.ultima_newsmonitor_model import get_news
 )
 def test_get_news(term, sources, recorder):
     df = get_news(term=term, sources=sources)
+    recorder.capture(df)
+
+
+# @pytest.mark.default_cassette("test_supported_terms.yaml")
+# @pytest.mark.vcr
+def test_supported_terms(recorder):
+    r = supported_terms()
+    recorder.capture(r)
+
+
+# @pytest.mark.default_cassette("test_get_company_info.yaml")
+# @pytest.mark.vcr
+@pytest.mark.parametrize("ticker", ["AAPL", "TSLA", "FCX", "asdf$#"], )
+def test_get_company_info(ticker, recorder):
+    df = get_company_info(ticker=ticker)
     recorder.capture(df)
