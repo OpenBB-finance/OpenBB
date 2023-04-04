87e9b5d1dadc9ead751c122d71f57928e11c53eb
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-04-04 15:20:33 -0700

    pylint disable=too-many-lines --> disable=C0302

--- openbb_terminal/stocks/stocks_helper.py
+++ openbb_terminal/stocks/stocks_helper.py
@@ -1,7 +1,8 @@
 """Main helper."""
 __docformat__ = "numpy"
 
-# pylint: disable=unsupported-assignment-operation,too-many-lines
+# pylint: disable=unsupported-assignment-operation
+# pylint: disable=C0302
 # pylint: disable=no-member,too-many-branches,too-many-arguments
 # pylint: disable=inconsistent-return-statements
 # pylint: disable=consider-using-dict-items
