ec2309f361fa8f30c967fa78c3093ad5806c0cb8
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-04-04 12:23:03 -0700

    adds s&p 500 sector presets

--- openbb_terminal/stocks/screener/finviz_view.py
+++ openbb_terminal/stocks/screener/finviz_view.py
@@ -255,6 +255,6 @@ def screener(
         return list(df_screen["Ticker"].values)
 
     console.print(
-        "The preset selected did not return a sufficient number of tickers. Two or more tickers are needed."
+        "Error: The preset selected did not return results. This might be a temporary error that is resolved by running the command again.  If no results continue to be returned, check the preset and expand the parameters."
     )
     return []
