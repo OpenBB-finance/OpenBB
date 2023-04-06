c0ac688472dc50048e8ae134def5a815f184744c
teh_coderer <me@tehcoderer.com>
2023-04-06 10:58:29 -0400

    Update stocks_helper.py

--- openbb_terminal/stocks/stocks_helper.py
+++ openbb_terminal/stocks/stocks_helper.py
@@ -1,8 +1,8 @@
 """Main helper."""
 __docformat__ = "numpy"
 
-# pylint: disable=unsupported-assignment-operation, too-many-lines
-# pylint: disable=no-member, too-many-branches, too-many-arguments
+# pylint: disable=unsupported-assignment-operation,C0302
+# pylint: disable=no-member,too-many-branches,too-many-arguments
 # pylint: disable=inconsistent-return-statements
 # pylint: disable=consider-using-dict-items
 
