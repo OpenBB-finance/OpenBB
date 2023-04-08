5408994feea1c2e08860b284556478a3d1a912bb
kulbinderdio <kulbinderdio@gmail.com>
2023-04-08 23:09:06 +0100

    Ruff fix

--- openbb_terminal/alternative/companieshouse/companieshouse_view.py
+++ openbb_terminal/alternative/companieshouse/companieshouse_view.py
@@ -5,10 +5,10 @@ import logging
 import os
 
 from openbb_terminal.alternative.companieshouse import companieshouse_model
+from openbb_terminal.core.session.current_user import get_current_user
 from openbb_terminal.decorators import log_start_end
 from openbb_terminal.helper_funcs import export_data, print_rich_table
 from openbb_terminal.rich_config import console
-from openbb_terminal.core.session.current_user import get_current_user
 
 logger = logging.getLogger(__name__)
 
