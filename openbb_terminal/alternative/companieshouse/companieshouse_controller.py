a7913cb03a59c79fe157bf1e55fe9869ffeac7f9
kulbinderdio <kulbinderdio@gmail.com>
2023-04-08 16:51:37 +0100

    fix codespell issue

--- openbb_terminal/alternative/companieshouse/companieshouse_controller.py
+++ openbb_terminal/alternative/companieshouse/companieshouse_controller.py
@@ -188,7 +188,7 @@ class CompaniesHouseController(BaseController):
             add_help=False,
             formatter_class=argparse.ArgumentDefaultsHelpFormatter,
             prog="signifcontrol",
-            description="Select the company number to retrieve persons with signifcant control of company. \
+            description="Select the company number to retrieve persons with significant control of company. \
             [Source: UK Companies House]",
         )
         parser.add_argument(
