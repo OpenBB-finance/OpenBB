27a8a7b777a328cb2b30465f6d376e8fa5ebf44c
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-04-10 16:58:43 +0100

    adding generation to the sdk

--- documentation/generate_documentation_commands.py
+++ documentation/generate_documentation_commands.py
@@ -4,9 +4,17 @@ import re
 from collections import defaultdict
 from pathlib import Path
 
+import pandas as pd
 import yaml
 
-from openbb_terminal.core.config.paths import I18N_DICT_LOCATION
+from openbb_terminal.core.config.paths import (
+    I18N_DICT_LOCATION,
+    MAP_FORECASTING_PATH,
+    MAP_OPTIMIZATION_PATH,
+    MAP_PATH,
+)
+
+trail_maps = [MAP_PATH, MAP_FORECASTING_PATH, MAP_OPTIMIZATION_PATH]
 
 en_file = I18N_DICT_LOCATION / "en.yml"
 
@@ -43,25 +51,43 @@ def get_command_list(en_dict: dict) -> list:
     return commands
 
 
-if __name__ == "__main__":
-    en_file_as_dict = read_yaml_file(path=en_file)
-    cmds = get_command_list(en_dict=en_file_as_dict)
+def get_sdk_command_list(trail_map: Path) -> list:
+    df = pd.read_csv(trail_map)
+    return df["trail"].tolist()
 
+
+def find_commands_in_files(files: list, commands: list) -> dict:
     found_commands = defaultdict(list)
 
-    # Find commands in markdown files
-    for file_path in md_files:
+    for file_path in files:
         with open(file_path) as file:
             file_contents = file.read()
 
-            for command in cmds:
-                pattern = rf"\b{re.escape(command)}\b"  # create regex pattern for whole word match
+            for command in commands:
+                # pattern to match whole words only
+                pattern = rf"\b{re.escape(command)}\b"
                 matches = re.finditer(pattern, file_contents)
 
-                for match in matches:
+                for _ in matches:
                     found_commands.setdefault(command, [])
                     if file_path not in found_commands[command]:
                         found_commands[command].append(file_path)
 
+    return found_commands
+
+
+if __name__ == "__main__":
+    en_file_as_dict = read_yaml_file(path=en_file)
+    cmds = get_command_list(en_dict=en_file_as_dict)
+    cmds_sdk = [
+        cmd for trail_map in trail_maps for cmd in get_sdk_command_list(trail_map)
+    ]
+
+    terminal_commands = find_commands_in_files(files=md_files, commands=cmds)
+    sdk_commands = find_commands_in_files(files=md_files, commands=cmds_sdk)
+
     with open("documentation/documentation_commands.json", "w") as fp:
-        json.dump(found_commands, fp, indent=4)
+        json.dump(terminal_commands, fp, indent=4)
+
+    with open("documentation/documentation_commands_sdk.json", "w") as fp:
+        json.dump(sdk_commands, fp, indent=4)
