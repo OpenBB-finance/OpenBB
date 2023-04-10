b5708af2f8b7a4853fd2b545ca3ea21b7411026b
teh_coderer <me@tehcoderer.com>
2023-04-10 15:41:36 -0400

    init

--- terminal.py
+++ terminal.py
@@ -6,13 +6,22 @@ import openbb_terminal.config_terminal as cfg
 # pylint:disable=unused-import,import-outside-toplevel
 import openbb_terminal.core.session.current_system as syst  # noqa: F401
 import openbb_terminal.core.session.current_user as user  # noqa: F401
-from openbb_terminal.terminal_helper import is_auth_enabled, is_installer
+from openbb_terminal.terminal_helper import (
+    hide_splashscreen,
+    is_auth_enabled,
+    is_installer,
+)
 
 
 def main():
     sent_args = sys.argv[1:]
-
     cfg.setup_config_terminal()
+
+    if "streamlit" in sent_args:
+        from openbb_terminal.dashboards import streamlit
+
+        hide_splashscreen()
+        sys.exit(streamlit.main())
 
     if "-t" in sent_args or "--test" in sent_args:
         from openbb_terminal.core.integration_tests import integration_controller
