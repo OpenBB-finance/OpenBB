5b362c9e986d8c40f121ca10518343af6f17a810
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-04-10 15:39:58 +0100

    using regex

--- documentation/generate_documentation_commands.py
+++ documentation/generate_documentation_commands.py
@@ -2,6 +2,7 @@ import glob
 import json
 from collections import defaultdict
 from pathlib import Path
+import re
 
 import yaml
 
@@ -52,10 +53,15 @@ if __name__ == "__main__":
     for file_path in md_files:
         with open(file_path) as file:
             file_contents = file.read()
+
             for command in cmds:
-                if command in file_contents:
-                    found_commands[command].append(file_path)
+                pattern = rf"\b{re.escape(command)}\b"  # create regex pattern for whole word match
+                matches = re.finditer(pattern, file_contents)
+
+                for match in matches:
+                    found_commands.setdefault(command, [])
+                    if file_path not in found_commands[command]:
+                        found_commands[command].append(file_path)
 
     with open("documentation/documentation_commands.json", "w") as fp:
-        # format the json nicely
         json.dump(found_commands, fp, indent=4)
