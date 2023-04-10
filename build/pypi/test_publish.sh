32ccec5b2c37f99c24e5c5f5e58774fac052521a
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-04-10 09:39:36 +0100

    reverting - chavi suggestion

--- build/pypi/test_publish.sh
+++ build/pypi/test_publish.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# backup existing .env file
-if [ -f openbb_terminal/.env ]; then
-    cp openbb_terminal/.env openbb_terminal/.env.backup
-fi
-
 # BUILD `openbb_terminal/.env`
 OPENBB_LOGGING_COMMIT_HASH="sha:$(git rev-parse --short=8 HEAD)"
 OPENBB_LOGGING_APP_NAME="gst_packaged_pypi"
@@ -12,8 +7,3 @@ echo "OPENBB_LOGGING_APP_NAME='${OPENBB_LOGGING_APP_NAME}'" > openbb_terminal/.e
 echo "OPENBB_LOGGING_COMMIT_HASH=${OPENBB_LOGGING_COMMIT_HASH}" >> openbb_terminal/.env
 
 poetry publish -r testpypi --build
-
-# restore original .env file
-if [ -f openbb_terminal/.env.backup ]; then
-    mv openbb_terminal/.env.backup openbb_terminal/.env
-fi
