f5be74a0a15a5cf72fe8678ee84ec88347b618b0
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-04-10 14:51:37 +0100

    adding a way of cheking commands present in the documentation

---
+++ documentation/generate_documentation_commands.py
@@ -0,0 +1,61 @@
+import glob
+import json
+from collections import defaultdict
+from pathlib import Path
+
+import yaml
+
+from openbb_terminal.core.config.paths import I18N_DICT_LOCATION
+
+en_file = I18N_DICT_LOCATION / "en.yml"
+
+md_files = ["CONTRIBUTING.md", "README.md", "openbb_terminal/SDK_README.md"]
+md_files.extend(glob.glob("website/content/sdk/faqs/*.md", recursive=True))
+md_files.extend(glob.glob("website/content/terminal/usage/basics/*.md", recursive=True))
+md_files.extend(glob.glob("website/content/terminal/usage/guides/*.md", recursive=True))
+md_files.extend(glob.glob("website/content/terminal/usage/intros/*.md", recursive=True))
+md_files.extend(glob.glob("website/content/sdk/usage/basics/*.md", recursive=True))
+md_files.extend(glob.glob("website/content/sdk/usage/guides/*.md", recursive=True))
+md_files.extend(glob.glob("website/content/sdk/usage/intros/*.md", recursive=True))
+
+
+def read_yaml_file(path: Path) -> dict:
+    with open(path) as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            raise exc
+
+
+def get_command_list(en_dict: dict) -> list:
+    commands = []
+    for key in en_dict["en"]:
+        if key.startswith("_"):
+            continue
+        if "/" in key:
+            potential_command = key.split("/")[-1]
+            if potential_command.startswith("_"):
+                continue
+            commands.append(potential_command)
+        else:
+            commands.append(key)
+    return commands
+
+
+if __name__ == "__main__":
+    en_file_as_dict = read_yaml_file(path=en_file)
+    cmds = get_command_list(en_dict=en_file_as_dict)
+
+    found_commands = defaultdict(list)
+
+    # Find commands in markdown files
+    for file_path in md_files:
+        with open(file_path) as file:
+            file_contents = file.read()
+            for command in cmds:
+                if command in file_contents:
+                    found_commands[command].append(file_path)
+
+    with open("documentation/documentation_commands.json", "w") as fp:
+        # format the json nicely
+        json.dump(found_commands, fp, indent=4)
