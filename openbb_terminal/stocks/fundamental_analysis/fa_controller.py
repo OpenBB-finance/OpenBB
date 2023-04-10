f991e1d2ffa8b5804d361c65948010fe8cb8b3a2
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-04-10 15:15:44 +0200

    Remove flags

--- openbb_terminal/stocks/fundamental_analysis/fa_controller.py
+++ openbb_terminal/stocks/fundamental_analysis/fa_controller.py
@@ -1624,9 +1624,7 @@ class FundamentalAnalysisController(StockBaseController):
             default=False,
             help="Whether to replace a linear regression estimate with a growth estimate.",
         )
-        ns_parser = self.parse_known_args_and_warn(
-            parser, other_args, EXPORT_ONLY_RAW_DATA_ALLOWED
-        )
+        ns_parser = self.parse_known_args_and_warn(parser, other_args)
 
         if ns_parser:
             if ns_parser.ticker:
