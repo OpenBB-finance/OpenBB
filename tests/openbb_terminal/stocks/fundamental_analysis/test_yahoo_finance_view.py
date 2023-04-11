e48b12621f671d04e4e7aaa37825c255c4c271f0
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-04-11 22:12:13 +0200

    Unit tests batch

--- tests/openbb_terminal/stocks/fundamental_analysis/test_yahoo_finance_view.py
+++ tests/openbb_terminal/stocks/fundamental_analysis/test_yahoo_finance_view.py
@@ -59,39 +59,87 @@ def test_call_func_empty_df(func, mocker, mocked_func):
     getattr(yahoo_finance_view, func)(symbol="PM")
 
 
-@pytest.mark.record_http
+# @pytest.mark.record_http
+# @pytest.mark.record_verify_screen
+# @pytest.mark.parametrize(
+#     "symbol, kwargs",
+#     [
+#         ("TSLA", {}),
+#     ],
+# )
+# def test_display_shareholders(symbol, kwargs):
+#     yahoo_finance_view.display_shareholders(symbol=symbol, **kwargs)
+
+
 @pytest.mark.record_verify_screen
 @pytest.mark.parametrize(
-    "symbol, kwargs",
+    "func, mocked_func",
     [
-        ("TSLA", {}),
+        ("display_shareholders", "get_shareholders"),
     ],
 )
-def test_display_shareholders(symbol, kwargs):
-    yahoo_finance_view.display_shareholders(symbol=symbol, **kwargs)
+def test_display_shareholders(func, mocker, mocked_func):
+    mocker.patch(
+        "openbb_terminal.stocks.fundamental_analysis.yahoo_finance_model."
+        + mocked_func,
+        return_value=pd.DataFrame(),
+    )
+    getattr(yahoo_finance_view, func)(symbol="PM")
 
 
-@pytest.mark.record_http
+# @pytest.mark.record_http
+# @pytest.mark.record_verify_screen
+# @pytest.mark.parametrize(
+#     "symbol, kwargs",
+#     [
+#         ("TSLA", {"limit": 1}),
+#     ],
+# )
+# def test_display_dividends(symbol, kwargs):
+#     yahoo_finance_view.display_dividends(symbol=symbol, **kwargs)
+
+
 @pytest.mark.record_verify_screen
 @pytest.mark.parametrize(
-    "symbol, kwargs",
+    "func, mocked_func",
     [
-        ("TSLA", {"limit": 1}),
+        ("display_dividends", "get_dividends"),
     ],
 )
-def test_display_dividends(symbol, kwargs):
-    yahoo_finance_view.display_dividends(symbol=symbol, **kwargs)
+def test_display_dividends(func, mocker, mocked_func):
+    mocker.patch(
+        "openbb_terminal.stocks.fundamental_analysis.yahoo_finance_model."
+        + mocked_func,
+        return_value=pd.DataFrame(),
+    )
+    getattr(yahoo_finance_view, func)(symbol="PM", limit=1)
 
 
-@pytest.mark.record_http
+# @pytest.mark.record_http
+# @pytest.mark.parametrize(
+#     "symbol, kwargs",
+#     [
+#         ("TSLA", {}),
+#     ],
+# )
+# def test_display_splits(symbol, kwargs):
+#     yahoo_finance_view.display_splits(symbol=symbol, **kwargs)
+
+
+@pytest.mark.record_verify_screen
 @pytest.mark.parametrize(
-    "symbol, kwargs",
+    "func, mocked_func",
     [
-        ("TSLA", {}),
+        ("display_splits", "get_splits"),
     ],
 )
-def test_display_splits(symbol, kwargs):
-    yahoo_finance_view.display_splits(symbol=symbol, **kwargs)
+def test_display_splits(func, mocker, mocked_func):
+    mocker.patch(
+        "openbb_terminal.stocks.fundamental_analysis.yahoo_finance_model."
+        + mocked_func,
+        return_value=pd.DataFrame(),
+    )
+    getattr(yahoo_finance_view, func)(symbol="TSLA")
 
 
 @pytest.mark.record_http
@@ -108,28 +156,66 @@ def test_display_mktcap(symbol, start_date, end_date, kwargs):
     )
 
 
-@pytest.mark.record_http
-@pytest.mark.parametrize(
-    "symbol, statement, kwargs",
-    [
-        ("TSLA", "cash-flow", {}),
-        ("TSLA", "financials", {}),
-        ("TSLA", "financials", {"plot": ["total_revenue"]}),
-    ],
-)
-def test_display_fundamentals(symbol, statement, kwargs):
-    yahoo_finance_view.display_fundamentals(
-        symbol=symbol, statement=statement, **kwargs
-    )
+# @pytest.mark.record_http
+# @pytest.mark.parametrize(
+#     "symbol, statement, kwargs",
+#     [
+#         ("TSLA", "cash-flow", {}),
+#         ("TSLA", "financials", {}),
+#         ("TSLA", "financials", {"plot": ["total_revenue"]}),
+#     ],
+# )
+# def test_display_fundamentals(symbol, statement, kwargs):
+#     yahoo_finance_view.display_fundamentals(
+#         symbol=symbol, statement=statement, **kwargs
+#     )
 
 
-@pytest.mark.record_http
 @pytest.mark.record_verify_screen
 @pytest.mark.parametrize(
-    "symbol, limit, kwargs",
+    "func, mocked_func, args",
     [
-        ("TSLA", 1, {}),
+        ("display_fundamentals", "get_financials", ("TSLA", "cash-flow")),
+        ("display_fundamentals", "get_financials", ("TSLA", "financials")),
+        (
+            "display_fundamentals",
+            "get_financials",
+            ("TSLA", "financials", ["total_revenue"]),
+        ),
     ],
 )
-def test_display_earnings(symbol, limit, kwargs):
-    yahoo_finance_view.display_earnings(symbol=symbol, limit=limit, **kwargs)
+def test_display_fundamentals(func, mocker, mocked_func, args):
+    mocker.patch(
+        "openbb_terminal.stocks.fundamental_analysis.yahoo_finance_model."
+        + mocked_func,
+        return_value=pd.DataFrame(),
+    )
+    getattr(yahoo_finance_view, func)(*args)
+
+
+# @pytest.mark.record_http
+# @pytest.mark.record_verify_screen
+# @pytest.mark.parametrize(
+#     "symbol, limit, kwargs",
+#     [
+#         ("TSLA", 1, {}),
+#     ],
+# )
+# def test_display_earnings(symbol, limit, kwargs):
+#     yahoo_finance_view.display_earnings(symbol=symbol, limit=limit, **kwargs)
+
+
+@pytest.mark.record_verify_screen
+@pytest.mark.parametrize(
+    "func, mocked_func",
+    [
+        ("display_earnings", "get_earnings_history"),
+    ],
+)
+def test_display_earnings(func, mocker, mocked_func):
+    mocker.patch(
+        "openbb_terminal.stocks.fundamental_analysis.yahoo_finance_model."
+        + mocked_func,
+        return_value=pd.DataFrame(),
+    )
+    getattr(yahoo_finance_view, func)(symbol="TSLA", limit=1)
