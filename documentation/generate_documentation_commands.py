6095d4f6dc76e0487c271176060cda1a99b92914
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-04-11 10:26:55 +0100

    handling export

--- documentation/generate_documentation_commands.py
+++ documentation/generate_documentation_commands.py
@@ -3,6 +3,7 @@ import json
 import re
 from collections import defaultdict
 from pathlib import Path
+from typing import Literal
 
 import pandas as pd
 import yaml
@@ -14,18 +15,21 @@ from openbb_terminal.core.config.paths import (
     MAP_PATH,
 )
 
-trail_maps = [MAP_PATH, MAP_FORECASTING_PATH, MAP_OPTIMIZATION_PATH]
+TRAIL_MAPS = [MAP_PATH, MAP_FORECASTING_PATH, MAP_OPTIMIZATION_PATH]
 
-en_file = I18N_DICT_LOCATION / "en.yml"
+EN_FILE = I18N_DICT_LOCATION / "en.yml"
 
-md_files = ["CONTRIBUTING.md", "README.md", "openbb_terminal/SDK_README.md"]
-md_files.extend(glob.glob("website/content/sdk/faqs/*.md", recursive=True))
-md_files.extend(glob.glob("website/content/terminal/usage/basics/*.md", recursive=True))
-md_files.extend(glob.glob("website/content/terminal/usage/guides/*.md", recursive=True))
-md_files.extend(glob.glob("website/content/terminal/usage/intros/*.md", recursive=True))
-md_files.extend(glob.glob("website/content/sdk/usage/basics/*.md", recursive=True))
-md_files.extend(glob.glob("website/content/sdk/usage/guides/*.md", recursive=True))
-md_files.extend(glob.glob("website/content/sdk/usage/intros/*.md", recursive=True))
+MD_FILES = ["CONTRIBUTING.md", "README.md", "openbb_terminal/SDK_README.md"]
+MD_FILES.extend(glob.glob("website/content/sdk/faqs/*.md", recursive=True))
+MD_FILES.extend(glob.glob("website/content/terminal/usage/basics/*.md", recursive=True))
+MD_FILES.extend(glob.glob("website/content/terminal/usage/guides/*.md", recursive=True))
+MD_FILES.extend(glob.glob("website/content/terminal/usage/intros/*.md", recursive=True))
+MD_FILES.extend(glob.glob("website/content/sdk/usage/basics/*.md", recursive=True))
+MD_FILES.extend(glob.glob("website/content/sdk/usage/guides/*.md", recursive=True))
+MD_FILES.extend(glob.glob("website/content/sdk/usage/intros/*.md", recursive=True))
+
+EXPORT_FILE_PATH = "documentation/documentation_commands"
+EXPORT_FILE_PATH_SDK = "documentation/documentation_commands_sdk"
 
 
 def read_yaml_file(path: Path) -> dict:
@@ -100,22 +104,51 @@ def find_commands_in_files(
     return found_commands
 
 
+def handle_export(
+    extension: Literal["json", "csv"], found_commands: dict, found_commands_sdk: dict
+):
+    def export_to_json(path: Path, commands: dict):
+        with open(path, "w") as fp:
+            json.dump(commands, fp, indent=4)
+
+    def export_to_csv(path: Path, commands: dict):
+        df = pd.DataFrame.from_dict(commands, orient="index").transpose()
+        df = pd.melt(df)
+        df.rename(columns={"variable": "command", "value": "file"}, inplace=True)
+        df = df[["file", "command"]]
+        df.sort_values(by=["file"], inplace=True)
+        df = df.dropna()
+        df.to_csv(path, index=False)
+
+    if extension == "json":
+        export_to_json(path=f"{EXPORT_FILE_PATH}.{extension}", commands=found_commands)
+        export_to_json(
+            path=f"{EXPORT_FILE_PATH_SDK}.{extension}", commands=found_commands_sdk
+        )
+
+    else:
+        export_to_csv(path=f"{EXPORT_FILE_PATH}.{extension}", commands=found_commands)
+        export_to_csv(
+            path=f"{EXPORT_FILE_PATH_SDK}.{extension}", commands=found_commands_sdk
+        )
+
+
 if __name__ == "__main__":
-    en_file_as_dict = read_yaml_file(path=en_file)
+    en_file_as_dict = read_yaml_file(path=EN_FILE)
     cmds = get_command_list(en_dict=en_file_as_dict)
     cmds_sdk = [
-        cmd for trail_map in trail_maps for cmd in get_sdk_command_list(trail_map)
+        cmd for trail_map in TRAIL_MAPS for cmd in get_sdk_command_list(trail_map)
     ]
 
     terminal_commands = find_commands_in_files(
-        files=md_files, commands=cmds, code_blocks_only=True
+        files=MD_FILES, commands=cmds, code_blocks_only=True
     )
     sdk_commands = find_commands_in_files(
-        files=md_files, commands=cmds_sdk, code_blocks_only=True
+        files=MD_FILES, commands=cmds_sdk, code_blocks_only=True
     )
 
-    with open("documentation/documentation_commands.json", "w") as fp:
-        json.dump(terminal_commands, fp, indent=4)
-
-    with open("documentation/documentation_commands_sdk.json", "w") as fp:
-        json.dump(sdk_commands, fp, indent=4)
+    handle_export(
+        extension="csv",
+        found_commands=terminal_commands,
+        found_commands_sdk=sdk_commands,
+    )
