ed584cfa205891dcf54382cf317fe86e32f48085
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-04-12 21:42:09 +0200

    stocks/fa unit tests (#4754)
    * Unit tests batch 1
    
    * Unit tests batch 2
    
    * Unit tests batch
    
    * Unit tests batch
    
    * Unit tests batch
    
    * Unit tests batch
    
    * Unit tests batch
    
    * Unit tests batch
    
    * Unit tests batch
    
    * Unit tests batch
    
    * Debloat
    
    * Debloat
    
    * Fix linting

--- openbb_terminal/stocks/fundamental_analysis/marketwatch_view.py
+++ openbb_terminal/stocks/fundamental_analysis/marketwatch_view.py
@@ -64,3 +64,45 @@ def sec_filings(
         df_financials,
         sheet_name,
     )
+
+
+@log_start_end(log=logger)
+def display_sean_seah_warnings(symbol: str, debug: bool = False):
+    """Display Sean Seah warnings
+
+    Parameters
+    ----------
+    symbol : str
+        Stock ticker
+    debug : bool, optional
+        Whether or not to return debug messages.
+        Defaults to False.
+    """
+
+    financials, warnings, debugged_warnings = marketwatch_model.get_sean_seah_warnings(
+        symbol, debug
+    )
+
+    if financials.empty:
+        console.print(f"No financials found for {symbol}\n")
+        return
+
+    print_rich_table(
+        financials,
+        headers=list(financials.columns),
+        title="Sean Seah Warnings",
+        show_index=True,
+    )
+
+    if not warnings:
+        console.print("No warnings found. Good stonk")
+        return
+
+    messages = (
+        [item for pair in zip(warnings, debugged_warnings) for item in pair]
+        if debug
+        else warnings
+    )
+
+    console.print("Warnings:\n")
+    console.print("\n".join(messages))
