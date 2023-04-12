6932815259b80924d826dca790f657f8fe70c2bf
Pratyush Shukla <ps4534@nyu.edu>
2023-04-12 16:59:54 +0530

    Bifurcated request URL into 2 lines

--- openbb_terminal/keys_model.py
+++ openbb_terminal/keys_model.py
@@ -545,8 +545,8 @@ def check_polygon_key(show_output: bool = False) -> str:
     else:
         check_date = date(date.today().year, date.today().month, 1).isoformat()
         r = request(
-            f"https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/{check_date}/{check_date}"
-            f"?apiKey={current_user.credentials.API_POLYGON_KEY}"
+            f"https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/{check_date}"
+            f"/{check_date}?apiKey={current_user.credentials.API_POLYGON_KEY}"
         )
         if r.status_code in [403, 401]:
             logger.warning("Polygon key defined, test failed")
