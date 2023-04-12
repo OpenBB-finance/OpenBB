2e79308ade2947528fd6699aa8100722fdf57d90
Colin Delahunty <72827203+colin99d@users.noreply.github.com>
2023-04-12 04:39:42 -0700

    Hotfix/4526 (#4682)
    * Added fixes
    
    * Merged
    
    * Merged
    
    * Added some handling of errors
    
    * Adds some read dbs
    
    * Removed useless commands
    
    * Added fixes
    
    * Added fixes
    
    * Added fix
    
    * Added fix
    
    * Added fix
    
    * Added fix
    
    * Added fix
    
    * Added fix
    
    * Fixed tests
    
    * Handle lowercase
    
    * Added fxes
    
    * small ticker thing
    
    ---------
    
    Co-authored-by: James Maslek <jmaslek11@gmail.com>

--- openbb_terminal/stocks/fundamental_analysis/marketwatch_model.py
+++ openbb_terminal/stocks/fundamental_analysis/marketwatch_model.py
@@ -2,70 +2,90 @@
 __docformat__ = "numpy"
 
 import logging
+from typing import Optional
 
 import pandas as pd
-from bs4 import BeautifulSoup
+from requests.exceptions import ReadTimeout
 
 from openbb_terminal.decorators import log_start_end
-from openbb_terminal.helper_funcs import get_user_agent, request
+from openbb_terminal.helper_funcs import request
+from openbb_terminal.rich_config import console
 
 # pylint: disable=too-many-branches
 
 
 logger = logging.getLogger(__name__)
 
+FORM_GROUP = {
+    "annual": "Annual%20Reports",
+    "quarterly": "Quarterly%20Reports",
+    "proxies": "Proxies%20and%20Info%20Statements",
+    "insiders": "Insider%20Transactions",
+    "8-K": "8-K%20Related",
+    "registrations": "Registration%20Statements",
+    "comments": "Comment%20Letters",
+}
+
 
 @log_start_end(log=logger)
-def get_sec_filings(symbol: str) -> pd.DataFrame:
+def get_sec_filings(
+    symbol: str,
+    limit: int = 20,
+    year: Optional[int] = None,
+    form_group: Optional[str] = None,
+) -> pd.DataFrame:
     """Get SEC filings for a given stock ticker. [Source: Market Watch]
 
     Parameters
     ----------
     symbol : str
         Stock ticker symbol
+    limit : int
+        The number of filings to return
+    year: Optional[int]
+        The year to grab from. The year will be ignored if form_group is not specified
+    form_group: Optional[str]
+        The form type to filter for:
+        Choose from: annual, quarterly, proxies, insiders, 8-K, registrations, comments
 
     Returns
     -------
     df_financials : pd.DataFrame
         SEC filings data
     """
-
+    base_url = f"https://api.nasdaq.com/api/company/{symbol}/sec-filings"
+    arguments = f"?limit={limit}&sortColumn=filed&sortOrder=desc&IsQuoteMedia=true"
+    valid_form_group = False
+    if form_group in FORM_GROUP:
+        arguments = f"{arguments}&formGroup={FORM_GROUP[form_group]}"
+        valid_form_group = True
+    if year and valid_form_group:
+        arguments = f"{arguments}&Year={year}"
+    elif year:
+        console.print("Year will be ignored if form_group is not specified")
+    try:
+        response = request(base_url + arguments)
+    except ReadTimeout:
+        return pd.DataFrame()
+    try:
+        data = response.json()["data"]["rows"]
+    except KeyError:
+        return pd.DataFrame()
+    final_df = pd.DataFrame(data)
     pd.set_option("display.max_colwidth", None)
-
-    url_financials = (
-        f"https://www.marketwatch.com/investing/stock/{symbol}/financials/secfilings"
+    try:
+        final_df["view"] = final_df["view"].apply(lambda x: x["htmlLink"])
+    except KeyError:
+        return pd.DataFrame()
+    final_df = final_df.rename(
+        columns={
+            "companyName": "Company Name",
+            "reportingOwner": "Reporting Owner",
+            "formType": "Form Type",
+            "filed": "Filed",
+            "view": "View",
+            "period": "Period",
+        }
     )
-
-    text_soup_financials = BeautifulSoup(
-        request(url_financials, headers={"User-Agent": get_user_agent()}).text,
-        "lxml",
-    )
-
-    # a_financials_header = list()
-    df_financials = None
-    b_ready_to_process_info = False
-    soup_financials = text_soup_financials.findAll("tr", {"class": "table__row"})
-    for financials_info in soup_financials:
-        a_financials = financials_info.text.split("\n")
-
-        # If header has been processed and dataframe created ready to populate the SEC information
-        if b_ready_to_process_info and len(a_financials) > 1:
-            l_financials_info = [a_financials[2]]
-            l_financials_info.extend(a_financials[5:-1])
-            l_financials_info.append(financials_info.a["href"])
-            # Append data values to financials
-            df_financials.loc[len(df_financials.index)] = l_financials_info  # type: ignore
-
-        if "Filing Date" in a_financials and len(a_financials) > 1:
-            l_financials_header = [a_financials[2]]
-            l_financials_header.extend(a_financials[5:-1])
-            l_financials_header.append("Link")
-
-            df_financials = pd.DataFrame(columns=l_financials_header)
-            df_financials.set_index("Filing Date")
-            b_ready_to_process_info = True
-
-    # Set Filing Date as index
-    df_financials = df_financials.set_index("Filing Date")  # type: ignore
-
-    return df_financials
+    final_df = final_df.set_index("Filed")
+    return final_df
