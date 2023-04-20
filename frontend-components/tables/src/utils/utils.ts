f1d9ec8ce869f76ca2b906823807d93249fc1859
teh_coderer <me@tehcoderer.com>
2023-04-20 16:43:54 -0400

    init

--- frontend-components/tables/src/utils/utils.ts
+++ frontend-components/tables/src/utils/utils.ts
@@ -3,25 +3,28 @@ import domtoimage from "dom-to-image";
 import { utils, writeFile } from "xlsx";
 
 export function formatNumberMagnitude(number: number) {
-  if (number < 10) {
-    return number.toFixed(4);
-  } else if (number < 100) {
-    return number.toFixed(3);
-  } else if (number < 1000) {
-    return number.toFixed(2);
+  if (number % 1 !== 0) {
+    const decimalPlaces = Math.max(2, number.toString().split(".")[1].length);
+    const toFixed = Math.min(4, decimalPlaces);
+    if (number < 1000) {
+      return number.toFixed(toFixed) || 0;
+    }
   }
-  const abs = Math.abs(number);
-  if (abs >= 1000000000000) {
-    return `${(number / 1000000000).toFixed(2)}T`;
-  } else if (abs >= 1000000000) {
-    return `${(number / 1000000000).toFixed(2)}B`;
-  } else if (abs >= 1000000) {
-    return `${(number / 1000000).toFixed(2)}M`;
-  } else if (abs >= 1000) {
-    return `${(number / 1000).toFixed(2)}K`;
-  } else {
-    return number.toFixed(2);
+
+  if (number > 1000) {
+    const magnitude = Math.min(4, Math.floor(Math.log10(Math.abs(number)) / 3));
+    const suffix = ["", "K", "M", "B", "T"][magnitude];
+    const formatted = (number / 10 ** (magnitude * 3)).toFixed(2);
+    return `${formatted} ${suffix}`;
   }
+
+  return number;
+}
+
+export function includesDateNames(column: string) {
+  return ["date", "day", "time", "timestamp", "year"].some((dateName) =>
+    column.toLowerCase().includes(dateName)
+  );
 }
 
 export function isEqual(a: any, b: any) {
@@ -46,7 +49,82 @@ export const fuzzyFilter = (
   return itemRank;
 };
 
-export const saveToFile = (blob: Blob, fileName: string) => {
+const writeFileHandler = async ({
+  fileHandle,
+  blob,
+}: {
+  fileHandle?: FileSystemFileHandle | null;
+  blob: Blob;
+}) => {
+  if (!fileHandle) {
+    throw new Error("Cannot access filesystem");
+  }
+  const writer = await fileHandle.createWritable();
+  await writer.write(blob);
+  await writer.close();
+};
+
+const IMAGE_TYPE: FilePickerAcceptType[] = [
+  {
+    description: "PNG Image",
+    accept: {
+      "image/png": [".png"],
+    },
+  },
+  {
+    description: "JPEG Image",
+    accept: {
+      "image/jpeg": [".jpeg"],
+    },
+  },
+  {
+    description: "SVG Image",
+    accept: {
+      "image/svg+xml": [".svg"],
+    },
+  },
+];
+
+const getNewFileHandle = ({
+  filename,
+  is_image,
+}: {
+  filename: string;
+  is_image?: boolean;
+}): Promise<FileSystemFileHandle | null> => {
+  if ("showSaveFilePicker"! in window) {
+    return new Promise((resolve) => {
+      resolve(null);
+    });
+  }
+
+  const opts: SaveFilePickerOptions = {
+    suggestedName: filename,
+    types: is_image
+      ? IMAGE_TYPE
+      : [
+          {
+            description: "CSV File",
+            accept: {
+              "image/csv": [".csv"],
+            },
+          },
+        ],
+    excludeAcceptAllOption: true,
+  };
+
+  return showSaveFilePicker(opts);
+};
+
+export const saveToFile = (
+  blob: Blob,
+  fileName: string,
+  fileHandle?: FileSystemFileHandle | null
+) => {
+  if ("showSaveFilePicker" in window) {
+    return writeFileHandler({ fileHandle, blob });
+  }
+
   const url = URL.createObjectURL(blob);
   const link = document.createElement("a");
   link.setAttribute("href", url);
@@ -63,21 +141,33 @@ export const downloadData = (type: "csv" | "xlsx", columns: any, data: any) => {
     headers.map((column: any) => row[column])
   );
   const csvData = [headers, ...rows];
+
   if (type === "csv") {
     const csvContent = csvData.map((e) => e.join(",")).join("\n");
     const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
-    saveToFile(blob, `${window.title}.csv`);
-  } else {
-    const wb = utils.book_new();
-    const ws = utils.aoa_to_sheet(csvData);
-    utils.book_append_sheet(wb, ws, "Sheet1");
-    writeFile(wb, `${window.title}.xlsx`);
+    const filename = `${window.title}.png`;
+    return getNewFileHandle({
+      filename: filename,
+    }).then((fileHandle) => {
+      saveToFile(blob, filename, fileHandle);
+    });
   }
+
+  const wb = utils.book_new();
+  const ws = utils.aoa_to_sheet(csvData);
+  utils.book_append_sheet(wb, ws, "Sheet1");
+  writeFile(wb, `${window.title}.xlsx`);
 };
 
 export const downloadImage = (id: string) => {
   const table = document.getElementById(id);
-  domtoimage.toBlob(table).then(function (blob) {
-    saveToFile(blob, `${window.title}.png`);
+  const filename = `${window.title}.png`;
+  getNewFileHandle({
+    filename: filename,
+    is_image: true,
+  }).then((fileHandle) => {
+    domtoimage.toBlob(table).then(function (blob: Blob) {
+      saveToFile(blob, filename, fileHandle);
+    });
   });
 };
--- openbb_terminal/core/plots/backend.py
+++ openbb_terminal/core/plots/backend.py
@@ -262,7 +262,7 @@ class Backend(PyWry):
         # pylint: disable=C0415
         from openbb_terminal.helper_funcs import command_location
 
-        json_data = json.loads(df_table.to_json(orient="split"))
+        json_data = json.loads(df_table.to_json(orient="split", date_format="iso"))
         json_data.update(
             dict(
                 title=title,
