71ef013a58c6c69ecc9908c9a8a7c5d99b69fcb9
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-05-11 03:32:01 -0700

    Allow to set variables and do loops

--- openbb_terminal/terminal_controller.py
+++ openbb_terminal/terminal_controller.py
@@ -683,40 +683,189 @@ class TerminalController(BaseController):
                     if raw_line.strip("\n")
                 ]
 
-                lines = list()
-                for rawline in raw_lines:
-                    templine = rawline
+                ROUTINE_VARS = dict()
+                # Capture ARGV either as list if args separated by commas or as single value
+                ROUTINE_VARS["$ARGV"] = (
+                    ns_parser.routine_args
+                    if "," not in ns_parser.routine_args
+                    else ns_parser.routine_args.split(",")
+                )
 
-                    # Check if dynamic parameter exists in script
-                    if "$ARGV" in rawline:
-                        # Check if user has provided inputs through -i or --input
-                        if ns_parser.routine_args:
-                            for i, arg in enumerate(ns_parser.routine_args):
-                                # Check what is the location of the ARGV to be replaced
-                                if f"$ARGV[{i}]" in templine:
-                                    templine = templine.replace(f"$ARGV[{i}]", arg)
-
-                            # Check if all ARGV have been removed, otherwise means that there are less inputs
-                            # when running the script than the script expects
-                            if "$ARGV" in templine:
-                                console.print(
-                                    "[red]Not enough inputs were provided to fill in dynamic variables. "
-                                    "E.g. --input VAR1,VAR2,VAR3[/red]\n"
-                                )
-                                return
-
-                            lines.append(templine)
-                        # The script expects a parameter that the user has not provided
+                ## LOOK FOR NEW VARIABLES BEING DECLARED FROM USERS
+                lines_without_declarations = list()
+                for line in raw_lines:
+                    # Check if this line has a variable attribution
+                    # This currently allows user to override ARGV parameter, we may want to protect it in the future?
+                    # This doesn't implement protection against $$, should we add it?
+                    if "$" in line and "=" in line:
+                        match = re.search(r"\$(\w+)\s*=\s*([\d,-.\s]+)", line)
+                        if match:
+                            VAR_NAME = match.group(1)
+                            VAR_VALUES = match.group(2)
+                            ROUTINE_VARS["$" + VAR_NAME] = (
+                                VAR_VALUES
+                                if "," not in VAR_VALUES
+                                else VAR_VALUES.split(",")
+                            )
                         else:
+                            lines_without_declarations.append(line)
+                    else:
+                        lines_without_declarations.append(line)
+
+                # At this stage our ROUTINE_VARS should be completed coming from external AND from internal
+                # Now we want to replace the ROUTINE_VARS to where applicable throughout the .openbb script
+                # Due to this implementation, a variable declared at the end will still be effective
+
+                lines_with_vars_replaced = list()
+                foreach_loop_found = False
+                for line in lines_without_declarations:
+                    # Save temporary line to ensure that all vars get replaced by correct vars
+                    templine = line
+
+                    # Found 'end' keyword which means that a loop has terminated
+                    if re.match(r"^\s*end\s*$", line, re.IGNORECASE):
+                        # Check whether the foreach loop has started or not
+                        if not foreach_loop_found:
                             console.print(
-                                "[red]The script expects parameters, "
-                                "run the script again with --input defined.[/red]\n"
+                                "[red]The script has a foreach loop that terminates before it gets started."
+                                "Add the keyword 'foreach' to explicitly start loop[/red]"
                             )
                             return
-                    else:
-                        lines.append(templine)
+                        foreach_loop_found = False
 
-                simulate_argv = f"/{'/'.join([line.rstrip() for line in lines])}"
+                    else:
+                        # Found 'foreach' keyword which means there needs to be a matching 'end'
+                        if re.search(r"foreach", line, re.IGNORECASE):
+                            foreach_loop_found = True
+
+                        # Regular expression pattern to match variables starting with $
+                        pattern = r"(?<!\$)(\$(\w+)(\[[^]]*\])?)(?![^\[]*\])"
+
+                        # Find all matches of the pattern in the line
+                        matches = re.findall(pattern, line)
+
+                        if matches:
+                            for match in matches:
+                                VAR_NAME = "$" + match[1]
+                                VAR_SLICE = match[2][1:-1] if match[2] else ""
+
+                                # Within a list refers to a single element
+                                if VAR_SLICE.isdigit():
+                                    # This is an edge case for when the user has a variable such as $DATE = 2022-01-01
+                                    # We want the user to be able to access it with $DATE or $DATE[0] and the latest
+                                    # in python will only take the first '2'
+                                    if VAR_SLICE == "0":
+                                        values = eval(f'ROUTINE_VARS["{VAR_NAME}"]')
+                                        if type(values) == list:
+                                            templine = templine.replace(
+                                                match[0], eval(f"values[{VAR_SLICE}]")
+                                            )
+                                        else:
+                                            templine = templine.replace(
+                                                match[0], values
+                                            )
+
+                                    else:
+                                        templine = templine.replace(
+                                            match[0],
+                                            eval(
+                                                f'ROUTINE_VARS["{VAR_NAME}"][{VAR_SLICE}]'
+                                            ),
+                                        )
+
+                                # Involves slicing which is a bit more tricky to use eval on
+                                elif (
+                                    ":" in VAR_SLICE
+                                    and len(VAR_SLICE.split(":")) == 2
+                                    and (
+                                        VAR_SLICE.split(":")[0].isdigit()
+                                        or VAR_SLICE.split(":")[1].isdigit()
+                                    )
+                                ):
+                                    slicing_tuple = "slice("
+                                    slicing_tuple += (
+                                        VAR_SLICE.split(":")[0]
+                                        if VAR_SLICE.split(":")[0].isdigit()
+                                        else "None"
+                                    )
+                                    slicing_tuple += ","
+                                    slicing_tuple += (
+                                        VAR_SLICE.split(":")[1]
+                                        if VAR_SLICE.split(":")[1].isdigit()
+                                        else "None"
+                                    )
+                                    slicing_tuple += ")"
+
+                                    templine = templine.replace(
+                                        match[0],
+                                        ",".join(
+                                            eval(
+                                                f'ROUTINE_VARS["{VAR_NAME}"][{slicing_tuple}]'
+                                            )
+                                        ),
+                                    )
+
+                                # Just replace value without slicing or list
+                                else:
+                                    templine = templine.replace(
+                                        match[0],
+                                        ",".join(eval(f'ROUTINE_VARS["{VAR_NAME}"]')),
+                                    )
+
+                    lines_with_vars_replaced.append(templine)
+
+                # If this flags ends in True it means that the script routine has a foreach loop that never terminates
+                if foreach_loop_found:
+                    console.print(
+                        "[red]The script has a foreach loop that doesn't terminate. "
+                        "Add the keyword 'end' to explicitly terminate loop[/red]"
+                    )
+                    return
+
+                # Finally the only remaining thing to address are the foreach loops. For that we'll go through
+                # those lines and unroll the arguments that will be iterated by.
+                # Note that the fact that we checked before that the amount of foreach and end matches allow us
+                # to be confident that the script has no clear issues.
+
+                within_foreach = False
+                foreach_lines_loop = list()
+
+                final_lines = list()
+                for line in lines_with_vars_replaced:
+                    # Found 'foreach' header associated with loop
+                    match = re.search(
+                        r"(?<=foreach \$\$VAR in )([A-Z,]+)", line, re.IGNORECASE
+                    )
+                    if match:
+                        foreach_loop = match.group(1).split(",")
+                        within_foreach = True
+
+                    # We are inside a loop and this is a line that we will want to replicate,
+                    # so we need to temporarily store it until we reach the end
+                    elif within_foreach:
+                        # Found 'end' keyword which means that the foreach loop has reached the end
+                        if re.match(r"^\s*end\s*$", line, re.IGNORECASE):
+                            # Now we want to process what we were waiting for before
+
+                            # Iterate through main foreach header
+                            for var in foreach_loop:
+                                # Iterate through all lines within foreach and end loop
+                                for foreach_line_loop in foreach_lines_loop:
+                                    final_lines.append(
+                                        foreach_line_loop.replace("$$VAR", var).strip()
+                                    )
+
+                            # Since this has been processed we reset the foreach loop lines
+                            within_foreach = False
+                            foreach_lines_loop = list()
+
+                        else:
+                            foreach_lines_loop.append(line)
+
+                    else:
+                        final_lines.append(line)
+
+                simulate_argv = f"/{'/'.join([line.rstrip() for line in final_lines])}"
                 file_cmds = simulate_argv.replace("//", "/home/").split()
                 file_cmds = insert_start_slash(file_cmds) if file_cmds else file_cmds
                 cmds_with_params = " ".join(file_cmds)
