c732c44de07d77a86f26cf088508d72b6cdcb70e
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-05-15 21:09:05 -0700

    adds additional tests for intrinio_model

--- tests/openbb_terminal/stocks/options/test_intrinio_model.py
+++ tests/openbb_terminal/stocks/options/test_intrinio_model.py
@@ -79,3 +79,13 @@ def test_option_chain_bad_symbol(recorder):
     )
     recorder.capture(df2)
     assert df2.empty
+
+
+@pytest.mark.vcr
+def test_get_ticker_info(recorder):
+    df = intrinio_model.get_ticker_info("AAPL")
+    recorder.capture(df)
+    assert df.loc["ticker"] == "AAPL"
+    data = intrinio_model.load_options("AAPL")
+    assert df.loc["name"] == data.underlying_name
+    assert data.symbol in data.SYMBOLS
