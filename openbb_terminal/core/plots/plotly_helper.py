OpenBB-finance/OpenBB

commit b6b42afd713fe7baa77b3f76a07168a2380e21e0

Hub | Add colors, sources sync (#4546)
* fix show version

* update login prompt and account

* update set preference

* fix account controller

* block login/logout if not called from account

* change conditions to start terminal

* fix required args account model

* add not to login command

* pylint

* accounte controller messages

* set default user when logout

* logout then show menu

* remove unused functions

* fix logging bug

* ruff

* fix account unittests

* fix session unittests

* refactor colors preference

* no theme, no party

* add colors sync

* dynamic theme

* add comment

* add colors to settings

* ruff

* linting

* update menu desc

* adjust keys sync flag

* write to dotenv sync flag

* english

* fix pull command

* fix edge cases

* handle some errors

* small bug

* change func name

* pylint

* no light

* colors preview

* ruff

* fix login logout preferences

* ruff

* delete folder

* pylint

* default, custom, hub

* fix clear

* docstring

* add preference exceptions

* set only rich style

* request theme only if hub

* linting

* change guest msg

* add option to update chart style on the fly

* apply style

* rollback to old terminal style

* fix console theme

* remove print

* remove hub rich style

* ruff

* mypy

* black

* Hub | Add sources sync (#4565)

* Add sources model

* adapt session model

* fetch sources dict

* rename local funcs

* set sources inside current user

* set sources inside current user

* generate sources dict from hub

* fix dict generator

* send request to hub

* fix conflict

* revert datasources to develop head

* fix docstring

* avoid invalid patch types

* add default option to sources

* fix small bug on sources

* ruff

* black

* remove (

* mypy failing only on git

* ignore mypy

* pylint

* fix settings controller

* Tests: fix keys model writing to .env

* fix keys controller test writing to .env

* ruff

* Test: fix account controller

openbb_terminal/core/plots/plotly_helper.py
@@ -23,7 +23,9 @@ from plotly.subplots import make_subplots
 from scipy import stats
 
 from openbb_terminal.base_helpers import console, strtobool
-from openbb_terminal.core.config.paths import MISCELLANEOUS_DIRECTORY
+from openbb_terminal.core.config.paths import (
+    STYLES_DIRECTORY_REPO,
+)
 from openbb_terminal.core.plots.backend import PLOTLYJS_PATH, plots_backend
 from openbb_terminal.core.plots.config.openbb_styles import (
     PLT_COLORWAY,
@@ -44,10 +46,8 @@ class TerminalStyle:
     styles as python dictionaries.
     """
 
-    DEFAULT_STYLES_LOCATION = MISCELLANEOUS_DIRECTORY / "styles" / "default"
-    USER_STYLES_LOCATION = (
-        get_current_user().preferences.USER_DATA_DIRECTORY / "styles" / "user"
-    )
+    STYLES_REPO = STYLES_DIRECTORY_REPO
+    USER_STYLES_DIRECTORY = get_current_user().preferences.USER_STYLES_DIRECTORY
 
     plt_styles_available: Dict[str, Path] = {}
     plt_style: str = "dark"
@@ -81,22 +81,33 @@ class TerminalStyle:
             The name of the Rich style to use, by default ""
         """
         self.plt_style = plt_style or self.plt_style
-        console_style = console_style or "dark"
         self.load_available_styles()
-
-        console_json_path = self.console_styles_available.get("dark", None)
-        for style in ["openbb_config", console_style]:
-            if style in self.console_styles_available:
-                console_json_path = self.console_styles_available[style]
-                break
-
-        if console_json_path:
-            self.console_style = self.load_json_style(console_json_path)
         self.load_style(plt_style)
+        self.load_style(plt_style)
+        self.apply_console_style(console_style)
+
+    def apply_console_style(self, style: Optional[str] = "") -> None:
+        """Apply the style to the console."""
+
+        if style in self.console_styles_available:
+            json_path: Optional[Path] = self.console_styles_available[style]
+        else:
+            self.load_available_styles()
+            if style in self.console_styles_available:
+                json_path = self.console_styles_available[style]
+            else:
+                console.print("\nInvalid console style. Using default.")
+                json_path = self.console_styles_available.get("dark", None)
+
+        if json_path:
+            self.console_style = self.load_json_style(json_path)
+        else:
+            console.print("Error loading default.")
 
     def apply_style(self, style: Optional[str] = "") -> None:
         """Apply the style to the libraries."""
-        style = style or self.plt_style
+        if not style:
+            style = get_current_user().preferences.PLOT_STYLE
 
         if style != self.plt_style:
             self.load_style(style)
@@ -131,20 +142,20 @@ class TerminalStyle:
             ["plt_styles_available", "console_styles_available"],
             [".pltstyle.json", ".richstyle.json"],
         ):
-            for file in folder.glob(f"*{ext}"):
+            for file in folder.rglob(f"*{ext}"):
                 getattr(self, attr)[file.name.replace(ext, "")] = file
 
     def load_available_styles(self) -> None:
         """Load custom styles from default and user folders."""
-        self.load_available_styles_from_folder(self.DEFAULT_STYLES_LOCATION)
-        self.load_available_styles_from_folder(self.USER_STYLES_LOCATION)
+        self.load_available_styles_from_folder(self.STYLES_REPO)
+        self.load_available_styles_from_folder(self.USER_STYLES_DIRECTORY)
 
     def load_json_style(self, file: Path) -> Dict[str, Any]:
         """Load style from json file.
 
         Parameters
         ----------
-        file : str
+        file : Path
             Path to the file containing the style
 
         Returns
@@ -206,6 +217,7 @@ class TerminalStyle:
         list
             List of colors e.g. ["#00ACFF", "#FF0000"]
         """
+        self.apply_style()
         colors = self.plotly_template.get("layout", {}).get("colorway", PLT_COLORWAY)
         if reverse:
             colors.reverse()
@@ -1000,6 +1012,7 @@ class OpenBBFigure(go.Figure):
         if kwargs.pop("margin", True):
             self._adjust_margins()
 
+        theme.apply_style()
         self._apply_feature_flags()
         self._xaxis_tickformatstops()
 
@@ -1317,6 +1330,7 @@ class OpenBBFigure(go.Figure):
                 full_html=False,
             )
         )
+        theme.apply_style()
         self._apply_feature_flags()
         self._xaxis_tickformatstops()
 

.gitignore
@@ -54,7 +54,7 @@ DMG/*
 *.dmg
 
 # User stylesheets
-styles/user
+openbb_terminal/miscellaneous/styles/user
 
 # the file containing the API keys for the web version
 openbb_terminal_web/setenv
@@ -71,4 +71,3 @@ cache/
 
 # lightning logs
 lightning_logs/
-

openbb_terminal/account/account_controller.py
@@ -20,6 +20,7 @@ from openbb_terminal.core.session.current_user import (
     is_local,
     set_preference,
 )
+from openbb_terminal.core.session.env_handler import write_to_dotenv
 from openbb_terminal.core.session.session_model import logout
 from openbb_terminal.custom_prompt_toolkit import NestedCompleter
 from openbb_terminal.decorators import log_start_end
@@ -198,10 +199,13 @@ class AccountController(BaseController):
                         name="SYNC_ENABLED",
                         value=ns_parser.sync,
                     )
+                    current_user = get_current_user()
+                    write_to_dotenv(
+                        "OPENBB_SYNC_ENABLED",
+                        str(current_user.preferences.SYNC_ENABLED),
+                    )
                     sync = (
-                        "ON"
-                        if get_current_user().preferences.SYNC_ENABLED is True
-                        else "OFF"
+                        "ON" if current_user.preferences.SYNC_ENABLED is True else "OFF"
                     )
                     console.print(f"[info]sync:[/info] {sync}")
 
@@ -229,7 +233,7 @@ class AccountController(BaseController):
                         )
                         console.print("")
                         if i.lower() in ["y", "yes"]:
-                            Local.apply_configs(configs=configs_diff)
+                            Local.set_credentials_from_hub(configs=configs_diff)
                             console.print("[info]Done.[/info]")
                         else:
                             console.print("[info]Aborted.[/info]")
@@ -252,7 +256,7 @@ class AccountController(BaseController):
             if ns_parser:
                 i = console.input(
                     "[bold red]This action is irreversible![/bold red]\n"
-                    "Are you sure you want to permanently delete your data? (y/n): "
+                    "Are you sure you want to permanently delete your keys? (y/n): "
                 )
                 console.print("")
                 if i.lower() in ["y", "yes"]:

openbb_terminal/account/account_model.py
@@ -47,16 +47,8 @@ def get_diff(configs: dict) -> dict:
     dict
         The diff.
     """
-    SETTINGS = "features_settings"
     KEYS = "features_keys"
-    configs_diff: Dict[str, Dict[str, Any]] = {SETTINGS: {}, KEYS: {}}
-
-    diff_settings = get_diff_settings(configs.get(SETTINGS, {}))
-    if diff_settings:
-        console.print("[info]Settings:[/info]")
-        for k, v in diff_settings.items():
-            configs_diff[SETTINGS][k] = v[1]
-            console.print(f"  [menu]{k}[/menu]: {v[0]} -> {v[1]}")
+    configs_diff: Dict[str, Dict[str, Any]] = {KEYS: {}}
 
     diff_keys = get_diff_keys(configs.get(KEYS, {}))
     if diff_keys:
@@ -65,38 +57,10 @@ def get_diff(configs: dict) -> dict:
             configs_diff[KEYS][k] = v[1]
             console.print(f"  [menu]{k}[/menu]: {v[0]} -> {v[1]}")
 
-    if not configs_diff[SETTINGS]:
-        configs_diff.pop(SETTINGS)
-
     if not configs_diff[KEYS]:
         configs_diff.pop(KEYS)
 
     return configs_diff
-
-
-def get_diff_settings(settings: dict) -> dict:
-    """Get the diff between the local and remote settings.
-
-    Parameters
-    ----------
-    configs : dict
-        The configs.
-
-    Returns
-    -------
-    dict
-        The diff.
-    """
-    current_user = get_current_user()
-    diff = {}
-    if settings:
-        for k, v in sorted(settings.items()):
-            if hasattr(current_user.preferences, k):
-                old, new = get_var_diff(current_user.preferences, k, v)
-                if new is not None:
-                    diff[k] = (old, new)
-
-    return diff
 
 
 def get_diff_keys(keys: dict) -> dict:

openbb_terminal/core/config/paths.py
@@ -17,3 +17,6 @@ HIST_FILE_PATH = SETTINGS_DIRECTORY / ".openbb_terminal.his"
 
 # i18n_dict_location
 I18N_DICT_LOCATION = MISCELLANEOUS_DIRECTORY / "i18n"
+
+# styles
+STYLES_DIRECTORY_REPO = MISCELLANEOUS_DIRECTORY / "styles"

openbb_terminal/core/models/__init__.py
@@ -2,6 +2,7 @@ from openbb_terminal.core.models.base_model import BaseModel
 from openbb_terminal.core.models.credentials_model import CredentialsModel
 from openbb_terminal.core.models.preferences_model import PreferencesModel
 from openbb_terminal.core.models.profile_model import ProfileModel
+from openbb_terminal.core.models.sources_model import SourcesModel
 from openbb_terminal.core.models.system_model import SystemModel
 from openbb_terminal.core.models.user_model import UserModel
 
@@ -10,6 +11,7 @@ __all__ = [
     "CredentialsModel",
     "PreferencesModel",
     "ProfileModel",
+    "SourcesModel",
     "SystemModel",
     "UserModel",
 ]

openbb_terminal/core/models/base_model.py
@@ -22,8 +22,14 @@ class BaseModel:
         """Get dict of fields."""
         return cls.__dataclass_fields__  # type: ignore
 
-    def get_field_value(self, field: str) -> Optional[str]:
+    def get_value(self, field: str) -> Optional[Any]:
         """Get field value."""
         if hasattr(self, field):
             return getattr(self, field)
+        return None
+
+    def get_default(self, field: str) -> Optional[Any]:
+        """Get default field value."""
+        if hasattr(self, field):
+            return self.get_fields()[field].default
         return None

openbb_terminal/core/models/credentials_model.py
@@ -4,7 +4,7 @@ from pydantic.dataclasses import dataclass
 
 from openbb_terminal.core.models import BaseModel
 
-# pylint: disable=too-many-instance-attributes, disable=no-member
+# pylint: disable=too-many-instance-attributes, disable=no-member, useless-parent-delegation
 
 
 @dataclass(config=dict(validate_assignment=True, frozen=True))
@@ -66,8 +66,5 @@ class CredentialsModel(BaseModel):
     API_COINBASE_SECRET: str = "REPLACE_ME"
     API_COINBASE_PASS_PHRASE: str = "REPLACE_ME"
 
-    # Others
-    OPENBB_PERSONAL_ACCESS_TOKEN: str = "REPLACE_ME"
-
-    def __repr__(self) -> str:  # pylint: disable=useless-super-delegation
+    def __repr__(self) -> str:
         return super().__repr__()

commit a35cfc2e7175934a7952cbfe1dbf632cc0dea8a7

revert and fix bug introduced in #4546

openbb_terminal/core/plots/plotly_helper.py
@@ -125,8 +125,7 @@ class TerminalStyle:
 
     def apply_style(self, style: Optional[str] = "") -> None:
         """Apply the style to the libraries."""
-        if not style:
-            style = get_current_user().preferences.CHART_STYLE
+        style = style or self.plt_style
 
         if style != self.plt_style:
             self.load_style(style)