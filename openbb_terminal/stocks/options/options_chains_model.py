a90d736d27379e4b4071cea57662d28df7d2f631
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-05-22 07:28:38 -0700

    add function for calculating chains stats

--- openbb_terminal/stocks/options/options_chains_model.py
+++ openbb_terminal/stocks/options/options_chains_model.py
@@ -4,6 +4,12 @@ __docformat__ = "numpy"
 # IMPORTATION STANDARD
 import logging
 
+# IMPORTATION THIRDPARTY
+from typing import Callable
+
+import numpy as np
+import pandas as pd
+
 # IMPORTATION INTERNAL
 from openbb_terminal.decorators import log_start_end
 from openbb_terminal.stocks.options.cboe_model import load_options as load_cboe
@@ -119,3 +125,108 @@ def load_options_chains(
         return load_intrinio(symbol, pydantic=pydantic)
 
     return load_cboe(symbol, pydantic)
+
+
+@log_start_end(log=logger)
+def calculate_stats(options: object, type: str = "expiration") -> pd.DataFrame:
+    """Calculates basic statistics for the options chains, like OI and Vol/OI ratios.
+
+    Parameters
+    ----------
+    options : object
+        The OptionsChains data object.
+        Accepts both Pydantic and Pandas object types, as defined by `load_options_chains()`.
+        A Pandas DataFrame, or dictionary, with the options chains data is also accepted.
+    type: str
+        Whether to calculate by strike or expiration.  Default is expiration.
+
+    Returns
+    -------
+    pd.DataFrame
+        Pandas DataFrame with the calcualted statistics.
+
+    Examples
+    --------
+    >>> from openbb_terminal.stocks.options.options_chains_model import OptionsChains
+    >>> data = OptionsChains().load_options_chains("SPY")
+    >>> OptionsChains().calculate_stats(data)
+    >>> OptionsChains().calculate_stats(data, "strike")
+    >>> OptionsChains().calculate_stats(data.chains, "expiration")
+    """
+
+    types = ["expiration", "strike"]
+    try:
+        if type not in types:
+            print("Invalid choice. Choose from: expiration, strike")
+            return pd.DataFrame()
+
+        if isinstance(options, pd.DataFrame):
+            chains = options.copy()
+
+        if isinstance(options, dict):
+            chains = pd.DataFrame(options)
+
+        elif isinstance(options, object) and not isinstance(options, pd.DataFrame):
+            chains = (
+                pd.DataFrame(options.chains)
+                if isinstance(options.chains, dict)
+                else options.chains.copy()
+            )
+
+            if options is None or chains.empty:
+                print(
+                    "No options chains data found in the supplied object.  Use load_options_chains()."
+                )
+                return pd.DataFrame()
+
+        if "openInterest" not in chains.columns:
+            print("Expected column, openInterest, not found.")
+            return pd.DataFrame()
+
+        if "volume" not in chains.columns:
+            print("Expected column, volume, not found.")
+            return pd.DataFrame()
+    except AttributeError:
+        print("Error: Invalid data type supplied.")
+        return pd.DataFrame()
+
+    stats = pd.DataFrame()
+    stats["Puts OI"] = (
+        chains[chains["optionType"] == "put"]
+        .groupby(f"{type}")
+        .sum(numeric_only=True)[["openInterest"]]
+        .astype(int)
+    )
+    stats["Calls OI"] = (
+        chains[chains["optionType"] == "call"]
+        .groupby(f"{type}")
+        .sum(numeric_only=True)[["openInterest"]]
+        .astype(int)
+    )
+    stats["Total OI"] = stats["Calls OI"] + stats["Puts OI"]
+    stats["OI Ratio"] = round(stats["Puts OI"] / stats["Calls OI"], 2)
+    stats["Puts Volume"] = (
+        chains[chains["optionType"] == "put"]
+        .groupby(f"{type}")
+        .sum(numeric_only=True)[["volume"]]
+        .astype(int)
+    )
+    stats["Calls Volume"] = (
+        chains[chains["optionType"] == "call"]
+        .groupby(f"{type}")
+        .sum(numeric_only=True)[["volume"]]
+        .astype(int)
+    )
+    stats["Total Volume"] = stats["Calls Volume"] + stats["Puts Volume"]
+    stats["Volume Ratio"] = round(stats["Puts Volume"] / stats["Calls Volume"], 2)
+    stats["Vol-OI Ratio"] = round(stats["Total Volume"] / stats["Total OI"], 2)
+
+    return stats.replace([np.nan, np.inf], "")
+
+
+class OptionsChains:
+    """Class for Options Chains."""
+
+    def __init__(self) -> None:
+        self.load_options_chains: Callable = load_options_chains
+        self.calculate_stats: Callable = calculate_stats
