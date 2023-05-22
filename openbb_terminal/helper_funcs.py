c80ed9c2608e1bf507885120666541e10bb68799
Liangyew <wongly280@gmail.com>
2023-05-22 21:28:07 +0100

    added limit logic to interactive table

--- openbb_terminal/helper_funcs.py
+++ openbb_terminal/helper_funcs.py
@@ -319,7 +319,7 @@ def print_rich_table(
         return output
 
     if enable_interactive and not print_to_console:
-        df_outgoing = df.copy()
+        df_outgoing = df.copy().iloc[:limit]
         # If headers are provided, use them
         if headers is not None:
             # We check if headers are valid
