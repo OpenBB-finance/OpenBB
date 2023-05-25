d0e7f2edcfc309a0d0a880443800744ce9aff042
teh_coderer <me@tehcoderer.com>
2023-05-25 13:37:32 -0400

    Update main.tsx

--- frontend-components/plotly/src/main.tsx
+++ frontend-components/plotly/src/main.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import * as ReactDOM from 'react-dom/client';
+import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 );
-
