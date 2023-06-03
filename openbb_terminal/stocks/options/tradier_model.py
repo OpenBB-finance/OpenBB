aaa8fcef988e3589f568abc34c93cc8ba4a05a98
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-06-02 21:52:59 -0700

    black

--- openbb_terminal/stocks/options/tradier_model.py
+++ openbb_terminal/stocks/options/tradier_model.py
@@ -123,7 +123,7 @@ def get_historical_options(
             "https://sandbox.tradier.com/v1/markets/history",
             params={"symbol": {symbol}, "interval": "daily"},
             headers={
-                "Authorization": f"Bearer {get_current_user().credentials.API_TRADIER_TOKEN}",# type: ignore[attr-defined]
+                "Authorization": f"Bearer {get_current_user().credentials.API_TRADIER_TOKEN}",  # type: ignore[attr-defined]
                 "Accept": "application/json",
             },
         )
