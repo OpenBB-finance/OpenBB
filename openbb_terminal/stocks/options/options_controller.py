a7edda330da07e569ca3da5cfeca7b973c37a5bf
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-06-19 06:11:16 -0700

    Feature/options - OpenBB Options Enhancement Proposal (Part 1 - SDK) (#5079)
    * add model file for Montreal Options Exchange
    
    * mypy fixes
    
    * define attributes under __init__
    
    * type error fix
    
    * another pylint fix
    
    * adds tests for tmx_model
    
    * black
    
    * mypy
    
    * adds expirations test and re-records
    
    * black
    
    * capture error message in test
    
    * mypy
    
    * add __docformat__ line
    
    * improves get_all_ticker_symbols
    
    * removes python magic command for new cell
    
    * adds TMX as source for SDK chains
    
    * starts on a new method for loading options data returning a standardized object
    
    * add load_options as SDK endpoint
    
    * intrinio response - not complete
    
    * adds tradier to load_options
    
    * add source attribute to options object
    
    * restore legacy nasdaq last price function
    
    * yFinance underlying_price fixes
    
    * yFinance model fix
    
    * adds cboe to sdk options load
    
    * table fix
    
    * creates options_chains_model.py and updates SDK map
    
    * docstrings added for the class.
    
    * updates tmx_model tests
    
    * fix options_controller test
    
    * adds cboe_model tests
    
    * spelling in docstring
    
    * ruff - unused import
    
    * mypy fixes
    
    * more mypy fixes
    
    * pylint
    
    * attempt to manually resolve merge conflict.
    
    * nasdaq_model fix
    
    * adds test coverage for options/nasdaq_model
    
    * docstrings update for nasdaq_model
    
    * mypy fix
    
    * adds unit testing for options_chains_model
    
    * adds additional tests for intrinio_model
    
    * unused import fix
    
    * fix for .TO symbol and TMX EOD chains
    
    * make CBOE Pydantic
    
    * cboe update
    
    * adds switch for pydantic output from all sources
    
    * black
    
    * index options error handling for Intrinio
    
    * add warning message for index options from Intrinio
    
    * tmx_model enhancements
    
    * cleans up get_ticker_iv() in cboe_model
    
    * fix ticker info for index symbols from cboe
    
    * typo in comments
    
    * adds test for tradier_model.load_options
    
    * cboe optionType as lower case
    
    * add function for calculating chains stats
    
    * black
    
    * trailmap for calculate_chains_stats
    
    * adds test for calculate_stats()
    
    * more details to nasdaq.underlying_price
    
    * typo in docstrings
    
    * mypy - options_chains_model.py
    
    * black - options_chains_model.py
    
    * pylint options_chains_model.py
    
    * standardize response from intrinio underlying price
    
    * add function to validate input of function
    
    * missed type hint
    
    * additional error handling for stats
    
    * adds DTE as column for all sources
    
    * adds strategies function and input validation
    
    * add yFinance test for load_options()
    
    * add Nasdaq test for load_options()
    
    * tests for new functions in options_chains_model
    
    * update sdk trailmap
    
    * rewrite tests
    
    * fixes for tests
    
    * fix tmx_model test
    
    * more pytest
    
    * pytest..
    
    * add vertical call spread function
    
    * some cleanup
    
    * update nasdaq tests
    
    * more cleanup
    
    * black
    
    * pylint
    
    * black
    
    * ruff
    
    * black
    
    * pylint
    
    * black
    
    * pylint..
    
    * tests rewrite
    
    * more pytest
    
    * recapture tests
    
    * Different tests were collected between gw1 and gw0
    
    * adds vertical spreads
    
    * vertical_put_spread
    
    * improve calculate_stats
    
    * mypy
    
    * edge cases
    
    * black
    
    * update tmx,yfinance tests
    
    * new cboe tests
    
    * new tmx_model tests
    
    * fix nasdaq test
    
    * test updates
    
    * fix nasdaq tests
    
    * add test_options_chains_model
    
    * refactor data sources
    
    * black
    
    * pylint
    
    * fixing stuff
    
    * forgot a file..
    
    * codespell
    
    * linting
    
    * mypy...
    
    * refactor OptionsChains class
    
    * pylint
    
    * reconfigure SDK entry point
    
    * fix error handling when data request fails
    
    * ruff
    
    * pylint
    
    * pylint
    
    * Add inheritance and a repr string
    
    * add method for skew
    
    * add method for skew
    
    * fix nasdaq test
    
    * add skew to tests
    
    ---------
    
    Co-authored-by: James Maslek <jmaslek11@gmail.com>

--- openbb_terminal/stocks/options/options_controller.py
+++ openbb_terminal/stocks/options/options_controller.py
@@ -199,7 +199,9 @@ class OptionsController(BaseController):
                 last_price = tradier_model.get_last_price(self.ticker)
                 self.current_price = last_price or 0.0
             elif self.source == "Nasdaq":
-                self.current_price = nasdaq_model.get_last_price(self.ticker)
+                self.current_price = nasdaq_model.get_underlying_price(self.ticker)[
+                    "lastPrice"
+                ]
             elif self.source == "Intrinio":
                 self.current_price = intrinio_model.get_last_price(self.ticker)
             else:
