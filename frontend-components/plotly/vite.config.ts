7ac77913462cbfe29a90138a0cd4712bfe7983cc
teh_coderer <me@tehcoderer.com>
2023-06-19 11:32:41 -0400

    update plotly frontend packages

--- frontend-components/plotly/vite.config.ts
+++ frontend-components/plotly/vite.config.ts
@@ -4,5 +4,5 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteSingleFile()],
+  plugins: [react(), viteSingleFile()],
 });
