9c7f796b99a8cf5796a980650357b3b8df6fe2f9
martinb-bb <105685594+martinb-bb@users.noreply.github.com>
2023-06-19 12:25:10 -0400

    0.3

--- openbb_terminal/helper_funcs.py
+++ openbb_terminal/helper_funcs.py
@@ -87,7 +87,7 @@ MENU_QUIT = 1
 MENU_RESET = 2
 
 GPT_INDEX_DIRECTORY = MISCELLANEOUS_DIRECTORY / "gpt_index/"
-GPT_INDEX_VER = 0.23
+GPT_INDEX_VER = 0.3
 
 
 # Command location path to be shown in the figures depending on watermark flag
