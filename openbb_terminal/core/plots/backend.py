4803614463e12ed0db23fe7fb77c6fb2b989ea45
tehcoderer <me@tehcoderer.com>
2023-07-06 16:43:04 -0400

    change plotlyjs download to run in thread

--- openbb_terminal/core/plots/backend.py
+++ openbb_terminal/core/plots/backend.py
@@ -6,6 +6,7 @@ import os
 import re
 import subprocess  # nosec: B404
 import sys
+import threading
 from multiprocessing import current_process
 from pathlib import Path
 from typing import Any, Dict, Optional, Union
@@ -500,4 +501,5 @@ def plots_backend() -> Backend:
 
 # To avoid having plotly.js in the repo, we download it if it's not present
 if not PLOTLYJS_PATH.exists() and not JUPYTER_NOTEBOOK:
-    plots_backend().loop.run_until_complete(download_plotly_js())
+    # We run this in a thread so we don't block the main thread
+    threading.Thread(target=asyncio.run, args=(download_plotly_js(),)).start()
