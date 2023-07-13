156ad59808858f4f679a797b8d164505ebac48bd
Pratyush Shukla <ps4534@nyu.edu>
2023-07-13 23:24:32 +0530

    modify data params names

--- openbb_sdk/sdk/provider/openbb_provider/model/data/forex_pairs.py
+++ openbb_sdk/sdk/provider/openbb_provider/model/data/forex_pairs.py
@@ -21,14 +21,14 @@ class ForexPairsData(Data):
         The name of the currency pair separated by '/'.
     currency : str
         The base currency of the currency pair.
-    stockExchange : str
+    stock_exchange : Optional[str]
         The stock exchange of the currency pair.
-    exchangeShortName : str
+    exchange_short_name : Optional[str]
         The short name of the stock exchange of the currency pair.
     """
 
     symbol: str
     name: str
     currency: str
-    stockExchange: Optional[str]
-    exchangeShortName: Optional[str]
+    stock_exchange: Optional[str]
+    exchange_short_name: Optional[str]
