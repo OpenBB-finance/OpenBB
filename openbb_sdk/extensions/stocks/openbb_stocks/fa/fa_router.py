0a1bc15882b2852fd1a72a49179f2958862d8a94
Pratyush Shukla <ps4534@nyu.edu>
2023-07-15 03:47:48 +0530

    Restructured functions in alphabetical order in `fa_router`

--- openbb_sdk/extensions/stocks/openbb_stocks/fa/fa_router.py
+++ openbb_sdk/extensions/stocks/openbb_stocks/fa/fa_router.py
@@ -16,6 +16,12 @@ from pydantic import BaseModel
 router = Router(prefix="/fa")
 
 
+@router.command
+def analysis() -> CommandOutput[Empty]:  # type: ignore
+    """Analyse SEC filings with the help of machine learning."""
+    return CommandOutput(results=Empty())
+
+
 @router.command(query="BalanceSheet")
 def balance(
     cc: CommandContext,
@@ -24,6 +30,17 @@ def balance(
     extra_params: ExtraParams,
 ) -> CommandOutput[BaseModel]:
     """Balance Sheet."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command(query="HistoricalDividends")
+def cal(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Historical Dividends."""
     return CommandOutput(results=Query(**locals()).execute())
 
 
@@ -49,6 +66,41 @@ def comp(
     return CommandOutput(results=Query(**locals()).execute())
 
 
+@router.command(query="StockSplitCalendar")
+def comsplit(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Stock Split Calendar."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command
+def customer() -> CommandOutput[Empty]:  # type: ignore
+    """List of customers of the company."""
+    return CommandOutput(results=Empty())
+
+
+@router.command
+def dcfc() -> CommandOutput[Empty]:  # type: ignore
+    """Determine the (historical) discounted cash flow."""
+    return CommandOutput(results=Empty())
+
+
+@router.command
+def divs() -> CommandOutput[Empty]:  # type: ignore
+    """Show historical dividends for company."""
+    return CommandOutput(results=Empty())
+
+
+@router.command
+def dupont() -> CommandOutput[Empty]:  # type: ignore
+    """Detailed breakdown for Return on Equity (RoE)."""
+    return CommandOutput(results=Empty())
+
+
 @router.command(query="EarningsCalendar")
 def earning(
     cc: CommandContext,
@@ -71,211 +123,6 @@ def emp(
     return CommandOutput(results=Query(**locals()).execute())
 
 
-@router.command(query="AnalystEstimates")
-def est(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Analyst Estimates."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="IncomeStatement")
-def income(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Income Statement."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="StockInsiderTrading")
-def ins(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Stock Insider Trading."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="KeyMetrics")
-def metrics(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Key Metrics."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="KeyExecutives")
-def mgmt(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Key Executives."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="CompanyOverview")
-def overview(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Company Overview."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="InstitutionalOwnership")
-def own(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Institutional Ownership."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="PriceTarget")
-def pta(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Price Target."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="PriceTargetConsensus")
-def pt(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Price Target Consensus."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="RevenueGeographic")
-def revgeo(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Revenue Geographic."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="RevenueBusinessLine")
-def revseg(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Revenue Business Line."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="ShareStatistics")
-def shrs(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Share Statistics."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command
-def shares() -> CommandOutput[Empty]:  # type: ignore
-    return CommandOutput(results=Empty())
-
-
-@router.command(query="EarningsCallTranscript")
-def transcript(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Earnings Call Transcript."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="HistoricalStockSplits")
-def split(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Historical Stock Splits."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="HistoricalDividends")
-def cal(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Historical Dividends."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command(query="StockSplitCalendar")
-def comsplit(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> CommandOutput[BaseModel]:
-    """Stock Split Calendar."""
-    return CommandOutput(results=Query(**locals()).execute())
-
-
-@router.command
-def customer() -> CommandOutput[Empty]:  # type: ignore
-    """List of customers of the company."""
-    return CommandOutput(results=Empty())
-
-
-@router.command
-def divs() -> CommandOutput[Empty]:  # type: ignore
-    """Show historical dividends for company."""
-    return CommandOutput(results=Empty())
-
-
-@router.command
-def dcfc() -> CommandOutput[Empty]:  # type: ignore
-    """Determine the (historical) discounted cash flow."""
-    return CommandOutput(results=Empty())
-
-
-@router.command
-def dupont() -> CommandOutput[Empty]:  # type: ignore
-    """Detailed breakdown for Return on Equity (RoE)."""
-    return CommandOutput(results=Empty())
-
-
 @router.command
 def enterprise() -> CommandOutput[Empty]:  # type: ignore
     """Enterprise value."""
@@ -288,10 +135,15 @@ def epsfc() -> CommandOutput[Empty]:  # type: ignore
     return CommandOutput(results=Empty())
 
 
-@router.command
-def analysis() -> CommandOutput[Empty]:  # type: ignore
-    """Analyse SEC filings with the help of machine learning."""
-    return CommandOutput(results=Empty())
+@router.command(query="AnalystEstimates")
+def est(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Analyst Estimates."""
+    return CommandOutput(results=Query(**locals()).execute())
 
 
 @router.command
@@ -323,9 +175,53 @@ def historical_5() -> CommandOutput[Empty]:  # type: ignore
     return CommandOutput(results=Empty())
 
 
+@router.command(query="IncomeStatement")
+def income(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Income Statement."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command(query="StockInsiderTrading")
+def ins(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Stock Insider Trading."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
 @router.command
 def key() -> CommandOutput[Empty]:  # type: ignore
     return CommandOutput(results=Empty())
+
+
+@router.command(query="KeyMetrics")
+def metrics(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Key Metrics."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command(query="KeyExecutives")
+def mgmt(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Key Executives."""
+    return CommandOutput(results=Query(**locals()).execute())
 
 
 @router.command
@@ -334,9 +230,53 @@ def mktcap() -> CommandOutput[Empty]:  # type: ignore
     return CommandOutput(results=Empty())
 
 
-@router.command  # CHECK IF NEWS ARE NEEDED HERE
+@router.command  # CHECK IF NEWS IS NEEDED HERE
 def news() -> CommandOutput[Empty]:  # type: ignore
     return CommandOutput(results=Empty())
+
+
+@router.command(query="CompanyOverview")
+def overview(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Company Overview."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command(query="InstitutionalOwnership")
+def own(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Institutional Ownership."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command(query="PriceTargetConsensus")
+def pt(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Price Target Consensus."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command(query="PriceTarget")
+def pta(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Price Target."""
+    return CommandOutput(results=Query(**locals()).execute())
 
 
 @router.command
@@ -357,6 +297,28 @@ def revfc() -> CommandOutput[Empty]:  # type: ignore
     return CommandOutput(results=Empty())
 
 
+@router.command(query="RevenueGeographic")
+def revgeo(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Revenue Geographic."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command(query="RevenueBusinessLine")
+def revseg(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Revenue Business Line."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
 @router.command
 def rot() -> CommandOutput[Empty]:  # type: ignore
     """Number of analyst ratings over time on a monthly basis."""
@@ -375,6 +337,44 @@ def sec() -> CommandOutput[Empty]:  # type: ignore
 
 
 @router.command
+def shares() -> CommandOutput[Empty]:  # type: ignore
+    return CommandOutput(results=Empty())
+
+
+@router.command(query="ShareStatistics")
+def shrs(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Share Statistics."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command(query="HistoricalStockSplits")
+def split(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Historical Stock Splits."""
+    return CommandOutput(results=Query(**locals()).execute())
+
+
+@router.command
 def supplier() -> CommandOutput[Empty]:  # type: ignore
     """List of suppliers of the company."""
     return CommandOutput(results=Empty())
+
+
+@router.command(query="EarningsCallTranscript")
+def transcript(
+    cc: CommandContext,
+    provider_choices: ProviderChoices,
+    standard_params: StandardParams,
+    extra_params: ExtraParams,
+) -> CommandOutput[BaseModel]:
+    """Earnings Call Transcript."""
+    return CommandOutput(results=Query(**locals()).execute())
