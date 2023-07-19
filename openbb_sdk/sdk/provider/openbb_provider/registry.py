89386d5445e6ffa1820b47a51f8b2deada7d6838
Diogo Sousa <montezdesousa@gmail.com>
2023-07-19 18:22:14 +0100

    raise provider error if failure

--- openbb_sdk/sdk/provider/openbb_provider/registry.py
+++ openbb_sdk/sdk/provider/openbb_provider/registry.py
@@ -13,6 +13,10 @@ from openbb_provider.settings import settings
 orients = Literal["LIST", "RECORDS"]
 
 
+class ProviderError(Exception):
+    pass
+
+
 def process(
     data: Union[List[ProviderDataType], ProviderDataType], orientation: orients
 ) -> Any:
@@ -103,8 +107,8 @@ class ProviderRegistry:
                 result = getattr(fetcher, data_name)(query, extra_params, api_key)
             return process(result, data_orientation)
         except Exception as e:
-            raise RuntimeError(
-                f"The {provider_name} provider failed to fetch the data: {e}"
+            raise ProviderError(
+                f"The '{provider_name}' provider failed to fetch the data: {e}"
             ) from e
 
     def fetch(
