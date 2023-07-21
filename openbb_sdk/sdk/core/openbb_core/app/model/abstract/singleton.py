a9795f80feaed956c94001e8d4e756b3350f882f
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-07-21 14:37:20 +0100

    introducing singleton abstract class

---
+++ openbb_sdk/sdk/core/openbb_core/app/model/abstract/singleton.py
@@ -0,0 +1,9 @@
+class SingletonMeta(type):
+    # TODO : check if we want to update this to be thread safe
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
