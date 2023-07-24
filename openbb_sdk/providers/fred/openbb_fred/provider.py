cc2e2c9cbc889e5d7519842daf67f9b30653ed7e
Diogo Sousa <montezdesousa@gmail.com>
2023-07-24 15:55:06 +0100

    remove ProviderName

--- openbb_sdk/providers/fred/openbb_fred/provider.py
+++ openbb_sdk/providers/fred/openbb_fred/provider.py
@@ -3,14 +3,14 @@
 # IMPORT STANDARD
 # IMPORT THIRD-PARTY
 # IMPORT INTERNAL
-from openbb_provider.abstract.provider import Provider, ProviderNameType
+from openbb_provider.abstract.provider import Provider
 
 from openbb_fred.cpi import FREDCPIFetcher
 
 # mypy: disable-error-code="list-item"
 
 fred_provider = Provider(
-    name=ProviderNameType("fred"),
+    name="fred",
     description="Provider for FRED.",
     required_credentials=["api_key"],
     fetcher_list=[FREDCPIFetcher],
