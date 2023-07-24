f7d003d3148993288a81060fe41a2bfa51f97e68
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-07-24 13:08:45 +0200

    More fixes

--- openbb_sdk/providers/fmp/openbb_fmp/historical_employees.py
+++ openbb_sdk/providers/fmp/openbb_fmp/historical_employees.py
@@ -4,6 +4,8 @@
 from datetime import date, datetime
 from typing import Dict, List, Optional
 
+from openbb_provider.abstract.data import Data
+
 # IMPORT INTERNAL
 from openbb_provider.abstract.fetcher import Fetcher
 from openbb_provider.helpers import data_transformer
@@ -30,7 +32,7 @@ class FMPHistoricalEmployeesQueryParams(HistoricalEmployeesQueryParams):
     """
 
 
-class FMPHistoricalEmployeesData(HistoricalEmployeesData):
+class FMPHistoricalEmployeesData(Data):
     """FMP Historical Employees data."""
 
     symbol: str = Field(min_length=1)
