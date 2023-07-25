6d8337c948f7f0220d42a361f4ffa0b1b8972105
Pratyush Shukla <ps4534@nyu.edu>
2023-07-26 03:10:45 +0530

    added Field descriptions

--- openbb_sdk/providers/polygon/openbb_polygon/utils/types.py
+++ openbb_sdk/providers/polygon/openbb_polygon/utils/types.py
@@ -23,71 +23,28 @@ class PolygonFundamentalQueryParams(QueryParams):
     """Polygon Fundamental QueryParams.
 
     Source: https://polygon.io/docs/stocks#!/get_vx_reference_financials
-
-    Parameter
-    ---------
-    symbol : Optional[str]
-        The symbol of the company if no CIK is provided.
-    cik : Optional[str]
-        The CIK of the company if no symbol is provided.
-    company_name : Optional[str]
-        The name of the company.
-    company_name_search : Optional[str]
-        The name of the company to search.
-    sic : Optional[str]
-        The Standard Industrial Classification (SIC) of the company.
-    filing_date : Optional[date]
-        The filing date of the financial statement.
-    filing_date_lt : Optional[date]
-        The filing date less than the given date.
-    filing_date_lte : Optional[date]
-        The filing date less than or equal to the given date.
-    filing_date_gt : Optional[date]
-        The filing date greater than the given date.
-    filing_date_gte : Optional[date]
-        The filing date greater than or equal to the given date.
-    period_of_report_date : Optional[date]
-        The period of report date of the financial statement.
-    period_of_report_date_lt : Optional[date]
-        The period of report date less than the given date.
-    period_of_report_date_lte : Optional[date]
-        The period of report date less than or equal to the given date.
-    period_of_report_date_gt : Optional[date]
-        The period of report date greater than the given date.
-    period_of_report_date_gte : Optional[date]
-        The period of report date greater than or equal to the given date.
-    timeframe : Optional[Literal["annual", "quarterly", "ttm"]]
-        The timeframe of the financial statement.
-    include_sources : Optional[bool]
-        Whether to include the sources of the financial statement.
-    order : Optional[Literal["asc", "desc"]]
-        The order of the financial statement.
-    limit : Optional[PositiveInt]
-        The limit of the financial statement.
-    sort : Optional[Literal["filing_date", "period_of_report_date"]]
-        The sort of the financial statement.
     """
 
-    ticker: Optional[str] = Field(alias="symbol")
-    cik: Optional[str]
-    company_name: Optional[str]
-    company_name_search: Optional[str] = Field(alias="company_name.search")
-    sic: Optional[str]
-    filing_date: Optional[date]
-    filing_date_lt: Optional[date] = Field(alias="filing_date.lt")
-    filing_date_lte: Optional[date] = Field(alias="filing_date.lte")
-    filing_date_gt: Optional[date] = Field(alias="filing_date.gt")
-    filing_date_gte: Optional[date] = Field(alias="filing_date.gte")
-    period_of_report_date: Optional[date]
-    period_of_report_date_lt: Optional[date] = Field(alias="period_of_report_date.lt")
-    period_of_report_date_lte: Optional[date] = Field(alias="period_of_report_date.lte")
-    period_of_report_date_gt: Optional[date] = Field(alias="period_of_report_date.gt")
-    period_of_report_date_gte: Optional[date] = Field(alias="period_of_report_date.gte")
-    timeframe: Optional[Literal["annual", "quarterly", "ttm"]] = Field(alias="period")
-    include_sources: Optional[bool]
-    order: Optional[Literal["asc", "desc"]]
-    limit: Optional[PositiveInt] = 100
-    sort: Optional[Literal["filing_date", "period_of_report_date"]]
+    ticker: Optional[str] = Field(alias="symbol", description="The symbol of the company if no CIK is provided.")
+    cik: Optional[str] = Field(description="The CIK of the company if no symbol is provided.")
+    company_name: Optional[str] = Field(description="The name of the company.")
+    company_name_search: Optional[str] = Field(alias="company_name.search", description="The name of the company to search.")
+    sic: Optional[str] = Field(description="The Standard Industrial Classification (SIC) of the company.")
+    filing_date: Optional[date] = Field(description="The filing date of the financial statement.")
+    filing_date_lt: Optional[date] = Field(alias="filing_date.lt", description="The filing date less than the given date.")
+    filing_date_lte: Optional[date] = Field(alias="filing_date.lte", description="The filing date less than or equal to the given date.")
+    filing_date_gt: Optional[date] = Field(alias="filing_date.gt", description="The filing date greater than the given date.")
+    filing_date_gte: Optional[date] = Field(alias="filing_date.gte", description="The filing date greater than or equal to the given date.")
+    period_of_report_date: Optional[date] = Field(description="The period of report date of the financial statement.")
+    period_of_report_date_lt: Optional[date] = Field(alias="period_of_report_date.lt", description="The period of report date less than the given date.")
+    period_of_report_date_lte: Optional[date] = Field(alias="period_of_report_date.lte", description="The period of report date less than or equal to the given date.")
+    period_of_report_date_gt: Optional[date] = Field(alias="period_of_report_date.gt", description="The period of report date greater than the given date.")
+    period_of_report_date_gte: Optional[date] = Field(alias="period_of_report_date.gte", description="The period of report date greater than or equal to the given date.")
+    timeframe: Optional[Literal["annual", "quarterly", "ttm"]] = Field(alias="period", description="The timeframe of the financial statement.")
+    include_sources: Optional[bool] = Field(description="Whether to include the sources of the financial statement.")
+    order: Optional[Literal["asc", "desc"]] = Field(description="The order of the financial statement.")
+    limit: Optional[PositiveInt] = 100 = Field(description="The limit of the financial statement.")
+    sort: Optional[Literal["filing_date", "period_of_report_date"]] = Field(description="The sort of the financial statement.")
 
 
 class BaseStockQueryParams(QueryParams):
