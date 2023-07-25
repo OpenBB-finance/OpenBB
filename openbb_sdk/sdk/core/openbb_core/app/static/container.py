2a603d7a13997ac92de32cc37fcf7a4e35aa5240
Theodore Aptekarev <aptekarev@gmail.com>
2023-07-25 10:05:15 +0300

    Lint black

--- openbb_sdk/sdk/core/openbb_core/app/static/container.py
+++ openbb_sdk/sdk/core/openbb_core/app/static/container.py
@@ -2,6 +2,5 @@ from openbb_core.app.command_runner import CommandRunnerSession
 
 
 class Container:
-
     def __init__(self, command_runner_session: CommandRunnerSession) -> None:
         self._command_runner_session = command_runner_session
