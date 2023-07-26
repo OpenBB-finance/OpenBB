dca3001ea6aa55d2e9bd90562a30c0f694b5aa17
Pratyush Shukla <ps4534@nyu.edu>
2023-07-26 22:23:14 +0530

    refactored `EarningsCalendar` for `stocks.fa.cal`

--- openbb_sdk/providers/fmp/openbb_fmp/models/earnings_calendar.py
+++ openbb_sdk/providers/fmp/openbb_fmp/models/earnings_calendar.py
@@ -1,10 +1,10 @@
 """FMP Earnings Calendar fetcher."""
 
 
-from datetime import date as dateType
+from datetime import datetime
+
 from typing import Dict, List, Optional
 
-from openbb_provider.abstract.data import Data
 from openbb_provider.abstract.fetcher import Fetcher
 from openbb_provider.helpers import data_transformer
 from openbb_provider.models.earnings_calendar import (
@@ -14,30 +14,38 @@ from openbb_provider.models.earnings_calendar import (
 
 from openbb_fmp.utils.helpers import create_url, get_data_many
 
+from pydantic import validator
+
 
 class FMPEarningsCalendarQueryParams(EarningsCalendarQueryParams):
-    """FMP Earnings Calendar query.
+    """FMP Earnings Calendar Query.
 
     Source: https://site.financialmodelingprep.com/developer/docs/earnings-calendar-api/
-
-    Parameter
-    ---------
-    symbol : str
-        The symbol of the company.
-    limit : int
     """
 
 
-class FMPEarningsCalendarData(Data):
-    date: dateType
-    symbol: str
-    eps: Optional[float]
-    epsEstimated: Optional[float]
-    time: str
-    revenue: Optional[int]
-    revenueEstimated: Optional[int]
-    updatedFromDate: Optional[dateType]
-    fiscalDateEnding: dateType
+class FMPEarningsCalendarData(EarningsCalendarData):
+    """FMP Earnings Calendar Data."""
+
+    class Config:
+        fields = {
+            "eps_estimated": "epsEstimated",
+            "revenue_estimated": "revenueEstimated",
+            "updated_from_date": "updatedFromDate",
+            "fiscal_date_ending": "fiscalDateEnding",
+        }
+
+    @validator("date", pre=True, check_fields=False)
+    def date_validate(cls, v: str):  # pylint: disable=E0213
+        return datetime.strptime(v, "%Y-%m-%d") if v else None
+
+    @validator("updatedFromDate", pre=True, check_fields=False)
+    def updated_from_date_validate(cls, v: str):  # pylint: disable=E0213
+        return datetime.strptime(v, "%Y-%m-%d") if v else None
+
+    @validator("fiscalDateEnding", pre=True, check_fields=False)
+    def fiscal_date_ending_validate(cls, v: str):  # pylint: disable=E0213
+        return datetime.strptime(v, "%Y-%m-%d") if v else None
 
 
 class FMPEarningsCalendarFetcher(
