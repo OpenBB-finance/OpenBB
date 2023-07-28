ecf9e58e083930dd0f85a3bd03aa194b293807fc
James Maslek <jmaslek11@gmail.com>
2023-07-28 12:04:12 -0400

    update version

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session):
     session.install("./openbb_sdk")
     session.install("pytest")
