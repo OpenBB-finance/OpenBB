f3d20af7e8fecc6c3a4a97adfc16193ef9f0de94
Diogo Sousa <montezdesousa@gmail.com>
2023-07-29 15:48:34 +0100

    move query params

--- openbb_sdk/sdk/provider/openbb_provider/abstract/data.py
+++ openbb_sdk/sdk/provider/openbb_provider/abstract/data.py
@@ -10,13 +10,3 @@ class Data(BaseModel):
     class Config:
         extra = Extra.allow
         allow_population_by_field_name = True
-
-
-class QueryParams(BaseModel):
-    """The OpenBB Standardized QueryParams Model that holds the query input parameters."""
-
-    class Config:
-        allow_population_by_field_name = True
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}({', '.join([f'{k}={v}' for k, v in self.dict().items()])})"
