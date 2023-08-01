ec42c463e2d551cb7a1be085670206c3e137e5f5
Diogo Sousa <montezdesousa@gmail.com>
2023-08-01 19:18:46 +0100

    sry again

--- openbb_sdk/sdk/core/openbb_core/app/static/coverage.py
+++ openbb_sdk/sdk/core/openbb_core/app/static/coverage.py
@@ -3,7 +3,7 @@ from openbb_core.app.router import CommandMap
 
 class Coverage:
     def __init__(self):
-        self._command_map = CommandMap(sep=".")
+        self._command_map = CommandMap(coverage_sep=".")
 
     @property
     def providers(self):
