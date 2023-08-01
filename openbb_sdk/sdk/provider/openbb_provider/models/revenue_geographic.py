60f9033909bbab71bbfdc4c3c2080c4f6b27b6f2
Pratyush Shukla <ps4534@nyu.edu>
2023-08-02 01:35:51 +0530

    refactored `RevenueGeographic` for `stocks.fa.revgeo`

--- openbb_sdk/sdk/provider/openbb_provider/models/revenue_geographic.py
+++ openbb_sdk/sdk/provider/openbb_provider/models/revenue_geographic.py
@@ -1,60 +1,42 @@
 """Revenue by geographic segments data model."""
 
 
-from datetime import date
+from datetime import date as dateType
 from typing import Literal, Optional
-
-from pydantic import Field
 
 from openbb_provider.abstract.data import Data
 from openbb_provider.abstract.query_params import QueryParams
-from openbb_provider.descriptions import QUERY_DESCRIPTIONS
+from openbb_provider.descriptions import QUERY_DESCRIPTIONS, DATA_DESCRIPTIONS
 from openbb_provider.models.base import BaseSymbol
+
+from pydantic import Field
 
 
 class RevenueGeographicQueryParams(QueryParams, BaseSymbol):
-    """Revenue geographic query.
-
-    Parameter
-    ---------
-    symbol : str
-        The symbol of the company.
-    period : Literal["quarterly", "annually"]
-        The period of the income statement.
-    structure : Literal["hierarchical", "flat"]
-        The structure of the revenue geographic. Should always be flat.
-    """
+    """Revenue by Geographic Segments Query."""
 
     period: Literal["quarterly", "annually"] = Field(
-        default="quarterly", description=QUERY_DESCRIPTIONS.get("period", "")
+        default="annually", description=QUERY_DESCRIPTIONS.get("period", "")
     )
     structure: Literal["hierarchical", "flat"] = Field(
-        default="flat"
-    )  # should always be flat
+        default="flat", description="The structure of the returned data."
+    )  # should always be flat # should always be flat
 
 
 class RevenueGeographicData(Data):
-    """Revenue by geographic segments data.
+    """Revenue by Geographic Segments Data."""
 
-    Returns
-    -------
-    date : date
-        The date of the revenue.
-    americas : Optional[int]
-        The revenue of the America segment.
-    europe : Optional[int]
-        The revenue of the Europe segment.
-    greater_china : Optional[int]
-        The revenue of the Greater China segment.
-    japan : Optional[int]
-        The revenue of the Japan segment.
-    rest_of_asia_pacific : Optional[int]
-        The revenue of the Rest of Asia Pacific segment.
-    """
-
-    date: date
-    americas: Optional[int]
-    europe: Optional[int]
-    greater_china: Optional[int]
-    japan: Optional[int]
-    rest_of_asia_pacific: Optional[int]
+    date: dateType = Field(description=DATA_DESCRIPTIONS.get("date", ""))
+    americas: Optional[int] = Field(
+        description="The revenue from the the American segment."
+    )
+    europe: Optional[int] = Field(
+        description="The revenue from the the European segment."
+    )
+    greater_china: Optional[int] = Field(
+        description="The revenue from the the Greater China segment."
+    )
+    japan: Optional[int] = Field(description="The revenue from the the Japan segment.")
+    rest_of_asia_pacific: Optional[int] = Field(
+        description="The revenue from the the Rest of Asia Pacific segment."
+    )
