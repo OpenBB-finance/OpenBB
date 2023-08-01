83ed6f167503ee9ac5450475b90a7b7b644b30e3
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-08-01 05:29:31 +0100

    placeholders

--- openbb_sdk/sdk/core/tests/app/logs/utils/test_system_utils.py
+++ openbb_sdk/sdk/core/tests/app/logs/utils/test_system_utils.py
@@ -1,1 +1,11 @@
 # TODO : add tests once the code is fixed
+
+from openbb_core.app.logs.utils.system_utils import get_commit_hash, get_branch
+
+
+def test_get_commit_hash():
+    pass
+
+
+def test_get_branch():
+    pass
