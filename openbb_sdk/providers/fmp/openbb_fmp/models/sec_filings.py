da08d83943a0d4bd40e6843df10dd50c3fabbb59
Pratyush Shukla <ps4534@nyu.edu>
2023-08-02 02:20:51 +0530

    refactored `SECFilings` for `stocks.dd.sec`

--- openbb_sdk/providers/fmp/openbb_fmp/models/sec_filings.py
+++ openbb_sdk/providers/fmp/openbb_fmp/models/sec_filings.py
@@ -1,50 +1,37 @@
 """SEC Filings fetcher."""
 
 
-from datetime import (
-    date as dateType,
-    datetime,
-)
+from datetime import datetime
 from typing import Any, Dict, List, Optional
 
-from openbb_provider.abstract.data import Data
 from openbb_provider.abstract.fetcher import Fetcher
 from openbb_provider.models.sec_filings import SECFilingsData, SECFilingsQueryParams
-from pydantic import validator
 
 from openbb_fmp.utils.helpers import create_url, get_data_many
 
+from pydantic import validator
+
 
 class FMPSECFilingsQueryParams(SECFilingsQueryParams):
-    """FMP SEC Filings QueryParams.
+    """FMP SEC Filings Query.
 
     Source: https://site.financialmodelingprep.com/developer/docs/sec-filings-api/
-
-    Parameter
-    ---------
-    symbol : str
-        The symbol of the company.
-    type : str
-        The type of the SEC filing form. (full list: https://www.sec.gov/forms)
-    page : int
-        The page of the results.
-    limit : int
-        The limit of the results.
     """
 
 
-class FMPSECFilingsData(Data):
-    symbol: str
-    fillingDate: dateType
-    acceptedDate: dateType
-    cik: str
-    type: str
-    link: str
-    finalLink: str
+class FMPSECFilingsData(SECFilingsData):
+    """FMP SEC Filings Data."""
 
-    @validator("fillingDate", "acceptedDate", pre=True)
+    class Config:
+        fields = {
+            "filling_date": "fillingDate",
+            "accepted_date": "acceptedDate",
+            "final_link": "finalLink",
+        }
+
+    @validator("fillingDate", "acceptedDate", pre=True, check_fields=False)
     def convert_date(cls, v):  # pylint: disable=no-self-argument
-        return datetime.strptime(v, "%Y-%m-%d %H:%M:%S").date()
+        return datetime.strptime(v, "%Y-%m-%d %H:%M:%S")
 
 
 class FMPSECFilingsFetcher(
@@ -68,6 +55,7 @@ class FMPSECFilingsFetcher(
         url = create_url(
             3, f"sec_filings/{query.symbol}", api_key, query, exclude=["symbol"]
         )
+
         return get_data_many(url, FMPSECFilingsData)
 
     @staticmethod
