5a17d06e67d14d9d72d3ba403f9ae69601255ba6
Diogo Sousa <montezdesousa@gmail.com>
2023-08-01 18:55:48 +0100

    rename __commands__ to __extensions__

--- openbb_sdk/sdk/core/openbb_core/app/static/package/__extensions__.py
+++ openbb_sdk/sdk/core/openbb_core/app/static/package/__extensions__.py
@@ -5,7 +5,7 @@
 from openbb_core.app.static.container import Container
 
 
-class Commands(Container):
+class Extensions(Container):
     @property
     def crypto(self):  # route = "/crypto"
         from openbb_core.app.static.package import crypto
