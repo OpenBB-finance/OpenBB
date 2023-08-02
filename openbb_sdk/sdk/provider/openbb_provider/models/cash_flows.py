4f71018d75e4d5daf7772a271e38fd104938632e
Pratyush Shukla <ps4534@nyu.edu>
2023-08-02 18:42:21 +0530

    refactored `CashFlowStatement` for `stocks.fa.cash`

--- openbb_sdk/sdk/provider/openbb_provider/models/cash_flows.py
+++ openbb_sdk/sdk/provider/openbb_provider/models/cash_flows.py
@@ -1,117 +1,74 @@
 """Cash Flow Statement Data Model."""
 
 
-from datetime import date as dateType
+from datetime import date as dateType, datetime
 from typing import Optional
 
 from openbb_provider.abstract.data import Data
 from openbb_provider.models.base import FinancialStatementQueryParams
+from openbb_provider.descriptions import DATA_DESCRIPTIONS
+
+from pydantic import Field
 
 
 class CashFlowStatementQueryParams(FinancialStatementQueryParams):
-    """Cash flow statement query.
-
-    Parameter
-    ---------
-    symbol : str
-        The symbol of the company.
-    period : Literal["annually", "quarterly"]
-        The period of the income statement.
-    """
+    """Cash Flow Statement Query."""
 
 
 class CashFlowStatementData(Data):
-    """Return Cash Flow Statement Data.
+    """Cash Flow Statement Data."""
 
-    Returns
-    -------
-    date : date
-        The date of the cash flow statement.
-    symbol : str
-        The symbol of the company.
-    currency : Optional[str]
-        The currency of the cash flow statement.
-    cik : Optional[int]
-        The Central Index Key (CIK) of the company.
-    period : Optional[str]
-        The period of the cash flow statement.
-    cash_at_beginning_of_period : Optional[int]
-        The cash at the beginning of the period.
-    net_income : Optional[int]
-        The net income.
-    depreciation_and_amortization : Optional[int]
-        The depreciation and amortization.
-    stock_based_compensation : Optional[int]
-        The stock based compensation.
-    accounts_receivables : Optional[int]
-        The accounts receivables.
-    inventory : Optional[int]
-        The inventory.
-    accounts_payables : Optional[int]
-        The accounts payables.
-    net_cash_flow_from_operating_activities : Optional[int]
-        The net cash flow from operating activities.
-    purchases_of_investments : Optional[int]
-        The purchases of investments.
-    sales_maturities_of_investments : Optional[int]
-        The sales maturities of investments.
-    investments_in_property_plant_and_equipment : Optional[int]
-        The investments in property plant and equipment.
-    net_cash_flow_from_investing_activities : Optional[int]
-        The net cash flow from investing activities.
-    dividends_paid : Optional[int]
-        The dividends paid.
-    common_stock_repurchased : Optional[int]
-        The common stock repurchased.
-    debt_repayment : Optional[int]
-        The debt repayment.
-    other_financing_activities : Optional[int]
-        The other financing activities.
-    net_cash_flow_from_financing_activities : Optional[int]
-        The net cash flow from financing activities.
-    net_cash_flow : Optional[int]
-        The net cash flow.
-    cash_at_end_of_period : Optional[int]
-        The cash at the end of the period.
-    """
-
-    date: dateType
-    symbol: str
-    currency: Optional[str] = None
-    cik: Optional[int]
-    period: Optional[str]
+    date: dateType = Field(description="Date of the fetched statement.")
+    symbol: Optional[str] = Field(description="Symbol of the company.")
+    cik: Optional[int] = Field(description="Central Index Key.")
+    currency: Optional[str] = Field(description="Reporting currency.")
+    filing_date: Optional[dateType] = Field(description="Filling date.")
+    accepted_date: Optional[datetime] = Field(description="Accepted date.")
+    period: Optional[str] = Field(description="Reporting period of the statement.")
 
     cash_at_beginning_of_period: Optional[int]
     net_income: Optional[int]
     depreciation_and_amortization: Optional[int]
     stock_based_compensation: Optional[int]
-    # OTHER
+    other_non_cash_items: Optional[int] = Field(description="Other non-cash items.")
+    deferred_income_tax: Optional[int] = Field(description="Deferred income tax.")
 
-    accounts_receivables: Optional[int]
     inventory: Optional[int]
-    # VENDOR NON-TRADE_RECEIVABLES
-    # OTHER CURRENT AND NON-CURRENT ASSETS
+    accounts_receivables: Optional[int]
+
     accounts_payables: Optional[int]
-    # DEFERRED_REVENUE
-    # OTHER CURRENT AND NON-CURRENT LIABILITIES
-    other_investing_activities: Optional[int]
+    other_working_capital: Optional[int] = Field(
+        description="Accrued expenses and other, Unearned revenue."
+    )
     net_cash_flow_from_operating_activities: Optional[int]
 
-    purchases_of_investments: Optional[int]
+    capital_expenditure: Optional[int] = Field(
+        description="Purchases of property and equipment."
+    )
+    other_investing_activities: Optional[int] = Field(
+        description="Proceeds from property and equipment sales and incentives."
+    )
+    acquisitions_net: Optional[int] = Field(
+        description="Acquisitions, net of cash acquired, and other"
+    )
     sales_maturities_of_investments: Optional[int]
-    investments_in_property_plant_and_equipment: Optional[int]
-    # OTHER
+    purchases_of_investments: Optional[int]
     net_cash_flow_from_investing_activities: Optional[int]
+    investments_in_property_plant_and_equipment: Optional[int]
 
     dividends_paid: Optional[int]
     common_stock_repurchased: Optional[int]
     debt_repayment: Optional[int]
 
     other_financing_activities: Optional[int]
-    # REPAYMENT OF COMMERCIAL PAPER
-    # OTHER
 
     net_cash_flow_from_financing_activities: Optional[int]
+    effect_of_forex_changes_on_cash: Optional[int] = Field(
+        description="Foreign currency effect on cash, cash equivalents, and restricted cash"
+    )
 
-    net_cash_flow: Optional[int]
+    net_change_in_cash: Optional[int] = Field(
+        description="Net increase (decrease) in cash, cash equivalents, and restricted cash"
+    )
+
     cash_at_end_of_period: Optional[int]
