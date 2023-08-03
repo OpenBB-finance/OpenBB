1589277310743f4d4cd3011bbc359c2313978dfb
Pratyush Shukla <ps4534@nyu.edu>
2023-08-03 17:46:30 +0530

    added `ForexEOD` yfinance model for `forex.load`

--- openbb_sdk/providers/yfinance/openbb_yfinance/__init__.py
+++ openbb_sdk/providers/yfinance/openbb_yfinance/__init__.py
@@ -2,6 +2,7 @@
 from openbb_provider.abstract.provider import Provider
 
 from openbb_yfinance.models.crypto_eod import YFinanceCryptoEODFetcher
+from openbb_yfinance.models.forex_eod import YFinanceForexEODFetcher
 
 yfinance_provider = Provider(
     name="yfinance",
@@ -12,5 +13,6 @@ yfinance_provider = Provider(
     required_credentials=None,
     fetcher_dict={
         "CryptoEOD": YFinanceCryptoEODFetcher,
+        "ForexEOD": YFinanceForexEODFetcher,
     },
 )
