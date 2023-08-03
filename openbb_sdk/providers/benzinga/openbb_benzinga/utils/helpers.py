7790a062ef653801c645c1750a7cea81d4254ec6
James Maslek <jmaslek11@gmail.com>
2023-08-03 13:13:58 -0400

    benzinga requests

--- openbb_sdk/providers/benzinga/openbb_benzinga/utils/helpers.py
+++ openbb_sdk/providers/benzinga/openbb_benzinga/utils/helpers.py
@@ -1,12 +1,16 @@
 """Benzinga Helpers."""
 
 
-import requests
+from typing import Any
 from pydantic import BaseModel
 
+from openbb_provider import utils
 
-def get_data(url: str) -> dict:
-    r = requests.get(url, timeout=10, headers={"accept": "application/json"})
+
+def get_data(url: str, **kwargs: Any) -> dict:
+    r = utils.make_request(
+        url, timeout=10, headers={"accept": "application/json"}, **kwargs
+    )
     if r.status_code != 200:
         data = r.json()
         if data == ['Access denied for user 0 "anonymous"']:
