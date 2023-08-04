60939f821bbb3d38368c0e595741628464eac7da
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-08-04 06:43:31 +0100

    remove references to to_plotly_json

--- openbb_sdk/extensions/charting/openbb_charting/__init__.py
+++ openbb_sdk/extensions/charting/openbb_charting/__init__.py
@@ -3,5 +3,4 @@ from openbb_charting.core.backend import (
     create_backend,
     get_backend,
 )
-from openbb_charting.core.to_plotly_json import to_plotly_json
 from openbb_charting.core.render_chart import render_chart
