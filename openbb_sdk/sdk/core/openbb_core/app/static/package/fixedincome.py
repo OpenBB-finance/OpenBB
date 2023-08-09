86f951f1b188d5310a303d13294e319b5061bd30
Henrique Joaquim <h.joaquim@campus.fct.unl.pt>
2023-08-09 10:51:08 +0100

    Argparse custom params (#5303)
    * Argparse custom params POC
    
    * small improvements to the package builder
    
    * generation of static assets
    
    * minor adjustments
    
    ---------
    
    Co-authored-by: Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>

--- openbb_sdk/sdk/core/openbb_core/app/static/package/fixedincome.py
+++ openbb_sdk/sdk/core/openbb_core/app/static/package/fixedincome.py
@@ -2,13 +2,14 @@
 
 import datetime
 import typing
-from typing import Literal, Optional, Union
+from typing import Annotated, Literal, Optional, Union
 
 from pydantic import validate_arguments
 
 import openbb_core.app.model.command_context
 import openbb_core.app.model.results.empty
 from openbb_core.app.model.command_output import CommandOutput
+from openbb_core.app.model.custom_parameter import OpenBBCustomParameter
 from openbb_core.app.static.container import Container
 from openbb_core.app.static.filters import filter_call, filter_inputs, filter_output
 
@@ -18,8 +19,18 @@ class CLASS_fixedincome(Container):
     @validate_arguments
     def treasury(
         self,
-        start_date: Union[datetime.date, None, str] = None,
-        end_date: Union[datetime.date, None, str] = None,
+        start_date: Annotated[
+            Union[datetime.date, None, str],
+            OpenBBCustomParameter(
+                description="Start date of the data, in YYYY-MM-DD format."
+            ),
+        ] = None,
+        end_date: Annotated[
+            Union[datetime.date, None, str],
+            OpenBBCustomParameter(
+                description="End date of the data, in YYYY-MM-DD format."
+            ),
+        ] = None,
         chart: bool = False,
         provider: Optional[Literal["fmp"]] = None,
         **kwargs
