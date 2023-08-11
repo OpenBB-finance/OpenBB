febd12c39ea6913e06e95928eacbcfaf03f9be04
Pratyush Shukla <ps4534@nyu.edu>
2023-08-11 20:59:30 +0530

    fix `Undefined name `typing``

--- openbb_sdk/sdk/core/openbb_core/app/static/package_builder.py
+++ openbb_sdk/sdk/core/openbb_core/app/static/package_builder.py
@@ -193,6 +193,7 @@ class ImportDefinition:
         code += "\nimport pydantic"
         code += "\nfrom pydantic import validate_arguments, BaseModel"
         code += "\nfrom inspect import Parameter"
+        code += "\nimport typing"
         code += "\nfrom typing import List, Dict, Union, Optional, Literal, Annotated"
         code += "\nfrom openbb_core.app.utils import df_to_basemodel"
         code += "\nfrom openbb_core.app.static.filters import filter_call, filter_inputs, filter_output\n"
