9b5f962d1906d35280b382a4ce97deec20a07220
Diogo Sousa <montezdesousa@gmail.com>
2023-08-14 16:57:35 +0100

    add description to OBBject

--- openbb_sdk/sdk/core/openbb_core/app/model/obbject.py
+++ openbb_sdk/sdk/core/openbb_core/app/model/obbject.py
@@ -21,6 +21,7 @@ class OpenBBError(Exception):
 
 
 class OBBject(GenericModel, Generic[T], Tagged):
+    """OpenBB custom class that holds command outputs and utility methods."""
     results: Optional[T] = Field(
         default=None,
         description="Serializable results.",
