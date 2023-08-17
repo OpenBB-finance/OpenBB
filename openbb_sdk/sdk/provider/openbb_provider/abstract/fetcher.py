22ac7c17a051db53565513971d4a28d97e7c0825
Diogo Sousa <montezdesousa@gmail.com>
2023-08-17 19:15:19 +0100

    change generic var names

--- openbb_sdk/sdk/provider/openbb_provider/abstract/fetcher.py
+++ openbb_sdk/sdk/provider/openbb_provider/abstract/fetcher.py
@@ -7,9 +7,10 @@ from openbb_provider.abstract.query_params import QueryParams
 
 Q = TypeVar("Q", bound=QueryParams)
 D = TypeVar("D")  # Data
+R = TypeVar("R")  # Return, usually the same as Data but can be List[Data] for example
 
 
-class Fetcher(Generic[Q, D]):
+class Fetcher(Generic[Q, R]):
     """Abstract class for the fetcher."""
 
     @staticmethod
@@ -23,7 +24,7 @@ class Fetcher(Generic[Q, D]):
         raise NotImplementedError
 
     @staticmethod
-    def transform_data(data: Any) -> D:
+    def transform_data(data: Any) -> R:
         """Transform the provider-specific data."""
         raise NotImplementedError
 
@@ -33,32 +34,32 @@ class Fetcher(Generic[Q, D]):
         params: Dict[str, Any],
         credentials: Optional[Dict[str, str]] = None,
         **kwargs,
-    ) -> D:
+    ) -> R:
         """Fetch data from a provider."""
         query = cls.transform_query(params=params)
         data = cls.extract_data(query=query, credentials=credentials, **kwargs)
         return cls.transform_data(data=data)
 
     @property
-    def provider_query_params_type(self):
+    def provider_query_params_type(self) -> Q:
         """Get the type of the provider query."""
         # pylint: disable=E1101
-        return self.__orig_bases__[0].__args__[0]
+        return self.__orig_bases__[0].__args__[0]  # type: ignore
 
     @property
-    def generic_return_type(self):
+    def generic_return_type(self) -> R:
         """Get the type of the return."""
         # pylint: disable=E1101
-        return self.__orig_bases__[0].__args__[1]
+        return self.__orig_bases__[0].__args__[1]  # type: ignore
 
     @property
-    def provider_data_type(self):
+    def provider_data_type(self) -> D:  # type: ignore
         """Get the type of the provider data."""
         # pylint: disable=E1101
-        return self.get_data_type(self.__orig_bases__[0].__args__[1])
+        return self.get_data_type(self.__orig_bases__[0].__args__[1])  # type: ignore
 
     @staticmethod
-    def get_data_type(data: Any) -> D:
+    def get_data_type(data: Any) -> D:  # type: ignore
         """Get the type of the data."""
         if get_origin(data) == list:
             data = get_args(data)[0]
