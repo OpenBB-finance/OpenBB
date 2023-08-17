3f43773ddedc8d23b81670b69f696e5d2a5ac9e3
Diogo Sousa <montezdesousa@gmail.com>
2023-08-17 18:10:27 +0100

    black

--- openbb_sdk/sdk/provider/openbb_provider/standard_models/price_target.py
+++ openbb_sdk/sdk/provider/openbb_provider/standard_models/price_target.py
@@ -18,21 +18,13 @@ class PriceTargetQueryParams(QueryParams, BaseSymbol):
 class PriceTargetData(Data, BaseSymbol):
     """Price target Data."""
 
-    published_date: datetime = Field(
-        description="Published date of the price target."
-    )
+    published_date: datetime = Field(description="Published date of the price target.")
     news_url: str = Field(description="News URL of the price target.")
     news_title: Optional[str] = Field(description="News title of the price target.")
-    analyst_name: Optional[str] = Field(
-        description="Analyst name."
-    )
+    analyst_name: Optional[str] = Field(description="Analyst name.")
     analyst_company: Optional[str] = Field(description="Analyst company.")
     price_target: Optional[float] = Field(description="Price target.")
-    adj_price_target: Optional[float] = Field(
-        description="Adjusted price target."
-    )
-    price_when_posted: Optional[float] = Field(
-        description="Price when posted."
-    )
+    adj_price_target: Optional[float] = Field(description="Adjusted price target.")
+    price_when_posted: Optional[float] = Field(description="Price when posted.")
     news_publisher: str = Field(description="News publisher of the price target.")
     news_base_url: str = Field(description="News base URL of the price target.")
