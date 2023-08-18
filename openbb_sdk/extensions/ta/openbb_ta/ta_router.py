59c9cd2700c651f24b51e4f22668f629e4106eee
tehcoderer <me@tehcoderer.com>
2023-08-18 18:01:04 -0400

    docstring

--- openbb_sdk/extensions/ta/openbb_ta/ta_router.py
+++ openbb_sdk/extensions/ta/openbb_ta/ta_router.py
@@ -151,10 +151,10 @@ def obv(
     to the running total, and when the close is lower than the previous close, the volume
     is subtracted from the running total.
 
-    To interpret the OBV, look for the OBV
-    to move with the price or precede price moves. If the price moves before the OBV,
-    then it is a non-confirmed move. A series of rising peaks, or falling troughs, in the
-    OBV indicates a strong trend. If the OBV is flat, then the market is not trending.
+    To interpret the OBV, look for the OBV to move with the price or precede price moves.
+    If the price moves before the OBV, then it is a non-confirmed move. A series of rising peaks,
+    or falling troughs, in the OBV indicates a strong trend. If the OBV is flat, then the market
+    is not trending.
 
     Parameters
     ----------
--- openbb_sdk/sdk/core/openbb_core/app/static/package/ta.py
+++ openbb_sdk/sdk/core/openbb_core/app/static/package/ta.py
@@ -1187,10 +1187,10 @@ class CLASS_ta(Container):
         to the running total, and when the close is lower than the previous close, the volume
         is subtracted from the running total.
 
-        To interpret the OBV, look for the OBV
-        to move with the price or precede price moves. If the price moves before the OBV,
-        then it is a non-confirmed move. A series of rising peaks, or falling troughs, in the
-        OBV indicates a strong trend. If the OBV is flat, then the market is not trending.
+        To interpret the OBV, look for the OBV to move with the price or precede price moves.
+        If the price moves before the OBV, then it is a non-confirmed move. A series of rising peaks,
+        or falling troughs, in the OBV indicates a strong trend. If the OBV is flat, then the market
+        is not trending.
 
         Parameters
         ----------
