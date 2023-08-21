1f7a32a57f98c826d48b254d2fc2b22f9d49c6d7
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-08-21 17:09:30 +0200

    Snake case

--- openbb_sdk/sdk/provider/openbb_provider/abstract/query_params.py
+++ openbb_sdk/sdk/provider/openbb_provider/abstract/query_params.py
@@ -11,12 +11,7 @@ class QueryParams(BaseModel):
         """Pydantic Config."""
 
         allow_population_by_field_name = True
-
-    def __init__(self, **data):
-        """Update the fields to snake case."""
-        for field in self.__fields__.values():
-            field.name = to_snake_case(field.name)
-        super().__init__(**data)
+        alias_generator = to_snake_case
 
     def __repr__(self):
         """Return the string representation of the QueryParams object."""
