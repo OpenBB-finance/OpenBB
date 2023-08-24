e5f45afaaac6ce90913c8014dad018a9c35e2bdc
Pratyush Shukla <ps4534@nyu.edu>
2023-08-24 18:00:41 +0530

    intrinio provider package init

---
+++ openbb_sdk/providers/intrinio/openbb_intrinio/__init__.py
@@ -0,0 +1,14 @@
+"""intrinio provider module."""
+
+
+from openbb_provider.abstract.provider import Provider
+
+
+intrinio_provider = Provider(
+    name="intrinio",
+    website="https://intrinio.com/",
+    description="""Intrinio is a financial data platform that provides real-time and
+    historical financial market data to businesses and developers through an API.""",
+    required_credentials=["api_key"],
+    fetcher_dict={},
+)
