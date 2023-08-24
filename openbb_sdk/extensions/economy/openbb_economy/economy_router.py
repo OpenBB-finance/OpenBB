dd851fbe48d9e7cc65b65b28ea55ead8b129580e
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-08-23 20:11:37 -0700

    update docstring in economy_router

--- openbb_sdk/extensions/economy/openbb_economy/economy_router.py
+++ openbb_sdk/extensions/economy/openbb_economy/economy_router.py
@@ -363,5 +363,20 @@ def sp500_multiples(
     standard_params: StandardParams,
     extra_params: ExtraParams,
 ) -> OBBject[BaseModel]:
-    """Get historical values, multiples, and ratios for the S&P 500."""
+    """Get historical values, multiples, and ratios for the S&P 500.
+
+    Choices for `series_name` are: [
+        'Shiller PE Ratio by Month', 'Shiller PE Ratio by Year', 'PE Ratio by Year', 'PE Ratio by Month',
+        'Dividend by Year', 'Dividend by Month', 'Dividend Growth by Quarter', 'Dividend Growth by Year',
+        'Dividend Yield by Year', 'Dividend Yield by Month', 'Earnings by Year', 'Earnings by Month',
+        'Earnings Growth by Year', 'Earnings Growth by Quarter', 'Real Earnings Growth by Year',
+        'Real Earnings Growth by Quarter', 'Earnings Yield by Year', 'Earnings Yield by Month',
+        'Real Price by Year', 'Real Price by Month', 'Inflation Adjusted Price by Year',
+        'Inflation Adjusted Price by Month', 'Sales by Year', 'Sales by Quarter', 'Sales Growth by Year',
+        'Sales Growth by Quarter', 'Real Sales by Year', 'Real Sales by Quarter', 'Real Sales Growth by Year',
+        'Real Sales Growth by Quarter', 'Price to Sales Ratio by Year', 'Price to Sales Ratio by Quarter',
+        'Price to Book Value Ratio by Year', 'Price to Book Value Ratio by Quarter',
+        'Book Value per Share by Year', 'Book Value per Share by Quarter'
+    ]
+    """
     return OBBject(results=Query(**locals()).execute())
