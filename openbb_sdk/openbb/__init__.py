ef302c410de673fe3b43fb2ca09c4a631fffb75b
montezdesousa <montezdesousa@gmail.com>
2023-08-31 18:16:56 +0100

    check and build

--- openbb_sdk/openbb/__init__.py
+++ openbb_sdk/openbb/__init__.py
@@ -1,15 +1,14 @@
 """OpenBB SDK."""
 # flake8: noqa
 
-from .utils import check_package_diff as _check_package_diff, build
+from openbb.utils import check_and_build as _check_and_build, build
 from typing import Union
 from openbb_core.app.static.app_factory import (
     create_app as _create_app,
     BaseApp as _BaseApp,
 )
 
-if _check_package_diff():
-    build()
+_check_and_build()
 
 try:
     # pylint: disable=import-outside-toplevel
--- openbb_sdk/openbb/utils.py
+++ openbb_sdk/openbb/utils.py
@@ -1,38 +1,52 @@
 import os
 from json import load
 from pathlib import Path
-from typing import List, Optional, Set, Union
+from typing import List, Optional, Set, Tuple, Union
 
 from importlib_metadata import entry_points
 
 CURRENT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
-def get_ext_map(directory: Path) -> dict:
-    """Get extension map"""
-    ext_map_file = Path(directory, Path("package", "extension_map.json"))
+def get_ext_map(package: Path) -> dict:
+    """Get extension map from package folder"""
+    ext_map_file = Path(package, "extension_map.json")
     try:
         with open(ext_map_file) as fp:
             ext_map = load(fp)
-    except:
+    except Exception:
         ext_map = {}
 
     return ext_map
 
 
-def check_package_diff(directory: Path = CURRENT_DIR) -> Set[str]:
-    """Check for differences between written and installed packages."""
+def package_diff(directory: Path = CURRENT_DIR) -> Tuple[Set[str], Set[str]]:
+    """Check differences between written and installed extensions.
 
-    ext_map = get_ext_map(directory)
+    Parameters
+    ----------
+    directory: Path
+        The path to the directory where "package" folder was created
 
-    diff: Set[str] = set()
+    Returns
+    -------
+    Tuple[Set[str], Set[str]]
+        First element: set of installed extensions that are not in the package.
+        Second element: set of extensions in the package that are not installed.
+    """
+
+    ext_map = get_ext_map(Path(directory, "package"))
+
+    add: Set[str] = set()
+    remove: Set[str] = set()
     groups = ("openbb_core_extension", "openbb_provider_extension")
     for g in groups:
         written = set(ext_map.get(g, {}))
         installed = entry_points(group=g).names
-        diff = diff.union(written ^ installed)
+        add = add.union(installed - written)
+        remove = remove.union(written - installed)
 
-    return diff
+    return add, remove
 
 
 def build(
@@ -55,6 +69,7 @@ def build(
     """
     # pylint: disable=import-outside-toplevel
     from multiprocessing import Pool
+
     from openbb_core.app.static.package_builder import PackageBuilder
 
     # `build` is running in a separate process. This avoids consecutive calls to this
@@ -65,3 +80,19 @@ def build(
             PackageBuilder(CURRENT_DIR, lint, verbose).build,
             args=(modules,),
         )
+
+
+def check_and_build():
+    """Check and build"""
+    add, remove = package_diff()
+    if add:
+        a = ", ".join(add)
+        print(f"Extensions to add: {a}")
+
+    if remove:
+        r = ", ".join(remove)
+        print(f"Extensions to remove: {r}")
+
+    if add or remove:
+        print("\nBuilding...")
+        build()
