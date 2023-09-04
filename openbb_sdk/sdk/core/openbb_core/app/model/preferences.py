19879f2ef45645fa6f7d8513c07ccce852cb160f
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-09-04 09:48:10 +0100

    adjusting the preferences

--- openbb_sdk/sdk/core/openbb_core/app/model/preferences.py
+++ openbb_sdk/sdk/core/openbb_core/app/model/preferences.py
@@ -8,7 +8,7 @@ class Preferences(BaseModel):
     data_directory: str = str(Path.home() / "OpenBBUserData")
     export_directory: str = str(Path.home() / "OpenBBUserData" / "exports")
     user_styles_directory: str = str(Path.home() / "OpenBBUserData" / "styles" / "user")
-    charting_extension: Literal["openbb_charting"] = "openbb_charting"
+    charting_extension: Literal["charting"] = "charting"
     chart_style: Literal["dark", "light"] = "dark"
     plot_enable_pywry: bool = True
     plot_pywry_width: PositiveInt = 1400
