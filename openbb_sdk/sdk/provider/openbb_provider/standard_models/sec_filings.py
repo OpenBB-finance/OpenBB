06f483c64d3177380ee0087dbb47a893f60224d7
Pratyush Shukla <ps4534@nyu.edu>
2023-09-12 17:51:59 +0530

    set `type` to `Optional`

--- openbb_sdk/sdk/provider/openbb_provider/standard_models/sec_filings.py
+++ openbb_sdk/sdk/provider/openbb_provider/standard_models/sec_filings.py
@@ -118,7 +118,7 @@ class SECFilingsQueryParams(QueryParams):
     """SEC Filings Query."""
 
     symbol: str = Field(description=QUERY_DESCRIPTIONS.get("symbol", ""))
-    type: FORM_TYPES = Field(default="10-K", description="Type of the SEC filing form.")
+    type: Optional[FORM_TYPES] = Field(description="Type of the SEC filing form.")
     page: Optional[int] = Field(default=0, description="Page number of the results.")
     limit: Optional[int] = Field(
         default=100, description=QUERY_DESCRIPTIONS.get("limit", "")
