369db699c05b060f255e48e7ea5f8f6fa2e1e4ab
James Maslek <jmaslek11@gmail.com>
2023-09-14 11:21:06 -0400

    install all

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ test_locations = ["openbb_sdk/sdk/core/tests", "openbb_sdk/providers/tests"]
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def tests(session):
-    session.install("./openbb_sdk")
+    session.install("'./openbb_sdk[all]'")
     session.install("pytest")
     session.install("pytest-cov")
     session.run("pytest", *test_locations, "--cov=openbb_sdk/")
