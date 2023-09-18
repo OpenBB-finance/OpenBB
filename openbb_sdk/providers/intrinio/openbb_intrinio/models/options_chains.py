5c7b4b105e0a768afe047321f819ffbbec1ce9bd
tehcoderer <me@tehcoderer.com>
2023-09-18 16:24:10 -0400

    fix and refactor intrinio `options_chains`

--- openbb_sdk/providers/intrinio/openbb_intrinio/models/options_chains.py
+++ openbb_sdk/providers/intrinio/openbb_intrinio/models/options_chains.py
@@ -1,6 +1,6 @@
 """Intrinio Options Chains fetcher."""
 
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 from datetime import (
     date as dateType,
     datetime,
@@ -61,8 +61,8 @@ def get_options_tickers(api_key: str) -> List[str]:
 def get_options_expirations(
     symbol: str,
     api_key: str,
-    after: Optional[Union[datetime, str]] = datetime.now().strftime("%Y-%m-%d"),
-    before: Optional[str] = "",
+    after: Optional[datetime] = None,
+    before: Optional[datetime] = None,
 ) -> list:
     """Returns a list of all current and upcoming option contract expiration dates for a particular symbol.
 
@@ -72,21 +72,26 @@ def get_options_expirations(
         The options symbol, corresponding to the underlying security.
     api_key: str
         Intrinio API key.
-    after: str
-        Return option contract expiration dates after this date. Format: YYYY-MM-DD
-    before: str
-        Return option contract expiration dates before this date. Format: YYYY-MM-DD
+    after: datetime
+        Return option contract expiration dates after this date.
+    before: datetime
+        Return option contract expiration dates before this date.
     """
+    after = after.strftime("%Y-%m-%d") if after else ""
+    before = before.strftime("%Y-%m-%d") if before else ""
 
     url = f"https://api-v2.intrinio.com/options/expirations/{symbol}/eod?before={before}&after={after}&api_key={api_key}"
-
     r = make_request(url)
 
     expirations = pd.DatetimeIndex(sorted(list(r.json()["expirations"])))
     return list(filter(lambda x: x > pd.to_datetime(after), expirations))
 
 
-def generate_url(symbol, expiration, date, api_key):
+def generate_url(
+    symbol: str, expiration: dateType, date: dateType, api_key: str
+) -> str:
+    date = date.strftime("%Y-%m-%d")
+    expiration = expiration.strftime("%Y-%m-%d")
     return f"https://api-v2.intrinio.com/options/chain/{symbol}/{expiration}/eod?date={date}&api_key={api_key}"
 
 
@@ -94,135 +99,88 @@ def download_json_files(urls):
     results = []  # List to store the downloaded JSON data
 
     def download_single_file(url):
-        response = make_request(url)
-        json_data = response.json()
-        results.append(json_data["chain"])
+        results.append(make_request(url).json().get("chain", []))
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor() as executor:
         executor.map(download_single_file, urls)
 
     return results
 
 
+def get_weekday(date: dateType):
+    if date.weekday() in [5, 6]:
+        date = date - timedelta(days=2 if date.weekday() == 6 else 1)
+    return date
+
+
 def get_historical_chain_with_greeks(
     symbol: str, date: Optional[Union[str, dateType]] = "", api_key: str = ""
-):
-    symbol = symbol.upper()
+) -> pd.DataFrame:
     SYMBOLS = get_options_tickers(api_key)
     if symbol not in SYMBOLS:
         raise RuntimeError(f"{symbol}", "is not supported by Intrinio.")
 
-    if not date:
-        date = datetime.now().strftime("%Y-%m-%d")
+    date = date if date else datetime.now()
+
+    if date.weekday() in [5, 6]:
+        date = get_weekday(date)
 
     # If the symbol is an index, it needs to be preceded with, $.
     if symbol in TICKER_EXCEPTIONS:
         symbol = "$" + symbol
 
     expirations = get_options_expirations(symbol=symbol, api_key=api_key, after=date)
-    urls = []
+    urls = [
+        generate_url(symbol, expiration, date, api_key) for expiration in expirations
+    ]
 
-    for expiration in expirations:
-        urls.append(generate_url(symbol, expiration, date, api_key))
+    if len(results := download_json_files(urls)) == 0:
+        date = get_weekday(date - timedelta(days=1))
+        results = download_json_files(
+            [
+                generate_url(symbol, expiration, date, api_key)
+                for expiration in expirations
+            ]
+        )
 
-    results = download_json_files(urls)
+    data = [
+        {
+            "contract_symbol": item["option"]["code"],
+            "expiration": item["option"]["expiration"],
+            "strike": item["option"]["strike"] or 0,
+            "option_type": item["option"]["type"],
+            "close": item["prices"]["close"] or 0,
+            "close_bid": item["prices"]["close_bid"] or 0,
+            "close_ask": item["prices"]["close_ask"] or 0,
+            "volume": item["prices"]["volume"] or 0,
+            "open": item["prices"]["open"] or 0,
+            "open_bid": item["prices"]["open_bid"] or 0,
+            "open_ask": item["prices"]["open_ask"] or 0,
+            "open_interest": item["prices"]["open_interest"] or 0,
+            "high": item["prices"]["high"] or 0,
+            "low": item["prices"]["low"] or 0,
+            "mark": item["prices"]["mark"],
+            "bid_high": item["prices"]["bid_high"] or 0,
+            "ask_high": item["prices"]["ask_high"] or 0,
+            "bid_low": item["prices"]["bid_low"] or 0,
+            "ask_low": item["prices"]["ask_low"] or 0,
+            "implied_volatility": item["prices"]["implied_volatility"] or 0,
+            "delta": item["prices"]["delta"] or 0,
+            "gamma": item["prices"]["gamma"] or 0,
+            "theta": item["prices"]["theta"] or 0,
+            "vega": item["prices"]["vega"] or 0,
+            "eod_date": item["prices"]["date"],
+        }
+        for chains in results
+        for item in chains
+    ]
 
-    if len(results) == 0:
-        date = pd.to_datetime(date) + timedelta(days=-1)
-        for expiration in expirations:
-            urls.append(generate_url(symbol, expiration, date, api_key))
-        results = download_json_files(urls)
-
-    df = pd.DataFrame()
-
-    for i in range(0, len(results)):
-        chains = results[i]
-
-        contract_symbol: List[str] = []
-        expiry: List[str] = []
-        strikes: List[float] = []
-        option_type: List[str] = []
-        close: List[float] = []
-        close_bid: List[float] = []
-        close_ask: List[float] = []
-        volume: List[int] = []
-        open: List[float] = []
-        open_bid: List[float] = []
-        open_ask: List[float] = []
-        open_interest: List[int] = []
-        high: List[float] = []
-        low: List[float] = []
-        mark: List[float] = []
-        ask_high: List[float] = []
-        ask_low: List[float] = []
-        bid_high: List[float] = []
-        bid_low: List[float] = []
-        implied_volatility: List[float] = []
-        delta: List[float] = []
-        gamma: List[float] = []
-        theta: List[float] = []
-        vega: List[float] = []
-        eod_date: List[str] = []
-
-        for item in chains:
-            contract_symbol.append(item["option"]["code"])
-            expiry.append(item["option"]["expiration"])
-            strikes.append(item["option"]["strike"])
-            option_type.append(item["option"]["type"])
-            close.append(item["prices"]["close"])
-            close_bid.append(item["prices"]["close_bid"])
-            close_ask.append(item["prices"]["close_ask"])
-            volume.append(item["prices"]["volume"])
-            open.append(item["prices"]["open"])
-            open_bid.append(item["prices"]["open_bid"])
-            open_ask.append(item["prices"]["open_ask"])
-            open_interest.append(item["prices"]["open_interest"])
-            high.append(item["prices"]["high"])
-            low.append(item["prices"]["low"])
-            mark.append(item["prices"]["mark"])
-            ask_high.append(item["prices"]["ask_high"])
-            ask_low.append(item["prices"]["ask_low"])
-            bid_high.append(item["prices"]["bid_high"])
-            bid_low.append(item["prices"]["bid_low"])
-            implied_volatility.append(item["prices"]["implied_volatility"])
-            delta.append(item["prices"]["delta"])
-            gamma.append(item["prices"]["gamma"])
-            theta.append(item["prices"]["theta"])
-            vega.append(item["prices"]["vega"])
-            eod_date.append(item["prices"]["date"])
-
-        data = pd.DataFrame()
-        data["contract_symbol"] = contract_symbol
-        data["expiration"] = expiry
-        data["strike"] = strikes
-        data["option_type"] = option_type
-        data["close"] = close
-        data["close_bid"] = close_bid
-        data["close_ask"] = close_ask
-        data["volume"] = volume
-        data["open"] = open
-        data["open_bid"] = open_bid
-        data["open_ask"] = open_ask
-        data["open_interest"] = open_interest
-        data["high"] = high
-        data["low"] = low
-        data["mark"] = mark
-        data["bid_high"] = bid_high
-        data["ask_high"] = ask_high
-        data["bid_low"] = bid_low
-        data["ask_low"] = ask_low
-        data["implied_volatility"] = implied_volatility
-        data["delta"] = delta
-        data["gamma"] = gamma
-        data["theta"] = theta
-        data["vega"] = vega
-        data["eod_date"] = eod_date
-
-        data = data.set_index(["expiration", "strike", "option_type"]).sort_index()
-
-        df = pd.concat([df, data])
-
-    df = df.sort_index().reset_index()
+    df = (
+        pd.DataFrame(data)
+        .set_index(["expiration", "strike", "option_type"])
+        .sort_index()
+        .reset_index()
+    )
     now = pd.DatetimeIndex(df["eod_date"])
     temp = pd.DatetimeIndex(df["expiration"])
     temp_ = (temp - now).days + 1
@@ -237,9 +195,9 @@ class IntrinioOptionsChainsQueryParams(OptionsChainsQueryParams):
     source: https://docs.intrinio.com/documentation/web_api/get_options_chain_eod_v2
     """
 
-    date: Optional[Union[str, dateType]] = Field(
+    date: Optional[dateType] = Field(
+        default=None,
         description="Date for which the options chains are returned.",
-        default="",
     )
 
 
@@ -249,41 +207,48 @@ class IntrinioOptionsChainsData(OptionsChainsData):
     __alias_dict__ = {"bid": "close_bid", "ask": "close_ask"}
 
     mark: Optional[float] = Field(
-        description="The mid-price between the latest bid-ask spread."
+        default=None, description="The mid-price between the latest bid-ask spread."
     )
     open_bid: Optional[float] = Field(
-        description="The lowest bid price for the option that day."
+        default=None, description="The lowest bid price for the option that day."
     )
     open_ask: Optional[float] = Field(
-        description="The lowest ask price for the option that day."
+        default=None, description="The lowest ask price for the option that day."
     )
     bid_low: Optional[float] = Field(
-        description="The lowest bid price for the option that day."
+        default=None, description="The lowest bid price for the option that day."
     )
     ask_low: Optional[float] = Field(
-        description="The lowest ask price for the option that day."
+        default=None, description="The lowest ask price for the option that day."
     )
     bid_high: Optional[float] = Field(
-        description="The highest bid price for the option that day."
+        default=None, description="The highest bid price for the option that day."
     )
     ask_high: Optional[float] = Field(
-        description="The highest ask price for the option that day."
+        default=None, description="The highest ask price for the option that day."
     )
-    open: Optional[float] = Field(description="Opening price of the option.")
-    high: Optional[float] = Field(description="High price of the option.")
-    low: Optional[float] = Field(description="Low price of the option.")
-    close: Optional[float] = Field(description="Close price for the option that day.")
+    open: Optional[float] = Field(
+        default=None, description="Opening price of the option."
+    )
+    high: Optional[float] = Field(default=None, description="High price of the option.")
+    low: Optional[float] = Field(default=None, description="Low price of the option.")
+    close: Optional[float] = Field(
+        default=None, description="Close price for the option that day."
+    )
     implied_volatility: Optional[float] = Field(
-        description="Implied volatility of the option."
+        default=None, description="Implied volatility of the option."
     )
-    delta: Optional[float] = Field(description="Delta of the option.")
-    gamma: Optional[float] = Field(description="Gamma of the option.")
-    vega: Optional[float] = Field(description="Vega of the option.")
-    theta: Optional[float] = Field(description="Theta of the option.")
+    delta: Optional[float] = Field(default=None, description="Delta of the option.")
+    gamma: Optional[float] = Field(default=None, description="Gamma of the option.")
+    vega: Optional[float] = Field(default=None, description="Vega of the option.")
+    theta: Optional[float] = Field(default=None, description="Theta of the option.")
     eod_date: Optional[dateType] = Field(
+        default=None,
         description="Historical date for which the options chains data is from.",
     )
-    dte: Optional[int] = Field(description="Days to expiration for the option.")
+    dte: Optional[int] = Field(
+        default=None, description="Days to expiration for the option."
+    )
 
     @validator("expiration", pre=True, check_fields=False)
     def date_validate(cls, v):  # pylint: disable=E0213
@@ -314,7 +279,7 @@ class IntrinioOptionsChainsFetcher(
             symbol=query.symbol, date=query.date, api_key=api_key  # type: ignore
         )
 
-        return data.to_dict("records")
+        return data.to_dict(orient="records")
 
     @staticmethod
     def transform_data(data: List[Dict]) -> List[IntrinioOptionsChainsData]:
