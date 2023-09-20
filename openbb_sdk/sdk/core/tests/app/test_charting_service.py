7afe9ea67b23aa20608f96305e9f7c216a717e4d
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-09-20 16:52:14 +0100

    test_check_and_get_charting_extension_name

--- openbb_sdk/sdk/core/tests/app/test_charting_service.py
+++ openbb_sdk/sdk/core/tests/app/test_charting_service.py
@@ -1,5 +1,6 @@
 import pytest
-from openbb_core.app.charting_service import ChartingService
+from unittest.mock import patch
+from openbb_core.app.charting_service import ChartingService, ChartingServiceError
 from openbb_core.app.model.system_settings import SystemSettings
 from openbb_core.app.model.user_settings import UserSettings
 
@@ -51,6 +52,45 @@ def test_charting_settings_setter():
 
     assert cm.charting_settings.plot_enable_pywry is False
     assert cm.charting_settings.test_mode is True
+
+
+@pytest.mark.parametrize(
+    "user_preferences_charting_extension, expected_result",
+    [
+        ("openbb_charting", "openbb_charting"),  # Matching extension names
+        ("openbb_charting", ChartingServiceError),  # Mismatching extension names
+    ],
+)
+def test_check_and_get_charting_extension_name(
+    user_preferences_charting_extension, expected_result
+):
+    sys = SystemSettings()
+    user = UserSettings(
+        preferences={"charting_extension": user_preferences_charting_extension}
+    )
+    cm = ChartingService(user_settings=user, system_settings=sys)
+
+    if expected_result == ChartingServiceError:
+        with pytest.raises(ChartingServiceError) as exc_info:
+            # patch EXTENSION_NAME
+            with patch(
+                "openbb_core.app.charting_service.EXTENSION_NAME", "other_extension"
+            ):
+                cm._check_and_get_charting_extension_name(
+                    user_preferences_charting_extension
+                )
+                assert str(exc_info.value) == (
+                    f"The charting extension defined on user preferences must be the same as the one defined in the env file."
+                    f"diff: {user_preferences_charting_extension} != openbb_charting"
+                )
+    else:
+        with patch(
+            "openbb_core.app.charting_service.EXTENSION_NAME", "openbb_charting"
+        ):
+            result = cm._check_and_get_charting_extension_name(
+                user_preferences_charting_extension
+            )
+            assert result == expected_result
 
 
 @pytest.mark.skip(reason="function needs review")
