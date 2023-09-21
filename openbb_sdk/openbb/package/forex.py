a4638527b2602fe4675ddc27130c2ddb7049c122
tehcoderer <me@tehcoderer.com>
2023-09-21 08:42:24 -0400

    build package

--- openbb_sdk/openbb/package/forex.py
+++ openbb_sdk/openbb/package/forex.py
@@ -1,14 +1,15 @@
 ### THIS FILE IS AUTO-GENERATED. DO NOT EDIT. ###
 
 import datetime
-from typing import List, Literal, Union
+from typing import List, Literal, Optional, Union
 
-import typing_extensions
 from openbb_core.app.model.custom_parameter import OpenBBCustomParameter
 from openbb_core.app.model.obbject import OBBject
 from openbb_core.app.static.container import Container
 from openbb_core.app.static.filters import filter_inputs
-from pydantic import validate_arguments
+from openbb_provider.abstract.data import Data
+from pydantic import validate_call
+from typing_extensions import Annotated
 
 
 class CLASS_forex(Container):
@@ -20,39 +21,39 @@ class CLASS_forex(Container):
     def __repr__(self) -> str:
         return self.__doc__ or ""
 
-    @validate_arguments
+    @validate_call
     def load(
         self,
-        symbol: typing_extensions.Annotated[
+        symbol: Annotated[
             Union[str, List[str]],
             OpenBBCustomParameter(description="Symbol to get data for."),
         ],
-        start_date: typing_extensions.Annotated[
+        start_date: Annotated[
             Union[datetime.date, None, str],
             OpenBBCustomParameter(
                 description="Start date of the data, in YYYY-MM-DD format."
             ),
         ] = None,
-        end_date: typing_extensions.Annotated[
+        end_date: Annotated[
             Union[datetime.date, None, str],
             OpenBBCustomParameter(
                 description="End date of the data, in YYYY-MM-DD format."
             ),
         ] = None,
-        provider: Union[Literal["fmp", "polygon"], None] = None,
-        **kwargs
-    ) -> OBBject[List]:
+        provider: Optional[Literal["fmp", "polygon", "yfinance"]] = None,
+        **kwargs,
+    ) -> OBBject[List[Data]]:
         """Forex Intraday Price.
 
         Parameters
         ----------
-        symbol : Union[str, List[str]]
+        symbol : str
             Symbol to get data for.
-        start_date : Union[datetime.date, None, str]
+        start_date : Optional[datetime.date]
             Start date of the data, in YYYY-MM-DD format.
-        end_date : Union[datetime.date, None, str]
+        end_date : Optional[datetime.date]
             End date of the data, in YYYY-MM-DD format.
-        provider : Union[Literal['fmp', 'polygon'], None]
+        provider : Optional[Literal['fmp', 'polygon', 'yfinance']]
             The provider to use for the query, by default None.
             If None, the provider specified in defaults is selected or 'fmp' if there is
             no default.
@@ -60,19 +61,23 @@ class CLASS_forex(Container):
             Timespan of the data. (provider: polygon)
         sort : Literal['asc', 'desc']
             Sort order of the data. (provider: polygon)
-        limit : PositiveInt
+        limit : int
             The number of data entries to return. (provider: polygon)
         adjusted : bool
             Whether the data is adjusted. (provider: polygon)
-        multiplier : PositiveInt
+        multiplier : int
             Multiplier of the timespan. (provider: polygon)
+        interval : Optional[Literal['1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo']]
+            Data granularity. (provider: yfinance)
+        period : Optional[Literal['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']]
+            Period of the data to return. (provider: yfinance)
 
         Returns
         -------
         OBBject
             results : List[ForexHistorical]
                 Serializable results.
-            provider : Union[Literal['fmp', 'polygon'], None]
+            provider : Optional[Literal['fmp', 'polygon', 'yfinance']]
                 Provider name.
             warnings : Optional[List[Warning_]]
                 List of warnings.
@@ -85,17 +90,17 @@ class CLASS_forex(Container):
         ---------------
         date : Union[date, datetime]
             The date of the data.
-        open : Optional[PositiveFloat]
+        open : float
             The open price of the symbol.
-        high : Optional[PositiveFloat]
+        high : float
             The high price of the symbol.
-        low : Optional[PositiveFloat]
+        low : float
             The low price of the symbol.
-        close : Optional[PositiveFloat]
+        close : float
             The close price of the symbol.
-        volume : Optional[NonNegativeFloat]
+        volume : float
             The volume of the symbol.
-        vwap : Optional[PositiveFloat]
+        vwap : Optional[Annotated[float, Gt(gt=0)]]
             Volume Weighted Average Price of the symbol.
         adj_close : Optional[float]
             Adjusted Close Price of the symbol. (provider: fmp)
@@ -109,7 +114,7 @@ class CLASS_forex(Container):
             Human readable format of the date. (provider: fmp)
         change_over_time : Optional[float]
             Change % in the price of the symbol over a period of time. (provider: fmp)
-        n : Optional[PositiveInt]
+        transactions : Optional[Annotated[int, Gt(gt=0)]]
             Number of transactions for the symbol in the time period. (provider: polygon)
         """  # noqa: E501
 
@@ -130,33 +135,31 @@ class CLASS_forex(Container):
             **inputs,
         )
 
-    @validate_arguments
+    @validate_call
     def pairs(
-        self,
-        provider: Union[Literal["fmp", "intrinio", "polygon"], None] = None,
-        **kwargs
-    ) -> OBBject[List]:
+        self, provider: Optional[Literal["fmp", "intrinio", "polygon"]] = None, **kwargs
+    ) -> OBBject[List[Data]]:
         """Forex Available Pairs.
 
         Parameters
         ----------
-        provider : Union[Literal['fmp', 'intrinio', 'polygon'], None]
+        provider : Optional[Literal['fmp', 'intrinio', 'polygon']]
             The provider to use for the query, by default None.
             If None, the provider specified in defaults is selected or 'fmp' if there is
             no default.
-        symbol : Union[str, None]
+        symbol : Optional[str]
             Symbol of the pair to search. (provider: polygon)
-        date : Union[datetime.date, None]
+        date : Optional[datetime.date]
             A specific date to get data for. (provider: polygon)
-        search : Union[str, None]
+        search : Optional[str]
             Search for terms within the ticker and/or company name. (provider: polygon)
-        active : Union[Literal[True, False], None]
+        active : Optional[bool]
             Specify if the tickers returned should be actively traded on the queried date. (provider: polygon)
-        order : Union[Literal['asc', 'desc'], None]
+        order : Optional[Literal['asc', 'desc']]
             Order data by ascending or descending. (provider: polygon)
-        sort : Union[Literal['ticker', 'name', 'market', 'locale', 'currency_symbol', 'currency_name', 'base_currency_symbol', 'base_currency_name', 'last_updated_utc', 'delisted_utc'], None]
+        sort : Optional[Literal['ticker', 'name', 'market', 'locale', 'currency_symbol', 'currency_name', 'base_currency_symbol', 'base_currency_name', 'last_updated_utc', 'delisted_utc']]
             Sort field used for ordering. (provider: polygon)
-        limit : Union[pydantic.types.PositiveInt, None]
+        limit : Optional[Annotated[int, Gt(gt=0)]]
             The number of data entries to return. (provider: polygon)
 
         Returns
@@ -164,7 +167,7 @@ class CLASS_forex(Container):
         OBBject
             results : List[ForexPairs]
                 Serializable results.
-            provider : Union[Literal['fmp', 'intrinio', 'polygon'], None]
+            provider : Optional[Literal['fmp', 'intrinio', 'polygon']]
                 Provider name.
             warnings : Optional[List[Warning_]]
                 List of warnings.
@@ -175,7 +178,7 @@ class CLASS_forex(Container):
 
         ForexPairs
         ----------
-        name : Optional[str]
+        name : str
             Name of the currency pair.
         symbol : Optional[str]
             Symbol of the currency pair. (provider: fmp)
@@ -192,17 +195,17 @@ class CLASS_forex(Container):
         quote_currency : Optional[str]
             ISO 4217 currency code of the quote currency. (provider: intrinio)
         market : Optional[str]
-            The name of the trading market. Always 'fx'. (provider: polygon)
+            Name of the trading market. Always 'fx'. (provider: polygon)
         locale : Optional[str]
-            The locale of the currency pair. (provider: polygon)
+            Locale of the currency pair. (provider: polygon)
         currency_symbol : Optional[str]
             The symbol of the quote currency. (provider: polygon)
         currency_name : Optional[str]
-            The name of the quote currency. (provider: polygon)
+            Name of the quote currency. (provider: polygon)
         base_currency_symbol : Optional[str]
             The symbol of the base currency. (provider: polygon)
         base_currency_name : Optional[str]
-            The name of the base currency. (provider: polygon)
+            Name of the base currency. (provider: polygon)
         last_updated_utc : Optional[datetime]
             The last updated timestamp in UTC. (provider: polygon)
         delisted_utc : Optional[datetime]
--- openbb_sdk/openbb/package/stocks_ca.py
+++ openbb_sdk/openbb/package/stocks_ca.py
@@ -1,13 +1,14 @@
 ### THIS FILE IS AUTO-GENERATED. DO NOT EDIT. ###
 
-from typing import List, Literal, Union
+from typing import List, Literal, Optional, Union
 
-import typing_extensions
 from openbb_core.app.model.custom_parameter import OpenBBCustomParameter
 from openbb_core.app.model.obbject import OBBject
 from openbb_core.app.static.container import Container
 from openbb_core.app.static.filters import filter_inputs
-from pydantic import BaseModel, validate_arguments
+from openbb_provider.abstract.data import Data
+from pydantic import validate_call
+from typing_extensions import Annotated
 
 
 class CLASS_stocks_ca(Container):
@@ -18,23 +19,23 @@ class CLASS_stocks_ca(Container):
     def __repr__(self) -> str:
         return self.__doc__ or ""
 
-    @validate_arguments
+    @validate_call
     def peers(
         self,
-        symbol: typing_extensions.Annotated[
+        symbol: Annotated[
             Union[str, List[str]],
             OpenBBCustomParameter(description="Symbol to get data for."),
         ],
-        provider: Union[Literal["fmp"], None] = None,
-        **kwargs
-    ) -> OBBject[BaseModel]:
+        provider: Optional[Literal["fmp"]] = None,
+        **kwargs,
+    ) -> OBBject[Data]:
         """Company peers.
 
         Parameters
         ----------
-        symbol : Union[str, List[str]]
+        symbol : str
             Symbol to get data for.
-        provider : Union[Literal['fmp'], None]
+        provider : Optional[Literal['fmp']]
             The provider to use for the query, by default None.
             If None, the provider specified in defaults is selected or 'fmp' if there is
             no default.
@@ -42,9 +43,9 @@ class CLASS_stocks_ca(Container):
         Returns
         -------
         OBBject
-            results : List[StockPeers]
+            results : StockPeers
                 Serializable results.
-            provider : Union[Literal['fmp'], None]
+            provider : Optional[Literal['fmp']]
                 Provider name.
             warnings : Optional[List[Warning_]]
                 List of warnings.
@@ -55,9 +56,9 @@ class CLASS_stocks_ca(Container):
 
         StockPeers
         ----------
-        symbol : Optional[str]
+        symbol : str
             Symbol representing the entity requested in the data.
-        peers_list : Optional[List[str]]
+        peers_list : List[str]
             A list of stock peers based on sector, exchange and market cap."""  # noqa: E501
 
         inputs = filter_inputs(
