a11a8de5dc34860d351ecf6a30f69778375a95d1
Pratyush Shukla <ps4534@nyu.edu>
2023-09-21 22:42:41 +0530

    fix `ForexHistorical`

--- openbb_sdk/providers/yfinance/openbb_yfinance/models/forex_historical.py
+++ openbb_sdk/providers/yfinance/openbb_yfinance/models/forex_historical.py
@@ -55,23 +55,9 @@ class YFinanceForexHistoricalFetcher(
 
         if params.get("start_date") is None:
             transformed_params["start_date"] = now - relativedelta(years=1)
-        else:
-            try:
-                transformed_params["start_date"] = datetime.strptime(
-                    params["start_date"], "%Y-%m-%d"
-                ).date()
-            except TypeError:
-                pass
 
         if params.get("end_date") is None:
             transformed_params["end_date"] = now
-        else:
-            try:
-                transformed_params["end_date"] = datetime.strptime(
-                    params["end_date"], "%Y-%m-%d"
-                ).date()
-            except TypeError:
-                pass
 
         return YFinanceForexHistoricalQueryParams(**transformed_params)
 
@@ -98,9 +84,16 @@ class YFinanceForexHistoricalFetcher(
             else 0
         )
         if query.start_date:
-            data["date"] = to_datetime(data["date"])
             data.set_index("date", inplace=True)
-            data = data.loc[query.start_date : (query.end_date + timedelta(days=days))]
+            data.index = to_datetime(data.index)
+
+            start_date_dt = datetime.combine(query.start_date, datetime.min.time())
+            end_date_dt = datetime.combine(query.end_date, datetime.min.time())
+
+            data = data[
+                (data.index >= start_date_dt + timedelta(days=days))
+                & (data.index <= end_date_dt)
+            ]
 
         data.reset_index(inplace=True)
         data.rename(columns={"index": "date"}, inplace=True)
