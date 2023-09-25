b7d201a0e05b71eb58fc1e67f15c4c5c82b9a311
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-09-25 13:04:28 +0100

    integration tests generator for the api

---
+++ openbb_sdk/extensions/tests/utils/integration_tests_api_generator.py
@@ -0,0 +1,121 @@
+import requests
+from typing import Dict, Type
+from typing import get_args, get_type_hints, Callable
+from openbb_core.app.router import CommandMap
+import os
+
+
+def get_http_method(api_paths: Dict[str, dict], route: str):
+    route_info = api_paths.get(route, None)
+    if not route_info:
+        return route_info
+    return list(route_info.keys())[0]
+
+
+def get_flat_params(hints: Dict[str, Type]):
+    params = {}
+
+    for k, v in hints.items():
+        models = get_args(v)[0]
+        params[k] = list(get_type_hints(models).keys())
+
+    flat_params = []
+    for _, value in params.items():
+        if isinstance(value, list):
+            for item in value:
+                flat_params.append(item)
+        else:
+            flat_params.append(value)
+    return flat_params
+
+
+def write_init_test_template(path: str):
+    template = """
+import pytest
+import requests
+from openbb_provider.utils.helpers import get_querystring
+
+
+def get_token():
+    return requests.post(
+        "http://0.0.0.0:8000/api/v1/account/token",
+        data={"username": "openbb", "password": "openbb"},
+    )
+
+
+@pytest.fixture(scope="session")
+def headers():
+    access_token = get_token().json()["access_token"]
+    return {"Authorization": f"Bearer {access_token}"}
+"""
+
+    with open(path, "w") as f:
+        f.write(template)
+
+
+def write_test_w_template(params: Dict[str, str], route: str, path: str):
+    template = f"""
+@pytest.mark.parametrize(
+    "params",
+    [({params})],
+)
+def test_{route.replace("/", "_")[1:]}(params, headers):
+    params = {{p: v for p, v in params.items() if v}}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1{route}?{{query_str}}"
+    result = requests.get(url, headers=headers)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+"""
+
+    with open(path, "a") as f:
+        f.write(template)
+
+
+def write_integration_tests(
+    commandmap_map: Dict[str, Callable], api_paths: Dict[str, dict]
+):
+    commands_not_found = []
+
+    for route in commandmap_map:
+        http_method = get_http_method(api_paths, f"/api/v1{route}")
+
+        menu = route.split("/")[1]
+        path = os.path.join(
+            "openbb_sdk", "extensions", menu, "integration", f"test_{menu}_api.py"
+        )
+        if not os.path.exists(path):
+            write_init_test_template(path=path)
+
+        if http_method == "post":
+            # TODO: add post tests
+            pass
+        elif http_method == "get":
+            hints = get_type_hints(commandmap_map[route])
+            hints.pop("cc", None)
+            hints.pop("return", None)
+            params = get_flat_params(hints)
+
+            params = {k: "" for k in params}
+
+            write_test_w_template(params=params, route=route, path=path)
+        else:
+            commands_not_found.append(route)
+
+    return commands_not_found
+
+
+if __name__ == "__main__":
+    r = requests.get("http://0.0.0.0:8000/openapi.json").json()
+
+    if not r:
+        raise Exception("Could not get openapi.json")
+
+    command_map = CommandMap()
+    commands_not_found_in_openapi = write_integration_tests(
+        commandmap_map=command_map.map, api_paths=r["paths"]
+    )
+
+    if commands_not_found_in_openapi:
+        print(f"Commands not found in openapi.json: {commands_not_found_in_openapi}")
