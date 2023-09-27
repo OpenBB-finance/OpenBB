c9dc3c663977d47fc468a71d4bd0cebd7766492e
Theodore Aptekarev <aptekarev@gmail.com>
2023-09-27 15:11:33 +0200

    Install toml into test environment

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ test_locations = [
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def tests(session):
-    session.install("poetry")
+    session.install("poetry", "toml")
     session.run("python", "./openbb_sdk/dev_install.py", external=True)
     session.install("pytest")
     session.install("pytest-cov")
