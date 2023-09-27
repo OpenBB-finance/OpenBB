75f93c12ef43cd23a38c15372281265457360c24
Theodore Aptekarev <aptekarev@gmail.com>
2023-09-27 14:57:19 +0200

    Update CI installation instructions

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@ test_locations = [
 @nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def tests(session):
     session.install("poetry")
-    session.run("sh", "./openbb_sdk/install_all.sh", external=True)
+    session.run("python", "./openbb_sdk/dev_install.sh", external=True)
     session.install("pytest")
     session.install("pytest-cov")
     session.run("pytest", *test_locations, "--cov=openbb_sdk/")
