51ea4dc84c8bdb7e681193e8911688c8f5fba919
Theodore Aptekarev <aptekarev@gmail.com>
2023-09-28 12:11:35 +0200

    Fix tests

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@ test_locations = [
 @nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def tests(session):
     session.install("poetry", "toml")
-    session.run("python", "./openbb_sdk/dev_install.py", external=True)
+    session.run("python", "./openbb_sdk/dev_install.py", "-e", "all", external=True)
     session.install("pytest")
     session.install("pytest-cov")
     session.run("pytest", *test_locations, "--cov=openbb_sdk/")
