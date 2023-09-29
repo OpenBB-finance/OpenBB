644967a2eee0a48f2ac1cfab483e042e8a4ff081
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-09-28 18:10:02 -0700

    add some fmp tests

--- openbb_sdk/providers/fmp/tests/test_fmp_fetchers.py
+++ openbb_sdk/providers/fmp/tests/test_fmp_fetchers.py
@@ -13,6 +13,11 @@ from openbb_fmp.models.crypto_historical import FMPCryptoHistoricalFetcher
 from openbb_fmp.models.dividend_calendar import FMPDividendCalendarFetcher
 from openbb_fmp.models.earnings_calendar import FMPEarningsCalendarFetcher
 from openbb_fmp.models.earnings_call_transcript import FMPEarningsCallTranscriptFetcher
+from openbb_fmp.models.etf_countries import FMPEtfCountriesFetcher
+from openbb_fmp.models.etf_holdings import FMPEtfHoldingsFetcher
+from openbb_fmp.models.etf_info import FMPEtfInfoFetcher
+from openbb_fmp.models.etf_search import FMPEtfSearchFetcher
+from openbb_fmp.models.etf_sectors import FMPEtfSectorsFetcher
 from openbb_fmp.models.executive_compensation import FMPExecutiveCompensationFetcher
 from openbb_fmp.models.financial_ratios import FMPFinancialRatiosFetcher
 from openbb_fmp.models.forex_historical import FMPForexHistoricalFetcher
@@ -450,5 +455,50 @@ def test_fmp_financial_ratios_fetcher(credentials=test_credentials):
     params = {"symbol": "AAPL"}
 
     fetcher = FMPFinancialRatiosFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+
+@pytest.mark.record_http
+def test_fmp_etf_countries_fetcher(credentials=test_credentials):
+    params = {"symbol": "IWM,XIU.TO"}
+
+    fetcher = FMPEtfCountriesFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+
+@pytest.mark.record_http
+def test_fmp_etf_holdings_fetcher(credentials=test_credentials):
+    params = {"symbol": "QQQ"}
+
+    fetcher = FMPEtfHoldingsFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+
+@pytest.mark.record_http
+def test_fmp_etf_info_fetcher(credentials=test_credentials):
+    params = {"symbol": "IWN,IWM,QQQ,SPY,VOO"}
+
+    fetcher = FMPEtfInfoFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+
+@pytest.mark.record_http
+def test_fmp_etf_search_fetcher(credentials=test_credentials):
+    params = {"query": "Vanguard"}
+
+    fetcher = FMPEtfSearchFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+
+@pytest.mark.record_http
+def test_fmp_etf_sectors_fetcher(credentials=test_credentials):
+    params = {"symbol": "VTI,VWO,VB,VTV"}
+
+    fetcher = FMPEtfSectorsFetcher()
     result = fetcher.test(params, credentials)
     assert result is None
