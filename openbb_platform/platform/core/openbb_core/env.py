e56807d95e655998dd040823b7a321b58f41d454
Diogo Sousa <montezdesousa@gmail.com>
2023-10-03 17:05:46 +0100

    remove unused env var

--- openbb_platform/platform/core/openbb_core/env.py
+++ openbb_platform/platform/core/openbb_core/env.py
@@ -45,11 +45,6 @@ class Env(metaclass=SingletonMeta):
         """API authentication: enables commands authentication in FastAPI"""
         return self.str2bool(self._environ.get("OPENBB_API_AUTH", True))
 
-    @property
-    def API_HUB_CONNECTION(self) -> str:
-        """User auth extension: enables user auth extension"""
-        return self.str2bool(self._environ.get("OPENBB_API_HUB_CONNECTION", True))
-
     @staticmethod
     def str2bool(value) -> bool:
         """Match a value to its boolean correspondent."""
