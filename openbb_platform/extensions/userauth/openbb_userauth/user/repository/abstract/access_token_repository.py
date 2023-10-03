6756f653528c0713c197ead9ef09f22620213729
Diogo Sousa <montezdesousa@gmail.com>
2023-10-03 17:01:17 +0100

    remove userauth ext

--- openbb_platform/extensions/userauth/openbb_userauth/user/repository/abstract/access_token_repository.py
+++
@@ -1,6 +0,0 @@
-from openbb_userauth.auth.model.access_token import AccessToken
-from openbb_userauth.user.repository.abstract.repository import Repository
-
-
-class AccessTokenRepository(Repository[AccessToken]):
-    pass
