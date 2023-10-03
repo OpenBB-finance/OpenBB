de349f8fda63d4bfe70244e3604e728c34bc39e8
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-10-02 22:53:24 -0700

    black

--- openbb_platform/platform/provider/openbb_provider/standard_models/price_target_consensus.py
+++ openbb_platform/platform/provider/openbb_provider/standard_models/price_target_consensus.py
@@ -26,7 +26,9 @@ class PriceTargetConsensusQueryParams(QueryParams):
 class PriceTargetConsensusData(Data):
     """Price target consensus Data."""
 
-    symbol: Optional[str] = Field(default=None, description=QUERY_DESCRIPTIONS.get("symbol", ""))
+    symbol: Optional[str] = Field(
+        default=None, description=QUERY_DESCRIPTIONS.get("symbol", "")
+    )
     target_high: Optional[float] = Field(
         default=None, description="High target of the price target consensus."
     )
