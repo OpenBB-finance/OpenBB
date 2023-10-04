d1f4450503c398f83382498f262b314dd0c5bb64
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-10-04 18:58:05 +0200

    Cleanup

--- openbb_platform/extensions/tests/test_integration_tests_python.py
+++ openbb_platform/extensions/tests/test_integration_tests_python.py
@@ -16,7 +16,7 @@ def run_test(test_type: str, check_function) -> None:
     assert not missing_items, "\n".join(missing_items)
 
 
-def test_python_interface_integration_tests() -> None:
+def test_python_interface_integration_test_providers() -> None:
     """Test if there are any missing providers for integration tests."""
     run_test("unit", check_missing_integration_test_providers)
 
@@ -24,16 +24,3 @@ def test_python_interface_integration_tests() -> None:
 def test_python_interface_integration_test_params() -> None:
     """Test if there are any missing params for integration tests."""
     run_test("unit", check_missing_integration_test_params)
-
-
-def test_api_interface_integration_tests() -> None:
-    """Test if there are any missing providers for integration tests."""
-    run_test("integration", check_missing_integration_test_providers)
-
-
-def test_api_interface_integration_test_params() -> None:
-    """Test if there are any missing params for integration tests."""
-    run_test("integration", check_missing_integration_test_params)
-
-
-test_python_interface_integration_test_params()
