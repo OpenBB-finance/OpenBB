5c0bc3ba2e5cec1138f50bde823a387f391bec17
Diogo Sousa <montezdesousa@gmail.com>
2023-10-04 23:57:59 +0100

    remove unused repository code

--- openbb_platform/platform/core/tests/app/service/test_user_service.py
+++ openbb_platform/platform/core/tests/app/service/test_user_service.py
@@ -1,48 +1,12 @@
 """Test the user_service.py module."""
 
 from pathlib import Path
-from unittest.mock import MagicMock
 
 import pytest
 from openbb_core.app.service.user_service import (
-    AbstractAccessTokenRepository,
-    InMemoryAccessTokenRepository,
     UserService,
     UserSettings,
 )
-
-
-def test_build_token_repository_with_given_repository():
-    """Test build with repository."""
-    mock_repository = MagicMock(spec=AbstractAccessTokenRepository)
-    result = UserService.build_token_repository(access_token_repository=mock_repository)
-
-    assert result == mock_repository
-
-
-def test_build_token_repository_memory():
-    """Test build with in-memory access."""
-    result = UserService.build_token_repository()
-
-    assert isinstance(result, InMemoryAccessTokenRepository)
-
-
-def test_build_user_settings_repository_with_given_repository():
-    """Test build with repository."""
-    mock_repository = MagicMock(spec=AbstractAccessTokenRepository)
-    result = UserService.build_user_settings_repository(
-        user_settings_repository=mock_repository
-    )
-
-    assert result == mock_repository
-
-
-def test_build_user_settings_repository_memory():
-    """Test build with in-memory access."""
-    result = UserService.build_user_settings_repository()
-
-    assert result
-    assert isinstance(type(result), type(InMemoryAccessTokenRepository))
 
 
 def test_read_default_user_settings_file_exists():
@@ -65,7 +29,7 @@ def test_update_default():
 
 def test_merge_dicts():
     """Test merge dicts."""
-    result = UserService._merge_dicts(
+    result = UserService._merge_dicts(  # pylint: disable=protected-access
         list_of_dicts=[
             {"a": 1, "b": 2},
             {"a": 3, "b": 4},
