2bde8e8fabaaea676615b8a05d6df0bb2bb48a91
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-10-06 23:37:55 +0200

    Cleanup

--- openbb_platform/extensions/tests/test_integration_tests_api.py
+++ openbb_platform/extensions/tests/test_integration_tests_api.py
@@ -1,5 +1,4 @@
 """Test the integration tests."""
-import pytest
 from extensions.tests.utils.integration_tests_testers import (
     check_missing_integration_test_params,
     check_missing_integration_test_providers,
@@ -17,17 +16,11 @@ def run_test(test_type: str, check_function) -> None:
     assert not missing_items, "\n".join(missing_items)
 
 
-# TODO: Check if this can work without being an integration test
-@pytest.mark.integration
 def test_api_interface_integration_test_providers() -> None:
     """Test if there are any missing providers for integration tests."""
     run_test("api", check_missing_integration_test_providers)
 
 
-@pytest.mark.integration
 def test_api_interface_integration_test_params() -> None:
     """Test if there are any missing params for integration tests."""
     run_test("api", check_missing_integration_test_params)
-
-
-test_api_interface_integration_test_params()
