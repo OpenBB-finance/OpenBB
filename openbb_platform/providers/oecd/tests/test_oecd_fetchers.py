cb9eba4af6ceb4f76ec9633b1fd32855db965b1e
James Maslek <jmaslek11@gmail.com>
2023-10-10 11:18:22 -0400

    test

--- openbb_platform/providers/oecd/tests/test_oecd_fetchers.py
+++ openbb_platform/providers/oecd/tests/test_oecd_fetchers.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import pytest
 from openbb_core.app.service.user_service import UserService
 from openbb_oecd.models.gdpnom import OECDGDPNomFetcher
@@ -13,36 +13,41 @@ def vcr_config():
     return {
         "filter_headers": [("User-Agent", None)],
         "filter_query_parameters": [
-            ('token', 'MOCK_TOKEN'),
+            ("token", "MOCK_TOKEN"),
         ],
     }
 
+
 @pytest.mark.record_http
 def test_oecdgdp_nom_fetcher(credentials=test_credentials):
-    params = {'start_date': datetime.date(2023, 1, 1), 'end_date': datetime.date(2023, 6, 6)}
+    params = {
+        "start_date": datetime.date(2020, 1, 1),
+        "end_date": datetime.date(2023, 6, 6),
+    }
 
     fetcher = OECDGDPNomFetcher()
     result = fetcher.test(params, credentials)
     assert result is None
 
 
-
 @pytest.mark.record_http
 def test_oecdgdp_real_fetcher(credentials=test_credentials):
-    params = {'start_date': datetime.date(2023, 1, 1), 'end_date': datetime.date(2023, 6, 6)}
-
+    params = {
+        "start_date": datetime.date(2020, 1, 1),
+        "end_date": datetime.date(2023, 6, 6),
+    }
     fetcher = OECDGDPRealFetcher()
     result = fetcher.test(params, credentials)
     assert result is None
 
 
-
 @pytest.mark.record_http
 def test_oecdgdp_forecast_fetcher(credentials=test_credentials):
-    params = {'start_date': datetime.date(2023, 1, 1), 'end_date': datetime.date(2023, 6, 6)}
+    params = {
+        "start_date": datetime.date(2020, 1, 1),
+        "end_date": datetime.date(2023, 6, 6),
+    }
 
     fetcher = OECDGDPForecastFetcher()
     result = fetcher.test(params, credentials)
     assert result is None
-
-
