f143a0b6a565db38002c0b03d62103e749955dda
Pratyush Shukla <ps4534@nyu.edu>
2023-10-12 00:24:13 +0530

    renamed in core

--- openbb_platform/platform/core/openbb_core/app/model/system_settings.py
+++ openbb_platform/platform/core/openbb_core/app/model/system_settings.py
@@ -25,7 +25,7 @@ class SystemSettings(Tagged):
     platform: str = FrozenField(default=str(pl.platform()))
 
     # OpenBB section
-    # TODO: Get the version of the SDK from somewhere that's not pyproject.toml
+    # TODO: Get the version of the Platform from somewhere that's not pyproject.toml
     version: str = FrozenField(default=VERSION)
     home_directory: str = FrozenField(default=str(HOME_DIRECTORY))
     openbb_directory: str = FrozenField(default=str(OPENBB_DIRECTORY))
