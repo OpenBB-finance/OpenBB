8b14e30907ff06c5ccdf33b7c5fbd53f6e05fc7c
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-10-12 16:51:55 -0700

    duplicate oecd entries in dev_install and pyproject

--- openbb_platform/dev_install.py
+++ openbb_platform/dev_install.py
@@ -35,7 +35,6 @@ openbb-stocks = { path = "./extensions/stocks", develop = true }
 # Community dependencies
 openbb-alpha-vantage = { path = "./providers/alpha_vantage", optional = true, develop = true }
 openbb-cboe = { path = "./providers/cboe", optional = true, develop = true }
-openbb-oecd= { path = "./providers/oecd", optional = true, develop = true }
 openbb-quandl = { path = "./providers/quandl", optional = true, develop = true }
 openbb-sec = { path = "./providers/sec", optional = true, develop = true }
 openbb-yfinance = { path = "./providers/yfinance", optional = true, develop = true }
