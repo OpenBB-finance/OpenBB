4ce22edf9245a9236c366d86e29c56fef33d4851
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-10-12 10:57:14 +0100

    boilerplate templates

--- openbb_platform/extensions/charting/integration/test_charting_api.py
+++ openbb_platform/extensions/charting/integration/test_charting_api.py
@@ -1,4 +1,5 @@
 import base64
+import json
 
 import pytest
 import requests
@@ -22,8 +23,6 @@ def headers():
             {
                 "provider": "fmp",
                 "symbol": "AAPL",
-                "period": "annual",
-                "limit": 12,
                 "chart": True,
             }
         ),
@@ -39,3 +38,213 @@ def test_chart_stocks_load(params, headers):
     assert isinstance(result, requests.Response)
     assert result.status_code == 200
     assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_stocks_multiples(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/stocks/multiples?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_stocks_news(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/stocks/news?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_ta_adx(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/ta/adx?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_ta_aroon(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/ta/aroon?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_ta_ema(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/ta/ema?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_ta_hma(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/ta/hma?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_ta_macd(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/ta/macd?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_ta_rsi(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/ta/rsi?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_ta_sma(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/ta/sma?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_ta_wma(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/ta/wma?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [({"chart": True})],
+)
+@pytest.mark.integration
+def test_chart_ta_zlma(params, headers):
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(params.pop("data"))
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/ta/zlma?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    assert result.json()["chart"]
+    assert list(result.json()["chart"].keys()) == ["content", "format"]
--- openbb_platform/extensions/charting/integration/test_charting_python.py
+++ openbb_platform/extensions/charting/integration/test_charting_python.py
@@ -1,8 +1,8 @@
 """Test charting extension."""
 
 import pytest
-from openbb_core.app.model.obbject import OBBject
 from openbb_charting.core.openbb_figure import OpenBBFigure
+from openbb_core.app.model.obbject import OBBject
 
 
 @pytest.fixture(scope="session")
@@ -21,8 +21,6 @@ def obb(pytestconfig):
             {
                 "provider": "fmp",
                 "symbol": "AAPL",
-                "period": "annual",
-                "limit": 12,
                 "chart": True,
             }
         ),
@@ -31,6 +29,204 @@ def obb(pytestconfig):
 @pytest.mark.integration
 def test_chart_stocks_load(params, obb):
     result = obb.stocks.load(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_stocks_multiples(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.stocks.multiples(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_stocks_news(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.stocks.news(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_ta_adx(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.ta.adx(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_ta_aroon(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.ta.aroon(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_ta_ema(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.ta.ema(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_ta_hma(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.ta.hma(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_ta_macd(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.ta.macd(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_ta_rsi(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.ta.rsi(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_ta_sma(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.ta.sma(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_ta_wma(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.ta.wma(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"chart": "True"}),
+    ],
+)
+@pytest.mark.integration
+def test_chart_ta_zlma(params, obb):
+    params = {p: v for p, v in params.items() if v}
+
+    result = obb.ta.zlma(**params)
     assert result
     assert isinstance(result, OBBject)
     assert len(result.results) > 0
