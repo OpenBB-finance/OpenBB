1fb3bb0e33350d7bb9bf611412aaf4c97dc20b29
Pratyush Shukla <ps4534@nyu.edu>
2023-10-19 03:35:55 +0530

    cash flow standard model field type to `StrictFloat`

--- openbb_platform/platform/provider/openbb_provider/standard_models/cash_flow.py
+++ openbb_platform/platform/provider/openbb_provider/standard_models/cash_flow.py
@@ -6,9 +6,9 @@ from datetime import (
 )
 from typing import List, Literal, Optional, Set, Union
 
-from pydantic import Field, NonNegativeInt, field_validator
+from pydantic import Field, NonNegativeInt, StrictFloat, field_validator
 
-from openbb_provider.abstract.data import Data, StrictInt
+from openbb_provider.abstract.data import Data
 from openbb_provider.abstract.query_params import QueryParams
 from openbb_provider.utils.descriptions import QUERY_DESCRIPTIONS
 
@@ -18,7 +18,7 @@ class CashFlowStatementQueryParams(QueryParams):
 
     symbol: str = Field(description=QUERY_DESCRIPTIONS.get("symbol", ""))
     period: Literal["annual", "quarter"] = Field(
-        default="annual", description=QUERY_DESCRIPTIONS.get("period", "")
+        default="quarter", description=QUERY_DESCRIPTIONS.get("period", "")
     )
     limit: NonNegativeInt = Field(
         default=12, description=QUERY_DESCRIPTIONS.get("limit", "")
@@ -47,88 +47,88 @@ class CashFlowStatementData(Data):
         default=None, description="Central Index Key (CIK) of the company."
     )
 
-    net_income: Optional[StrictInt] = Field(default=None, description="Net income.")
+    net_income: Optional[StrictFloat] = Field(default=None, description="Net income.")
 
-    depreciation_and_amortization: Optional[StrictInt] = Field(
+    depreciation_and_amortization: Optional[StrictFloat] = Field(
         default=None, description="Depreciation and amortization."
     )
-    stock_based_compensation: Optional[StrictInt] = Field(
+    stock_based_compensation: Optional[StrictFloat] = Field(
         default=None, description="Stock based compensation."
     )
-    deferred_income_tax: Optional[StrictInt] = Field(
+    deferred_income_tax: Optional[StrictFloat] = Field(
         default=None, description="Deferred income tax."
     )
-    other_non_cash_items: Optional[StrictInt] = Field(
+    other_non_cash_items: Optional[StrictFloat] = Field(
         default=None, description="Other non-cash items."
     )
-    changes_in_operating_assets_and_liabilities: Optional[StrictInt] = Field(
+    changes_in_operating_assets_and_liabilities: Optional[StrictFloat] = Field(
         default=None, description="Changes in operating assets and liabilities."
     )
 
-    accounts_receivables: Optional[StrictInt] = Field(
+    accounts_receivables: Optional[StrictFloat] = Field(
         default=None, description="Accounts receivables."
     )
-    inventory: Optional[StrictInt] = Field(default=None, description="Inventory.")
-    vendor_non_trade_receivables: Optional[StrictInt] = Field(
+    inventory: Optional[StrictFloat] = Field(default=None, description="Inventory.")
+    vendor_non_trade_receivables: Optional[StrictFloat] = Field(
         default=None, description="Vendor non-trade receivables."
     )
-    other_current_and_non_current_assets: Optional[StrictInt] = Field(
+    other_current_and_non_current_assets: Optional[StrictFloat] = Field(
         default=None, description="Other current and non-current assets."
     )
-    accounts_payables: Optional[StrictInt] = Field(
+    accounts_payables: Optional[StrictFloat] = Field(
         default=None, description="Accounts payables."
     )
-    deferred_revenue: Optional[StrictInt] = Field(
+    deferred_revenue: Optional[StrictFloat] = Field(
         default=None, description="Deferred revenue."
     )
-    other_current_and_non_current_liabilities: Optional[StrictInt] = Field(
+    other_current_and_non_current_liabilities: Optional[StrictFloat] = Field(
         default=None, description="Other current and non-current liabilities."
     )
-    net_cash_flow_from_operating_activities: Optional[StrictInt] = Field(
+    net_cash_flow_from_operating_activities: Optional[StrictFloat] = Field(
         default=None, description="Net cash flow from operating activities."
     )
 
-    purchases_of_marketable_securities: Optional[StrictInt] = Field(
+    purchases_of_marketable_securities: Optional[StrictFloat] = Field(
         default=None, description="Purchases of investments."
     )
-    sales_from_maturities_of_investments: Optional[StrictInt] = Field(
+    sales_from_maturities_of_investments: Optional[StrictFloat] = Field(
         default=None, description="Sales and maturities of investments."
     )
-    investments_in_property_plant_and_equipment: Optional[StrictInt] = Field(
+    investments_in_property_plant_and_equipment: Optional[StrictFloat] = Field(
         default=None, description="Investments in property, plant, and equipment."
     )
-    payments_from_acquisitions: Optional[StrictInt] = Field(
+    payments_from_acquisitions: Optional[StrictFloat] = Field(
         default=None, description="Acquisitions, net of cash acquired, and other"
     )
-    other_investing_activities: Optional[StrictInt] = Field(
+    other_investing_activities: Optional[StrictFloat] = Field(
         default=None, description="Other investing activities"
     )
-    net_cash_flow_from_investing_activities: Optional[StrictInt] = Field(
+    net_cash_flow_from_investing_activities: Optional[StrictFloat] = Field(
         default=None, description="Net cash used for investing activities."
     )
 
-    taxes_paid_on_net_share_settlement: Optional[StrictInt] = Field(
+    taxes_paid_on_net_share_settlement: Optional[StrictFloat] = Field(
         default=None, description="Taxes paid on net share settlement of equity awards."
     )
-    dividends_paid: Optional[StrictInt] = Field(
+    dividends_paid: Optional[StrictFloat] = Field(
         default=None, description="Payments for dividends and dividend equivalents"
     )
-    common_stock_repurchased: Optional[StrictInt] = Field(
+    common_stock_repurchased: Optional[StrictFloat] = Field(
         default=None, description="Payments related to repurchase of common stock"
     )
-    debt_proceeds: Optional[StrictInt] = Field(
+    debt_proceeds: Optional[StrictFloat] = Field(
         default=None, description="Proceeds from issuance of term debt"
     )
-    debt_repayment: Optional[StrictInt] = Field(
+    debt_repayment: Optional[StrictFloat] = Field(
         default=None, description="Payments of long-term debt"
     )
-    other_financing_activities: Optional[StrictInt] = Field(
+    other_financing_activities: Optional[StrictFloat] = Field(
         default=None, description="Other financing activities, net"
     )
-    net_cash_flow_from_financing_activities: Optional[StrictInt] = Field(
+    net_cash_flow_from_financing_activities: Optional[StrictFloat] = Field(
         default=None, description="Net cash flow from financing activities."
     )
-    net_change_in_cash: Optional[StrictInt] = Field(
+    net_change_in_cash: Optional[StrictFloat] = Field(
         default=None,
         description="Net increase (decrease) in cash, cash equivalents, and restricted cash",
     )
