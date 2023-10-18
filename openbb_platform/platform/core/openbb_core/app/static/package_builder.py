e09629ea0e0668d8c9860edfde0d9ca9fb38069b
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-10-18 12:49:54 +0200

    Lint

--- openbb_platform/platform/core/openbb_core/app/static/package_builder.py
+++ openbb_platform/platform/core/openbb_core/app/static/package_builder.py
@@ -25,15 +25,16 @@ from typing import (
 
 import pandas as pd
 from importlib_metadata import entry_points
+from pydantic.fields import FieldInfo
+from pydantic_core import PydanticUndefined
+from starlette.routing import BaseRoute
+from typing_extensions import Annotated, _AnnotatedAlias
+
 from openbb_core.app.charting_service import ChartingService
 from openbb_core.app.model.custom_parameter import OpenBBCustomParameter
 from openbb_core.app.provider_interface import ProviderInterface
 from openbb_core.app.router import CommandMap, RouterLoader
 from openbb_core.env import Env
-from pydantic.fields import FieldInfo
-from pydantic_core import PydanticUndefined
-from starlette.routing import BaseRoute
-from typing_extensions import Annotated, _AnnotatedAlias
 
 
 class Console:
