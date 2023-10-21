33993c2e98505e6ff272ea2e39a1598b71abd039
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-10-20 17:18:27 -0700

    Add Docs to the navbar

--- website/src/theme/Navbar/Logo/index.js
+++ website/src/theme/Navbar/Logo/index.js
@@ -1,64 +1,10 @@
 import Link from "@docusaurus/Link";
 import { useLocation } from "@docusaurus/router";
-import LetteringLogo from "@site/src/components/Icons/LetteringLogo";
-import OnlyBotLetteringLogo from "@site/src/components/Icons/OnlyBotLettering";
-import OnlyProLetteringLogo from "@site/src/components/Icons/OnlyProLettering";
-import OnlySDKLetteringLogo from "@site/src/components/Icons/OnlySDKLettering";
-import OnlyPlatformLettering from "@site/src/components/Icons/OnlyPlatformLettering";
-import OnlyTerminalLetteringLogo from "@site/src/components/Icons/OnlyTerminalLettering";
+import LetteringDocsLogo from "@site/src/components/Icons/LetteringDocsLogo";
 import React from "react";
-import OnlyPlatformLetteringLogo from "@site/src/components/Icons/OnlyPlatformLettering";
 
 function getLogo(type) {
-  switch (type) {
-    case "hub":
-      return <LetteringLogo />;
-    case "terminal":
-      return (
-        <div className="flex mb-0">
-          <LetteringLogo />
-          <div className="pt-2 mx-4">
-            <OnlyTerminalLetteringLogo />
-          </div>
-        </div>
-      );
-    case "platform":
-      return (
-        <div className="flex mb-0">
-          <LetteringLogo />
-          <div className="pt-2 mx-4">
-            <OnlyPlatformLetteringLogo />
-          </div>
-        </div>
-      );
-    case "sdk":
-      return (
-        <div className="flex mb-0">
-          <LetteringLogo />
-          <div className="pt-2 mx-4">
-            <OnlySDKLetteringLogo />
-          </div>
-        </div>
-      );
-    case "pro":
-      return (
-        <div className="flex mb-0">
-          <LetteringLogo />
-          <div className="pt-2 mx-4">
-            <OnlyProLetteringLogo />
-          </div>
-        </div>
-      );
-    case "bot":
-      return (
-        <div className="flex mb-0">
-          <LetteringLogo />
-          <div className="pt-2 mx-4">
-            <OnlyBotLetteringLogo />
-          </div>
-        </div>
-      );
-  }
+  return <LetteringDocsLogo />;
 }
 
 export default function NavbarLogo() {
