f486d24c711ab74a3cbfce974a5b0304fcb5d497
teh_coderer <me@tehcoderer.com>
2023-10-23 04:38:57 -0400

    feature/v4-pypi nightly (#5589)
    * init
    
    * Update nightly.py
    
    * cleanup

--- openbb_platform/dev_utils/nightly.py
+++ openbb_platform/dev_utils/nightly.py
@@ -1,155 +1,79 @@
 import subprocess
 import sys
-from pathlib import Path
+from typing import Any, Dict
 
 import toml
+from dev_utils.dev_install import PLATFORM_PATH, PYPROJECT
 
-repo_dir = Path(__file__).parent.parent.parent
-
-# core
-CORE_DIR = repo_dir / "openbb_platform/platform/core"
-PYPROJECT_CORE = CORE_DIR / "pyproject.toml"
-
-# providers
-PROVIDERS_DIR = repo_dir / "openbb_platform/providers"
-PROVIDERS = [x for x in PROVIDERS_DIR.iterdir() if x.is_dir()]
-
-# extensions
-EXTENSIONS_DIR = repo_dir / "openbb_platform/extensions"
-EXTENSIONS = [x for x in EXTENSIONS_DIR.iterdir() if x.is_dir()]
+SUB_PACKAGES = {}
+DEPENDENCIES: Dict[str, Any] = {}
+PLUGINS = {"openbb_core_extension": {}, "openbb_provider_extension": {}}
+CMD = [sys.executable, "-m", "poetry", "build"]
 
 
-PLATFORM_PATH = Path(__file__).parent.parent.resolve()
-PYPROJECT = PLATFORM_PATH / "pyproject.toml"
+PYPROJECT_TOML = toml.load(PYPROJECT)
+POETRY_DICT: Dict[str, dict] = PYPROJECT_TOML["tool"]["poetry"]
+POETRY_DICT.pop("extras", None)
 
 
-def is_provider(dep: str):
-    """Check if the dependency is a provider."""
-
-    return dep in [provider.name for provider in PROVIDERS]
+original_pyproject = PYPROJECT.read_text()
 
 
-def is_extension(dep: str):
-    """Check if the dependency is an extension."""
+def gather_metadata(sub_path: str):
+    """Gather metadata from the pyproject.toml files.
 
-    return dep in [extension.name for extension in EXTENSIONS]
+    Parameters
+    ----------
+    sub_path : str
+        The path to the sub packages.
+    """
+    for path in PLATFORM_PATH.rglob(f"{sub_path}/**/pyproject.toml"):
+        pyproject_toml = toml.load(path)
+        poetry_dict: Dict[str, dict] = pyproject_toml["tool"]["poetry"]
+        package_name = poetry_dict["packages"][0]["include"]
 
+        for extension in list(PLUGINS.keys()):
+            PLUGINS[extension].update(poetry_dict.get("plugins", {}).get(extension, {}))
 
-def change_pyproject():
-    """Change the openbb_platform/pyproject.toml file"""
-
-    pyproject_toml = toml.load(PYPROJECT)
-    dependencies = pyproject_toml["tool"]["poetry"]["dependencies"]
-    obb_dependencies = []
-
-    # remove extras as we don't need them
-    pyproject_toml["tool"]["poetry"].pop("extras", None)
-
-    # remove all dependencies that start with openbb
-    for dep in list(dependencies.keys()):
-        if dep.startswith("openbb"):
-            pyproject_toml["tool"]["poetry"]["dependencies"].pop(dep)
-            obb_dependencies.append(dep)
-
-    # add to included packages and adjust dependencies
-    for dep in obb_dependencies:
-        adjusted_dep = dep.replace("openbb-", "").replace("-", "_")
-
-        # providers
-        if is_provider(adjusted_dep):
-            # add local package to included packages
-            pyproject_toml["tool"]["poetry"]["packages"].append(
-                {"include": f"providers/{adjusted_dep}"}
-            )
-            # point dependency to local package
-            dep_name = f"openbb-{adjusted_dep.replace('_', '-')}"
-            pyproject_toml["tool"]["poetry"]["dependencies"][dep_name] = {
-                "path": f"providers/{adjusted_dep}"
-            }
-        # extensions
-        elif is_extension(adjusted_dep):
-            # add local package to included packages
-            pyproject_toml["tool"]["poetry"]["packages"].append(
-                {"include": f"extensions/{adjusted_dep}"}
-            )
-            # point dependency to local package
-            dep_name = f"openbb-{adjusted_dep.replace('_', '-')}"
-            pyproject_toml["tool"]["poetry"]["dependencies"][dep_name] = {
-                "path": f"extensions/{adjusted_dep}"
-            }
-        # core
-        else:
-            # add local package to included packages
-            pyproject_toml["tool"]["poetry"]["packages"].append(
-                {"include": f"platform/{adjusted_dep}"}
-            )
-            # point dependency to local package
-            dep_name = f"openbb-{adjusted_dep.replace('_', '-')}"
-            pyproject_toml["tool"]["poetry"]["dependencies"][dep_name] = {
-                "path": f"platform/{adjusted_dep}"
-            }
-
-    # provider
-    # add local package to included packages
-    pyproject_toml["tool"]["poetry"]["packages"].append(
-        {"include": "platform/provider"}
-    )
-    # point dependency to local package
-    pyproject_toml["tool"]["poetry"]["dependencies"]["openbb-provider"] = {
-        "path": "platform/provider"
-    }
-
-    return pyproject_toml
-
-
-def change_pyproject_core():
-    """Change the openbb_platform/platform/core/pyproject.toml file"""
-
-    pyproject_toml = toml.load(PYPROJECT_CORE)
-    dependencies = pyproject_toml["tool"]["poetry"]["dependencies"]
-    obb_dependencies = []
-
-    # remove all dependencies that start with openbb
-    for dep in list(dependencies.keys()):
-        if dep.startswith("openbb"):
-            pyproject_toml["tool"]["poetry"]["dependencies"].pop(dep)
-            obb_dependencies.append(dep)
-
-    if obb_dependencies:
-        # provider
-        pyproject_toml["tool"]["poetry"]["dependencies"]["openbb-provider"] = {
-            "path": "../provider"
-        }
-
-    return pyproject_toml
+        DEPENDENCIES.update(pyproject_toml["tool"]["poetry"]["dependencies"])
+        SUB_PACKAGES[package_name] = path.relative_to(PLATFORM_PATH).parent.as_posix()
 
 
 def build():
     """Build the Platform package."""
+    for sub_path in ["platform", "providers", "extensions"]:
+        gather_metadata(sub_path)
 
-    original_pyproject = PYPROJECT.read_text()
-    original_pyproject_core = PYPROJECT_CORE.read_text()
+    # need to pop these from the dependencies
+    DEPENDENCIES.pop("openbb-core", None)
+    DEPENDENCIES.pop("openbb-provider", None)
 
-    modified_pyproject_core = change_pyproject_core()
-    modified_pyproject = change_pyproject()
+    # add the sub packages
+    for package_name, path in SUB_PACKAGES.items():
+        POETRY_DICT["packages"].append({"include": package_name, "from": f"./{path}"})
 
-    with open(PYPROJECT, "w") as f:
-        toml.dump(modified_pyproject, f)
+    # add the plugins extensions
+    for extension, plugins in PLUGINS.items():
+        POETRY_DICT.setdefault("plugins", {}).setdefault(extension, {}).update(plugins)
 
-    with open(PYPROJECT_CORE, "w") as f:
-        toml.dump(modified_pyproject_core, f)
+    # update the dependencies and platform poetry dict
+    POETRY_DICT["dependencies"] = DEPENDENCIES
+    PYPROJECT_TOML["tool"]["poetry"] = POETRY_DICT
 
-    CMD = [sys.executable, "-m", "poetry"]
+    temp_pyproject = toml.dumps(PYPROJECT_TOML)
 
-    subprocess.run(  # noqa: PLW1510
-        CMD + ["build"], cwd=PLATFORM_PATH, check=True  # noqa: S603
-    )
+    try:
+        with open(PYPROJECT, "w", encoding="utf-8", newline="\n") as f:
+            f.write(temp_pyproject)
 
-    # we restore the original pyproject files
+        subprocess.run(CMD, cwd=PLATFORM_PATH, check=True)  # noqa: S603,PLW1510
+    except (Exception, KeyboardInterrupt) as e:
+        print(e)  # noqa: T201
+        print("Restoring pyproject.toml")  # noqa: T201
+
+    # we restore the original pyproject.toml
     with open(PYPROJECT, "w", encoding="utf-8", newline="\n") as f:
         f.write(original_pyproject)
-    with open(PYPROJECT_CORE, "w", encoding="utf-8", newline="\n") as f:
-        f.write(original_pyproject_core)
 
 
 if __name__ == "__main__":
