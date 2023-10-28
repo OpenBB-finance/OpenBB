e50221e30ce3d7445d0fe34a0528a5fee13b3315
Theodore Aptekarev <aptekarev@gmail.com>
2023-10-28 08:39:44 +0200

    Add integration tests

--- openbb_platform/extensions/etf/integration/test_etf_api.py
+++ openbb_platform/extensions/etf/integration/test_etf_api.py
@@ -49,3 +49,21 @@ def test_etf_historical(params, headers):
     result = requests.get(url, headers=headers, timeout=10)
     assert isinstance(result, requests.Response)
     assert result.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        ({"symbol": "IOO"}),
+        ({"symbol": "MISL"}),
+    ],
+)
+@pytest.mark.integration
+def test_etf_info(params, headers):
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/etf/info?{query_str}"
+    result = requests.get(url, headers=headers, timeout=10)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
