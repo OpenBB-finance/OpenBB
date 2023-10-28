ed8ff252a3d4c4df41a168d72a57cd0980d1f216
Theodore Aptekarev <aptekarev@gmail.com>
2023-10-28 08:50:51 +0200

    Add unit tests

--- openbb_platform/providers/fmp/tests/test_fmp_fetchers.py
+++ openbb_platform/providers/fmp/tests/test_fmp_fetchers.py
@@ -14,6 +14,7 @@ from openbb_fmp.models.crypto_historical import FMPCryptoHistoricalFetcher
 from openbb_fmp.models.dividend_calendar import FMPDividendCalendarFetcher
 from openbb_fmp.models.earnings_calendar import FMPEarningsCalendarFetcher
 from openbb_fmp.models.earnings_call_transcript import FMPEarningsCallTranscriptFetcher
+from openbb_fmp.models.etf_info import FMPEtfInfoFetcher
 from openbb_fmp.models.etf_search import FMPEtfSearchFetcher
 from openbb_fmp.models.executive_compensation import FMPExecutiveCompensationFetcher
 from openbb_fmp.models.financial_ratios import FMPFinancialRatiosFetcher
@@ -463,5 +464,14 @@ def test_fmp_etf_search_fetcher(credentials=test_credentials):
     params = {"query": "India"}
 
     fetcher = FMPEtfSearchFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+
+@pytest.mark.record_http
+def test_fmp_etf_info_fetcher(credentials=test_credentials):
+    params = {"symbol": "IOO"}
+
+    fetcher = FMPEtfInfoFetcher()
     result = fetcher.test(params, credentials)
     assert result is None
