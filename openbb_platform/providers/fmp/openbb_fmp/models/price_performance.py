73264ab1dd9bf6b304cbcc57244e363e77f44e37
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-10-27 22:56:48 -0700

    ruff

--- openbb_platform/providers/fmp/openbb_fmp/models/price_performance.py
+++ openbb_platform/providers/fmp/openbb_fmp/models/price_performance.py
@@ -1,11 +1,9 @@
 """FMP Price Performance Model"""
 
 
-from datetime import date
 from typing import Any, Dict, List, Optional
 
-from dateutil.relativedelta import relativedelta
-from openbb_fmp.utils.helpers import create_url, get_data_one
+from openbb_fmp.utils.helpers import get_data_one
 from openbb_provider.abstract.fetcher import Fetcher
 from openbb_provider.standard_models.recent_performance import (
     RecentPerformanceData,
