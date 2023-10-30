47289944f5938984d3844bcf331c24049f3feb05
Diogo Sousa <montezdesousa@gmail.com>
2023-10-30 23:17:56 +0000

    unittest

--- openbb_platform/providers/quandl/tests/test_quandl_fetchers.py
+++ openbb_platform/providers/quandl/tests/test_quandl_fetchers.py
@@ -1,8 +1,10 @@
+import datetime
 import pytest
 from openbb_core.app.service.user_service import UserService
 from openbb_quandl.models.cot import QuandlCotFetcher
 from openbb_quandl.models.cot_search import QuandlCotSearchFetcher
 from openbb_quandl.models.sp500_multiples import QuandlSP500MultiplesFetcher
+from openbb_quandl.models.economic_calendar import QuandlEconomicCalendarFetcher
 
 test_credentials = UserService().default_user_settings.credentials.model_dump(
     mode="json"
@@ -38,5 +40,13 @@ def test_quandl_cot_search_fetcher(credentials=test_credentials):
     params = {}
 
     fetcher = QuandlCotSearchFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+@pytest.mark.record_http
+def test_quandl_economic_calendar_fetcher(credentials=test_credentials):
+    params = {'start_date': datetime.date(2023, 10, 26), 'end_date': datetime.date(2023, 10, 30)}
+
+    fetcher = QuandlEconomicCalendarFetcher()
     result = fetcher.test(params, credentials)
     assert result is None
