a7c01c16a60153e2a68ef189afe399aadd8fea1c
James Maslek <jmaslek11@gmail.com>
2023-10-30 14:23:49 -0400

    spelling.  oops.  almost like we have a pre commit or something

--- openbb_platform/platform/core/openbb_core/app/model/obbject.py
+++ openbb_platform/platform/core/openbb_core/app/model/obbject.py
@@ -190,7 +190,7 @@ class OBBject(Tagged, Generic[T]):
             "dict", "list", "series", "split", "tight", "records", "index"
         ] = "list",
     ) -> Dict[str, List]:
-        """Convert results field to a dictionary uisng any of pandas to_dict options.
+        """Convert results field to a dictionary using any of pandas to_dict options.
 
         Parameters
         ----------
