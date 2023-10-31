b2de32a5cdb86fd6f6b2fd1981d4e0112cd4fa56
Diogo Sousa <montezdesousa@gmail.com>
2023-10-31 14:45:49 +0000

    lint

--- openbb_platform/providers/nasdaq/tests/test_nasdaq_fetchers.py
+++ openbb_platform/providers/nasdaq/tests/test_nasdaq_fetchers.py
@@ -1,9 +1,12 @@
 import datetime
-from openbb_core.app.service.user_service import UserService
+
 import pytest
+from openbb_core.app.service.user_service import UserService
 from openbb_nasdaq.models.economic_calendar import NasdaqEconomicCalendarFetcher
 
-test_credentials = UserService().default_user_settings.credentials.model_dump(mode="json")
+test_credentials = UserService().default_user_settings.credentials.model_dump(
+    mode="json"
+)
 
 
 @pytest.fixture(scope="module")
@@ -12,9 +15,13 @@ def vcr_config():
         "filter_headers": [("User-Agent", None)],
     }
 
+
 @pytest.mark.record_http
 def test_nasdaq_economic_calendar_fetcher(credentials=test_credentials):
-    params = {'start_date': datetime.date(2023, 10, 23), 'end_date': datetime.date(2023, 10, 26)}
+    params = {
+        "start_date": datetime.date(2023, 10, 23),
+        "end_date": datetime.date(2023, 10, 26),
+    }
 
     fetcher = NasdaqEconomicCalendarFetcher()
     result = fetcher.test(params, credentials)
