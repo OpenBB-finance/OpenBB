645976faed57283521e140860d5a5d47035a6624
Adi Sai <adithya.sairam1@gmail.com>
2023-10-31 17:22:42 -0400

    inegration complete

---
+++ openbb_platform/providers/ultima/README.md
@@ -0,0 +1,15 @@
+# OpenBB Ultima Provider
+
+This extension integrates the [Ultima Insights](https://www.ultimainsights.ai/) data provider into the OpenBB Platform.
+
+## Installation
+
+To install the extension:
+
+```bash
+pip install openbb-ultima
+```
+
+For development please check [Contribution Guidelines](https://github.com/OpenBB-finance/OpenBBTerminal/blob/feature/openbb-sdk-v4/openbb_platform/CONTRIBUTING.md).
+
+Documentation available [here](https://docs.openbb.co/sdk).
---
+++ openbb_platform/providers/ultima/tests/test_ultima_fetchers.py
@@ -0,0 +1,24 @@
+import pytest
+from openbb_ultima.models.stock_news import UltimaStockNewsFetcher
+from openbb_core.app.service.user_service import UserService
+
+test_credentials = UserService().default_user_settings.credentials.dict()
+
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": [("User-Agent", None)],
+        "filter_query_parameters": [
+            ("token", "MOCK_TOKEN"),
+        ],
+    }
+
+
+@pytest.mark.record_http
+def test_ultima_stock_news_fetcher(credentials={}):
+    params = {"symbols": "AAPL, MSFT"}
+
+    fetcher = UltimaStockNewsFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
