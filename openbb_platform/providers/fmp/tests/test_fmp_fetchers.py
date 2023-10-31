5e0f863257d957d519324ee1cc2d0f0af5af816a
Theodore Aptekarev <aptekarev@gmail.com>
2023-10-31 11:51:42 +0100

    Add unit tests

--- openbb_platform/providers/fmp/tests/test_fmp_fetchers.py
+++ openbb_platform/providers/fmp/tests/test_fmp_fetchers.py
@@ -14,6 +14,8 @@ from openbb_fmp.models.crypto_historical import FMPCryptoHistoricalFetcher
 from openbb_fmp.models.dividend_calendar import FMPDividendCalendarFetcher
 from openbb_fmp.models.earnings_calendar import FMPEarningsCalendarFetcher
 from openbb_fmp.models.earnings_call_transcript import FMPEarningsCallTranscriptFetcher
+from openbb_fmp.models.etf_holdings import FMPEtfHoldingsFetcher
+from openbb_fmp.models.etf_holdings_date import FMPEtfHoldingsDateFetcher
 from openbb_fmp.models.etf_info import FMPEtfInfoFetcher
 from openbb_fmp.models.etf_search import FMPEtfSearchFetcher
 from openbb_fmp.models.etf_sectors import FMPEtfSectorsFetcher
@@ -483,5 +485,23 @@ def test_fmp_etf_sectors_fetcher(credentials=test_credentials):
     params = {"symbol": "IOO"}
 
     fetcher = FMPEtfSectorsFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+
+@pytest.mark.record_http
+def test_fmp_etf_holdings_fetcher(credentials=test_credentials):
+    params = {"symbol": "IOO", "date": date(2023, 4, 22)}
+
+    fetcher = FMPEtfHoldingsFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+
+@pytest.mark.record_http
+def test_fmp_etf_holdings_date_fetcher(credentials=test_credentials):
+    params = {"symbol": "IOO"}
+
+    fetcher = FMPEtfHoldingsDateFetcher()
     result = fetcher.test(params, credentials)
     assert result is None
