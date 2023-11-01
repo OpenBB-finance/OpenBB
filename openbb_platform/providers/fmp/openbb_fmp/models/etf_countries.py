6cadfec1c2bef4017df0cb8acee3b973bf2930d2
Theodore Aptekarev <aptekarev@gmail.com>
2023-11-01 14:34:03 +0100

    Update sector and country weighting fmp models

--- openbb_platform/providers/fmp/openbb_fmp/models/etf_countries.py
+++ openbb_platform/providers/fmp/openbb_fmp/models/etf_countries.py
@@ -1,41 +1,41 @@
-"""FMP ETF Sector Weighting fetcher."""
+"""FMP ETF Info fetcher."""
 
 from typing import Any, Dict, List, Optional
 
 from openbb_fmp.utils.helpers import create_url, get_data_many
 from openbb_provider.abstract.fetcher import Fetcher
-from openbb_provider.standard_models.etf_sectors import (
-    EtfSectorsData,
-    EtfSectorsQueryParams,
+from openbb_provider.standard_models.etf_countries import (
+    EtfCountriesData,
+    EtfCountriesQueryParams,
 )
 
 
-class FMPEtfSectorsQueryParams(EtfSectorsQueryParams):
-    """FMP ETF Sector Weighting Params."""
+class FMPEtfCountriesQueryParams(EtfCountriesQueryParams):
+    """FMP ETF Country Weighting Params."""
 
 
-class FMPEtfSectorsData(EtfSectorsData):
-    """FMP ETF Sector Weighting Data."""
+class FMPEtfCountriesData(EtfCountriesData):
+    """FMP ETF Country Weighting Data."""
 
     __alias_dict__ = {"weight": "weightPercentage"}
 
 
-class FMPEtfSectorsFetcher(
+class FMPEtfCountriesFetcher(
     Fetcher[
-        FMPEtfSectorsQueryParams,
-        List[FMPEtfSectorsData],
+        FMPEtfCountriesQueryParams,
+        List[FMPEtfCountriesData],
     ]
 ):
     """Transform the query, extract and transform the data from the FMP endpoints."""
 
     @staticmethod
-    def transform_query(params: Dict[str, Any]) -> FMPEtfSectorsQueryParams:
+    def transform_query(params: Dict[str, Any]) -> FMPEtfCountriesQueryParams:
         """Transform the query."""
-        return FMPEtfSectorsQueryParams(**params)
+        return FMPEtfCountriesQueryParams(**params)
 
     @staticmethod
     def extract_data(
-        query: FMPEtfSectorsQueryParams,
+        query: FMPEtfCountriesQueryParams,
         credentials: Optional[Dict[str, str]],
         **kwargs: Any,
     ) -> List[Dict]:
@@ -44,7 +44,7 @@ class FMPEtfSectorsFetcher(
 
         url = create_url(
             version=3,
-            endpoint=f"etf-sector-weightings/{query.symbol.upper()}",
+            endpoint=f"etf-country-weightings/{query.symbol.upper()}",
             api_key=api_key,
         )
 
@@ -52,12 +52,12 @@ class FMPEtfSectorsFetcher(
 
     @staticmethod
     def transform_data(
-        query: FMPEtfSectorsQueryParams, data: List[Dict], **kwargs: Any
-    ) -> List[FMPEtfSectorsData]:
+        query: FMPEtfCountriesQueryParams, data: List[Dict], **kwargs: Any
+    ) -> List[FMPEtfCountriesData]:
         """Return the transformed data."""
         for d in data:
             if d["weightPercentage"] is not None and d["weightPercentage"].endswith(
                 "%"
             ):
                 d["weightPercentage"] = float(d["weightPercentage"][:-1]) / 100
-        return [FMPEtfSectorsData.model_validate(d) for d in data]
+        return [FMPEtfCountriesData.model_validate(d) for d in data]
