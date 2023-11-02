d8679ac466705827bda313c6d10f43db4f25d4cd
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-11-02 10:01:31 +0000

    unused import

--- openbb_platform/providers/fred/openbb_fred/models/moody.py
+++ openbb_platform/providers/fred/openbb_fred/models/moody.py
@@ -4,7 +4,6 @@
 from typing import Any, Dict, List, Literal, Optional
 
 from openbb_fred.utils.fred_base import Fred
-from openbb_fred.utils.fred_helpers import get_ice_bofa_series_id
 from openbb_provider.abstract.fetcher import Fetcher
 from openbb_provider.standard_models.moody import (
     MoodyCorporateBondIndexData,
