23271d5aff254a6c5d094cda7cd3f5378845c93f
hjoaquim <h.joaquim@campus.fct.unl.pt>
2023-11-03 16:13:26 +0000

    test for ecb fetcher

---
+++ openbb_platform/providers/ecb/tests/test_ecb_fetchers.py
@@ -0,0 +1,27 @@
+import datetime
+import pytest
+from openbb_core.app.service.user_service import UserService
+from openbb_ecb.models.eu_yield_curve import ECBEUYieldCurveFetcher
+
+test_credentials = UserService().default_user_settings.credentials.model_dump(
+    mode="json"
+)
+
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": [("User-Agent", None)],
+        "filter_query_parameters": [
+            ("token", "MOCK_TOKEN"),
+        ],
+    }
+
+
+@pytest.mark.record_http
+def test_ecbeu_yield_curve_fetcher(credentials=test_credentials):
+    params = {"date": datetime.date(2023, 1, 1)}
+
+    fetcher = ECBEUYieldCurveFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
