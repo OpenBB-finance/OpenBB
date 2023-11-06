b1c98f46562f8b9fe2c1c89c6b7728a2b3b5a559
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-11-06 03:31:51 -0800

    Feature/v4-dividend-calendar: Adds Nasdaq dividend calendar and moves the command out of fundamental_analysis (#5672)
    * add nasdaq dividend calendar and move out of fundamental_analysis
    
    * black
    
    * ruff
    
    * add random-user-agent to pyproject.toml
    
    * add nasdaq ipo calendar
    
    * add nasdaq flag for SPO to IPO calendar
    
    * add spo test param
    
    * black
    
    * dividend test
    
    * fix transform_query - nasdaq_calendar_ipo
    
    * bring back different headers for ipo calendar

--- openbb_platform/providers/nasdaq/openbb_nasdaq/__init__.py
+++ openbb_platform/providers/nasdaq/openbb_nasdaq/__init__.py
@@ -1,4 +1,6 @@
 """Nasdaq provider module."""
+from openbb_nasdaq.models.calendar_dividend import NasdaqDividendCalendarFetcher
+from openbb_nasdaq.models.calendar_ipo import NasdaqCalendarIpoFetcher
 from openbb_nasdaq.models.economic_calendar import NasdaqEconomicCalendarFetcher
 from openbb_provider.abstract.provider import Provider
 
@@ -9,6 +11,8 @@ nasdaq_provider = Provider(
 provides premier platforms and services for global capital markets and beyond with
 unmatched technology, insights and markets expertise.""",
     fetcher_dict={
+        "CalendarDividend": NasdaqDividendCalendarFetcher,
+        "CalendarIpo": NasdaqCalendarIpoFetcher,
         "EconomicCalendar": NasdaqEconomicCalendarFetcher,
     },
 )
