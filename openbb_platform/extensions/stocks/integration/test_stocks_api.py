e5e10d3d598ea1843e82df1c2b5c3aa2763f37f2
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-11-07 14:31:15 -0800

    update FMP fetcher to use the V4 endpoint (#5681)

--- openbb_platform/extensions/stocks/integration/test_stocks_api.py
+++ openbb_platform/extensions/stocks/integration/test_stocks_api.py
@@ -1193,7 +1193,28 @@ def test_stocks_disc_upcoming_release_days(params, headers):
 
 @pytest.mark.parametrize(
     "params",
-    [({"pages": 1, "limit": 5, "today": True, "provider": "fmp"})],
+    [
+        (
+            {
+                "start_date": None,
+                "end_date": None,
+                "limit": 10,
+                "form_type": None,
+                "is_done": None,
+                "provider": "fmp",
+            }
+        ),
+        (
+            {
+                "start_date": "2023-11-06",
+                "end_date": "2023-11-07",
+                "limit": 50,
+                "form_type": "10-Q",
+                "is_done": "true",
+                "provider": "fmp",
+            }
+        ),
+    ],
 )
 @pytest.mark.integration
 def test_stocks_disc_filings(params, headers):
