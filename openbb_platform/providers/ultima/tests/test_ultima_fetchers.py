54238bc1eacd6c0bb6c700e8cc6abb4a511543c7
Theodore Aptekarev <aptekarev@gmail.com>
2023-11-08 15:00:39 +0100

    Properly mock api key

--- openbb_platform/providers/ultima/tests/test_ultima_fetchers.py
+++ openbb_platform/providers/ultima/tests/test_ultima_fetchers.py
@@ -10,7 +10,10 @@ test_credentials = UserService().default_user_settings.credentials.model_dump(
 @pytest.fixture(scope="module")
 def vcr_config():
     return {
-        "filter_headers": [("User-Agent", None), ("Authorization", None)],
+        "filter_headers": [
+            ("User-Agent", None),
+            ("Authorization", "Bearer MOCK_API_KEY"),
+        ],
     }
 
 
