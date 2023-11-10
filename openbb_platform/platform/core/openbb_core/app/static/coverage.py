7ae881ab333fe55c5fe7db381758d62163251cea
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-11-10 18:35:29 +0100

    Expose command to model map (#5706)

--- openbb_platform/platform/core/openbb_core/app/static/coverage.py
+++ openbb_platform/platform/core/openbb_core/app/static/coverage.py
@@ -1,16 +1,23 @@
 """Coverage module."""
+from openbb_core.app.provider_interface import ProviderInterface
 from openbb_core.app.router import CommandMap
 
 
 class Coverage:
-    """/coverage
+    """/coverage endpoint.
+
     providers
-    commands"""
+    commands
+    command_model
+    """
 
     def __init__(self):
+        """Initialize coverage."""
         self._command_map = CommandMap(coverage_sep=".")
+        self._provider_interface = ProviderInterface()
 
     def __repr__(self) -> str:
+        """Return docstring."""
         return self.__doc__ or ""
 
     @property
@@ -22,3 +29,13 @@ class Coverage:
     def commands(self):
         """Return commands coverage."""
         return self._command_map.command_coverage
+
+    @property
+    def command_model(self):
+        """Return command to model mapping."""
+        return {
+            command: self._provider_interface.map[
+                self._command_map.commands_model[command]
+            ]
+            for command in self._command_map.commands_model  # pylint: disable=C0206
+        }
