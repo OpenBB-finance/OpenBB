78075b0d36771920f662afcc2c4386891241916f
Pratyush Shukla <ps4534@nyu.edu>
2023-11-11 04:18:18 +0530

    equity cleanup

--- openbb_platform/extensions/equity/integration/test_equity_api.py
+++ openbb_platform/extensions/equity/integration/test_equity_api.py
@@ -683,44 +683,6 @@ def test_equity_compare_peers(params, headers):
 @pytest.mark.parametrize(
     "params",
     [
-        ({"date": "2023-01-25", "provider": "intrinio", "symbol": "AAPL"}),
-        ({"provider": "cboe", "symbol": "AAPL"}),
-    ],
-)
-@pytest.mark.integration
-def test_equity_options_chains(params, headers):
-    params = {p: v for p, v in params.items() if v}
-
-    query_str = get_querystring(params, [])
-    url = f"http://0.0.0.0:8000/api/v1/equity/options/chains?{query_str}"
-    result = requests.get(url, headers=headers, timeout=10)
-    assert isinstance(result, requests.Response)
-    assert result.status_code == 200
-
-
-@pytest.mark.parametrize(
-    "params",
-    [
-        ({"symbol": "AAPL"}),
-        ({"source": "delayed", "provider": "intrinio", "symbol": "AAPL"}),
-        ({"symbol": None, "source": "delayed", "provider": "intrinio"}),
-        ({"symbol": "PLTR", "source": "delayed", "provider": "intrinio"}),
-    ],
-)
-@pytest.mark.integration
-def test_equity_options_unusual(params, headers):
-    params = {p: v for p, v in params.items() if v}
-
-    query_str = get_querystring(params, [])
-    url = f"http://0.0.0.0:8000/api/v1/equity/options/unusual?{query_str}"
-    result = requests.get(url, headers=headers, timeout=10)
-    assert isinstance(result, requests.Response)
-    assert result.status_code == 200
-
-
-@pytest.mark.parametrize(
-    "params",
-    [
         (
             {
                 "symbol": "AAPL",
