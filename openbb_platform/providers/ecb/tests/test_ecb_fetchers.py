7ceacb391b77e9112e247e8d51dc6ca1a3880668
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-11-17 06:53:52 -0800

    feature/v4-bop - Add ECB Balance of Payments (Replaces #5716) (#5726)
    * add ecb balance of payments
    
    * missing __init__.py files

--- openbb_platform/providers/ecb/tests/test_ecb_fetchers.py
+++ openbb_platform/providers/ecb/tests/test_ecb_fetchers.py
@@ -4,6 +4,7 @@ import datetime
 
 import pytest
 from openbb_core.app.service.user_service import UserService
+from openbb_ecb.models.balance_of_payments import ECBBalanceOfPaymentsFetcher
 from openbb_ecb.models.currency_reference_rates import ECBCurrencyReferenceRatesFetcher
 from openbb_ecb.models.eu_yield_curve import ECBEUYieldCurveFetcher
 
@@ -39,5 +40,15 @@ def test_ecb_currency_reference_rates_fetcher(credentials=test_credentials):
     params = {}
 
     fetcher = ECBCurrencyReferenceRatesFetcher()
+    result = fetcher.test(params, credentials)
+    assert result is None
+
+
+@pytest.mark.record_http
+def test_ecb_balance_of_payments_fetcher(credentials=test_credentials):
+    """Test ECB Balance Of Payments Fetcher."""
+    params = {"date": datetime.date(2023, 1, 1)}
+
+    fetcher = ECBBalanceOfPaymentsFetcher()
     result = fetcher.test(params, credentials)
     assert result is None
