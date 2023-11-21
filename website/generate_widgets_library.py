23dc50d0600317c22b313c9f40e6924e0533e329
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-11-21 00:41:07 -0800

    add index to widgets-library

--- website/generate_widgets_library.py
+++ website/generate_widgets_library.py
@@ -4,6 +4,7 @@ import requests
 
 
 def main() -> bool:
+
     widgets_json_url = "https://raw.githubusercontent.com/OpenBB-finance/widgets-library/main/widgets.json"
 
     response = requests.get(widgets_json_url, timeout=10)
@@ -14,10 +15,12 @@ def main() -> bool:
     for widget_id, widget_info in widgets_data.items():
         if widget_info.get("disabled", False):
             continue
+
         category = widget_info.get("category")
         if category:
             if category not in all_widgets:
                 all_widgets[category] = []
+
             all_widgets[category].append(
                 {
                     "name": widget_info.get("name"),
@@ -27,40 +30,83 @@ def main() -> bool:
                 }
             )
 
-    base_path = Path("content/pro/widgets-library")
+    # Create the category index file
+    text = f"""# Widgets Library
+
+import NewReferenceCard from "@site/src/components/General/NewReferenceCard";
+
+<ul className="grid grid-cols-1 gap-4 -ml-6">
+"""
+    for category, widgets in all_widgets.items():
+        text += f"""
+<NewReferenceCard
+    title="{category}"
+    description="{', '.join([widget['name'] for widget in widgets])}"
+    url="/pro/widgets-library/{category}"
+/>"""
+    text += "\n</ul>"
+
+    website_path = Path(__file__).parent.absolute()
+    base_path = website_path / "content" / "pro" / "widgets-library"
+
+    index_path = base_path / "index.mdx"
+    with index_path.open("w") as index_file:
+        index_file.write(f"{text}\n")
 
     for category, widgets in all_widgets.items():
         category_path = base_path / category
         category_path.mkdir(parents=True, exist_ok=True)
 
+        # Create the category index file
+        text = f"""# {category}
+
+import NewReferenceCard from "@site/src/components/General/NewReferenceCard";
+
+<ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 -ml-6">
+"""
+        for widget in widgets:
+            text += f"""
+<NewReferenceCard
+    title="{widget['name']}"
+    description="{widget['description']}"
+    url="/pro/widgets-library/{category}/{widget['widgetId']}"
+    chevron={{false}}
+/>"""
+        text += "\n</ul>"
+
+        index_path = category_path / "index.mdx"
+        with index_path.open("w") as index_file:
+            index_file.write(f"{text}\n")
+
+        # Create each individual widget file
         for widget in widgets:
             widget_path = category_path / f"{widget['widgetId']}.md"
 
             text = f"""---
-    title: {widget['name']}
-    description: {widget['description']}
-    keywords:
-    - OpenBB
-    - Terminal Pro
-    - Investment Research
-    - Widgets
-    - {widget['name']}
-    - {category}
-    ---
+title: {widget['name']}
+description: {widget['description']}
+keywords:
+- OpenBB
+- Terminal Pro
+- Investment Research
+- Widgets
+- {widget['name']}
+- {category}
+---
 
-    import HeadTitle from '@site/src/components/General/HeadTitle.tsx';
+import HeadTitle from '@site/src/components/General/HeadTitle.tsx';
 
-    <HeadTitle title="{widget['name']} - {category} | OpenBB Terminal Pro Docs" />
+<HeadTitle title="{widget['name']} - {category} | OpenBB Terminal Pro Docs" />
 
-    <img
-        src="https://raw.githubusercontent.com/OpenBB-finance/widgets-library/main/{category}/{widget['widgetId']}.png"
-        alt="OpenBB Terminal Pro Widgets Library"
-    />
+<img
+    src="https://raw.githubusercontent.com/OpenBB-finance/widgets-library/main/{category}/{widget['widgetId']}.png"
+    alt="OpenBB Terminal Pro Widgets Library"
+/>
 
-    {widget['description']}.
+{widget['description']}.
 
-    {'Source: ' + ','.join(widget['source']) if widget['source'] else ''}
-    """
+{'**Source:** ' + ','.join(widget['source']) if widget['source'] else ''}
+"""
             with widget_path.open("w") as widget_file:
                 widget_file.write(f"{text}\n")
 
