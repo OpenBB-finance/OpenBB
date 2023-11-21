cef6f7747509ece1d81ee4ca5126d4d44f642c69
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-11-21 14:41:18 -0800

    fix news tests

--- openbb_platform/extensions/news/integration/test_news_api.py
+++ openbb_platform/extensions/news/integration/test_news_api.py
@@ -14,24 +14,24 @@ def headers():
 @pytest.mark.parametrize(
     "params",
     [
-        ({"limit": 20, "provider": "benzinga"}),
         (
             {
                 "display": "full",
-                "date": "2023-01-01",
+                "date": None,
                 "start_date": "2023-01-01",
                 "end_date": "2023-06-06",
-                "updated_since": 1,
-                "published_since": 1,
+                "updated_since": None,
+                "published_since": None,
                 "sort": "created",
                 "order": "desc",
-                "isin": "US0378331005",
-                "cusip": "037833100",
+                "isin": None,
+                "cusip": None,
                 "channels": "General",
-                "topics": "AAPL",
-                "authors": "Benzinga Insights",
+                "topics": "earnings",
+                "authors": None,
                 "content_types": "headline",
                 "provider": "benzinga",
+                "symbols": "AAPL,MSFT",
                 "limit": 20,
             }
         ),
