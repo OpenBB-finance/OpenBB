33915718671590550e15b143b9af2560faf76663
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-11-21 21:48:41 -0800

    black

--- openbb_platform/platform/provider/openbb_provider/standard_models/treasury_prices.py
+++ openbb_platform/platform/provider/openbb_provider/standard_models/treasury_prices.py
@@ -15,19 +15,32 @@ class USTreasuryPricesQueryParams(QueryParams):
     """US Treasury Prices Query."""
 
     date: Optional[dateType] = Field(
-        description=QUERY_DESCRIPTIONS.get("date", "")+ " No date will return the current posted data.",
+        description=QUERY_DESCRIPTIONS.get("date", "")
+        + " No date will return the current posted data.",
         default=None,
     )
+
 
 class USTreasuryPricesData(Data):
     """US Treasuries Prices Data."""
 
     date: dateType = Field(description=DATA_DESCRIPTIONS.get("date", ""))
     cusip: str = Field(description="CUSIP of the security.")
-    security_type: str = Field(description="The type of US Treasury security. Bill, Note, Bond, TIPS, FRN.")
-    rate: Optional[float] = Field(description = "The annualized interest rate or coupon of the security.",default=None)
+    security_type: str = Field(
+        description="The type of US Treasury security. Bill, Note, Bond, TIPS, FRN."
+    )
+    rate: Optional[float] = Field(
+        description="The annualized interest rate or coupon of the security.",
+        default=None,
+    )
     maturity_date: dateType = Field(description="The maturity date of the security.")
-    call_date: Optional[dateType] = Field(description="The call date of the security.", default=None)
-    bid: Optional[float] = Field(description="The bid price of the security.", default=None)
-    offer: Optional[float] = Field(description="The offer price of the security.", default=None)
+    call_date: Optional[dateType] = Field(
+        description="The call date of the security.", default=None
+    )
+    bid: Optional[float] = Field(
+        description="The bid price of the security.", default=None
+    )
+    offer: Optional[float] = Field(
+        description="The offer price of the security.", default=None
+    )
     eod_price: float = Field(description="The end-of-day price of the security.")
