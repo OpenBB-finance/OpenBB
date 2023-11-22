b34389b33950498fb26757b4810b29e84e4e1a9f
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-11-22 13:00:14 -0800

    from openbb_core.provider.....

--- openbb_platform/providers/nasdaq/openbb_nasdaq/models/lbma_fixing.py
+++ openbb_platform/providers/nasdaq/openbb_nasdaq/models/lbma_fixing.py
@@ -4,8 +4,8 @@ from typing import Any, Dict, List, Literal, Optional
 
 import nasdaqdatalink
 from openbb_nasdaq.models.query_params import DataLinkQueryParams
-from openbb_provider.core.provider.abstract.fetcher import Fetcher
-from openbb_provider.core.provider.standard_models.lbma_fixing import (
+from openbb_core.provider.abstract.fetcher import Fetcher
+from openbb_core.provider.standard_models.lbma_fixing import (
     LbmaFixingData,
     LbmaFixingQueryParams,
 )
