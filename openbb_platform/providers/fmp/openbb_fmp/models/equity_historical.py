931c8b40fcf70f9ccd9be68c658873c950246f44
Diogo Sousa <montezdesousa@gmail.com>
2023-11-23 15:27:23 +0000

    add comment async/sync

--- openbb_platform/providers/fmp/openbb_fmp/models/equity_historical.py
+++ openbb_platform/providers/fmp/openbb_fmp/models/equity_historical.py
@@ -79,8 +79,11 @@ class FMPEquityHistoricalFetcher(
 
         return FMPEquityHistoricalQueryParams(**transformed_params)
 
+    # TODO: Fix fetcher to allow async/sync methods or turn everything async
+    # @overloading the method does not work, it breaks the type hinting for the
+    # synchronous method implemented in other providers
     @staticmethod
-    async def extract_data(
+    async def extract_data(  # pylint: disable=invalid-overridden-method
         query: FMPEquityHistoricalQueryParams,
         credentials: Optional[Dict[str, str]],
         **kwargs: Any,
