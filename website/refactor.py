7142ec2ab03ddce59d9304cf33a68d00de94bf17
Diogo Sousa <montezdesousa@gmail.com>
2023-11-29 14:56:06 +0000

    generate index.mdx

--- website/refactor.py
+++ website/refactor.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 from typing import Callable, List, Literal, Optional
+
 from openbb_core.app.static.package_builder import PathHandler
 
 # Paths
@@ -20,8 +21,8 @@ class CommandLib(PathHandler):
         """Get the func of the command."""
         return getattr(self.route_map.get(cmd, None), "endpoint", None)
 
-    def get_cmd_metadata(self, cmd: str) -> dict:
-        """Get the metadata for a command."""
+    def get_cmd_info(self, cmd: str) -> dict:
+        """Get the info for a command."""
         return {}
 
 
@@ -57,49 +58,96 @@ class Editor:
         with open(path, "w", encoding="utf-8", newline="\n") as f:  # type: ignore
             f.write(content)
 
-    @staticmethod
-    def gen_md_section(metadata: dict) -> str:
-        return ""
+    def generate_sidebar(self):
+        """Write the group of index.mdx and _category_.json to create a sidebar."""
 
-    @staticmethod
-    def write_mdx_and_category(target_dir: Path, label: str, position: int):
-        Editor.write(path=target_dir / "index.mdx", content="")
-        Editor.write(
-            path=target_dir / "_category_.json",
-            content=json.dumps({"label": label, "position": position}, indent=2),
-        )
+        def get_card(title: str, description: str, url: str, command: bool):
+            return f"""
+<ReferenceCard
+    title="{title}"
+    description="{description}"
+    url="{url}"
+    command="{str(command).lower()}"
+/>"""
 
-    def create_header(self, metadata: dict):
-        """Generate the markdown for a command."""
-        markdown = metadata.get("header", "")
+        def get_index(path: Path, label: str) -> str:
+            md_files = [sub for sub in path.glob("*md")]
 
-        markdown += "<!-- markdownlint-disable MD012 MD031 MD033 -->\n\n"
-        markdown += (
-            "import Tabs from '@theme/Tabs';\nimport TabItem from '@theme/TabItem';\n\n"
-        )
+            content = f"# {label}\n\n"
+            content += "import ReferenceCard from '@site/src/components/General/NewReferenceCard';\n\n"
+            content += "<ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 -ml-6'>"
+            for md in md_files:
+                ref = md.parts.index("reference")
+                url = "/".join([*md.parts[ref:-1], md.stem])
+                content += get_card(
+                    title=md.stem,
+                    description="abc",
+                    url=url,
+                    command=True,
+                )
+            content += "\n</ul>\n"
+            return content
 
-        markdown += self.gen_md_section(metadata)
+        def write_mdx_and_category(path: Path, label: str, position: int):
+            Editor.write(path=path / "index.mdx", content=get_index(path, label))
+            Editor.write(
+                path=path / "_category_.json",
+                content=json.dumps({"label": label, "position": position}, indent=2),
+            )
 
-        return markdown
-
-    def write_md(self, path: Path, metadata: dict):
-        markdown = self.create_header(metadata)
-        path.write_text("")
-
-    def write_group(self):
-        """Write the group of index.mdx and _category_.json"""
-        Editor.write_mdx_and_category(self.target_dir, "Reference", 5)
-
-        def recursive(target_dir: Path):
-            """Generate category json recursively"""
+        def recursive(path: Path):
             position = 1
-            for p in target_dir.iterdir():
+            for p in path.iterdir():
                 if p.is_dir():
-                    Editor.write_mdx_and_category(p, p.name.title(), position)
+                    write_mdx_and_category(p, p.name.title(), position)
                     recursive(p)
                     position += 1
 
+        write_mdx_and_category(self.target_dir, "Reference", 5)
         recursive(self.target_dir)
+
+    def generate_md(self, path: Path, cmd_info: dict):
+        def get_header() -> str:
+            title = cmd_info["name"]
+
+            header = "---\n"
+            header += f"title: {title}\n"
+            header += "description: test\n"
+            header += "keywords: \n"
+            header += "- abc\n"
+            header += "- def\n"
+            header += "---\n\n"
+            return header
+
+        def get_tab() -> str:
+            tab = "<!-- markdownlint-disable MD012 MD031 MD033 -->\n\n"
+            tab += "import Tabs from '@theme/Tabs';\n"
+            tab += "import TabItem from '@theme/TabItem';\n\n"
+            return tab
+
+        def get_description() -> str:
+            return ""
+
+        def get_signature() -> str:
+            return ""
+
+        def get_parameters() -> str:
+            return ""
+
+        def get_returns() -> str:
+            return ""
+
+        def get_data() -> str:
+            return ""
+
+        content = get_header()
+        content += get_tab()
+        content += get_description()
+        content += get_signature()
+        content += get_parameters()
+        content += get_returns()
+        content += get_data()
+        self.write(path, content)
 
     def generate(self):
         """Generate the website reference."""
@@ -112,19 +160,21 @@ class Editor:
             if not func_name:
                 continue
             func_name = "index_cmd" if func_name == "index" else func_name
-            folder = "/".join(cmd.strip("/").split("/")[:-1])
-            filepath = self.target_dir / folder / f"{func_name}.md"
 
-            # metadata = cmd_lib.get_cmd_metadata(cmd)
+            folder = "/".join(cmd.strip("/").split("/")[:-1])
+            path = self.target_dir / folder / f"{func_name}.md"
+
+            cmd_info = self.cmd_lib.get_cmd_info(cmd)
+            cmd_info["name"] = func_name
 
             if self.output == "data_models":
                 pass
             elif self.output == "reference":
-                filepath.parent.mkdir(parents=True, exist_ok=True)
-                self.write_md(path=filepath, metadata={})
-                self.write_group()
+                path.parent.mkdir(parents=True, exist_ok=True)
+                self.generate_md(path, cmd_info)
 
-            print(f"Markdown files generated, check the {self.target_dir} folder.")
+        self.generate_sidebar()
+        print(f"Markdown files generated, check the {self.target_dir} folder.")
 
 
 if __name__ == "__main__":
