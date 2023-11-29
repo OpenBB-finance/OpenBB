fc50b1a82d16e5ff1709eda2dd05ff1f3e1eaf0b
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-11-28 16:47:32 -0800

    no ruff in this file

--- website/generate_widgets_library.py
+++ website/generate_widgets_library.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 import requests
 
+# ruff: noqa
 
 def main() -> bool:
     widgets_json_url = "https://raw.githubusercontent.com/OpenBB-finance/widgets-library/main/widgets.json"
@@ -54,7 +55,6 @@ def main() -> bool:
         else:
             print("SHOULD NEVER GET HERE BECAUSE CATEGORY_1 IS A MANDATORY FIELD!")
 
-
     website_path = Path(__file__).parent.absolute()
     base_path = website_path / "content" / "pro" / "widgets-library"
 
@@ -78,8 +78,7 @@ import NewReferenceCard from "@site/src/components/General/NewReferenceCard";
         if "CATEGORYLESS" in new_all_widgets[category_1]:
             l_elements.remove("CATEGORYLESS")
             l_elements += [
-                k["name"]
-                for k in new_all_widgets[category_1]["CATEGORYLESS"]
+                k["name"] for k in new_all_widgets[category_1]["CATEGORYLESS"]
             ]
 
         text += f"""
@@ -94,12 +93,10 @@ import NewReferenceCard from "@site/src/components/General/NewReferenceCard";
     with index_path.open("w", encoding="utf-8", newline="\n") as index_file:
         index_file.write(f"{text}\n")
 
-    ###########
-
     ## Handle category_1 level
     for category_1, widgets in new_all_widgets.items():
         # Check if the directory exists, if not create it
-        index_path = base_path / category_1.lower().replace(' ', '-') / "index.mdx"
+        index_path = base_path / category_1.lower().replace(" ", "-") / "index.mdx"
         if not index_path.parent.exists():
             index_path.parent.mkdir(parents=True)
 
@@ -116,8 +113,7 @@ import NewReferenceCard from "@site/src/components/General/NewReferenceCard";
             if "CATEGORYLESS" in new_all_widgets[category_1]:
                 l_elements.remove("CATEGORYLESS")
                 l_elements += [
-                    k["name"]
-                    for k in new_all_widgets[category_1]["CATEGORYLESS"]
+                    k["name"] for k in new_all_widgets[category_1]["CATEGORYLESS"]
                 ]
 
             text += f"""
@@ -140,11 +136,7 @@ import NewReferenceCard from "@site/src/components/General/NewReferenceCard";
 
         for category_1, widgets in new_all_widgets.items():
             # Check if the directory exists, if not create it
-            index_path = (
-                base_path
-                / category_1.lower().replace(" ", "-")
-                / "index.mdx"
-            )
+            index_path = base_path / category_1.lower().replace(" ", "-") / "index.mdx"
             if not index_path.parent.exists():
                 index_path.parent.mkdir(parents=True)
 
@@ -155,9 +147,7 @@ import NewReferenceCard from "@site/src/components/General/NewReferenceCard";
 <ul className="grid grid-cols-1 gap-4 -ml-6">
 """
 
-            for category_2, widgets in new_all_widgets[
-                category_1
-            ].items():
+            for category_2, widgets in new_all_widgets[category_1].items():
                 # Get all elements
                 if category_2 != "CATEGORYLESS":
                     text += f"""
@@ -180,11 +170,7 @@ import NewReferenceCard from "@site/src/components/General/NewReferenceCard";
             text += "\n</ul>"
 
             # Check if the path is a directory
-            index_path = (
-                base_path
-                / category_1.lower().replace(" ", "-")
-                / "index.mdx"
-            )
+            index_path = base_path / category_1.lower().replace(" ", "-") / "index.mdx"
             if index_path.is_dir():
                 print(f"{index_path} is a directory, not a file.")
                 continue
@@ -194,9 +180,7 @@ import NewReferenceCard from "@site/src/components/General/NewReferenceCard";
                 index_file.write(f"{text}\n")
 
             # Handle category_2 level index
-            for category_2, widgets in new_all_widgets[
-                category_1
-            ].items():
+            for category_2, widgets in new_all_widgets[category_1].items():
                 if category_2 != "CATEGORYLESS":
                     # Check if the directory exists, if not create it
                     index_path = (
