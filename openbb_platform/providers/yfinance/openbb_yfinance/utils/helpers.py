fe92399c0c490829dfd2b27c6e13da518997a0be
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2023-12-01 09:23:14 -0800

    yfinance multi-ticker support

--- openbb_platform/providers/yfinance/openbb_yfinance/utils/helpers.py
+++ openbb_platform/providers/yfinance/openbb_yfinance/utils/helpers.py
@@ -91,7 +91,7 @@ def yf_download(
     keepna: bool = False,
     repair: bool = False,
     rounding: bool = False,
-    group_by: Literal["symbol", "column"] = "column",
+    group_by: Literal["symbol", "column"] = "symbol",
     adjusted: bool = False,
     **kwargs: Any,
 ) -> pd.DataFrame:
@@ -131,6 +131,21 @@ def yf_download(
         group_by=group_by,
         **kwargs,
     )
+
+    tickers = symbol.split(",") if "," in symbol else symbol
+    if len(tickers) > 1:
+        _data = pd.DataFrame()
+        for ticker in tickers:
+            temp = data[ticker].copy().dropna(how="all")
+            for i in temp.index:
+                temp.loc[i, "symbol"] = ticker
+            temp = temp.reset_index().rename(
+                columns={"Date": "date", "Datetime": "date"}
+            )
+            _data = pd.concat([_data, temp])
+        _data = _data.set_index(["date", "symbol"]).sort_index()
+        data = _data
+
     if not data.empty:
         data = data.reset_index()
         data = data.rename(columns={"Date": "date", "Datetime": "date"})
@@ -159,11 +174,9 @@ def yf_download(
             "5y",
             "10y",
         ]:
-            data["date"] = (
-                data["date"].dt.tz_localize(None).dt.strftime("%Y-%m-%d %H:%M:%S")
-            )
+            data["date"] = data["date"].dt.strftime("%Y-%m-%d %H:%M:%S")
         if interval not in ["1m", "2m", "5m", "15m", "30m", "90m", "60m", "1h"]:
-            data["date"] = data["date"].dt.tz_localize(None).dt.strftime("%Y-%m-%d")
+            data["date"] = data["date"].dt.strftime("%Y-%m-%d")
 
         if adjusted is False:
             data = data.drop(columns=["Adj Close"])
