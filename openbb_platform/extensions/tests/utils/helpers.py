7c09ec4952d026837387f0fc41ec95dcd1431962
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2023-12-06 11:26:02 +0100

    Fix Python 3.9 error

--- openbb_platform/extensions/tests/utils/helpers.py
+++ openbb_platform/extensions/tests/utils/helpers.py
@@ -85,10 +85,10 @@ def list_openbb_extensions() -> Tuple[Set[str], Set[str]]:
     core_extensions = set()
     provider_extensions = set()
 
-    for entry_point in entry_points().select(group="openbb_core_extension"):
+    for entry_point in entry_points()["openbb_core_extension"]:
         core_extensions.add(f"{entry_point.name}")
 
-    for entry_point in entry_points().select(group="openbb_provider_extension"):
+    for entry_point in entry_points()["openbb_provider_extension"]:
         provider_extensions.add(f"{entry_point.name}")
 
     return core_extensions, provider_extensions
