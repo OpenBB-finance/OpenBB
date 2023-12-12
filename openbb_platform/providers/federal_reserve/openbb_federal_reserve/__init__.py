21de73b3cdf1b930245057480ba54e5dbf0e46b8
James Maslek <jmaslek11@gmail.com>
2023-12-12 14:08:06 -0500

    Add Money Measures models and commands
    This commit introduces new Money Measures models to handle money-related data, defines Money Measures commands in the economy router, and implements the Federal Reserve Money Measures model. The core model includes fields like start_date, end_date, and various components of the money supply. The new command added to the economy router allows the querying of these data.

--- openbb_platform/providers/federal_reserve/openbb_federal_reserve/__init__.py
+++ openbb_platform/providers/federal_reserve/openbb_federal_reserve/__init__.py
@@ -1,6 +1,9 @@
 """Government US provider module."""
 
 from openbb_core.provider.abstract.provider import Provider
+from openbb_federal_reserve.models.money_measures import (
+    FederalReserveMoneyMeasuresFetcher,
+)
 from openbb_federal_reserve.models.treasury_rates import (
     FederalReserveTreasuryRatesFetcher,
 )
@@ -11,5 +14,6 @@ federal_reserve_provider = Provider(
     description=(),
     fetcher_dict={
         "TreasuryRates": FederalReserveTreasuryRatesFetcher,
+        "MoneyMeasures": FederalReserveMoneyMeasuresFetcher,
     },
 )
---
+++ openbb_platform/providers/federal_reserve/openbb_federal_reserve/models/money_measures.py
@@ -0,0 +1,103 @@
+"""FederalReserve Money Measures Model."""
+
+from datetime import datetime, timedelta
+from io import BytesIO
+from typing import Any, Dict, List, Optional
+
+import pandas as pd
+from numpy import nan
+from openbb_core.provider.abstract.fetcher import Fetcher
+from openbb_core.provider.standard_models.money_measures import (
+    MoneyMeasuresData,
+    MoneyMeasuresQueryParams,
+)
+from openbb_core.provider.utils.helpers import make_request
+from pydantic import field_validator
+
+titles = {
+    "M1": "M1",
+    "M2": "M2",
+    "MCU": "Currency",
+    "MDD": "DemandDeposits",
+    "MMFGB": "RetailMoneyMarketFunds",
+    "MDL": "OtherLiquidDeposits",
+    "MDTS": "SmallDenominationTimeDeposits",
+}
+
+
+class FederalReserveMoneyMeasuresQueryParams(MoneyMeasuresQueryParams):
+    """FederalReserve Money Measures Query."""
+
+
+class FederalReserveMoneyMeasuresData(MoneyMeasuresData):
+    """FederalReserve Money Measures Data."""
+
+    @field_validator("date", mode="before", check_fields=False)
+    @classmethod
+    def date_validate(cls, v):  # pylint: disable=E0213
+        """Return the date as a datetime object."""
+        return datetime.strptime(v, "%Y-%m")
+
+
+class FederalReserveMoneyMeasuresFetcher(
+    Fetcher[
+        FederalReserveMoneyMeasuresQueryParams,
+        List[FederalReserveMoneyMeasuresData],
+    ]
+):
+    """Transform the query, extract and transform the data from the FederalReserve endpoints."""
+
+    @staticmethod
+    def transform_query(
+        params: Dict[str, Any]
+    ) -> FederalReserveMoneyMeasuresQueryParams:
+        """Transform the query params. Start and end dates are set to a 90 day interval."""
+        transformed_params = params
+
+        now = datetime.now().date()
+        if params.get("start_date") is None:
+            transformed_params["start_date"] = now - timedelta(days=10 * 365)
+
+        if params.get("end_date") is None:
+            transformed_params["end_date"] = now
+
+        return FederalReserveMoneyMeasuresQueryParams(**transformed_params)
+
+    @staticmethod
+    def extract_data(
+        query: FederalReserveMoneyMeasuresQueryParams,
+        credentials: Optional[Dict[str, str]],
+        **kwargs: Any,
+    ) -> List[Dict]:
+        """Return the raw data from the FederalReserve endpoint."""
+        url = (
+            "https://www.federalreserve.gov/datadownload/Output.aspx?rel=H6&series=798e2796917702a5f8423426ba7e6b42"
+            "&lastobs=&from=&to=&filetype=csv&label=include&layout=seriescolumn&type=package"
+        )
+
+        r = make_request(url, **kwargs)
+        df = pd.read_csv(BytesIO(r.content), header=5, index_col=None, parse_dates=True)
+
+        columns_to_get = ["Time Period"] + [
+            col + f'{"_N" if query.adjusted else ""}.M' for col in titles
+        ]
+        df = df[columns_to_get]
+        df.columns = ["month"] + list(titles.values())
+        df = df.replace("ND", nan)
+        df["month"] = pd.to_datetime(df["month"])
+        df = df[
+            (pd.to_datetime(df.month) >= pd.to_datetime(query.start_date))
+            & (pd.to_datetime(df.month) <= pd.to_datetime(query.end_date))
+        ].set_index("month")
+        # Needs the date to not be in the columns
+        df = df.applymap(lambda x: float(x) if x != "-" else x)
+        df = df.reset_index(drop=False)
+
+        return df.to_dict(orient="records")
+
+    @staticmethod
+    def transform_data(
+        query: FederalReserveMoneyMeasuresQueryParams, data: List[Dict], **kwargs: Any
+    ) -> List[FederalReserveMoneyMeasuresData]:
+        """Return the transformed data."""
+        return [FederalReserveMoneyMeasuresData.model_validate(d) for d in data]
