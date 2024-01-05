5eca8f35ab660577885421277f0842226b5144a9
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-01-05 12:36:54 -0800

    add fiscal_period to revenue by geography and business line

--- openbb_platform/core/openbb_core/provider/standard_models/revenue_business_line.py
+++ openbb_platform/core/openbb_core/provider/standard_models/revenue_business_line.py
@@ -1,8 +1,8 @@
 """Revenue by Business Line Standard Model."""
 
-
+import warnings
 from datetime import date as dateType
-from typing import Dict, List, Literal, Set, Union
+from typing import Dict, Literal, Optional
 
 from pydantic import Field, field_validator
 
@@ -12,6 +12,8 @@ from openbb_core.provider.utils.descriptions import (
     DATA_DESCRIPTIONS,
     QUERY_DESCRIPTIONS,
 )
+
+_warn = warnings.warn
 
 
 class RevenueBusinessLineQueryParams(QueryParams):
@@ -26,17 +28,29 @@ class RevenueBusinessLineQueryParams(QueryParams):
     )  # should always be flat
 
     @field_validator("symbol", mode="before", check_fields=False)
-    def upper_symbol(cls, v: Union[str, List[str], Set[str]]):
+    @classmethod
+    def upper_symbol(cls, v: str):
         """Convert symbol to uppercase."""
-        if isinstance(v, str):
-            return v.upper()
-        return ",".join([symbol.upper() for symbol in list(v)])
+        if "," in v:
+            _warn(
+                f"{QUERY_DESCRIPTIONS.get('symbol_list_warning', '')} {v.split(',')[0].upper()}"
+            )
+        return v.split(",")[0].upper() if "," in v else v.upper()
 
 
 class RevenueBusinessLineData(Data):
     """Revenue by Business Line Data."""
 
-    date: dateType = Field(description=DATA_DESCRIPTIONS.get("date", ""))
+    period_ending: dateType = Field(description=DATA_DESCRIPTIONS.get("date", ""))
+    fiscal_period: Optional[str] = Field(
+        default=None, description="The fiscal period of the reporting period."
+    )
+    fiscal_year: Optional[int] = Field(
+        default=None, description="The fiscal year of the reporting period."
+    )
+    filing_date: Optional[dateType] = Field(
+        default=None, description="The filing date of the report."
+    )
     business_line: Dict[str, ForceInt] = Field(
-        description="Day level data containing the revenue of the business line."
+        description="Dictionary containing the revenue of the business line."
     )
