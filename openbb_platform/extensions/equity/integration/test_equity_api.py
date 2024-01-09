3c59e8a0d0ee76c31d8779beef062a86c0128d8e
Diogo Sousa <montezdesousa@gmail.com>
2024-01-09 14:14:33 +0000

    tests fix

--- openbb_platform/extensions/equity/integration/test_equity_api.py
+++ openbb_platform/extensions/equity/integration/test_equity_api.py
@@ -231,11 +231,26 @@ def test_equity_fundamental_cash_growth(params, headers):
 @parametrize(
     "params",
     [
-        ({"symbol": "AAPL", "provider": "fmp"}),
-        ({"symbol": "AAPL", "start_date": "2020-01-01"}),
-        ({"symbol": "AAPL", "start_date": "2020-01-01", "end_date": "2021-01-01"}),
-        ({"symbol": "AAPL,MSFT", "provider": "fmp"}),
-        ({"symbol": "AAPL,MSFT", "start_date": "2020-01-01", "end_date": "2021-01-01"}),
+        (
+            {
+                "symbol": "AAPL",
+                "start_date": "2020-01-01",
+                "end_date": "2021-01-01",
+                "provider": "fmp",
+            }
+        ),
+        (
+            {
+                "symbol": "AAPL",
+                "provider": "fmp",
+            }
+        ),
+        (
+            {
+                "symbol": "AAPL,MSFT",
+                "provider": "fmp",
+            }
+        ),
     ],
 )
 @pytest.mark.integration
