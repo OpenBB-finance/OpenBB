d7c69187300579f4a5226f2ce0321021c5e7635d
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2024-01-10 16:40:01 +0100

    Disable auto_build on test run (#5937)
    * Disable auto_build on test run
    
    * Move to conftest

--- openbb_platform/tests/conftest.py
+++ openbb_platform/tests/conftest.py
@@ -1,3 +1,9 @@
 # TODO: See if we can dynamically import the test modules under the openbb_platform
 
 # TODO: Add global fixture for headers here
+
+import os
+
+
+def pytest_configure():
+    os.environ["OPENBB_AUTO_BUILD"] = "false"
