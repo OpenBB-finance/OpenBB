dffba3010726b9cd47d13687669e48eba79b4651
hjoaquim <h.joaquim@campus.fct.unl.pt>
2024-01-11 17:58:38 +0000

    fmp support for multiple ticker

--- openbb_platform/providers/fmp/openbb_fmp/models/price_target.py
+++ openbb_platform/providers/fmp/openbb_fmp/models/price_target.py
@@ -8,7 +8,7 @@ from openbb_core.provider.standard_models.price_target import (
     PriceTargetData,
     PriceTargetQueryParams,
 )
-from openbb_fmp.utils.helpers import create_url, get_data_many
+from openbb_fmp.utils.helpers import create_url, get_data_urls
 from pydantic import Field, field_validator
 
 
@@ -63,13 +63,17 @@ class FMPPriceTargetFetcher(
         api_key = credentials.get("fmp_api_key") if credentials else ""
         endpoint = "upgrades-downgrades" if query.with_grade else "price-target"
 
-        url = create_url(4, endpoint, api_key, query)
+        urls = []
+        for symbol in query.symbol.split(","):
+            query.symbol = symbol
+            urls.append(create_url(4, endpoint, api_key, query, exclude=["limit"]))
 
-        return await get_data_many(url, **kwargs)
+        return await get_data_urls(urls)
 
     @staticmethod
     def transform_data(
         query: FMPPriceTargetQueryParams, data: List[Dict], **kwargs: Any
     ) -> List[FMPPriceTargetData]:
         """Return the transformed data."""
+        data = data[: query.limit]
         return [FMPPriceTargetData.model_validate(d) for d in data]
