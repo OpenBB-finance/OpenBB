e90b6942e77408976a0ceedfbeb31d1a2fa3429c
Diogo Sousa <montezdesousa@gmail.com>
2024-01-18 13:31:41 +0000

    fix fundamental balance test

--- openbb_platform/extensions/equity/integration/test_equity_python.py
+++ openbb_platform/extensions/equity/integration/test_equity_python.py
@@ -26,7 +26,7 @@ def obb(pytestconfig):
             {
                 "provider": "intrinio",
                 "symbol": "AAPL",
-                "period": "ttm",
+                "period": "quarter",
                 "fiscal_year": 2014,
                 "limit": 2,
             }
