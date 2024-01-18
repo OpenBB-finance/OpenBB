49eb820ee48298a5fa4b15f33f48f4f76a7b284e
montezdesousa <79287829+montezdesousa@users.noreply.github.com>
2024-01-18 13:26:18 +0000

    Update dev_install.py

--- openbb_platform/dev_install.py
+++ openbb_platform/dev_install.py
@@ -34,7 +34,6 @@ openbb-economy = { path = "./extensions/economy", develop = true }
 openbb-equity = { path = "./extensions/equity", develop = true }
 openbb-etf = { path = "./extensions/etf", develop = true }
 openbb-fixedincome = { path = "./extensions/fixedincome", develop = true }
-openbb-government-us = { path = "./providers/government_us", develop = true }
 openbb-index = { path = "./extensions/index", develop = true }
 openbb-news = { path = "./extensions/news", develop = true }
 openbb-regulators = { path = "./extensions/regulators", develop = true }
