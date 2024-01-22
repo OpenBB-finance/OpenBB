1815685dce1bfb15504d342ad00e5499ded56ae5
Pratyush Shukla <ps4534@nyu.edu>
2024-01-22 21:06:25 +0530

    fix install script and typo in openbb toml

--- openbb_platform/dev_install.py
+++ openbb_platform/dev_install.py
@@ -46,7 +46,7 @@ openbb-cboe = { path = "./providers/cboe", optional = true, develop = true }
 openbb-ecb = { path = "./providers/ecb", optional = true, develop = true }
 openbb-finra = { path = "./providers/finra", optional = true, develop = true }
 openbb-finviz = { path = "./providers/finviz", optional = true, develop = true }
-openbb-government-us = { path = "./providers/government_us", develop = true }
+openbb-government-us = { path = "./providers/government_us", optional = true, develop = true }
 openbb-nasdaq = { path = "./providers/nasdaq", optional = true, develop = true }
 openbb-seeking-alpha = { path = "./providers/seeking_alpha", optional = true, develop = true }
 openbb-stockgrid = { path = "./providers/stockgrid" , optional = true,  develop = true }
