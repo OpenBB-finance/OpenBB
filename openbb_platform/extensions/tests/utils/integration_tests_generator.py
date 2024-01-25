48de17d65d08d6722f1d43ca9a4b69c37f6316b1
hjoaquim <h.joaquim@campus.fct.unl.pt>
2024-01-25 15:49:03 +0000

    fix tests

--- openbb_platform/extensions/tests/utils/integration_tests_generator.py
+++ openbb_platform/extensions/tests/utils/integration_tests_generator.py
@@ -43,6 +43,9 @@ def find_extensions(filter_chart: Optional[bool] = True):
     if filter_chart:
         filter_ext.append("charting")
     extensions = [x for x in Path("openbb_platform/extensions").iterdir() if x.is_dir()]
+    extensions.extend(
+        [x for x in Path("openbb_platform/obbject_extensions").iterdir() if x.is_dir()]
+    )
     extensions = [x for x in extensions if x.name not in filter_ext]
     return extensions
 
