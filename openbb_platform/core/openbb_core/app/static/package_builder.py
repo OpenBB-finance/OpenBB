edda18613bca4ef8652ac49cb442587824f41afd
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2024-01-27 00:22:21 +0100

    [Feature] - Support for custom examples in router commands (#5993)
    * Disable auto_build on test run
    
    * Add support for custom router examples
    
    * Revert odd file change
    
    * Fix API pollution
    
    * Refactor examples with @montezdesousa
    
    * Check if model is inside the PI
    
    * Fix
    
    * feat: add & fix examples (#6001)
    
    * feat: add & fix examples
    
    * fix: ruff + comment
    
    * feat: read parameter pool from file
    
    * feat: typing + unit test
    
    * Disable auto_build on test run
    
    * Add field order to OBBject
    
    * Revert
    
    * lint and revert
    
    * fix test
    
    ---------
    
    Co-authored-by: montezdesousa <79287829+montezdesousa@users.noreply.github.com>
    Co-authored-by: Diogo Sousa <montezdesousa@gmail.com>

--- openbb_platform/core/openbb_core/app/static/package_builder.py
+++ openbb_platform/core/openbb_core/app/static/package_builder.py
@@ -387,9 +387,13 @@ class ClassDefinition:
                         else None
                     ),
 <<<<<<< HEAD
+<<<<<<< HEAD
                     examples=route.openapi_extra.get("examples", None),
 =======
 >>>>>>> 13283fbfce (CI listing quick fix (#6002))
+=======
+                    examples=route.openapi_extra.get("examples", None),
+>>>>>>> c6eefd26b9 ([Feature] - Support for custom examples in router commands (#5993))
                 )  # type: ignore
             else:
                 doc += "    /" if path else "    /"
