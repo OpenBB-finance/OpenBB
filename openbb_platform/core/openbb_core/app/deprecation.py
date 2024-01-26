ba1e428ae8ae7eb7b30e7af00fab096675ffe0d8
hjoaquim <h.joaquim@campus.fct.unl.pt>
2024-01-26 17:35:47 +0000

    custom deprecation

---
+++ openbb_platform/core/openbb_core/app/deprecation.py
@@ -0,0 +1,52 @@
+"""
+OpenBB-specific deprecation warnings.
+
+This implementation was inspired from Pydantic's specific warnings and modified to suit OpenBB's needs.
+"""
+from typing import Optional, Tuple
+
+
+class OpenBBDeprecationWarning(DeprecationWarning):
+    """
+    A OpenBB specific deprecation warning.
+
+    This warning is raised when using deprecated functionality in OpenBB. It provides information on when the
+    deprecation was introduced and the expected version in which the corresponding functionality will be removed.
+
+    Attributes
+    ----------
+        message: Description of the warning.
+        since: Version in what the deprecation was introduced.
+        expected_removal: Version in what the corresponding functionality expected to be removed.
+    """
+
+    message: str
+    since: Tuple[int, int]
+    expected_removal: Tuple[int, int]
+
+    def __init__(
+        self,
+        message: str,
+        *args: object,
+        since: Tuple[int, int],
+        expected_removal: Optional[Tuple[int, int]] = None,
+    ) -> None:
+        super().__init__(message, *args)
+        self.message = message.rstrip(".")
+        self.since = since
+        self.expected_removal = expected_removal or (since[0] + 1, 0)
+        self.long_message = (
+            f"{self.message}. Deprecated in OpenBB Platform V{self.since[0]}.{self.since[1]}"
+            f" to be removed in V{self.expected_removal[0]}.{self.expected_removal[1]}."
+        )
+
+    def __str__(self) -> str:
+        """Return the warning message."""
+        return self.long_message
+
+
+class OpenBBDeprecatedSince41(OpenBBDeprecationWarning):
+    """A specific `OpenBBDeprecationWarning` subclass defining functionality deprecated since OpenBB 4.1."""
+
+    def __init__(self, message: str, *args: object) -> None:
+        super().__init__(message, *args, since=(4, 1), expected_removal=(4, 5))
