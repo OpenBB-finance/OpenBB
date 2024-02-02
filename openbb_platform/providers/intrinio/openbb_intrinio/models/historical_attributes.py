9ddbf34456b748fd38aa7e1bd4e95618dc253f94
Pratyush Shukla <ps4534@nyu.edu>
2024-02-02 16:42:50 +0530

    [Feature] Add support for multiple tags in `historical_attributes` and `latest_attributes` (#6013)
    * historical attributes standard model -
    * add support multiple tags
    * add `tag` field in data
    
    * add support for multiple tags in historical attributes
    
    * set correct return type in `callback` function
    
    * latest attributes standard model -
    * add support for multiple tags
    * add `tag` field in data
    
    * add support for multiple tags in latest attributes
    
    * add test params for the attributes endpoints
    
    * add support for multiple `symbol` and add `symbol` field in the data
    
    * modified code to fetch data from multiple symbols and throw warning as per @deeleeramone
    
    * add params for testing multiple symbols
    
    * linting
    
    * fix test params
    
    * fixed condition to check if its a `Dict` type
    
    * updated intrinio fetcher tests for the statements
    
    * updated static

--- openbb_platform/providers/intrinio/openbb_intrinio/models/historical_attributes.py
+++ openbb_platform/providers/intrinio/openbb_intrinio/models/historical_attributes.py
@@ -1,16 +1,22 @@
 """Intrinio Historical Attributes Model."""
 
+import warnings
 from datetime import datetime
 from typing import Any, Dict, List, Optional
 
 from dateutil.relativedelta import relativedelta
+from openbb_core.app.model.abstract.warning import OpenBBWarning
 from openbb_core.provider.abstract.fetcher import Fetcher
 from openbb_core.provider.standard_models.historical_attributes import (
     HistoricalAttributesData,
     HistoricalAttributesQueryParams,
 )
-from openbb_core.provider.utils.helpers import get_querystring
-from openbb_intrinio.utils.helpers import get_data_many
+from openbb_core.provider.utils.helpers import (
+    ClientResponse,
+    ClientSession,
+    amake_requests,
+    get_querystring,
+)
 
 
 class IntrinioHistoricalAttributesQueryParams(HistoricalAttributesQueryParams):
@@ -19,7 +25,7 @@ class IntrinioHistoricalAttributesQueryParams(HistoricalAttributesQueryParams):
     Source: https://docs.intrinio.com/documentation/web_api/get_historical_data_v2
     """
 
-    __alias_dict__ = {"sort": "sort_order", "limit": "page_size"}
+    __alias_dict__ = {"sort": "sort_order", "limit": "page_size", "tag_type": "type"}
 
 
 class IntrinioHistoricalAttributesData(HistoricalAttributesData):
@@ -62,8 +68,59 @@ class IntrinioHistoricalAttributesFetcher(
         base_url = "https://api-v2.intrinio.com"
         query_str = get_querystring(query.model_dump(by_alias=True), ["symbol", "tag"])
 
-        url = f"{base_url}/historical_data/{query.symbol}/{query.tag}?{query_str}&api_key={api_key}"
-        return await get_data_many(url, "historical_data")
+        def generate_url(symbol: str, tag: str) -> str:
+            """Returns the url for the given symbol and tag."""
+            url_params = f"{symbol}/{tag}?{query_str}&api_key={api_key}"
+            url = f"{base_url}/historical_data/{url_params}"
+            return url
+
+        async def callback(
+            response: ClientResponse, session: ClientSession
+        ) -> List[Dict]:
+            """Return the response."""
+            init_response = await response.json()
+
+            if message := init_response.get("error") or init_response.get("message"):
+                warnings.warn(message=message, category=OpenBBWarning)
+                return []
+
+            symbol = response.url.parts[-2]
+            tag = response.url.parts[-1]
+
+            all_data: list = init_response.get("historical_data", [])
+            all_data = [{**item, "symbol": symbol, "tag": tag} for item in all_data]
+
+            next_page = init_response.get("next_page", None)
+            while next_page:
+                url = response.url.update_query(next_page=next_page).human_repr()
+                response_data = await session.get_json(url)
+
+                if message := response_data.get("error") or response_data.get(
+                    "message"
+                ):
+                    warnings.warn(message=message, category=OpenBBWarning)
+                    return []
+
+                symbol = response.url.parts[-2]
+                tag = response_data.url.parts[-1]
+
+                response_data = response_data.get("historical_data", [])
+                response_data = [
+                    {**item, "symbol": symbol, "tag": tag} for item in response_data
+                ]
+
+                all_data.extend(response_data)
+                next_page = response_data.get("next_page", None)
+
+            return all_data
+
+        urls = [
+            generate_url(symbol, tag)
+            for symbol in query.symbol.split(",")
+            for tag in query.tag.split(",")
+        ]
+
+        return await amake_requests(urls, callback, **kwargs)
 
     @staticmethod
     def transform_data(
