aa87f269fd4efcbba343b1c4920792906e74f105
tehcoderer <me@tehcoderer.com>
2024-02-07 18:25:01 -0500

    Update registry_map.py

--- openbb_platform/core/openbb_core/provider/registry_map.py
+++ openbb_platform/core/openbb_core/provider/registry_map.py
@@ -116,22 +116,23 @@ class RegistryMap:
 
         fields = {}
         for field_name, field in model.model_fields.items():
-            field.alias_priority = None
+            field.serialization_alias = field_name
             fields[field_name] = (field.annotation, field)
+
+        fields["provider"] = (
+            Literal[provider_str],  # type: ignore
+            Field(
+                default=provider_str,
+                description="The data provider for the data.",
+                exclude=True,
+            ),
+        )
 
         provider_model = create_model(
             model.__name__.replace("Data", ""),
             __base__=model,
             __doc__=model.__doc__,
             __module__=model.__module__,
-            provider=(
-                Literal[provider_str],  # type: ignore
-                Field(
-                    default=provider_str,
-                    description="The data provider for the data.",
-                    exclude=True,
-                ),
-            ),
             **fields,
         )
 
