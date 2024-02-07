9ec1198d193e4e14e6e57b5b99663604460ec081
Pratyush Shukla <ps4534@nyu.edu>
2024-02-07 18:21:16 +0530

    improve /crypto description with custom examples

--- openbb_platform/extensions/crypto/openbb_crypto/crypto_router.py
+++ openbb_platform/extensions/crypto/openbb_crypto/crypto_router.py
@@ -17,12 +17,15 @@ router.include_router(price_router)
 
 
 # pylint: disable=unused-argument
-@router.command(model="CryptoSearch")
+@router.command(
+    model="CryptoSearch",
+    examples=['obb.crypto.search("BTCUSD")', 'obb.crypto.search("ETH-USD")'],
+)
 async def search(
     cc: CommandContext,
     provider_choices: ProviderChoices,
     standard_params: StandardParams,
     extra_params: ExtraParams,
 ) -> OBBject:
-    """Cryptocurrency Search. Search available cryptocurrency pairs."""
+    """Search available cryptocurrency pairs within a provider."""
     return await OBBject.from_query(Query(**locals()))
