160b5a146b58d13426876d6fe0eb4cdff8d58ead
Diogo Sousa <montezdesousa@gmail.com>
2024-02-09 15:43:00 +0000

    feat: revenue business line

--- openbb_platform/core/openbb_core/provider/standard_models/revenue_business_line.py
+++ openbb_platform/core/openbb_core/provider/standard_models/revenue_business_line.py
@@ -1,6 +1,5 @@
 """Revenue by Business Line Standard Model."""
 
-import warnings
 from datetime import date as dateType
 from typing import Dict, Literal, Optional
 
@@ -11,8 +10,6 @@ from openbb_core.provider.abstract.query_params import QueryParams
 from openbb_core.provider.utils.descriptions import (
     QUERY_DESCRIPTIONS,
 )
-
-_warn = warnings.warn
 
 
 class RevenueBusinessLineQueryParams(QueryParams):
@@ -30,11 +27,7 @@ class RevenueBusinessLineQueryParams(QueryParams):
     @classmethod
     def upper_symbol(cls, v: str):
         """Convert symbol to uppercase."""
-        if "," in v:
-            _warn(
-                f"{QUERY_DESCRIPTIONS.get('symbol_list_warning', '')} {v.split(',')[0].upper()}"
-            )
-        return v.split(",")[0].upper() if "," in v else v.upper()
+        return v.upper()
 
 
 class RevenueBusinessLineData(Data):
