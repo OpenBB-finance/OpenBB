d408e6f0f2119cc4c217ac720425c1406dc9c6f1
Diogo Sousa <montezdesousa@gmail.com>
2024-02-09 18:16:19 +0000

    fix: validator msg

--- openbb_platform/core/openbb_core/provider/utils/validators.py
+++ openbb_platform/core/openbb_core/provider/utils/validators.py
@@ -6,7 +6,7 @@ from typing import Optional
 def check_single(v: Optional[str]) -> Optional[str]:
     """Check that string is not comma-separated string"""
     if v and "," in v:
-        raise ValueError("multiple values are not allowed")
+        raise ValueError("comma-separated values are not allowed")
     return v
 
 
