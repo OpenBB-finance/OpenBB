1644cacd00929df412140c009bfd7516af39a00b
montezdesousa <montezdesousa@gmail.com>
2024-02-12 18:35:28 +0000

    unit test validator

---
+++ openbb_platform/core/tests/provider/utils/test_validators.py
@@ -0,0 +1,22 @@
+"""Test the provider validators."""
+
+import pytest
+from openbb_core.provider.utils.validators import check_single_value
+
+
+@pytest.mark.parametrize(
+    "field, value, expected",
+    [
+        ("test", "SYMBOL", "SYMBOL"),
+        ("test", None, None),
+        ("test", "", ""),
+        ("test", "SYMBOL1,SYMBOL2", ValueError),
+        ("test", "SYMBOL1;SYMBOL2", ValueError),
+    ],
+)
+def test_check_single_value(field, value, expected):
+    if expected == ValueError:
+        with pytest.raises(ValueError):
+            check_single_value(field, value)
+    else:
+        assert check_single_value(field, value) == expected
