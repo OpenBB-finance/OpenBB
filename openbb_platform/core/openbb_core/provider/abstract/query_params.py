6c89da651548a4f2e38b82c931bf86418a52bec5
montezdesousa <montezdesousa@gmail.com>
2024-02-12 16:43:09 +0000

    feat: query params description

--- openbb_platform/core/openbb_core/provider/abstract/query_params.py
+++ openbb_platform/core/openbb_core/provider/abstract/query_params.py
@@ -1,6 +1,6 @@
 """The OpenBB Standardized QueryParams Model that holds the query input parameters."""
 
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple
 
 from pydantic import BaseModel, ConfigDict, model_validator
 
@@ -8,10 +8,31 @@ from openbb_core.provider.utils import validators
 
 
 class QueryParams(BaseModel):
-    """The OpenBB Standardized QueryParams Model that holds the query input parameters."""
+    """The OpenBB Standardized QueryParams Model.
+
+    The `QueryParams` class is designed to hold query parameters, to be extended by
+    providers and to be used by fetchers when making data provider requests.
+
+    Key Features:
+    - Validators: The class applies a set of common validators if defined by the provider.
+    - Alias handling: Utilizes an aliasing mechanism to maintain compatibility with different naming
+        conventions across various data formats. The alias is only applied when running `model_dump`.
+
+    Attributes:
+    __validator_dict__ (Dict[str, tuple]):
+        A dictionary that maps validator names to a tuple of fields where that validator
+        should be applied. The validators must be defined in the validators.py module.
+    __alias_dict__ (Dict[str, str]):
+        A dictionary that maps field names to their aliases,
+        facilitating the use of different naming conventions.
+    model_config (ConfigDict):
+        A configuration dictionary that defines the model's behavior,
+        such as accepting extra fields, populating by name, and alias
+        generation.
+    """
 
     __alias_dict__: Dict[str, str] = {}
-    __validator_dict__: Dict[str, tuple] = {}
+    __validator_dict__: Dict[str, Tuple[str]] = {}
 
     def __repr__(self):
         """Return the string representation of the QueryParams object."""
