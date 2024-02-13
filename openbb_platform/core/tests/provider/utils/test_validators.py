80767c5012748377d6961bf29bd4193a8b1f7d18
montezdesousa <montezdesousa@gmail.com>
2024-02-13 22:49:41 +0000

    update test

--- openbb_platform/core/tests/provider/utils/test_validators.py
+++ openbb_platform/core/tests/provider/utils/test_validators.py
@@ -5,18 +5,18 @@ from openbb_core.provider.utils.validators import check_single_value
 
 
 @pytest.mark.parametrize(
-    "field, value, expected",
+    "value, expected",
     [
-        ("test", "SYMBOL", "SYMBOL"),
-        ("test", None, None),
-        ("test", "", ""),
-        ("test", "SYMBOL1,SYMBOL2", ValueError),
-        ("test", "SYMBOL1;SYMBOL2", ValueError),
+        ("SYMBOL", "SYMBOL"),
+        (None, None),
+        ("", ""),
+        ("SYMBOL1,SYMBOL2", ValueError),
+        ("SYMBOL1;SYMBOL2", ValueError),
     ],
 )
-def test_check_single_value(field, value, expected):
+def test_check_single_value(value, expected):
     if expected is ValueError:
         with pytest.raises(ValueError):
-            check_single_value(field, value)
+            check_single_value(value)
     else:
-        assert check_single_value(field, value) == expected
+        assert check_single_value(value) == expected
