a93847178c71a85641390a6d37f0e7c0d75949d6
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-02-13 19:04:38 -0800

    sort imports

--- openbb_platform/providers/fred/openbb_fred/models/regional.py
+++ openbb_platform/providers/fred/openbb_fred/models/regional.py
@@ -2,10 +2,11 @@
 
 # pylint: disable=unused-argument
 
-import json
-import warnings
 from datetime import date as dateType
 from typing import Any, Dict, List, Literal, Optional, Union
+
+import json
+import warnings
 
 from openbb_core.provider.abstract.fetcher import Fetcher
 from openbb_core.provider.standard_models.fred_series import (
