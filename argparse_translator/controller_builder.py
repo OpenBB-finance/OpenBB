869b7d8ad2810579fed05e10fa770042050cc9c5
hjoaquim <h.joaquim@campus.fct.unl.pt>
2024-02-19 11:32:41 +0000

    remove unnecessary file

--- argparse_translator/controller_builder.py
+++
@@ -1,5 +0,0 @@
-from openbb import obb
-from .argparse_class_processor import ArgparseClassProcessor
-
-equity_price_translations = ArgparseClassProcessor(target_class=obb.equity.price)
-print("nice")
