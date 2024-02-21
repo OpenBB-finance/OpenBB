4e98eb4895adc4cf444a2b63c32a6274ba2451e0
hjoaquim <h.joaquim@campus.fct.unl.pt>
2024-02-21 13:42:42 +0000

    making the new fetcher visible

--- openbb_platform/providers/tradingeconomics/openbb_tradingeconomics/__init__.py
+++ openbb_platform/providers/tradingeconomics/openbb_tradingeconomics/__init__.py
@@ -2,11 +2,15 @@
 
 from openbb_core.provider.abstract.provider import Provider
 from openbb_tradingeconomics.models.economic_calendar import TEEconomicCalendarFetcher
+from openbb_tradingeconomics.models.market_historical import TEMarketHistoricalFetcher
 
 tradingeconomics_provider = Provider(
     name="tradingeconomics",
     website="https://tradingeconomics.com/",
     description="""Trading Economics""",
     credentials=["api_key"],
-    fetcher_dict={"EconomicCalendar": TEEconomicCalendarFetcher},
+    fetcher_dict={
+        "EconomicCalendar": TEEconomicCalendarFetcher,
+        "MarketHistorical": TEMarketHistoricalFetcher,
+    },
 )
