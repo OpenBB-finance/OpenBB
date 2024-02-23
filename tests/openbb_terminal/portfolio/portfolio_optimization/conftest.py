34edfb4ad9745806f6314df2d73ecf3824f6bcad
hjoaquim <h.joaquim@campus.fct.unl.pt>
2024-02-23 08:57:27 +0000

    remove old tests

--- tests/openbb_terminal/portfolio/portfolio_optimization/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from _pytest.nodes import Node
-
-
-def pytest_runtest_setup(item: Node):
-    if not item.config.getoption("--optimization"):
-        pytest.skip(msg="Runs only with option : --optimization")
