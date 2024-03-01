3952c1cd4dcce5d39154b58da875a01f5603b754
Diogo Sousa <montezdesousa@gmail.com>
2024-03-01 14:00:43 +0000

    unblock api

--- openbb_platform/core/openbb_core/app/model/example.py
+++ openbb_platform/core/openbb_core/app/model/example.py
@@ -6,9 +6,10 @@ from pydantic import BaseModel, ConfigDict
 class Example(BaseModel):
     """Example model."""
 
+    # TODO: Evaluate making scope mandatory
     scope: Literal[
         "required", "standard", "other"
-    ]  # Required parameters, standard parameters, or other
+    ] = "other" # Required parameters, standard parameters, or other
     description: Optional[str] = None
     parameters: Dict[str, Any]
 
