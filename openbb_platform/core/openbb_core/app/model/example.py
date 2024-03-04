a5db02b705f8ce83eddc3897ad6998c11f1a99b9
Diogo Sousa <montezdesousa@gmail.com>
2024-03-04 11:22:13 +0000

    to_python

--- openbb_platform/core/openbb_core/app/model/example.py
+++ openbb_platform/core/openbb_core/app/model/example.py
@@ -29,33 +29,33 @@ class APIEx(Example):
     @property
     def provider(self) -> Optional[str]:
         """Return the provider from the parameters."""
-        return self.parameters.get("provider") if self.parameters else None
+        return self.parameters.get("provider")
 
     def to_python(self, **kwargs) -> str:
         """Return a Python code representation of the example."""
         indentation = kwargs.get("indentation", "")
         func_path = kwargs.get("func_path", ".func_router.func_name")
         func_params: Dict[str, Field] = kwargs.get("func_params", {})
-        eg = ""
-        if self.parameters is not None:
-            if self.description:
-                eg += f"{indentation}>>> # {self.description}\n"
-            eg += f"{indentation}>>> obb{func_path}("
 
-            for k, v in self.parameters.items():
-                if k in func_params and (field := func_params.get(k)):
-                    field_type_str = str(field.type)
-                    # TODO: Handle types better, some edge cases
-                    # like Union[str, List[str]] will be stringified
-                    # even if the type is a list
-                    if any(t in field_type_str for t in ["int", "float", "bool"]):
-                        eg += f"{k}={v}, "
-                    else:
-                        eg += f"{k}='{v}', "
+        eg = ""
+        if self.description:
+            eg += f"{indentation}>>> # {self.description}\n"
+
+        eg += f"{indentation}>>> obb{func_path}("
+        for k, v in self.parameters.items():
+            if k in func_params and (field := func_params.get(k)):
+                field_type_str = str(field.type)
+                # TODO: Handle types better, some edge cases
+                # like Union[str, List[str]] will be stringified
+                # even if the type is a list
+                if any(t in field_type_str for t in ["int", "float", "bool"]):
+                    eg += f"{k}={v}, "
                 else:
                     eg += f"{k}='{v}', "
+            else:
+                eg += f"{k}='{v}', "
 
-            eg = indentation + eg.strip(", ") + ")\n"
+        eg = indentation + eg.strip(", ") + ")\n"
         return eg
 
 
@@ -69,10 +69,8 @@ class PythonEx(Example):
         """Return a Python code representation of the example."""
         indentation = kwargs.get("indentation", "")
         eg = ""
-        if self.code is not None:
-            for line in self.code:  # pylint: disable=not-an-iterable
-                eg += f"{indentation}>>> {line}\n"
-
+        for line in self.code:
+            eg += f"{indentation}>>> {line}\n"
         return eg
 
 
