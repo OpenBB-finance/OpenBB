3bf7502b5bc400bba33c0c85dbcb35f57ea9f5d0
Diogo Sousa <montezdesousa@gmail.com>
2024-03-06 17:25:42 +0000

    inequality

--- openbb_platform/providers/fred/openbb_fred/models/spot.py
+++ openbb_platform/providers/fred/openbb_fred/models/spot.py
@@ -63,7 +63,7 @@ class FREDSpotRateFetcher(
             if isinstance(query.maturity, str)
             else [query.maturity]
         )
-        if any([m < 1 or m >= 100 for m in maturity]):
+        if any([m < 1 or m > 100 for m in maturity]):
             raise ValueError("Maturity must be between 1 and 100")
 
         series = get_spot_series_id(
