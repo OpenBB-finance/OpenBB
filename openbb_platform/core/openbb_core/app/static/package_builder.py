60c53f862e0a11afb3fdb7290fc970ede88decf4
Pratyush Shukla <ps4534@nyu.edu>
2024-03-12 23:04:30 +0530

    fix obbject extensions not shown in extension_map.json

--- openbb_platform/core/openbb_core/app/static/package_builder.py
+++ openbb_platform/core/openbb_core/app/static/package_builder.py
@@ -132,18 +132,10 @@ class PackageBuilder:
     def _get_extension_map(self) -> Dict[str, List[str]]:
         """Get map of extensions available at build time."""
         el = ExtensionLoader()
+        og = OpenBBGroups.groups()
         ext_map: Dict[str, List[str]] = {}
 
-        groups = [
-            OpenBBGroups.core.value,
-            OpenBBGroups.provider.value,
-        ]
-        entry_points_ = [
-            el.core_entry_points,
-            el.provider_entry_points,
-        ]
-
-        for group, entry_point in zip(groups, entry_points_):
+        for group, entry_point in zip(og, el.entry_points):
             ext_map[group] = [
                 f"{e.name}@{getattr(e.dist, 'version', '')}" for e in entry_point
             ]
@@ -252,7 +244,8 @@ class PackageBuilder:
 
         add: Set[str] = set()
         remove: Set[str] = set()
-        groups = ("openbb_core_extension", "openbb_provider_extension")
+        groups = OpenBBGroups.groups()
+
         for g in groups:
             built = set(ext_map.get(g, {}))
             installed = set(
@@ -556,7 +549,6 @@ class MethodDefinition:
         path: str, parameter_map: Dict[str, Parameter]
     ) -> OrderedDict[str, Parameter]:
         """Format the params."""
-
         parameter_map.pop("cc", None)
         # we need to add the chart parameter here bc of the docstring generation
         if CHARTING_INSTALLED and path.replace("/", "_")[1:] in Charting.functions():
