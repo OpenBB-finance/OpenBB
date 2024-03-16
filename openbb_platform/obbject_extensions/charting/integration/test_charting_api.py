36c626445304a582b6ca866775c92ad8ea9a8aa4
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-03-15 22:00:38 -0700

    add volatility cones to charting extension

--- openbb_platform/obbject_extensions/charting/integration/test_charting_api.py
+++ openbb_platform/obbject_extensions/charting/integration/test_charting_api.py
@@ -402,3 +402,35 @@ def test_charting_technical_zlma(params, headers):
     assert chart
     assert not fig
     assert list(chart.keys()) == ["content", "format"]
+
+
+@parametrize(
+    "params",
+    [
+        (
+            {
+                "data": "",
+                "model": "yang_zhang",
+                "chart": True,
+            }
+        )
+    ],
+)
+@pytest.mark.integration
+def test_charting_technical_cones(params, headers):
+    """Test chart ta cones."""
+    params = {p: v for p, v in params.items() if v}
+    body = json.dumps(get_equity_data())
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/technical/cones?{query_str}"
+    result = requests.post(url, headers=headers, timeout=10, data=body)
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+    chart = result.json()["chart"]
+    fig = chart.pop("fig", {})
+
+    assert chart
+    assert not fig
+    assert list(chart.keys()) == ["content", "format"]
