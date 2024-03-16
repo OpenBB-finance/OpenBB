72ccf4ed11ae33e0f8e675a83004ae493a53bdfa
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-03-16 07:49:59 -0700

    [Feature] Volatility Cones Chart (#6226)
    * add volatility cones to charting extension
    
    * test params

--- openbb_platform/obbject_extensions/charting/integration/test_charting_python.py
+++ openbb_platform/obbject_extensions/charting/integration/test_charting_python.py
@@ -348,3 +348,30 @@ def test_charting_technical_zlma(params, obb):
     assert len(result.results) > 0
     assert result.chart.content
     assert isinstance(result.chart.fig, OpenBBFigure)
+
+
+@parametrize(
+    "params",
+    [
+        (
+            {
+                "data": "",
+                "model": "yang_zhang",
+                "chart": True,
+            }
+        )
+    ],
+)
+@pytest.mark.integration
+def test_charting_technical_cones(params, obb):
+    """Test chart ta cones."""
+    params = {p: v for p, v in params.items() if v}
+
+    params["data"] = get_equity_data()
+
+    result = obb.technical.cones(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+    assert result.chart.content
+    assert isinstance(result.chart.fig, OpenBBFigure)
