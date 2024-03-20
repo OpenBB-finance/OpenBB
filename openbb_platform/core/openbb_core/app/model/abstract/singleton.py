42c103b75d13f98f0d88fd16c494576aa9a6af37
Diogo Sousa <montezdesousa@gmail.com>
2024-03-20 21:07:12 +0000

    another batch unyped defs

--- openbb_platform/core/openbb_core/app/model/abstract/singleton.py
+++ openbb_platform/core/openbb_core/app/model/abstract/singleton.py
@@ -7,7 +7,7 @@ class SingletonMeta(type, Generic[T]):
     # TODO : check if we want to update this to be thread safe
     _instances: Dict[T, T] = {}
 
-    def __call__(cls, *args, **kwargs):
+    def __call__(cls: "SingletonMeta", *args, **kwargs):
         if cls not in cls._instances:
             instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
