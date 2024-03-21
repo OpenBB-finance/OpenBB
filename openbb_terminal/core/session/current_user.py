209fa3aaf28a12c10a03af3b16170de57a61fa56
hjoaquim <h.joaquim@campus.fct.unl.pt>
2024-03-21 11:54:53 +0000

    remove unused models

--- openbb_terminal/core/session/current_user.py
+++ openbb_terminal/core/session/current_user.py
@@ -6,27 +6,18 @@ from typing import Any, Dict, Optional
 
 # IMPORTS INTERNAL
 from openbb_terminal.core.models import (
-    CredentialsModel,
     PreferencesModel,
-    ProfileModel,
-    SourcesModel,
     UserModel,
 )
 from openbb_terminal.core.session.env_handler import read_env
-from openbb_terminal.core.session.sources_handler import load_file_to_model
 from openbb_terminal.core.session.utils import load_dict_to_model
 
 __env_dict = read_env()
-__credentials = load_dict_to_model(__env_dict, CredentialsModel)  # type: ignore
 __preferences = load_dict_to_model(__env_dict, PreferencesModel)
-__sources = load_file_to_model(Path(__preferences.USER_DATA_SOURCES_FILE))
 
-__profile = ProfileModel()
+
 __local_user = UserModel(  # type: ignore
-    credentials=__credentials,  # type: ignore
     preferences=__preferences,  # type: ignore
-    profile=__profile,
-    sources=__sources,
 )
 __current_user = __local_user
 
@@ -61,38 +52,19 @@ def is_local() -> bool:
 def set_default_user():
     """Set default user."""
     env_dict = read_env()
-    credentials = load_dict_to_model(env_dict, CredentialsModel)
     preferences = load_dict_to_model(env_dict, PreferencesModel)
-    sources = load_file_to_model(Path(__preferences.USER_DATA_SOURCES_FILE))
-    profile = ProfileModel()
-    default_user = UserModel(  # type: ignore
-        credentials=credentials,
-        preferences=preferences,
-        profile=profile,
-        sources=sources,
-    )
+    default_user = UserModel(preferences=preferences)
     set_current_user(default_user)
 
 
 def copy_user(
-    sources: Optional[SourcesModel] = None,
-    credentials: Optional[CredentialsModel] = None,  # type: ignore
     preferences: Optional[PreferencesModel] = None,
-    profile: Optional[ProfileModel] = None,
     user: Optional[UserModel] = None,
 ):
     current_user = user or get_current_user()
-    sources = sources or current_user.sources
-    credentials = credentials or current_user.credentials
     preferences = preferences or current_user.preferences
-    profile = profile or current_user.profile
 
-    user_copy = UserModel(  # type: ignore
-        credentials=credentials,
-        preferences=preferences,
-        profile=profile,
-        sources=sources,
-    )
+    user_copy = UserModel(preferences=preferences)
 
     return user_copy
 
