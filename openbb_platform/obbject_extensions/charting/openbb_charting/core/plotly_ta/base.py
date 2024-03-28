22b3ff727ce65c71182a8d8bf023743814750cf6
Diogo Sousa <montezdesousa@gmail.com>
2024-03-28 13:17:06 +0000

    revert

--- openbb_platform/obbject_extensions/charting/openbb_charting/core/plotly_ta/base.py
+++ openbb_platform/obbject_extensions/charting/openbb_charting/core/plotly_ta/base.py
@@ -91,7 +91,7 @@ class PluginMeta(type):
 class PltTA(metaclass=PluginMeta):
     """The base class that all Plotly plugins must inherit from."""
 
-    indicators: Optional[ChartIndicators] = None
+    indicators: ChartIndicators
     intraday: bool = False
     df_stock: Union[pd.DataFrame, pd.Series]
     df_ta: Optional[pd.DataFrame] = None
