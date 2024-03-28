f987524ff81b7718d888a4670b2419893ef4d81c
hjoaquim <h.joaquim@campus.fct.unl.pt>
2024-03-28 11:11:57 +0000

    multiples does not have a charting implementation

--- openbb_platform/obbject_extensions/charting/integration/test_charting_api.py
+++ openbb_platform/obbject_extensions/charting/integration/test_charting_api.py
@@ -77,29 +77,6 @@ def test_charting_equity_price_historical(params, headers):
 
 @parametrize(
     "params",
-    [({"symbol": "AAPL", "limit": 100, "chart": True})],
-)
-@pytest.mark.integration
-def test_charting_equity_fundamental_multiples(params, headers):
-    """Test chart equity multiples."""
-    params = {p: v for p, v in params.items() if v}
-
-    query_str = get_querystring(params, [])
-    url = f"http://0.0.0.0:8000/api/v1/equity/fundamental/multiples?{query_str}"
-    result = requests.get(url, headers=headers, timeout=10)
-    assert isinstance(result, requests.Response)
-    assert result.status_code == 200
-
-    chart = result.json()["chart"]
-    fig = chart.pop("fig", {})
-
-    assert chart
-    assert not fig
-    assert list(chart.keys()) == ["content", "format"]
-
-
-@parametrize(
-    "params",
     [
         (
             {
