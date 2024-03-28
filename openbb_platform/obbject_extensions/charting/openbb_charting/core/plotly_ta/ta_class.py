a8f3d1d10be03ee133a308a5289e0b2d5043f329
Diogo Sousa <montezdesousa@gmail.com>
2024-03-28 13:59:20 +0000

    mypy indicators

--- openbb_platform/obbject_extensions/charting/openbb_charting/core/plotly_ta/ta_class.py
+++ openbb_platform/obbject_extensions/charting/openbb_charting/core/plotly_ta/ta_class.py
@@ -289,7 +289,7 @@ class PlotlyTA(PltTA):
     def _clear_data(self):
         """Clear and reset all data to default values."""
         self.df_stock = None
-        self.indicators = {}
+        self.indicators = ChartIndicators.from_dict(dict(dict()))
         self.params = None
         self.intraday = False
         self.show_volume = True
