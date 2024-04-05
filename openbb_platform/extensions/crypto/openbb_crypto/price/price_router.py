a3c0b287e9d3f29489caf00b7d81d86d0d231a0f
Diogo Sousa <montezdesousa@gmail.com>
2024-04-05 12:14:20 +0100

    undo sub routers

--- openbb_platform/extensions/crypto/openbb_crypto/price/price_router.py
+++ openbb_platform/extensions/crypto/openbb_crypto/price/price_router.py
@@ -12,7 +12,7 @@ from openbb_core.app.provider_interface import (
 from openbb_core.app.query import Query
 from openbb_core.app.router import Router
 
-router = Router(prefix="/price", description="Price data.")
+router = Router(prefix="/price")
 
 
 # pylint: disable=unused-argument,line-too-long
