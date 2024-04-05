33d186f8eaab1e1836db17f2d1c58ef46ba63bbf
montezdesousa <79287829+montezdesousa@users.noreply.github.com>
2024-04-05 12:21:35 +0100

    [Feature] - Router descriptions (#6288)
    * First try
    
    * add routers to reference.json
    
    * fix apploader
    
    * add some router descriptions
    
    * rebuild
    
    * remove pydocstyle fixes
    
    * minor fix
    
    * docstring
    
    * new descriptions
    
    * undo sub routers
    
    ---------
    
    Co-authored-by: Henrique Joaquim <henriquecjoaquim@gmail.com>

--- openbb_platform/core/openbb_core/api/app_loader.py
+++ openbb_platform/core/openbb_core/api/app_loader.py
@@ -1,10 +1,28 @@
+"""App loader module."""
+
 from typing import List, Optional
 
 from fastapi import APIRouter, FastAPI
+from openbb_core.app.router import RouterLoader
 
 
 class AppLoader:
     """App loader."""
+
+    @staticmethod
+    def get_openapi_tags() -> List[dict]:
+        """Get openapi tags."""
+        main_router = RouterLoader.from_extensions()
+        openapi_tags = []
+        # Add tag data for each router in the main router
+        for r in main_router.routers:
+            openapi_tags.append(
+                {
+                    "name": r,
+                    "description": main_router.get_attr(r, "description"),
+                }
+            )
+        return openapi_tags
 
     @staticmethod
     def from_routers(
