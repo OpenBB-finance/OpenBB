246ff361b98597b464ef9b58ad6471295165b1eb
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2024-04-09 15:14:27 +0200

    Fix error message

--- openbb_platform/providers/tmx/openbb_tmx/utils/helpers.py
+++ openbb_platform/providers/tmx/openbb_tmx/utils/helpers.py
@@ -429,7 +429,7 @@ async def get_all_options_tickers(use_cache: bool = True) -> pd.DataFrame:
     r = await get_data_from_url(url, use_cache=use_cache, backend=tmx_companies_backend)
 
     if r is None or r == []:
-        raise RuntimeError(f"Error with the request")  # mypy: ignore
+        raise RuntimeError("Error with the request")  # mypy: ignore
 
     options_listings = pd.read_html(StringIO(r))
     listings = pd.concat(options_listings)
