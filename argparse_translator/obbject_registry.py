df4fdae8be9f354ad69de1c6555b58dcc8fdd642
hjoaquim <h.joaquim@campus.fct.unl.pt>
2024-04-19 11:55:57 +0100

    small improvements to correctly get the standard args

--- argparse_translator/obbject_registry.py
+++ argparse_translator/obbject_registry.py
@@ -27,14 +27,14 @@ class Registry(metaclass=SingletonMeta):
                 cls.ids.remove(id_)
 
     @classmethod
-    def get(cls, id_: str):
+    def get(cls, id_: str) -> OBBject:
         for obbject in cls.obbject_instances:
             if obbject.id == id_:
                 return obbject
         raise ValueError(f"OBBject with id {id_} not found")
 
     @classmethod
-    def pop(cls, id_: str):
+    def pop(cls, id_: str) -> OBBject:
         for obbject in cls.obbject_instances:
             if obbject.id == id_:
                 cls.obbject_instances.remove(obbject)
@@ -44,24 +44,34 @@ class Registry(metaclass=SingletonMeta):
 
     @property
     def all(self) -> Dict[str, Dict]:
+        """Return all obbjects in the registry"""
+
+        def _handle_standard_params(obbject: OBBject) -> str:
+            """Handle standard params for obbjects"""
+            standard_params_json = ""
+            std_params = obbject._standard_params  # pylint: disable=protected-access
+            if hasattr(std_params, "__dict__"):
+                standard_params = {k: v for k, v in std_params.__dict__.items() if v}
+                standard_params_json = json.dumps(standard_params)
+
+            return standard_params_json
+
         obbjects = {}
         for obbject in self.obbject_instances:
             obbjects[obbject.id] = {
                 "route": obbject._route,  # pylint: disable=protected-access
                 "provider": obbject.provider,
             }
-            standard_params = (
-                obbject._standard_params.__dict__  # pylint: disable=protected-access
-            )
-            standard_params = {k: v for k, v in standard_params.items() if v}
-            standard_params_json = json.dumps(standard_params)
+
             data_schema = (
                 obbject.results[0].model_json_schema()
                 if obbject.results and isinstance(obbject.results, list)
                 else ""
             )
-            data_repr = f"{data_schema['title']} - {data_schema['description']}"
-            obbjects[obbject.id]["standard params"] = standard_params_json
+            data_repr = (
+                f"{data_schema['title']} - {data_schema['description'].split('.')[0]}"
+            )
+            obbjects[obbject.id]["standard params"] = _handle_standard_params(obbject)
             obbjects[obbject.id]["data"] = data_repr
 
         return obbjects
