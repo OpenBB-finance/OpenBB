16d293b8c1173d100569a882bc7f2c3e28a9b00a
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-04-19 09:15:25 -0700

    pylint

--- openbb_platform/obbject_extensions/charting/openbb_charting/query_params.py
+++ openbb_platform/obbject_extensions/charting/openbb_charting/query_params.py
@@ -19,13 +19,13 @@ def _get_type_name(t):
         if hasattr(t.__origin__, "__name__"):
             return f"{t.__origin__.__name__}[{', '.join([_get_type_name(arg) for arg in t.__args__])}]"
         if hasattr(t.__origin__, "_name"):
-            return f"{t.__origin__._name}[{', '.join([_get_type_name(arg) for arg in t.__args__])}]"
+            return f"{t.__origin__._name}[{', '.join([_get_type_name(arg) for arg in t.__args__])}]"  # pylint: disable=W0212
     if isinstance(t, str):
         return t
     if hasattr(t, "__name__"):
         return t.__name__
     if hasattr(t, "_name"):
-        return t._name
+        return t._name  # pylint: disable=W0212
     return str(t)
 
 
