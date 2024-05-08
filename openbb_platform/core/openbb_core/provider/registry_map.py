4de2de746bf575c0062a36550bfd7a148590d9b9
Diogo Sousa <montezdesousa@gmail.com>
2024-05-08 17:03:48 +0100

    better

--- openbb_platform/core/openbb_core/provider/registry_map.py
+++ openbb_platform/core/openbb_core/provider/registry_map.py
@@ -153,29 +153,29 @@ class RegistryMap:
         found_first_standard = False
 
         hierarchy = RegistryMap._class_hierarchy(model)
-        for i, c in enumerate(hierarchy):
-            if c.__name__ in SKIP:
+        for i, child in enumerate(hierarchy):
+            if child.__name__ in SKIP:
                 continue
 
             parent = hierarchy[i + 1] if hierarchy[i + 1] not in SKIP else BaseModel
 
             fields = {
                 name: field
-                for name, field in c.model_fields.items()
+                for name, field in child.model_fields.items()
                 # This ensures fields inherited by c are discarded.
                 # We need to compare child and parent __annotations__
                 # because this attribute is redirected to the parent class
                 # when the child simply inherits the parent and does not
                 # define any attributes.
                 # TLDR: Only fields defined in c are included
-                if name in c.__annotations__
-                and c.__annotations__ is not parent.__annotations__
+                if name in child.__annotations__
+                and child.__annotations__ is not parent.__annotations__
             }
 
-            if Path(getfile(c)).parent == STANDARD_MODELS_FOLDER:
+            if Path(getfile(child)).parent == STANDARD_MODELS_FOLDER:
                 if not found_first_standard:
                     # If standard uses inheritance we just use the first docstring
-                    standard_info["docstring"] = c.__doc__
+                    standard_info["docstring"] = child.__doc__
                     found_first_standard = True
                 standard_info["fields"].update(fields)
             else:
