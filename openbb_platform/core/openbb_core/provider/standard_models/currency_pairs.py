afc21ccc05e45a78643d27af9254a689ae2fe868
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-05-08 22:17:55 -0700

    fix currency search

--- openbb_platform/core/openbb_core/provider/standard_models/currency_pairs.py
+++ openbb_platform/core/openbb_core/provider/standard_models/currency_pairs.py
@@ -1,5 +1,7 @@
 """Currency Available Pairs Standard Model."""
 
+from typing import Optional
+
 from pydantic import Field
 
 from openbb_core.provider.abstract.data import Data
@@ -9,8 +11,13 @@ from openbb_core.provider.abstract.query_params import QueryParams
 class CurrencyPairsQueryParams(QueryParams):
     """Currency Available Pairs Query."""
 
+    query: Optional[str] = Field(
+        default=None, description="Query to search for currency pairs."
+    )
+
 
 class CurrencyPairsData(Data):
     """Currency Available Pairs Data."""
 
-    name: str = Field(description="Name of the currency pair.")
+    symbol: str = Field(description="Symbol of the currency pair.")
+    name: Optional[str] = Field(default=None, description="Name of the currency pair.")
