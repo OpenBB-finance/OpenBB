47520abb464f8272506f2b9ef490b459932dc61d
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-05-08 22:42:20 -0700

    static files

--- openbb_platform/core/openbb_core/provider/standard_models/currency_pairs.py
+++ openbb_platform/core/openbb_core/provider/standard_models/currency_pairs.py
@@ -6,6 +6,7 @@ from pydantic import Field
 
 from openbb_core.provider.abstract.data import Data
 from openbb_core.provider.abstract.query_params import QueryParams
+from openbb_core.provider.utils.descriptions import DATA_DESCRIPTIONS
 
 
 class CurrencyPairsQueryParams(QueryParams):
@@ -19,5 +20,5 @@ class CurrencyPairsQueryParams(QueryParams):
 class CurrencyPairsData(Data):
     """Currency Available Pairs Data."""
 
-    symbol: str = Field(description="Symbol of the currency pair.")
+    symbol: str = Field(description=DATA_DESCRIPTIONS.get("symbol", ""))
     name: Optional[str] = Field(default=None, description="Name of the currency pair.")
