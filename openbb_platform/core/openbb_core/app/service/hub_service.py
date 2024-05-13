1b1e3a7b1f7aedeb8f3bf734a7abb15285f02019
Diogo Sousa <montezdesousa@gmail.com>
2024-05-13 19:59:49 +0100

    fix: replace python-jose by PyJWT

--- openbb_platform/core/openbb_core/app/service/hub_service.py
+++ openbb_platform/core/openbb_core/app/service/hub_service.py
@@ -4,9 +4,7 @@ from typing import Optional
 from warnings import warn
 
 from fastapi import HTTPException
-from jose import JWTError
-from jose.exceptions import ExpiredSignatureError
-from jose.jwt import decode, get_unverified_header
+from jwt import ExpiredSignatureError, PyJWTError, decode, get_unverified_header
 from openbb_core.app.model.abstract.error import OpenBBError
 from openbb_core.app.model.credentials import Credentials
 from openbb_core.app.model.hub.hub_session import HubSession
@@ -271,5 +269,5 @@ class HubService:
             )
         except ExpiredSignatureError as e:
             raise OpenBBError("Platform personal access token expired.") from e
-        except JWTError as e:
+        except PyJWTError as e:
             raise OpenBBError("Failed to decode Platform token.") from e
