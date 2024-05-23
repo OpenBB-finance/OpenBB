3dc6a6d52a62a7a2aea590782c496b6ead586ac9
Diogo Sousa <montezdesousa@gmail.com>
2024-05-23 16:11:56 +0100

    redirect url

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -40,6 +40,10 @@ export default {
             from: "/terminal/menus/forecasting",
             to: "/terminal/menus/forecast",
           },
+          {
+            to: "/platform/developer_guide/contributing",
+            from: "/platform/development/contributing",
+          }
         ],
       },
     ],
