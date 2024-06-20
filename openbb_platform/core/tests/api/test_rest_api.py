90c42f3aba777c5bca2fe29077f7435a6703f6e6
Diogo Sousa <montezdesousa@gmail.com>
2024-06-20 13:57:13 +0100

    try this

--- openbb_platform/core/tests/api/test_rest_api.py
+++ openbb_platform/core/tests/api/test_rest_api.py
@@ -1,8 +1,14 @@
 """Test rest_api.py."""
 
-# from openbb_core.api.rest_api import app
+import pytest
 
 
-# def test_openapi():
-#     """Test openapi schema generation."""
-#     assert app.openapi()
+# @pytest.mark.skip(
+#     "This test forces a side effect that makes",
+#     "test_integration_tests_ to fail in the GitHub actions.",
+# )
+def test_openapi():
+    """Test openapi schema generation."""
+    # pylint: disable=import-outside-toplevel
+    from openbb_core.api.rest_api import app
+    assert app.openapi()
