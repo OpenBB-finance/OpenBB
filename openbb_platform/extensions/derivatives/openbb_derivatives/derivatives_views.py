aad4f848e187885373366c7d5f3cf1fef9d2f0ab
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-06-26 16:45:06 -0700

    forgotten file

--- openbb_platform/extensions/derivatives/openbb_derivatives/derivatives_views.py
+++ openbb_platform/extensions/derivatives/openbb_derivatives/derivatives_views.py
@@ -1,9 +1,9 @@
 """Views for the Derivatives Extension."""
 
-from typing import Any, Dict, Tuple
+from typing import TYPE_CHECKING, Any, Dict, Tuple
 
-from openbb_charting.charts.price_historical import price_historical
-from openbb_charting.core.openbb_figure import OpenBBFigure
+if TYPE_CHECKING:
+    from openbb_charting.core.openbb_figure import OpenBBFigure
 
 
 class DerivativesViews:
@@ -12,6 +12,9 @@ class DerivativesViews:
     @staticmethod
     def derivatives_futures_historical(  # noqa: PLR0912
         **kwargs,
-    ) -> Tuple[OpenBBFigure, Dict[str, Any]]:
+    ) -> Tuple["OpenBBFigure", Dict[str, Any]]:
         """Get Derivatives Price Historical Chart."""
+        # pylint: disable=import-outside-toplevel
+        from openbb_charting.charts.price_historical import price_historical
+
         return price_historical(**kwargs)
