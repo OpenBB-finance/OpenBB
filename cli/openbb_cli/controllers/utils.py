9d3fc854f880c6b56f41a6b7470f6c6ba8e12510
hjoaquim <henriquecjoaquim@gmail.com>
2024-06-26 13:25:14 +0100

    handling chart arguments aka unknown arguments

--- cli/openbb_cli/controllers/utils.py
+++ cli/openbb_cli/controllers/utils.py
@@ -977,29 +977,74 @@ def request(
     )
 
 
-def handle_obbject_display(obbject: OBBject, chart: bool = False, export: bool = False):
-    """Handle the display of the object.
+def parse_unknown_args_to_dict(unknown_args: Optional[List[str]]) -> Dict[str, str]:
+    """Parse unknown arguments to a dictionary."""
+    unknown_args_dict = {}
+    if unknown_args:
+        for idx, arg in enumerate(unknown_args):
+            if arg.startswith("--"):
+                if idx + 1 < len(unknown_args):
+                    try:
+                        unknown_args_dict[arg.replace("--", "")] = (
+                            eval(  # noqa: S307, E501 pylint: disable=eval-used
+                                unknown_args[idx + 1]
+                            )
+                        )
+                    except Exception:
+                        unknown_args_dict[arg] = unknown_args[idx + 1]
+                else:
+                    session.console.print(
+                        f"Missing value for argument {arg}. Skipping this argument."
+                    )
+    return unknown_args_dict
 
-    Parameters
-    ----------
-    chart : bool
-        Whether the object is a chart
-    """
+
+def handle_obbject_display(
+    obbject: OBBject,
+    chart: bool = False,
+    export: str = "",
+    sheet_name: str = "",
+    **kwargs,
+):
+    """Handle the display of an OBBject."""
+    df: pd.DataFrame = pd.DataFrame()
+    fig: Optional[OpenBBFigure] = None
     if chart:
         try:
             if obbject.chart:
-                obbject.show()
+                obbject.show(**kwargs)
             else:
-                obbject.charting.to_chart(render=True)
-            return
+                obbject.charting.to_chart(**kwargs)
+            if export:
+                fig = obbject.chart.fig
+                df = obbject.to_dataframe()
         except Exception as e:
             session.console.print(f"Failed to display chart: {e}")
+    else:
+        df = obbject.to_dataframe()
+        print_rich_table(
+            df=df,
+            show_index=True,
+            title=obbject.extra.get("command", ""),
+            export=bool(export),
+        )
+    if export and not df.empty:
+        if sheet_name and isinstance(sheet_name, list):
+            sheet_name = sheet_name[0]
 
-    title = obbject.extra.get("command", "")
-    df = obbject.to_dataframe()
-    print_rich_table(
-        df=df,
-        show_index=True,
-        title=title,
-        export=export,
-    )
+        func_name = (
+            obbject.extra.get("command", "")
+            .replace("/", "_")
+            .replace(" ", "_")
+            .replace("--", "_")
+        )
+        export_data(
+            export_type=",".join(export),
+            dir_path=os.path.dirname(os.path.abspath(__file__)),
+            func_name=func_name,
+            df=df,
+            sheet_name=sheet_name,
+            figure=fig,
+        )
+    elif export and df.empty:
+        session.console.print("[yellow]No data to export.[/yellow]")
