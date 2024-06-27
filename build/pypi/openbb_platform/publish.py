355475cf7a1120628dcf0cea0a64f39a89591edd
Diogo Sousa <montezdesousa@gmail.com>
2024-06-27 22:46:24 +0100

    Refactor publish.py

--- build/pypi/openbb_platform/publish.py
+++ build/pypi/openbb_platform/publish.py
@@ -5,15 +5,11 @@ import subprocess
 import sys
 from pathlib import Path
 
-PLATFORM_PATH = Path(__file__).parent.parent.parent.parent.resolve() / "openbb_platform"
+DIR_PLATFORM = Path(__file__).parent.parent.parent.parent.resolve() / "openbb_platform"
+DIR_CORE = ["core"]
+DIR_EXTENSIONS = ["extensions", "providers", "obbject_extensions"]
 
-CORE_PACKAGES = ["core"]
-EXTENSION_PACKAGES = ["extensions", "providers", "obbject_extensions"]
-
-CMD = [sys.executable, "-m", "poetry"]
-EXTENSION_DEPENDENCIES_UPDATE_CMD = ["add", "openbb-core=latest", "--lock"]
-VERSION_BUMP_CMD = ["version", "patch"]
-PUBLISH_CMD = ["publish", "--build"]
+CMD_POETRY = [sys.executable, "-m", "poetry"]
 
 
 def parse_args():
@@ -31,6 +27,13 @@ def parse_args():
         dest="extensions",
     )
     parser.add_argument(
+        "-o",
+        "--openbb",
+        action="store_true",
+        help="Publish openbb package.",
+        dest="openbb",
+    )
+    parser.add_argument(
         "--dry-run",
         action="store_true",
         help="Run the commands without actually publishing.",
@@ -40,50 +43,78 @@ def parse_args():
     return parser.parse_args()
 
 
-def update_extension_dependencies(path: Path):
-    """Update the extension dependencies"""
-    subprocess.run(
-        CMD + EXTENSION_DEPENDENCIES_UPDATE_CMD,  # noqa: S603
-        cwd=path.parent,
-        check=True,
-    )
-
-
-def bump_version(path: Path):
-    """Bump the version of the package"""
-    print(f"\n> {path.parent.stem}")  # noqa: T201
-    subprocess.run(CMD + VERSION_BUMP_CMD, cwd=path.parent, check=True)  # noqa: S603
-
-
-def publish(dry_run: bool = False, core: bool = False, extensions: bool = False):
+def publish(
+    dry_run: bool = False,
+    core: bool = False,
+    extensions: bool = False,
+    openbb: bool = False,
+):
     """Publish the Platform to PyPi with optional core or extensions."""
-    package_paths = []
+    package_directories = []
     if core:
-        print("Working with core packages...")  # noqa: T201
-        package_paths.extend(CORE_PACKAGES)
+        package_directories.extend(DIR_CORE)
     if extensions:
-        print("Working with extensions...")  # noqa: T201
-        package_paths.extend(EXTENSION_PACKAGES)
+        package_directories.extend(DIR_EXTENSIONS)
 
-    for sub_path in package_paths:
-        is_extension = sub_path in EXTENSION_PACKAGES
-
-        for path in sorted(PLATFORM_PATH.rglob(f"{sub_path}/**/pyproject.toml")):
+    for _dir in package_directories:
+        is_extension = _dir in DIR_EXTENSIONS
+        paths = sorted(DIR_PLATFORM.rglob(f"{_dir}/**/pyproject.toml"))
+        total = len(paths)
+        print(f"\n~~~ {_dir.upper()} ~~~")  # noqa: T201
+        for i, path in enumerate(paths):
+            print(f"\n🚀 {i+1}/{total} | {path.parent.stem}")  # noqa: T201
             try:
-                # Update dependencies
+                # Update openbb-core to latest
                 if is_extension and "devtools" not in str(path):
-                    update_extension_dependencies(path)
-                # Bump version
-                bump_version(path)
+                    subprocess.run(
+                        CMD_POETRY
+                        + ["add", "openbb-core=latest", "--lock"],  # noqa: S603
+                        cwd=path.parent,
+                        check=True,
+                    )
+                    print("")
+                # Bump toml version
+                subprocess.run(
+                    CMD_POETRY + ["version", "patch"], cwd=path.parent, check=True
+                )  # noqa: S603
                 # Publish (if not dry run)
                 if not dry_run:
                     subprocess.run(
-                        CMD + PUBLISH_CMD,  # noqa: S603
+                        CMD_POETRY + ["build"],  # noqa: S603  # ["publish", "--build"]
                         cwd=path.parent,
                         check=True,  # noqa: S603
                     )
+                print("✅ Success\n")
             except Exception as e:
-                print(f"Error publishing {path.parent}:\n{e}")  # noqa: T201
+                print(
+                    f"\n❌ Failed to publish {path.parent.stem}:\n\n{e}\n"
+                )  # noqa: T201
+
+    if openbb:
+        subprocess.run(
+            CMD_POETRY + ["self", "add", "poetry-plugin-up"],
+            check=True,
+        )
+        subprocess.run(
+            CMD_POETRY + ["up", "--latest"],
+            cwd=DIR_PLATFORM,
+            check=True,
+        )
+        subprocess.run(
+            ["pip", "install", "-U", "--editable", "."],
+            cwd=DIR_PLATFORM,
+            check=True,
+        )
+        subprocess.run(
+            [sys.executable, "-c", '"import openbb; openbb.build()"'],
+            cwd=DIR_PLATFORM,
+            check=True,
+        )
+        subprocess.run(
+            [sys.executable, "-c", '"import openbb"'],
+            cwd=DIR_PLATFORM,
+            check=True,
+        )
 
 
 if __name__ == "__main__":
@@ -93,18 +124,14 @@ if __name__ == "__main__":
     Also, please double check with `poetry config --list` if you're publishing to PyPI or TestPyPI.
     """
     args = parse_args()
-
+    if not args.dry_run:
+        msg += "\n\n🛑 You are NOT using the --dry-run flag!"
     res = input(f"{msg}\n\nDo you want to continue? [y/N] ")
 
     if res.lower() == "y":
-        publish(dry_run=args.dry_run, core=args.core, extensions=args.extensions)
-
-        openbb_package_msg = """
-        In order to publish the `openbb` package you need to manually update the
-        versions in the `pyproject.toml` file. Follow the steps below:
-        1. Bump version: `poetry version prerelease --next-phase`
-        2. Re-build the static assets that are bundled with the package
-        3. Publish: `poetry publish --build`
-        """
-
-        print(openbb_package_msg)  # noqa: T201
+        publish(
+            dry_run=args.dry_run,
+            core=args.core,
+            extensions=args.extensions,
+            openbb=args.openbb,
+        )
