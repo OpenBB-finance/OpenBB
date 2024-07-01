c5c78c6c1883e91d6c5b068bedc358a610ebd1f4
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2024-07-01 11:23:39 +0200

    lint

--- openbb_platform/core/openbb_core/app/static/package_builder.py
+++ openbb_platform/core/openbb_core/app/static/package_builder.py
@@ -1478,7 +1478,6 @@ class ReferenceGenerator:
                     expanded_types[field], is_required, "website"
                 )
                 field_type = f"Union[{field_type}, {expanded_type}]"
-
             cleaned_description = (
                 str(field_info.description)
                 .strip().replace("\n", " ").replace("  ", " ").replace('"', "'")
@@ -1505,7 +1504,6 @@ class ReferenceGenerator:
                     # Manually setting to List[<field_type>] for multiple items
                     # Should be removed if TYPE_EXPANSION is updated to include this
                     field_type = f"Union[{field_type}, List[{field_type}]]"
-
             default_value = "" if field_info.default is PydanticUndefined else field_info.default  # fmt: skip
 
             provider_field_params.append(
