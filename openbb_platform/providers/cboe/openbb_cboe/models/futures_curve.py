c8fd02c5a8d43acc9c7d9fcff196e234f8e51ddc
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-07-02 09:19:02 -0700

    merge cleanup

--- openbb_platform/providers/cboe/openbb_cboe/models/futures_curve.py
+++ openbb_platform/providers/cboe/openbb_cboe/models/futures_curve.py
@@ -2,9 +2,8 @@
 
 # pylint: disable=unused-argument
 
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Literal, Optional
 
-from openbb_core.app.model.abstract.error import OpenBBError
 from openbb_core.provider.abstract.fetcher import Fetcher
 from openbb_core.provider.standard_models.futures_curve import (
     FuturesCurveData,
