1152c9d71e9c69ac438586ae07279f4aedb22574
Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>
2024-07-23 11:22:33 +0200

    Windows Edits

--- build/conda/installer/post_install.bat
+++ build/conda/installer/post_install.bat
@@ -1,40 +1,34 @@
 @echo off
 echo Running post-installation environment setup.
-
 REM Full path to the Python executable inside the constructed environment
 SET PYTHON_EXEC="%PREFIX%\python.exe"
 SET LOG_FILE="%PREFIX%\post_install_log.txt"
 
-REM Function to add timestamp
-:log_with_timestamp
-    echo %date%_%time% %1 >> %LOG_FILE%
-    goto :eof
-
 REM Use the specific Python that comes bundled with the installer
 "%PYTHON_EXEC%" -m pip install -U openbb[all] openbb-cli openbb-platform-pro-backend >> "%LOG_FILE%" 2>&1
 IF ERRORLEVEL 1 (
-    call :log_with_timestamp "Error during post-installation: pip install failed."
+    echo %date% %time% "Error during post-installation: pip install failed." >> %LOG_FILE%
     exit /b 1
 ) ELSE (
-    call :log_with_timestamp "pip install completed successfully."
+    echo %date% %time% "pip install completed successfully." >> %LOG_FILE%
 )
 
 REM Build OpenBB's python interface
 "%PYTHON_EXEC%" -c "import openbb; openbb.build()" >> "%LOG_FILE%" 2>&1
 IF ERRORLEVEL 1 (
-    call :log_with_timestamp "Error during post-installation: building OpenBB's python interface failed."
+    echo %date% %time% "Error during post-installation: building OpenBB's python interface failed."  >> %LOG_FILE%
     exit /b 1
 ) ELSE (
-    call :log_with_timestamp "OpenBB's python interface built successfully."
+    echo %date% %time% "OpenBB's python interface built successfully."  >> %LOG_FILE%
 )
 
 REM Create shortcuts using the VBS script
 cscript "%PREFIX%\assets\create_shortcut.vbs" >> "%LOG_FILE%" 2>&1
 IF ERRORLEVEL 1 (
-    call :log_with_timestamp "Error during post-installation: creating shortcuts failed."
+    echo %date% %time% "Error during post-installation: creating shortcuts failed."  >> %LOG_FILE%
     exit /b 1
 ) ELSE (
-    call :log_with_timestamp "Shortcuts created successfully."
+    echo %date% %time% "Shortcuts created successfully."  >> %LOG_FILE%
 )
 
 echo Post-installation steps completed successfully.
