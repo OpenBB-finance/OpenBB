f361cae7f9b6c65641b7f959c9f293b9765d865d
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-08-03 14:16:49 -0700

    linting

--- openbb_platform/providers/multpl/openbb_multpl/models/sp500_multiples.py
+++ openbb_platform/providers/multpl/openbb_multpl/models/sp500_multiples.py
@@ -74,14 +74,10 @@ class MultplSP500MultiplesQueryParams(SP500MultiplesQueryParams):
         new_values: List = []
         for s in series:
             if s not in URL_DICT:
-                raise OpenBBError(
-                    f"{s} is not a valid `series_name`. Choices are: \n{sorted(list(URL_DICT))}\n"
-                )
+                raise OpenBBError(f"{s} is not a valid `series_name`. Choices are: \n{sorted(list(URL_DICT))}\n")
             new_values.append(s)
         if not new_values:
-            raise OpenBBError(
-                f"No valid series names provided. Choices are: \n{sorted(list(URL_DICT))}\n"
-            )
+            raise OpenBBError(f"No valid series names provided. Choices are: \n{sorted(list(URL_DICT))}\n")
         return ",".join(new_values)
 
 
@@ -133,7 +129,7 @@ class MultplSP500MultiplesFetcher(
             """Callback for response."""
             res = await amake_request(url, response_callback=response_callback)
             if res:
-                df = read_html(StringIO(res))[0]
+                df = read_html(StringIO(res))[0]  # type: ignore
                 if not df.empty:
                     df["Date"] = to_datetime(df["Date"]).dt.date
                     df = df.sort_values("Date").reset_index(drop=True)
@@ -142,11 +138,7 @@ class MultplSP500MultiplesFetcher(
                     if query.end_date:
                         df = df[df["Date"] <= query.end_date]
                     df["Value"] = df["Value"].apply(
-                        lambda x: (
-                            x.strip().replace("† ", "").replace("%", "")
-                            if isinstance(x, str)
-                            else x
-                        )
+                        lambda x: (x.strip().replace("† ", "").replace("%", "") if isinstance(x, str) else x)
                     )
                     df["name"] = series
                     if "growth" in series or "yield" in series:
