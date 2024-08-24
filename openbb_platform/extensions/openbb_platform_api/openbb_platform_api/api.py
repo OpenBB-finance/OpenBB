001dcea467fa29218897d632269929459fb3bf10
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-08-24 09:54:48 -0700

    grammar police

--- openbb_platform/extensions/openbb_platform_api/openbb_platform_api/api.py
+++ openbb_platform/extensions/openbb_platform_api/openbb_platform_api/api.py
@@ -370,7 +370,7 @@ def main():
 
     @app.get("/")
     async def get_root():
-        """The API root response."""
+        """Root response and welcome message."""
         return JSONResponse(
             content="Welcome to the OpenBB Platform API."
             + " Learn how to connect to Pro in docs.openbb.co/pro/data-connectors,"
