bcb7279f2f7f7684a7a214ad4bc88b72cb18cc1e
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-09-18 15:16:11 -0700

    fix test?

--- openbb_platform/extensions/commodity/openbb_commodity/commodity_router.py
+++ openbb_platform/extensions/commodity/openbb_commodity/commodity_router.py
@@ -15,31 +15,31 @@ router = Router(prefix="", description="Commodity market data.")
 
 
 # pylint: disable=unused-argument
-@router.command(
-    model="LbmaFixing",
-    examples=[
-        APIEx(parameters={"provider": "nasdaq"}),
-        APIEx(
-            description="Get the daily LBMA fixing prices for silver in 2023.",
-            parameters={
-                "asset": "silver",
-                "start_date": "2023-01-01",
-                "end_date": "2023-12-31",
-                "transform": "rdiff",
-                "collapse": "monthly",
-                "provider": "nasdaq",
-            },
-        ),
-    ],
-)
-async def lbma_fixing(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> OBBject:
-    """Daily LBMA Fixing Prices in USD/EUR/GBP."""
-    return await OBBject.from_query(Query(**locals()))
+# @router.command(
+#    model="LbmaFixing",
+#    examples=[
+#        APIEx(parameters={"provider": "nasdaq"}),
+#        APIEx(
+#            description="Get the daily LBMA fixing prices for silver in 2023.",
+#            parameters={
+#                "asset": "silver",
+#                "start_date": "2023-01-01",
+#                "end_date": "2023-12-31",
+#                "transform": "rdiff",
+#                "collapse": "monthly",
+#                "provider": "nasdaq",
+#            },
+#        ),
+#    ],
+# )
+# async def lbma_fixing(
+#    cc: CommandContext,
+#    provider_choices: ProviderChoices,
+#    standard_params: StandardParams,
+#    extra_params: ExtraParams,
+# ) -> OBBject:
+#    """Daily LBMA Fixing Prices in USD/EUR/GBP."""
+#    return await OBBject.from_query(Query(**locals()))
 
 
 @router.command(
