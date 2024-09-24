786584652ec4870e1656c5ef963c87b6e37b4224
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-09-24 14:39:15 -0700

    add an openbb-build script to openbb-core package

---
+++ openbb_platform/core/openbb_core/build.py
@@ -0,0 +1,38 @@
+"""Script to build the OpenBB platform static assets."""
+
+# flake8: noqa: S603
+
+import subprocess
+import sys
+
+
+def main():
+    """Build the OpenBB platform static assets."""
+    message = "\nOpenBB build script not found, installing from PyPI...\n"
+    try:
+        from openbb import build
+
+        build()
+    except (
+        ImportError,
+        ModuleNotFoundError,
+        AttributeError,
+    ) as e:
+        print(message)  # noqa: T201
+        subprocess.run(
+            [sys.executable, "-m", "pip", "install", "openbb", "--no-deps"],
+            check=True,
+        )
+        try:
+            subprocess.run(
+                [sys.executable, "-c", "import openbb; openbb.build()"],
+                check=True,
+            )
+        except (subprocess.CalledProcessError, AttributeError):
+            raise RuntimeError(
+                "Failed to find the OpenBB build script. Install with `pip install openbb --no-deps`"
+            ) from e
+
+
+if __name__ == "__main__":
+    main()
