b9de848bc311487359b1a885ce87bc28231b39de
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-10-03 12:47:44 -0700

    [Feature] Add EIA Daily Spot Prices Data via FRED (#6677)
    * add EIA daily spot prices data via FRED
    
    * line too long
    
    * comment out the test skipped test
    
    * fix test?
    
    * delete commented out sections
    
    * unused argument
    
    * name things like Minh says
    
    ---------
    
    Co-authored-by: Igor Radovanovic <74266147+IgorWounds@users.noreply.github.com>

--- openbb_platform/extensions/commodity/openbb_commodity/commodity_router.py
+++ openbb_platform/extensions/commodity/openbb_commodity/commodity_router.py
@@ -1,5 +1,8 @@
 """The Commodity router."""
 
+# pylint: disable=unused-argument,unused-import
+# flake8: noqa: F401
+
 from openbb_core.app.model.command_context import CommandContext
 from openbb_core.app.model.example import APIEx
 from openbb_core.app.model.obbject import OBBject
@@ -11,32 +14,9 @@ from openbb_core.app.provider_interface import (
 from openbb_core.app.query import Query
 from openbb_core.app.router import Router
 
+from openbb_commodity.price.price_router import router as price_router
+
 router = Router(prefix="", description="Commodity market data.")
 
 
-# pylint: disable=unused-argument
-@router.command(
-    model="LbmaFixing",
-    examples=[
-        APIEx(parameters={"provider": "nasdaq"}),
-        APIEx(
-            description="Get the daily LBMA fixing prices for silver in 2023.",
-            parameters={
-                "asset": "silver",
-                "start_date": "2023-01-01",
-                "end_date": "2023-12-31",
-                "transform": "rdiff",
-                "collapse": "monthly",
-                "provider": "nasdaq",
-            },
-        ),
-    ],
-)
-async def lbma_fixing(
-    cc: CommandContext,
-    provider_choices: ProviderChoices,
-    standard_params: StandardParams,
-    extra_params: ExtraParams,
-) -> OBBject:
-    """Daily LBMA Fixing Prices in USD/EUR/GBP."""
-    return await OBBject.from_query(Query(**locals()))
+router.include_router(price_router)
