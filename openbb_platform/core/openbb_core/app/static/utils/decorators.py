dda5faa5b5cb94951290aa12b3065bd28ad2e1b7
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-10-17 22:36:34 -0700

    more linting

--- openbb_platform/core/openbb_core/app/static/utils/decorators.py
+++ openbb_platform/core/openbb_core/app/static/utils/decorators.py
@@ -100,6 +100,7 @@ def exception_handler(func: Callable[P, R]) -> Callable[P, R]:
                 raise OpenBBError(
                     f"\n[Unexpected Error] -> {e.__class__.__name__} -> {str(e.args[0] or e.args)}"
                 ).with_traceback(tb) from None
-            return
+
+        return None
 
     return wrapper
