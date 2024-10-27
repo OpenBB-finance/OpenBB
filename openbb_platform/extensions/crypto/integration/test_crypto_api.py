4f96f4d66abc13bf9ef3d743f814f3c50e7015a8
Pratyush Shukla <ps4534@nyu.edu>
2024-10-27 19:43:34 +0530

    add tests for defillama endpoints added to `/crypto` command

--- openbb_platform/extensions/crypto/integration/test_crypto_api.py
+++ openbb_platform/extensions/crypto/integration/test_crypto_api.py
@@ -122,5 +122,578 @@ def test_crypto_price_historical(params, headers):
     query_str = get_querystring(params, [])
     url = f"http://0.0.0.0:8000/api/v1/crypto/price/historical?{query_str}"
     result = requests.get(url, headers=headers, timeout=10)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {
+            "provider": "defillama",
+            "chain": "ethereum",
+            "timestamp": "2024-01-01",
+        },
+        {
+            "provider": "defillama",
+            "chain": "ethereum",
+            "timestamp": "2024-01-01T12:12:12",
+        },
+        {
+            "provider": "defillama",
+            "chain": "ethereum",
+            "timestamp": "1729957601",
+        },
+        {
+            "provider": "defillama",
+            "chain": "ethereum",
+            "timestamp": 1729957601,
+        },
+    ],
+)
+@pytest.mark.integration
+def test_crypto_coins_block(params, headers):
+    """Test the coins block timestamp endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/coins/block?{query_str}"
+    result = requests.get(url, headers=headers, timeout=10)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "search_width": "1D",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "search_width": "4H",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "search_width": "4m",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "search_width": "1W",
+        },
+    ],
+)
+@pytest.mark.integration
+def test_crypto_coins_current(params, headers):
+    """Test the coins current price endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/coins/current?{query_str}"
+    result = requests.get(url, headers=headers, timeout=10)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+        },
+    ],
+)
+@pytest.mark.integration
+def test_crypto_coins_first(params, headers):
+    """Test the coins first record endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/coins/first?{query_str}"
+    result = requests.get(url, headers=headers, timeout=10)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "timestamp": "2024-01-01",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "timestamp": "2024-01-01T12:12:12",
+            "period": "1W",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "timestamp": "1729957601",
+            "period": "7D",
+            "look_forward": True,
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "timestamp": 1729957601,
+            "period": "24m",
+            "look_forward": True,
+        },
+    ],
+)
+@pytest.mark.integration
+def test_crypto_coins_change(params, headers):
+    """Test the coins change endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/coins/change?{query_str}"
+    result = requests.get(url, headers=headers, timeout=10)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "start_date": "2024-09-01",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "end_date": "2024-10-01",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "start_date": 1725129000,
+            "span": 10,
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "start_date": 1725129000,
+            "span": 100,
+            "period": "1D",
+            "search_width": "8h",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "end_date": 1727721000,
+            "span": 10,
+            "period": "1W",
+            "search_width": "1D",
+        },
+    ],
+)
+@pytest.mark.integration
+def test_crypto_coins_chart(params, headers):
+    """Test the coins chart endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/coins/chart?{query_str}"
+    result = requests.get(url, headers=headers, timeout=20)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "timestamp": "2024-01-01",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "timestamp": "2024-01-01T12:12:12",
+            "search_width": "1W",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "timestamp": "1729957601",
+            "search_width": "7D",
+        },
+        {
+            "provider": "defillama",
+            "token": "coingecko:ethereum",
+            "timestamp": 1729957601,
+            "search_width": "4m",
+        },
+    ],
+)
+@pytest.mark.integration
+def test_crypto_coins_historical(params, headers):
+    """Test the coins historical price endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/coins/historical?{query_str}"
+    result = requests.get(url, headers=headers, timeout=10)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+        {"provider": "defillama", "all": True},
+        {"provider": "defillama", "chain": "ethereum"},
+        {"provider": "defillama", "chain": "ethereum", "all": True},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_fees_overview(params, headers):
+    """Test the fees overview endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/fees/overview?{query_str}"
+    result = requests.get(url, headers=headers, timeout=10)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama", "protocol": "litecoin"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_fees_summary(params, headers):
+    """Test the fees summary endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/fees/summary?{query_str}"
+    result = requests.get(url, headers=headers, timeout=20)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+        {"provider": "defillama", "all": True},
+        {"provider": "defillama", "chain": "ethereum"},
+        {"provider": "defillama", "chain": "ethereum", "all": True},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_revenue_overview(params, headers):
+    """Test the revenue overview endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/revenue/overview?{query_str}"
+    result = requests.get(url, headers=headers, timeout=10)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama", "protocol": "litecoin"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_revenue_summary(params, headers):
+    """Test the revenue summary endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/revenue/summary?{query_str}"
+    result = requests.get(url, headers=headers, timeout=20)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+        {"provider": "defillama", "include_prices": True},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_stablecoins_list(params, headers):
+    """Test the stablecoins list endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/stablecoins/list?{query_str}"
+    result = requests.get(url, headers=headers, timeout=20)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_stablecoins_current(params, headers):
+    """Test the stablecoins current endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/stablecoins/current?{query_str}"
+    result = requests.get(url, headers=headers, timeout=20)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_stablecoins_historical(params, headers):
+    """Test the stablecoins historical endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/stablecoins/historical?{query_str}"
+    result = requests.get(url, headers=headers, timeout=20)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama", "stablecoin": "1"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_stablecoins_distribution(params, headers):
+    """Test the stablecoins distribution endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/stablecoins/distribution?{query_str}"
+    result = requests.get(url, headers=headers, timeout=20)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+        {"provider": "defillama", "stablecoin": "1"},
+        {"provider": "defillama", "chain": "ethereum"},
+        {"provider": "defillama", "stablecoin": "1", "chain": "ethereum"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_stablecoins_charts(params, headers):
+    """Test the stablecoins charts endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/stablecoins/charts?{query_str}"
+    result = requests.get(url, headers=headers, timeout=30)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_tvl_chains(params, headers):
+    """Test the TVL chains endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/tvl/chains?{query_str}"
+    result = requests.get(url, headers=headers, timeout=20)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+        {"provider": "defillama", "symbol": "uniswap"},
+        {"provider": "defillama", "symbol_type": "chain"},
+        {"provider": "defillama", "symbol": "ethereum", "symbol_type": "chain"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_tvl_historical(params, headers):
+    """Test the TVL historical endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/tvl/historical?{query_str}"
+    result = requests.get(url, headers=headers, timeout=30)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama", "symbol": "uniswap"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_tvl_current(params, headers):
+    """Test the TVL current endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/tvl/current?{query_str}"
+    result = requests.get(url, headers=headers, timeout=20)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+        {"provider": "defillama", "all": True},
+        {"provider": "defillama", "is_options": True},
+        {"provider": "defillama", "chain": "ethereum"},
+        {"provider": "defillama", "chain": "ethereum", "is_options": True},
+        {
+            "provider": "defillama",
+            "chain": "ethereum",
+            "is_options": True,
+            "volume_type": "notional",
+        },
+    ],
+)
+@pytest.mark.integration
+def test_crypto_volumes_overview(params, headers):
+    """Test the volumes overview endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/volumes/overview?{query_str}"
+    result = requests.get(url, headers=headers, timeout=30)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama", "protocol": "uniswap"},
+        {
+            "provider": "defillama",
+            "protocol": "pancakeswap-options",
+            "is_options": True,
+        },
+        {"provider": "defillama", "protocol": "uniswap", "volume_type": "notional"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_volumes_summary(params, headers):
+    """Test the volumes summary endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/volumes/summary?{query_str}"
+    result = requests.get(url, headers=headers, timeout=30)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {"provider": "defillama"},
+    ],
+)
+@pytest.mark.integration
+def test_crypto_yields_pools(params, headers):
+    """Test the yields pools endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/yields/pools?{query_str}"
+    result = requests.get(url, headers=headers, timeout=30)
+
+    assert isinstance(result, requests.Response)
+    assert result.status_code == 200
+
+
+@parametrize(
+    "params",
+    [
+        {
+            "provider": "defillama",
+            "pool_id": "747c1d2a-c668-4682-b9f9-296708a3dd90",
+        },
+    ],
+)
+@pytest.mark.integration
+def test_crypto_yields_historical(params, headers):
+    """Test the yields historical endpoint."""
+    params = {p: v for p, v in params.items() if v}
+
+    query_str = get_querystring(params, [])
+    url = f"http://0.0.0.0:8000/api/v1/crypto/yields/historical?{query_str}"
+    result = requests.get(url, headers=headers, timeout=30)
+
     assert isinstance(result, requests.Response)
     assert result.status_code == 200
--- openbb_platform/extensions/crypto/integration/test_crypto_python.py
+++ openbb_platform/extensions/crypto/integration/test_crypto_python.py
@@ -115,3 +115,323 @@ def test_crypto_price_historical(params, obb):
     assert result
     assert isinstance(result, OBBject)
     assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+        ({"provider": "defillama", "all": True}),
+        ({"provider": "defillama", "chain": "ethereum"}),
+        ({"provider": "defillama", "chain": "ethereum", "all": True}),
+    ],
+)
+@pytest.mark.integration
+def test_fees_overview(params, obb):
+    """Test fees overview endpoint."""
+    result = obb.crypto.fees.overview(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama", "protocol": "litecoin"}),
+    ],
+)
+@pytest.mark.integration
+def test_fees_summary(params, obb):
+    """Test fees summary endpoint."""
+    result = obb.crypto.fees.summary(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+    ],
+)
+@pytest.mark.integration
+def test_tvl_chains(params, obb):
+    """Test TVL chains endpoint."""
+    result = obb.crypto.tvl.chains(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+        ({"provider": "defillama", "symbol": "uniswap"}),
+        ({"provider": "defillama", "symbol_type": "chain"}),
+        ({"provider": "defillama", "symbol": "ethereum", "symbol_type": "chain"}),
+    ],
+)
+@pytest.mark.integration
+def test_tvl_historical(params, obb):
+    """Test TVL historical endpoint."""
+    result = obb.crypto.tvl.historical(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama", "symbol": "uniswap"}),
+    ],
+)
+@pytest.mark.integration
+def test_tvl_current(params, obb):
+    """Test TVL current endpoint."""
+    result = obb.crypto.tvl.current(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+    ],
+)
+@pytest.mark.integration
+def test_yields_pools(params, obb):
+    """Test yields pools endpoint."""
+    result = obb.crypto.yields.pools(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        (
+            {
+                "provider": "defillama",
+                "pool_id": "747c1d2a-c668-4682-b9f9-296708a3dd90",
+            }
+        ),
+    ],
+)
+@pytest.mark.integration
+def test_yields_historical(params, obb):
+    """Test yields historical endpoint."""
+    result = obb.crypto.yields.historical(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+        ({"provider": "defillama", "include_prices": True}),
+    ],
+)
+@pytest.mark.integration
+def test_stablecoins_list(params, obb):
+    """Test stablecoins list endpoint."""
+    result = obb.crypto.stablecoins.list(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+    ],
+)
+@pytest.mark.integration
+def test_stablecoins_current(params, obb):
+    """Test stablecoins current endpoint."""
+    result = obb.crypto.stablecoins.current(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+    ],
+)
+@pytest.mark.integration
+def test_stablecoins_historical(params, obb):
+    """Test stablecoins historical endpoint."""
+    result = obb.crypto.stablecoins.historical(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama", "stablecoin": "1"}),
+    ],
+)
+@pytest.mark.integration
+def test_stablecoins_distribution(params, obb):
+    """Test stablecoins distribution endpoint."""
+    result = obb.crypto.stablecoins.distribution(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+        ({"provider": "defillama", "stablecoin": "1"}),
+        ({"provider": "defillama", "chain": "ethereum"}),
+        ({"provider": "defillama", "stablecoin": "1", "chain": "ethereum"}),
+    ],
+)
+@pytest.mark.integration
+def test_stablecoins_charts(params, obb):
+    """Test stablecoins charts endpoint."""
+    result = obb.crypto.stablecoins.charts(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        (
+            {
+                "provider": "defillama",
+                "token": "coingecko:ethereum",
+                "search_width": "1D",
+            }
+        ),
+        (
+            {
+                "provider": "defillama",
+                "token": "coingecko:ethereum",
+                "search_width": "4H",
+            }
+        ),
+        (
+            {
+                "provider": "defillama",
+                "token": "coingecko:ethereum",
+                "search_width": "4m",
+            }
+        ),
+        (
+            {
+                "provider": "defillama",
+                "token": "coingecko:ethereum",
+                "search_width": "1W",
+            }
+        ),
+    ],
+)
+@pytest.mark.integration
+def test_coins_current(params, obb):
+    """Test coins current endpoint."""
+    result = obb.crypto.coins.current(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+        ({"provider": "defillama", "all": True}),
+        ({"provider": "defillama", "is_options": True}),
+        ({"provider": "defillama", "chain": "ethereum"}),
+        ({"provider": "defillama", "chain": "ethereum", "is_options": True}),
+        (
+            {
+                "provider": "defillama",
+                "chain": "ethereum",
+                "is_options": True,
+                "volume_type": "notional",
+            }
+        ),
+    ],
+)
+@pytest.mark.integration
+def test_volumes_overview(params, obb):
+    """Test volumes overview endpoint."""
+    result = obb.crypto.volumes.overview(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama", "protocol": "litecoin"}),
+        (
+            {
+                "provider": "defillama",
+                "protocol": "pancakeswap-options",
+                "is_options": True,
+            }
+        ),
+        ({"provider": "defillama", "protocol": "uniswap", "volume_type": "notional"}),
+    ],
+)
+@pytest.mark.integration
+def test_volumes_summary(params, obb):
+    """Test volumes summary endpoint."""
+    result = obb.crypto.volumes.summary(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama"}),
+        ({"provider": "defillama", "all": True}),
+        ({"provider": "defillama", "chain": "ethereum"}),
+        ({"provider": "defillama", "chain": "ethereum", "all": True}),
+    ],
+)
+@pytest.mark.integration
+def test_revenue_overview(params, obb):
+    """Test revenue overview endpoint."""
+    result = obb.crypto.revenue.overview(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
+
+
+@parametrize(
+    "params",
+    [
+        ({"provider": "defillama", "protocol": "litecoin"}),
+    ],
+)
+@pytest.mark.integration
+def test_revenue_summary(params, obb):
+    """Test revenue summary endpoint."""
+    result = obb.crypto.revenue.summary(**params)
+    assert result
+    assert isinstance(result, OBBject)
+    assert len(result.results) > 0
