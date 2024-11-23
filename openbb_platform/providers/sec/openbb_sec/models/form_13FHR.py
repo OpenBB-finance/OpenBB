a19c0e39fd50fcb8bfbffee31dc994483b8cc58f
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-11-22 23:59:53 -0800

    fix parsing error

--- openbb_platform/providers/sec/openbb_sec/models/form_13FHR.py
+++ openbb_platform/providers/sec/openbb_sec/models/form_13FHR.py
@@ -2,7 +2,7 @@
 
 # pylint: disable =unused-argument
 
-from typing import Any, Dict, List, Optional
+from typing import Any, Optional
 
 from openbb_core.provider.abstract.fetcher import Fetcher
 from openbb_core.provider.standard_models.form_13FHR import (
@@ -35,61 +35,73 @@ class SecForm13FHRData(Form13FHRData):
     )
 
 
-class SecForm13FHRFetcher(Fetcher[SecForm13FHRQueryParams, List[SecForm13FHRData]]):
+class SecForm13FHRFetcher(Fetcher[SecForm13FHRQueryParams, list[SecForm13FHRData]]):
     """SEC Form 13F-HR Fetcher."""
 
     @staticmethod
-    def transform_query(params: Dict[str, Any]) -> SecForm13FHRQueryParams:
+    def transform_query(params: dict[str, Any]) -> SecForm13FHRQueryParams:
         """Transform the query."""
         return SecForm13FHRQueryParams(**params)
 
     @staticmethod
     async def aextract_data(
         query: SecForm13FHRQueryParams,
-        credentials: Optional[Dict[str, str]],
+        credentials: Optional[dict[str, str]],
         **kwargs: Any,
-    ) -> List[Dict]:
+    ) -> list[dict]:
         """Return the raw data from the SEC endpoint."""
         # pylint: disable=import-outside-toplevel
         import asyncio  # noqa
-        from openbb_sec.utils import parse_13f  # noqa
+        from openbb_core.app.model.abstract.error import OpenBBError
+        from openbb_core.provider.utils.errors import EmptyDataError
+        from openbb_sec.utils import parse_13f
 
         symbol = query.symbol
         urls = []
         cik = symbol.isnumeric()
-        filings = (
-            await parse_13f.get_13f_candidates(symbol=symbol)
-            if cik is False
-            else await parse_13f.get_13f_candidates(cik=symbol)
-        )
-        if query.limit and query.date is None:
-            urls = filings.iloc[: query.limit].to_list()
-        if query.date is not None:
-            date = parse_13f.date_to_quarter_end(query.date.strftime("%Y-%m-%d"))
-            filings.index = filings.index.astype(str)
-            urls = [filings.loc[date]]
+        try:
+            filings = (
+                await parse_13f.get_13f_candidates(symbol=symbol)
+                if cik is False
+                else await parse_13f.get_13f_candidates(cik=symbol)
+            )
+            if query.limit and query.date is None:
+                urls = filings.iloc[: query.limit].to_list()
+            if query.date is not None:
+                date = parse_13f.date_to_quarter_end(query.date.strftime("%Y-%m-%d"))
+                filings.index = filings.index.astype(str)
+                urls = [filings.loc[date]]
 
-        results = []
+            results: list = []
 
-        async def get_filing(url):
-            """Get a single 13F-HR filing and parse it."""
+            async def get_filing(url):
+                """Get a single 13F-HR filing and parse it."""
+                data = await parse_13f.parse_13f_hr(url)
 
-            data = await parse_13f.parse_13f_hr(url)
+                if len(data) > 0:
+                    results.extend(data)
 
-            if len(data) > 0:
-                results.extend(data.to_dict("records"))
+            await asyncio.gather(*[get_filing(url) for url in urls])
 
-        await asyncio.gather(*[get_filing(url) for url in urls])
+            if not results:
+                raise EmptyDataError("No data was returned with the given parameters.")
 
-        return sorted(
-            results, key=lambda d: [d["period_ending"], d["weight"]], reverse=True
-        )
+            return results
+        except OpenBBError as e:
+            raise e from e
 
     @staticmethod
     def transform_data(
         query: SecForm13FHRQueryParams,
-        data: List[Dict],
+        data: list[dict],
         **kwargs: Any,
-    ) -> List[SecForm13FHRData]:
+    ) -> list[SecForm13FHRData]:
         """Transform the data."""
-        return [SecForm13FHRData.model_validate(d) for d in data]
+        return [
+            SecForm13FHRData.model_validate(d)
+            for d in sorted(
+                data,
+                key=lambda d: [d["period_ending"], d["weight"]],
+                reverse=True,
+            )
+        ]
