7aa7e09288838f3e81c3ddf6f6b3aaacbaec8c5b
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-11-25 10:14:38 -0800

    [Feature] Add `yfinance` To `equity.screener` (#6949)
    * add yfinance to equity.screener
    
    * add limit param with default of 200
    
    * trailing comma

--- openbb_platform/extensions/equity/integration/test_equity_python.py
+++ openbb_platform/extensions/equity/integration/test_equity_python.py
@@ -1407,6 +1407,24 @@ def test_equity_search(params, obb):
                 "provider": "finviz",
             }
         ),
+        (
+            {
+                "country": "us",
+                "sector": "consumer_cyclical",
+                "industry": "auto_manufacturers",
+                "exchange": None,
+                "mktcap_min": 60000000000,
+                "mktcap_max": None,
+                "price_min": 10,
+                "price_max": None,
+                "volume_min": 5000000,
+                "volume_max": None,
+                "beta_min": None,
+                "beta_max": None,
+                "provider": "yfinance",
+                "limit": 200,
+            }
+        ),
     ],
 )
 @pytest.mark.integration
