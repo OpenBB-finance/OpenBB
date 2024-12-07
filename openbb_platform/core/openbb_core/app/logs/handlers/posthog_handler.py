808d93ba1a76221897848c1c00ee695443f2cd23
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-12-06 18:35:16 -0800

    Move setting session to PosthogHandler init

--- openbb_platform/core/openbb_core/app/logs/handlers/posthog_handler.py
+++ openbb_platform/core/openbb_core/app/logs/handlers/posthog_handler.py
@@ -26,12 +26,17 @@ class PosthogHandler(logging.Handler):
 
     def __init__(self, settings: LoggingSettings):
         """Initialize Posthog Handler."""
+        # pylint: disable=import-outside-toplevel
+        from openbb_core.provider.utils.helpers import get_requests_session
 
         super().__init__()
         self._settings = settings
         self.logged_in = False
         posthog.api_key = "phc_6FXLqu4uW9yxfyN8DpPdgzCdlYXOmIWdMGh6GnBgJLX"  # pragma: allowlist secret  # noqa
         posthog.host = "https://app.posthog.com"  # noqa
+        posthog.request._session = (  # pylint: disable=protected-access
+            get_requests_session()
+        )
 
     @property
     def settings(self) -> LoggingSettings:
@@ -94,12 +99,8 @@ class PosthogHandler(logging.Handler):
     def send(self, record: logging.LogRecord):
         """Send log record to Posthog."""
         # pylint: disable=import-outside-toplevel
-        import re  # noqa
-        from openbb_core.provider.utils.helpers import get_requests_session
+        import re
 
-        posthog.request._session = (  # pylint: disable=protected-access
-            get_requests_session()
-        )
         level_name = logging.getLevelName(record.levelno)
         log_line = FormatterWithExceptions.filter_log_line(text=record.getMessage())
 
