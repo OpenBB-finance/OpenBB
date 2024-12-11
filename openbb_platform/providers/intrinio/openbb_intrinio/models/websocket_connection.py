58ece30f8ad5516b3e74feefacc9891d086e1ffc
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2024-12-11 15:04:08 -0800

    pylint..

--- openbb_platform/providers/intrinio/openbb_intrinio/models/websocket_connection.py
+++ openbb_platform/providers/intrinio/openbb_intrinio/models/websocket_connection.py
@@ -129,7 +129,9 @@ class IntrinioWebSocketData(WebSocketData):
             new_conditions = []
             conditions = conditions.replace(" ", "")
 
-            for char in range(len(conditions)):  # pylint: C0200
+            for char in range(  # pylint: disable=consider-using-enumerate
+                len(conditions)
+            ):
                 if trade_type == "trade":
                     new_conditions.append(
                         TRADE_CONDITIONS.get(conditions[char], conditions[char])
