b3e1d19d4bf073a4d4513ee13151a28a0b1961e2
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2025-02-20 22:18:25 -0800

    docstsring

--- openbb_platform/extensions/platform_api/openbb_platform_api/reponse_models.py
+++ openbb_platform/extensions/platform_api/openbb_platform_api/reponse_models.py
@@ -8,7 +8,25 @@ from pydantic import ConfigDict, Field, model_serializer, model_validator
 
 
 class MetricResponseModel(Data):
-    """Metric Widget Response Model. Supply a label, value, and optional delta."""
+    """
+    Metric Widget Response Model.
+
+    Supply a label, value, and optional delta.
+
+    Fields
+    ------
+    label : str
+        The label to display in the metric widget.
+    value : int, float, or str
+        The value to display in the metric widget.
+    delta : int, float, or str
+        The, optional, delta value to display in the metric widget.
+
+    Returns
+    -------
+    object
+        Object with the label, value, and optional delta value.
+    """
 
     model_config = ConfigDict(
         extra="ignore",
@@ -36,7 +54,30 @@ class MetricResponseModel(Data):
 
 
 class PdfResponseModel(Data):
-    """PDF Widget Response Model. Supply either, a URL reference or content, and an optional filename."""
+    """
+    PDF Widget Response Model.
+
+    Supply the url_reference or content, and an optional filename.
+
+    Fields
+    ------
+    filename : str
+        The filename of the PDF content.
+    content : str or bytes
+        The PDF content to display in the PDF widget.
+    url_reference : str
+        The URL reference to the PDF
+
+    Returns
+    -------
+    object
+        Object with the PDF content serialized as a Base64 encoded string.
+
+    Raises
+    ------
+    ValueError
+        If neither 'content' or 'url_reference' is provided, or an invalid URL reference is provided.
+    """
 
     model_config = ConfigDict(
         extra="ignore",
