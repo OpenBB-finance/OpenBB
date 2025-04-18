6e86212b0bbc6591723d567e31495db58161e807
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2025-04-18 15:13:38 -0700

    move import openbb to subprocess to monitor stdout

--- openbb_platform/core/openbb_core/build.py
+++ openbb_platform/core/openbb_core/build.py
@@ -2,37 +2,77 @@
 
 # flake8: noqa: S603
 # pylint: disable=import-outside-toplevel,unused-import
-
+import logging
 import subprocess
 import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 def main():
     """Build the OpenBB platform static assets."""
     try:
-        import openbb  # noqa
-    except (
-        ImportError,
-        ModuleNotFoundError,
-        AttributeError,
-    ):
-        print(  # noqa: T201
+        logger.info("Attempting to import the OpenBB package...\n")
+        # Try importing openbb in a subprocess and capture output
+        result = subprocess.run(
+            [sys.executable, "-c", "import openbb"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        logger.info(result.stdout)
+        building_found = any(
+            line.startswith("Building") for line in result.stdout.splitlines()
+        )
+
+        if result.returncode != 0:
+            raise ModuleNotFoundError(result.stderr)
+
+    except ModuleNotFoundError:
+        logger.info(
             "\nOpenBB build script not found, installing from PyPI...\n",
         )
         subprocess.run(
             [sys.executable, "-m", "pip", "install", "openbb", "--no-deps"],
             check=True,
         )
-
         try:
-            subprocess.run(
-                [sys.executable, "-c", "import openbb; openbb.build()"],
-                check=True,
+            result = logger.info.run(
+                [
+                    sys.executable,
+                    "-c",
+                    "import openbb;openbb.build()",
+                ],
+                capture_output=True,
+                text=True,
+            )
+            logger.info(result.stdout)
+            building_found = any(
+                line.startswith("Building") for line in result.stdout.splitlines()
             )
         except Exception as e:
+            raise RuntimeError(f"Failed to import the OpenBB package. \n{e}") from e
+
+    if not building_found:
+        try:
+            import openbb  # noqa
+
+            logger.info("Did not build on import, triggering rebuild...\n")
+            openbb.build()
+
+        except Exception as e:
             raise RuntimeError(
-                f"Failed to build the OpenBB platform static assets. \n{e}"
+                "Failed to build the OpenBB platform static assets. \n"
+                f"{e} -> {e.__traceback__.tb_frame.f_code.co_filename}:"
+                f"{e.__traceback__.tb_lineno}"
             ) from e
+    sys.exit(0)
 
 
 if __name__ == "__main__":
