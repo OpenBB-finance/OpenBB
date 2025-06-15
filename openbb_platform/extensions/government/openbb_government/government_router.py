483b340558d118672389ec45c85d6fe39abe6c0a
DidierRLopes <didierrlopes@gmail.com>
2025-06-15 01:43:10 -0400

    pylint

--- openbb_platform/extensions/government/openbb_government/government_router.py
+++ openbb_platform/extensions/government/openbb_government/government_router.py
@@ -2,18 +2,7 @@
 
 # pylint: disable=unused-argument
 
-from typing import Union
 
-from openbb_core.app.deprecation import OpenBBDeprecationWarning
-from openbb_core.app.model.command_context import CommandContext
-from openbb_core.app.model.example import APIEx
-from openbb_core.app.model.obbject import OBBject
-from openbb_core.app.provider_interface import (
-    ExtraParams,
-    ProviderChoices,
-    StandardParams,
-)
-from openbb_core.app.query import Query
 from openbb_core.app.router import Router
 
 from openbb_government.us.us_router import router as us_router
--- openbb_platform/extensions/government/openbb_government/government_views.py
+++ openbb_platform/extensions/government/openbb_government/government_views.py
@@ -1,5 +1,6 @@
 """Views for the government Extension."""
 
 
+# pylint: disable=too-few-public-methods
 class GovernmentViews:
     """Government Views."""
