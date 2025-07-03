23464957e3943417d90205582a5a598a8927ed9f
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2025-07-03 08:41:20 -0700

    [Feature] Add Omni Widget Input and Response Models to `openbb-platform-api`. (#7155)
    * add support for omni widget with input and response models
    
    * readme
    
    * Update README.md
    
    * too-many-statements
    
    * update nasdaq app expected cassette
    
    * use __class__.__name__ instead of import try logic
    
    * unused import
    
    * docstring
    
    * less instrusive exclude filter.
    
    * advanced tables not the default
    
    * test artifact

--- openbb_platform/extensions/platform_api/tests/test_widgets_utils.py
+++ openbb_platform/extensions/platform_api/tests/test_widgets_utils.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 import pytest
+from openbb_platform_api.response_models import OmniWidgetResponseModel
 from openbb_platform_api.utils.widgets import build_json, modify_query_schema
 
 # pylint: disable=redefined-outer-name
@@ -21,6 +22,53 @@ def mock_widgets_json():
     mock_widgets_path = Path(__file__).parent / "mock_widgets.json"
     with open(mock_widgets_path) as file:
         return json.load(file)
+
+
+def test_omni_widget_response_model():
+    # Create a sample OmniWidgetResponseModel instance
+
+    test_table = [
+        {"symbol": "AAPL", "price": 150.0, "volume": 1000000},
+        {"symbol": "GOOGL", "price": 2800.0, "volume": 500000},
+    ]
+    widget = OmniWidgetResponseModel(
+        content=test_table,
+    )
+    # Assert the content and data_format fields
+    assert widget.content == test_table
+    assert widget.data_format == {
+        "data_type": "object",
+        "parse_as": "table",
+    }
+    assert not hasattr(widget, "parse_as")
+
+    widget = OmniWidgetResponseModel(
+        content=test_table,
+        parse_as="text",
+    )
+    # Assert the parse_as field takes precedence
+    assert widget.content == test_table
+    assert widget.data_format == {
+        "data_type": "object",
+        "parse_as": "text",
+    }
+    # Assert that the parse_as field is not sent at the root of the object.
+    assert not hasattr(widget, "parse_as")
+
+    # Test with JSON string content
+    widget = OmniWidgetResponseModel(
+        content=json.dumps(test_table),
+    )
+
+    # Assert the content and data_format fields
+    assert isinstance(widget.content, list)
+    assert widget.data_format == {
+        "data_type": "object",
+        "parse_as": "table",
+    }
+    assert not hasattr(widget, "parse_as")
+
+    assert "x-widget_config" in widget.schema_json()
 
 
 @pytest.mark.parametrize(
@@ -293,6 +341,10 @@ def mock_widgets_json():
                     "show": True,
                     "multiSelect": True,
                     "paramName": "symbol",
+                    "multiple": True,
+                    "style": {
+                        "popupWidth": 400,
+                    },
                 },
                 {
                     "label": "Start Date",
