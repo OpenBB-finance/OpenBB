24f9c6ab4cd4d1b1047fb3285e62ef9c230ece95
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2025-07-29 13:11:32 -0700

    fix tests and remove unused files

--- openbb_platform/extensions/government/openbb_government/__init__.py
+++
@@ -1,1 +0,0 @@
-"""OpenBB Government Extension."""
--- openbb_platform/extensions/government/openbb_government/government_views.py
+++
@@ -1,6 +0,0 @@
-"""Views for the government Extension."""
-
-
-# pylint: disable=too-few-public-methods
-class GovernmentViews:
-    """Government Views."""
