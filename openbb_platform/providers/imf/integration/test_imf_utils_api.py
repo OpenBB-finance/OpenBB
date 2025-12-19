6512da26128c8c7172174668be831ac338c8936d
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2025-12-19 10:39:31 -0800

    remove integration test for removed utils endpoint

--- openbb_platform/providers/imf/integration/test_imf_utils_api.py
+++ openbb_platform/providers/imf/integration/test_imf_utils_api.py
@@ -61,29 +61,6 @@ def test_imf_utils_get_dataflow_dimensions(params, headers):
     assert result.status_code == 200
 
 
-@pytest.mark.parametrize(
-    "params",
-    [
-        {
-            "dataflow_id": "CPI",
-            "symbol": None,
-        },
-    ],
-)
-@pytest.mark.integration
-def test_list_indicators_by_dataflow(params, headers):
-    """Test imf_utils_list_indicators_by_dataflow endpoint."""
-    params = {p: v for p, v in params.items() if v}
-
-    query_str = get_querystring(params, [])
-    url = (
-        f"http://0.0.0.0:8000/api/v1/imf_utils/list_indicators_by_dataflow?{query_str}"
-    )
-    result = requests.get(url, headers=headers, timeout=10)
-    assert isinstance(result, requests.Response)
-    assert result.status_code == 200
-
-
 @pytest.mark.parametrize("params", [{}])
 @pytest.mark.integration
 def test_imf_utils_list_port_id_choices(params, headers):
